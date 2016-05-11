6f853b6b2b8bccdf809295412d3139adc066c6e4
Dvir Volk <dvirsky@gmail.com>
2016-05-11 15:10:11 -0700

    added some documentation to tokenize

--- src/tokenize.h
+++ src/tokenize.h
@@ -8,11 +8,21 @@
 #include "util/khash.h"
 #include "varint.h"
 
+/* Represents a token found in a document */
 typedef struct  {
+    // token string
     const char *s;
+    // token string length
     size_t len;
+    
+    // position in the document - this is written to the inverted index
     u_int pos;
+    
+    // the token's score
     u_short score;
+    
+    // Field id - used later for filtering.
+    // NOT IMPLEMENTED YET
     u_char fieldId;
 } Token;
 
@@ -50,11 +60,19 @@ typedef struct {
 } TokenizerCtx;
 
 
+/* The actual tokenizing process runner */
 int _tokenize(TokenizerCtx *ctx);
+
+/** The extenral API. Tokenize text, and create tokens with the given score and fieldId.
+TokenFunc is a callback that will be called for each token found */
 int tokenize(const char *text, u_short score, u_char fieldId, void *ctx, TokenFunc f);
+
+/** A simple text normalizer that convertes all tokens to lowercase and removes accents. 
+Does NOT normalize unicode */
 char*DefaultNormalize(char *s, size_t *len);
 
 
+/* A query-specific tokenizer, that reads symbols like quots, pipes, etc */
 typedef struct {
     const char *text;
     size_t len;
@@ -64,6 +82,7 @@ typedef struct {
     
 } QueryTokenizer;
 
+/* Quer tokenizer token type */
 typedef enum {
     T_WORD,
     T_QUOTE,
@@ -72,14 +91,24 @@ typedef enum {
     T_END
 } QueryTokenType;
 
+/* A token in the process of parsing a query. Unlike the document tokenizer,  it
+works iteratively and is not callback based.  */
 typedef struct {
     const char *s;
     size_t len;
     QueryTokenType type;
 } QueryToken;
 
+/* Create a new query tokenizer. There is no need to free anything in the object */
 QueryTokenizer NewQueryTokenizer(char *text, size_t len);
+
+/* Read the next token from the tokenizer. If tit has reached the end of the 
+query text, it will return a token with type T_END and null content.
+
+Note: The token's text might not be null terminated, so use the len variable */
 QueryToken QueryTokenizer_Next(QueryTokenizer *t);
+
+/* Returns 1 if the tokenizer can read more tokens from the query text */
 int QueryTokenizer_HasNext(QueryTokenizer *);
 
 #endif
