8e4d5ad2e79b358ca0dc0d3418efd263349f5243
Dvir Volk <dvirsky@gmail.com>
2016-06-02 11:54:20 +0300

    removed unwanted libstem files

--- src/dep/snowball/java/org/tartarus/snowball/SnowballStemmer.java
+++
@@ -1,7 +0,0 @@
-
-package org.tartarus.snowball;
-import java.lang.reflect.InvocationTargetException;
-
-public abstract class SnowballStemmer extends SnowballProgram {
-    public abstract boolean stem();
-};
