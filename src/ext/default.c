cb65ff7a8e9d7c1760e4c60d208647545dcb6f20
Dvir Volk <dvirsky@gmail.com>
2017-03-08 18:12:52 +0200

    added default scoring function

--- src/ext/default.c
+++ src/ext/default.c
@@ -1,7 +1,8 @@
 #include "../redisearch.h"
+#include <stdio.h>
 
 /* Calculate sum(TF-IDF)*document score for each result */
-double TFIDFScorer(RSScoringFunctionCtx *ctx, RSDocumentMetadata *dmd, RSIndexResult *h,
+double TFIDFScorer(RSScoringFunctionCtx *ctx, RSIndexResult *h, RSDocumentMetadata *dmd,
                    double minScore) {
   if (dmd->score == 0) return 0;
 
@@ -10,18 +11,16 @@ double TFIDFScorer(RSScoringFunctionCtx *ctx, RSDocumentMetadata *dmd, RSIndexRe
     tfidf += (float)h->records[i].freq * (h->records[i].term ? h->records[i].term->idf : 0);
   }
   tfidf *= dmd->score / (double)dmd->maxFreq;
-  
+
   // no need to factor the distance if tfidf is already below minimal score
   if (tfidf < minScore) {
     return 0;
   }
   tfidf /= (double)ctx->GetSlop(h);
-
-  
+  // printf("tfidf: %f\n", tfidf);
   return tfidf;
 }
 
-
 int DefaultExtensionInit(RSExtensionCtx *ctx) {
-  ctx->RegisterScoringFunction("TFIDF", TFIDFScorer, NULL);
+  return ctx->RegisterScoringFunction("TFIDF", TFIDFScorer, NULL);
 }
---
+++ src/ext/default.h
@@ -0,0 +1,7 @@
+#ifndef __EXT_DEFAULT_H__
+#define __EXT_DEFAULT_H__
+#include "redisearch.h"
+
+int DefaultExtensionInit(RSExtensionCtx *ctx);
+
+#endif
