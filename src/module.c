f85131830f87c1354c8b5553cbdd990402a07b45
Dvir Volk <dvirsky@gmail.com>
2017-04-15 23:57:58 +0300

    version bump

--- src/module.c
+++ src/module.c
@@ -1181,7 +1181,7 @@ int SuggestGetCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
 int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
 
   // LOGGING_INIT(0xFFFFFFFF);
-  if (RedisModule_Init(ctx, "ft", 2, REDISMODULE_APIVER_1) == REDISMODULE_ERR)
+  if (RedisModule_Init(ctx, "ft", 3, REDISMODULE_APIVER_1) == REDISMODULE_ERR)
     return REDISMODULE_ERR;
 
   // Init extension mechanism
