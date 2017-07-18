371c8dc396137b4de9b4ca7c462c49dff315f2d6
Mark Nunberg <mnunberg@haskalah.org>
2017-07-18 12:25:11 +0300

    qint refactor:
    This refactors the qint decoding functions to eliminate the use of a
    table for offsets. Benchmarking the code, reading the byte
    2bits-by-2bits is actually quicker, presumably because of cache
    efficiency.
    
    This commit also fixes some latent bugs within the code wherein the
    decoding functions would skip too much, corrupting further reads.
    
    Finally, this adds the `QINT_API` macro which allows easy embedding of
    qint.c into other C source files without any kind of symbol collision.

---
+++ src/tests/test_qint.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <assert.h>
+#include "qint.h"
+#include "rmutil/alloc.h"
+
+int main(int argc, char **argv) {
+  RMUTil_InitAlloc();
+  Buffer *b = NewBuffer(1024);
+  BufferWriter w = NewBufferWriter(b);
+  qint_encode4(&w, 123, 456, 789, 101112);
+
+  uint32_t arr[4];
+  BufferReader r = NewBufferReader(b);
+  qint_decode(&r, arr, 4);
+  assert(arr[0] == 123);
+  assert(arr[1] == 456);
+  assert(arr[2] == 789);
+  assert(arr[3] == 101112);
+
+  memset(arr, 0, sizeof arr);
+  r = NewBufferReader(b);
+  qint_decode4(&r, &arr[0], &arr[1], &arr[2], &arr[3]);
+  assert(arr[0] == 123);
+  assert(arr[1] == 456);
+  assert(arr[2] == 789);
+  assert(arr[3] == 101112);
+
+  memset(arr, 0, sizeof arr);
+  r = NewBufferReader(b);
+  qint_decode3(&r, &arr[0], &arr[1], &arr[2]);
+  assert(arr[0] == 123);
+  assert(arr[1] == 456);
+  assert(arr[2] == 789);
+
+  return 0;
+}
