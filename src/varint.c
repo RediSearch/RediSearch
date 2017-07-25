b35da5124519a49cdc0548320725ebc41b1b7e70
Mark Nunberg <mnunberg@haskalah.org>
2017-07-25 15:05:43 -0700

    More efficient varint coding

--- src/varint.c
+++ src/varint.c
@@ -9,18 +9,32 @@
 
 // static int msb = (int)(~0ULL << 25);
 
-int WriteVarintRaw(int value, Buffer *buf) {
-  BufferWriter bw = {.buf = buf, .pos = buf->data + buf->offset};
-  return WriteVarint(value, &bw);
-}
-
-int WriteVarint(int value, BufferWriter *w) {
+size_t WriteVarintRaw(int value, char *buf) {
   unsigned char varint[16];
   unsigned pos = sizeof(varint) - 1;
   varint[pos] = value & 127;
   while (value >>= 7) varint[--pos] = 128 | (--value & 127);
+  size_t size = 16 - pos;
+  memcpy(buf, varint + pos, size);
+  return size;
+}
+
+size_t WriteVarintBuffer(int value, Buffer *buf) {
+  Buffer_Reserve(buf, 16);
+  size_t n = WriteVarintRaw(value, buf->data + buf->offset);
+  buf->offset += n;
+  return n;
+}
+
+int WriteVarint(int value, BufferWriter *w) {
   // printf("writing %d bytes\n", 16 - pos);
-  return Buffer_Write(w, varint + pos, 16 - pos);
+  if (Buffer_Reserve(w->buf, 16)) {
+    w->pos = w->buf->data + w->buf->offset;
+  }
+  size_t nw = WriteVarintRaw(value, w->pos);
+  w->buf->offset += nw;
+  w->pos += nw;
+  return nw;
 }
 
 size_t varintSize(int value) {
@@ -56,7 +70,8 @@ Write an integer to the vector.
 @retur 0 if we're out of capacity, the varint's actual size otherwise
 */
 size_t VVW_Write(VarintVectorWriter *w, int i) {
-  size_t n = WriteVarintRaw(i - w->lastValue, &w->buf);
+  Buffer_Reserve(&w->buf, 16);
+  size_t n = WriteVarintBuffer(i - w->lastValue, &w->buf);
   if (n != 0) {
     w->nmemb += 1;
     w->lastValue = i;
