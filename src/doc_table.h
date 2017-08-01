635d1dbb892066da7728d896bfb06027d9cd79bc
Mark Nunberg <mnunberg@haskalah.org>
2017-08-01 08:39:18 -0700

    Provide AOF implementation for term trie

--- src/doc_table.h
+++ src/doc_table.h
@@ -87,6 +87,6 @@ void DocTable_RdbSave(DocTable *t, RedisModuleIO *rdb);
 void DocTable_RdbLoad(DocTable *t, RedisModuleIO *rdb, int encver);
 
 /* Emit special FT.DTADD commands to recreate the table */
-void DocTable_AOFRewrite(DocTable *t, RedisModuleString *k, RedisModuleIO *aof);
+void DocTable_AOFRewrite(DocTable *t, const char *indexName, RedisModuleIO *aof);
 
 #endif
