e31043ea84b6d95939bfa4a66868015a175699fa
Dvir Volk <dvirsky@gmail.com>
2017-08-06 15:57:34 +0300

    Added config reader

---
+++ src/config.c
@@ -0,0 +1,20 @@
+#include "config.h"
+#include "rmutil/util.h"
+#include "rmutil/strings.h"
+
+RSConfig RSGlobalConfig = RS_DEFAULT_CONFIG;
+
+int ReadConfig(RedisModuleString **argv, int argc, const char **err) {
+  *err = NULL;
+  /* Read the extension we want to load */
+  if (argc > 1 && RMUtil_ArgIndex("EXTLOAD", argv, argc) >= 0) {
+    RMUtil_ParseArgsAfter("EXTLOAD", argv, argc, "c", &RSGlobalConfig.extLoad);
+  }
+
+  /* If SafeMode is enabled, we turn down concurrent mode in the default config */
+  if (RMUtil_ArgIndex("SAFEMODE", argv, argc) >= 0) {
+    RSGlobalConfig.concurrentMode = 0;
+  }
+
+  return REDISMODULE_OK;
+}
---
+++ src/config.h
@@ -0,0 +1,23 @@
+#ifndef RS_CONFIG_H_
+#define RS_CONFIG_H_
+
+#include "redismodule.h"
+
+typedef struct {
+  int concurrentMode;
+  const char *extLoad;
+} RSConfig;
+
+extern RSConfig RSGlobalConfig;
+
+/* Read configuration from redis module arguments into the global config object. Return
+ * REDISMODULE_ERR and sets an error message if something is invalid */
+int ReadConfig(RedisModuleString **argv, int argc, const char **err);
+
+// default configuration
+#define RS_DEFAULT_CONFIG                   \
+  (RSConfig){                               \
+      .concurrentMode = 1, .extLoad = NULL, \
+  };
+
+#endif
