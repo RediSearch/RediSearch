0570b11cce6ed93b6f041cd72570b30d89423cdf
Mark Nunberg <mnunberg@haskalah.org>
2017-08-16 06:54:16 -0700

    Issue #133: Don't crash if field does not exist

--- src/pytest/test.py
+++ src/pytest/test.py
@@ -1140,6 +1140,13 @@ class SearchTestCase(ModuleTestCase('../redisearch.so')):
             self.assertEqual(0, res_name[0])
             self.assertEqual(1, res_body[0])
 
+    def testSearchNonexistField(self):
+        self.cmd('ft.create', 'idx', 'schema', 'title', 'text',
+                 'weight', 5.0, 'body', 'text', 'url', 'text')
+        self.cmd('ft.add', 'idx', 'd1', 1.0, 'nosave', 'fields', 'title',
+                 'hello world', 'body', 'lorem dipsum', 'place', '-77.0366 38.8977')
+        self.cmd('ft.search', 'idx', 'Foo', 'GEOFILTER', 'place', '-77.0366', '38.8977', '1', 'km')
+
 
 def grouper(iterable, n, fillvalue=None):
     "Collect data into fixed-length chunks or blocks"
