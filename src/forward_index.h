182b0d3f7c306609c618bbac405c61576c0ebfa5
Mark Nunberg <mnunberg@haskalah.org>
2017-09-18 08:23:39 -0700

    Record byte offsets when tokenizing (Forward Index)

--- src/forward_index.h
+++ src/forward_index.h
@@ -36,16 +36,21 @@ typedef struct ForwardIndex {
 } ForwardIndex;
 
 typedef struct {
+  const char *doc;
+  VarintVectorWriter *allOffsets;
   ForwardIndex *idx;
   t_fieldMask fieldId;
   float fieldScore;
 } ForwardIndexTokenizerCtx;
 
 static inline void ForwardIndexTokenizerCtx_Init(ForwardIndexTokenizerCtx *ctx, ForwardIndex *idx,
+                                                 const char *doc, VarintVectorWriter *vvw,
                                                  t_fieldMask fieldId, float score) {
   ctx->idx = idx;
   ctx->fieldId = fieldId;
   ctx->fieldScore = score;
+  ctx->doc = doc;
+  ctx->allOffsets = vvw;
 }
 
 typedef struct {
