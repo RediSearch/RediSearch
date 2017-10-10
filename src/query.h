613ae7f200095bb5e820366cd6e2556122484716
Dvir Volk <dvirsky@gmail.com>
2017-10-10 18:44:00 +0300

    WIP - more separation of query objects

--- src/query.h
+++ src/query.h
@@ -19,6 +19,87 @@
 #include "concurrent_ctx.h"
 #include "value.h"
 
+/* A QueryParseCtx represents the parse tree and execution plan for a single search
+ * QueryParseCtx */
+typedef struct RSQuery {
+  // the raw QueryParseCtx text
+  char *raw;
+  // the raw text len
+  size_t len;
+
+  // the token count
+  int numTokens;
+
+  // the current token id (we assign ids to all token nodes)
+  int tokenId;
+
+  // Stopword list
+  StopWordList *stopwords;
+
+  // parsing state
+  int ok;
+
+  // Index spec
+  RedisSearchCtx *sctx;
+
+  // query root
+  QueryNode *root;
+
+  char *errorMsg;
+
+  t_fieldMask fieldMask;
+} QueryParseCtx;
+
+typedef struct {
+  ConcurrentSearchCtx *csx;
+  RedisSearchCtx *sctx;
+  int numTokens;
+  int tokenId;
+  RSSearchRequest *req;
+  DocTable *docTable;
+} QueryEvalCtx;
+
+/* Evaluate a QueryParseCtx stage and prepare it for execution. As execution is lazy
+this doesn't
+actually do anything besides prepare the execution chaing */
+IndexIterator *Query_EvalNode(QueryEvalCtx *q, QueryNode *n);
+
+/* Free the QueryParseCtx execution stage and its children recursively */
+void QueryNode_Free(QueryNode *n);
+QueryNode *NewTokenNode(QueryParseCtx *q, const char *s, size_t len);
+QueryNode *NewTokenNodeExpanded(QueryParseCtx *q, const char *s, size_t len, RSTokenFlags flags);
+QueryNode *NewPhraseNode(int exact);
+QueryNode *NewUnionNode();
+QueryNode *NewPrefixNode(QueryParseCtx *q, const char *s, size_t len);
+QueryNode *NewNotNode(QueryNode *n);
+QueryNode *NewOptionalNode(QueryNode *n);
+QueryNode *NewNumericNode(NumericFilter *flt);
+QueryNode *NewIdFilterNode(IdFilter *flt);
+QueryNode *NewWildcardNode();
+QueryNode *NewGeofilterNode(GeoFilter *flt);
+
+void Query_SetNumericFilter(QueryParseCtx *q, NumericFilter *nf);
+void Query_SetGeoFilter(QueryParseCtx *q, GeoFilter *gf);
+void Query_SetIdFilter(QueryParseCtx *q, IdFilter *f);
+
+/* Only used in tests, for now */
+void QueryNode_Print(QueryParseCtx *q, QueryNode *qs, int depth);
+
+#define QUERY_ERROR_INTERNAL_STR "Internal error processing QueryParseCtx"
+#define QUERY_ERROR_INTERNAL -1
+
+/* Initialize a new QueryParseCtx object from user input. This does not parse the QueryParseCtx
+ * just yet */
+QueryParseCtx *NewQueryParseCtx(RSSearchRequest *req);
+QueryNode *Query_Parse(QueryParseCtx *q, char **err);
+
+void Query_Expand(QueryParseCtx *q);
+/* Free a QueryParseCtx object */
+void Query_Free(QueryParseCtx *q);
+
+/******************************************************************************************************
+ *   Result Processor Definitions
+ ******************************************************************************************************/
 typedef struct {
   t_docId docId;
 
@@ -56,12 +137,12 @@ typedef struct {
   uint32_t totalResults;
   char *errorString;
   QueryState state;
-} SearchResultCtx;
+} QueryExecutionCtx;
 
 typedef struct {
   void *privdata;
   struct resultProcessor *upstream;
-  SearchResultCtx *resCtx;
+  QueryExecutionCtx *qxc;
 } ResultProcessorCtx;
 
 typedef struct resultProcessor {
@@ -85,10 +166,11 @@ ResultProcessor *NewResultProcessor(ResultProcessor *upstream, void *privdata);
 static inline int ResultProcessor_Next(ResultProcessor *rp, SearchResult *res, int allowSwitching);
 
 /* Helper function - get the total from a processor, and if the Total callback is NULL, climb up the
-chain until we find a processor with a Total callback. This allows processors to avoid implementing
-it if they have no calculations to add to Total (such as deeted/ignored results) */
+ * chain until we find a processor with a Total callback. This allows processors to avoid
+ * implementing it if they have no calculations to add to Total (such as deeted/ignored results)
+ * */
 static inline size_t ResultProcessor_Total(ResultProcessor *rp) {
-  return rp->ctx.resCtx->totalResults;
+  return rp->ctx.qxc->totalResults;
 }
 
 static void ResultProcessor_Free(ResultProcessor *rp) {
@@ -103,112 +185,41 @@ static void ResultProcessor_Free(ResultProcessor *rp) {
   if (upstream) ResultProcessor_Free(upstream);
 }
 
-/* A QueryParseCtx represents the parse tree and execution plan for a single search
- * QueryParseCtx */
-typedef struct RSQuery {
-  // the raw QueryParseCtx text
-  char *raw;
-  // the raw text len
-  size_t len;
-
-  // the token count
-  int numTokens;
-
-  // the current token id (we assign ids to all token nodes)
-  int tokenId;
-
-  // Stopword list
-  StopWordList *stopwords;
-
-  // parsing state
-  int ok;
-
-  // Index spec
-  RedisSearchCtx *sctx;
-
-  // query root
-  QueryNode *root;
-
-  char *errorMsg;
-
-  t_fieldMask fieldMask
-} QueryParseCtx;
+/******************************************************************************************************
+ *   Query Plan - the actual binding context of the whole execution plan - from filters to
+ *   processors
+ ******************************************************************************************************/
 
 typedef struct {
+  RedisSearchCtx *ctx;
 
   IndexIterator *rootFilter;
 
   ResultProcessor *rootProcessor;
-  SearchResultCtx resultCtx;
 
-  RedisSearchCtx *ctx;
+  QueryExecutionCtx execCtx;
 
   ConcurrentSearchCtx conc;
 
-  // Custom scorer
-  RSScoringFunction scorer;
-  RSFreeFunction scorerFree;
-  RSScoringFunctionCtx scorerCtx;
+  RSSearchRequest *req;
 
-  // sorting key by specific inline field
-  RSSortingKey *sortKey;
-
-} QueryExecutionCtx;
+} QueryPlan;
 
 /* Set the concurrent mode of the QueryParseCtx. By default it's on, setting here to 0 will turn it
  * off,
  * resulting in the QueryParseCtx not performing context switches */
-void Query_SetConcurrentMode(QueryExecutionCtx *q, int concurrent);
-
-/* Evaluate a QueryParseCtx stage and prepare it for execution. As execution is lazy
-this doesn't
-actually do anything besides prepare the execution chaing */
-IndexIterator *Query_EvalNode(QueryParseCtx *q, QueryNode *n);
+void Query_SetConcurrentMode(QueryPlan *q, int concurrent);
 
 /* Build the processor chain of the QueryParseCtx, returning the root processor */
-ResultProcessor *Query_BuildProcessorChain(RSSearchRequest *req, RSSearchRequest *r);
+QueryPlan *Query_BuildBlan(QueryNode *rootNode, RSSearchRequest *req);
 
-/* Free the QueryParseCtx execution stage and its children recursively */
-void QueryNode_Free(QueryNode *n);
-QueryNode *NewTokenNode(QueryParseCtx *q, const char *s, size_t len);
-QueryNode *NewTokenNodeExpanded(QueryParseCtx *q, const char *s, size_t len, RSTokenFlags flags);
-QueryNode *NewPhraseNode(int exact);
-QueryNode *NewUnionNode();
-QueryNode *NewPrefixNode(QueryParseCtx *q, const char *s, size_t len);
-QueryNode *NewNotNode(QueryNode *n);
-QueryNode *NewOptionalNode(QueryNode *n);
-QueryNode *NewNumericNode(NumericFilter *flt);
-QueryNode *NewIdFilterNode(IdFilter *flt);
-QueryNode *NewWildcardNode();
-QueryNode *NewGeofilterNode(GeoFilter *flt);
-
-void Query_SetNumericFilter(QueryParseCtx *q, NumericFilter *nf);
-void Query_SetGeoFilter(QueryParseCtx *q, GeoFilter *gf);
-void Query_SetIdFilter(QueryParseCtx *q, IdFilter *f);
+/* Lazily execute the parsed QueryParseCtx and all its stages, and return a final result
+ * object */
+int QueryPlan_Execute(QueryPlan *ctx, const char **err);
 
 /* Return a string representation of the QueryParseCtx parse tree. The string should be freed by the
  * caller
  */
-const char *Query_DumpExplain(QueryParseCtx *q);
-
-/* Only used in tests, for now */
-void QueryNode_Print(QueryParseCtx *q, QueryNode *qs, int depth);
-
-#define QUERY_ERROR_INTERNAL_STR "Internal error processing QueryParseCtx"
-#define QUERY_ERROR_INTERNAL -1
-
-/* Initialize a new QueryParseCtx object from user input. This does not parse the QueryParseCtx
- * just yet */
-QueryParseCtx *NewQueryParseCtx(RSSearchRequest *req);
-
-void Query_Expand(QueryParseCtx *q);
-/* Free a QueryParseCtx object */
-void Query_Free(QueryParseCtx *q);
-
-/* Lazily execute the parsed QueryParseCtx and all its stages, and return a final result
- * object */
-int Query_Execute(QueryExecutionCtx *ctx);
-
-QueryNode *Query_Parse(QueryParseCtx *q, char **err);
+const char *Query_DumpExplain(QueryPlan *q);
 
 #endif
