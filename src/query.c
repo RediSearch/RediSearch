2627fad60e032870c0d9f30d1816315a538bbf7f
Dvir Volk <dvirsky@gmail.com>
2017-10-11 20:45:12 +0300

    fixed memory leak in field map

--- src/query.c
+++ src/query.c
@@ -1090,6 +1090,7 @@ int Query_SerializeResults(QueryPlan *qex, RSSearchFlags flags) {
     count += serializeResult(qex, &r, flags);
 
     IndexResult_Free(r.indexResult);
+    free(r.fields);
   } while (rc != RS_RESULT_EOF);
   if (count == 0) {
     RedisModule_ReplyWithLongLong(ctx, ResultProcessor_Total(qex->rootProcessor));
