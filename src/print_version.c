c659a5ef573dd7288ed440e795738ff7d95600b5
Dvir Volk <dvirsky@gmail.com>
2017-10-17 21:58:33 +0300

    make print_version simpler and use an ifdef to avoid compilation

---
+++ src/print_version.c
@@ -0,0 +1,12 @@
+#ifdef PRINT_VERSION_TARGET
+#include <stdio.h>
+#include "version.h"
+
+/* This is a utility that prints the current semantic version string, to be used in make files */
+
+int main(int argc, char **argv) {
+  printf("%d.%02d.%02d\n", REDISEARCH_VERSION_MAJOR, REDISEARCH_VERSION_MINOR,
+         REDISEARCH_VERSION_PATCH);
+  return 0;
+}
+#endif
