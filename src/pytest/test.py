a10f8e7974522c8c4042fd4f0546654e14451bfb
Mark Nunberg <mnunberg@haskalah.org>
2017-10-25 19:22:51 -0400

    Add tests for raw AOF reloading
    Our previous tests only checked for the rewritten version of the AoF
    file. Here we check fully.

--- src/pytest/test.py
+++ src/pytest/test.py
@@ -1315,19 +1315,24 @@ class SearchTestCase(ModuleTestCase('../redisearch.so')):
                 for option in filter(None, combo):
                     self.assertTrue(option in opts)
 
-    def testAof(self):
+    def aofTestCommon(self, reloadfn):
         self.spawn_server(use_aof=True)
         self.cmd('ft.create', 'idx', 'schema', 'field1', 'text', 'field2', 'numeric')
-        self.restart_and_reload()
+        reloadfn()
         for x in range(1, 10):
             self.assertCmdOk('ft.add', 'idx', 'doc{}'.format(x), 1.0 / x, 'fields',
                              'field1', 'myText{}'.format(x), 'field2', 20 * x)
-
         exp = [9L, 'doc1', ['field1', 'myText1', 'field2', '20'], 'doc2', ['field1', 'myText2', 'field2', '40'], 'doc3', ['field1', 'myText3', 'field2', '60'], 'doc4', ['field1', 'myText4', 'field2', '80'], 'doc5', ['field1',
                                                                                                                                                                                                                         'myText5', 'field2', '100'], 'doc6', ['field1', 'myText6', 'field2', '120'], 'doc7', ['field1', 'myText7', 'field2', '140'], 'doc8', ['field1', 'myText8', 'field2', '160'], 'doc9', ['field1', 'myText9', 'field2', '180']]
-        self.restart_and_reload()
+        reloadfn()
         ret = self.cmd('ft.search', 'idx', 'myt*')
         self.assertEqual(exp, ret)
+
+    def testAof(self):
+        self.aofTestCommon(lambda: self.restart_and_reload())
+
+    def testRawAof(self):
+        self.aofTestCommon(lambda: self.cmd('debug loadaof'))
 
     def testNoStem(self):
         self.cmd('ft.create', 'idx', 'schema', 'body', 'text', 'name', 'text', 'nostem')
