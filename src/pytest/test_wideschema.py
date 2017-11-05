cb3847df4f6350706de6f9fef5f235d4d22136ec
Dvir Volk <dvirsky@gmail.com>
2017-11-05 12:19:30 +0200

    reduce the number of tested fields in 32 bit archs

--- src/pytest/test_wideschema.py
+++ src/pytest/test_wideschema.py
@@ -1,6 +1,7 @@
 from rmtest import ModuleTestCase
 import redis
 import unittest
+import platform
 
 
 class SearchTestCase(ModuleTestCase('../redisearch.so')):
@@ -12,18 +13,19 @@ class SearchTestCase(ModuleTestCase('../redisearch.so')):
         with self.redis() as r:
             r.flushdb()
             schema = []
-            for i in range(128):
+            FIELDS = 128 if platform.architecture()[0] == '64bit' else 64
+            for i in range(FIELDS):
                 schema.extend(('field_%d' % i, 'TEXT'))
             self.assertOk(r.execute_command(
                 'ft.create', 'idx', 'schema', *schema))
             N = 10
             for n in range(N):
                 fields = []
-                for i in range(128):
+                for i in range(FIELDS):
                     fields.extend(('field_%d' % i, 'hello token_%d' % i))
                 self.assertOk(r.execute_command('ft.add', 'idx', 'doc%d' %n, 1.0, 'fields', *fields))
             for _ in r.retry_with_rdb_reload():
-                for i in range(128):
+                for i in range(FIELDS):
 
                     res = self.search(r,'idx', '@field_%d:token_%d' % (i, i), 'NOCONTENT')
                     self.assertEqual(res[0], N)
@@ -34,10 +36,10 @@ class SearchTestCase(ModuleTestCase('../redisearch.so')):
                     res = self.search(r,'idx', 'hello @field_%d:token_%d' % (i, i), 'NOCONTENT')
                     self.assertEqual(res[0], N)
 
-                res = self.search(r, 'idx', ' '.join(('@field_%d:token_%d' % (i, i) for i in range(128))))
+                res = self.search(r, 'idx', ' '.join(('@field_%d:token_%d' % (i, i) for i in range(FIELDS))))
                 self.assertEqual(res[0], N)
 
-                res = self.search(r, 'idx', ' '.join(('token_%d' % (i) for i in range(128))))
+                res = self.search(r, 'idx', ' '.join(('token_%d' % (i) for i in range(FIELDS))))
                 self.assertEqual(res[0], N)
 
 if __name__ == '__main__':
