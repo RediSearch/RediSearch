f5e3713edd18718e98d1fde0b2265932b1212362
Dvir Volk <dvirsky@gmail.com>
2017-12-17 18:17:26 +0200

    separated cmdparse to h and c and moved to rmutil

---
+++ src/rmutil/cmdparse.h
@@ -0,0 +1,195 @@
+#ifndef RMUTIL_CMDPARSE_
+#define RMUTIL_CMDPARSE_
+
+#include <stdlib.h>
+
+#define CMDPARSE_OK 0
+#define CMDPARSE_ERR 1
+
+typedef enum {
+  CmdArg_Integer,
+  CmdArg_Double,
+  CmdArg_String,
+  CmdArg_Array,
+  CmdArg_Object,
+  CmdArg_Flag,
+} CmdArgType;
+
+struct CmdArg;
+
+typedef struct {
+  char *str;
+  size_t len;
+} CmdString;
+
+#define CMD_STRING(s) ((CmdString){.str = s, .len = strlen(s)})
+int CmdString_CaseEquals(CmdString *str, const char *other);
+
+typedef struct {
+  const char *k;
+  struct CmdArg *v;
+} CmdKeyValue;
+
+typedef struct {
+  size_t len;
+  size_t cap;
+  CmdKeyValue *entries;
+} CmdObject;
+
+typedef struct {
+  size_t len;
+  size_t cap;
+  struct CmdArg **args;
+} CmdArray;
+
+// Variant value union
+typedef struct CmdArg {
+  union {
+    // numeric value
+    double d;
+    int64_t i;
+
+    // boolean flag
+    int b;
+    // string value
+    CmdString s;
+
+    // array value
+    CmdArray a;
+
+    CmdObject obj;
+  };
+  CmdArgType type;
+} CmdArg;
+
+void CmdArg_Free(CmdArg *arg);
+
+/*************************************************************************************************************************
+ *
+ *  Command Schema Definition Objects
+ *************************************************************************************************************************/
+
+/* Single typed argument in a schema. Can have the following type chars:
+ *
+ *  - s: will be parsed as a string
+ *  - l: Will be parsed as a long integer
+ *  - d: Will be parsed as a double
+ */
+typedef struct {
+  char type;
+} CmdSchemaArg;
+
+/* dummy struct for flags - they don't need anything */
+typedef struct {
+} CmdSchemaFlag;
+
+/* Command schema option - represents a multiple choice, mutually exclusive options */
+typedef struct {
+  /* The number of options */
+  int num;
+  /* The option strings */
+  const char **opts;
+} CmdSchemaOption;
+
+/* Schema tuple - a fixed length array with known types */
+typedef struct {
+  /* Format string, containing s, l or d. The length of the tuple is the length of the format string
+   */
+  const char *fmt;
+  /* Element names - for help prints only */
+  const char **names;
+} CmdSchemaTuple;
+
+/* Schema vector - multiple elements (known only at run time by the first argument) with a single
+ * type,  either l, s or d */
+typedef struct {
+  char type;
+} CmdSchemaVector;
+
+/* Command schema element types */
+typedef enum {
+  CmdSchemaElement_Arg,
+  CmdSchemaElement_Tuple,
+  CmdSchemaElement_Vector,
+  CmdSchemaElement_Flag,
+  CmdSchemaElement_Option,
+} CmdSchemaElementType;
+
+/* A single element in a command schema. */
+typedef struct {
+  union {
+    CmdSchemaArg arg;
+    CmdSchemaTuple tup;
+    CmdSchemaVector vec;
+    CmdSchemaFlag flag;
+    CmdSchemaOption opt;
+  };
+  CmdSchemaElementType type;
+} CmdSchemaElement;
+
+/* Command schema node flags */
+typedef enum {
+  /* Required argument */
+  CmdSchema_Required = 0x01,
+  /* Optional argument */
+  CmdSchema_Optional = 0x02,
+  /* Repeating argument - my have more than one instance per schema */
+  CmdSchema_Repeating = 0x04,
+} CmdSchemaFlags;
+
+/* Schema node type.  */
+typedef enum {
+  /* A schema or sub-schema object. This is the root of the command schema */
+  CmdSchemaNode_Schema,
+  /* A position argument - it can only be parsed once, at a specific position */
+  CmdSchemaNode_PositionalArg,
+  /* A named argument. It can appear anywhere, after the last positional argument */
+  CmdSchemaNode_NamedArg,
+  /* A flag - an argument that may or may not appear, setting a boolean value to 0 or 1 */
+  CmdSchemaNode_Flag,
+} CmdSchemaNodeType;
+
+/* Schema nodes. Each node contains an element (apart from schema nodes). The node has its name
+ * and flags, and the element defines how to parse the element this node contains */
+typedef struct CmdSchemaNode {
+  /* The value element conatined in this node. NULL for schema/sub-schema nodes */
+  CmdSchemaElement *val;
+  /* Flags - required / optional and repeatable */
+  CmdSchemaFlags flags;
+  /* The node type */
+  CmdSchemaNodeType type;
+  /* The node name. Even positional nodes have names so we can refer to them */
+  const char *name;
+  /* Optional help string to extract documentation */
+  const char *help;
+  /* If this is a schema node, it may have edge nodes - other nodes that may be traversed from it.
+   */
+  struct CmdSchemaNode **edges;
+  /* The number of edges */
+  int size;
+} CmdSchemaNode;
+
+CmdSchemaNode *NewSchema(const char *name, const char *help);
+
+int CmdSchema_AddNamed(CmdSchemaNode *s, const char *param, CmdSchemaElement *elem,
+                       CmdSchemaFlags flags);
+int CmdSchema_AddPostional(CmdSchemaNode *s, const char *param, CmdSchemaElement *elem,
+                           CmdSchemaFlags flags);
+int CmdSchema_AddNamedWithHelp(CmdSchemaNode *s, const char *param, CmdSchemaElement *elem,
+                               CmdSchemaFlags flags, const char *help);
+int CmdSchema_AddPostionalWithHelp(CmdSchemaNode *s, const char *param, CmdSchemaElement *elem,
+                                   CmdSchemaFlags flags, const char *help);
+CmdSchemaElement *CmdSchema_NewTuple(const char *fmt, const char **names);
+CmdSchemaElement *CmdSchema_NewArg(const char type);
+CmdSchemaElement *CmdSchema_NewVector(const char type);
+CmdSchemaElement *CmdSchema_NewOption(int num, const char **opts);
+CmdSchemaNode *CmdSchema_AddFlag(CmdSchemaNode *parent, const char *name);
+CmdSchemaNode *CmdSchema_AddFlagWithHelp(CmdSchemaNode *parent, const char *name, const char *help);
+CmdSchemaNode *CmdSchema_NewSubSchema(CmdSchemaNode *parent, const char *param, int flags,
+                                      const char *help);
+void CmdSchemaNode_Print(CmdSchemaNode *n, int depth);
+
+int CmdParser_ParseCmd(CmdSchemaNode *schema, CmdArg **arg, CmdString *argv, int argc, char **err,
+                       int strict);
+
+#endif
