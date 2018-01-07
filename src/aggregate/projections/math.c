369b60a1a2d4b0c40bd668a16161e8ad8112624b
Dvir Volk <dvirsky@gmail.com>
2018-01-07 18:06:51 +0200

    Added parametric dynamic functios with first impl. - adder

--- src/aggregate/projections/math.c
+++ src/aggregate/projections/math.c
@@ -3,20 +3,28 @@
 
 /* A macro to generate math projection functions for wrapping math functions that are applied to a
  * double and return double */
-#define NUMERIC_PROJECTION_WRAPPER(f, math_func)                                                   \
-  static int f(ResultProcessorCtx *ctx, SearchResult *res) {                                       \
-    ProjectorCtx *pc = ctx->privdata;                                                              \
-    /* this will return EOF if needed */                                                           \
-    ResultProcessor_ReadOrEOF(ctx->upstream, res, 0);                                              \
-    RSValue v = SearchResult_GetValue(res, QueryProcessingCtx_GetSortingTable(ctx->qxc),           \
-                                      pc->properties->keys[0]);                                    \
-    if (v.t == RSValue_Number) {                                                                   \
-      RSFieldMap_Set(&res->fields, pc->alias ? pc->alias : pc->properties->keys[0],                \
-                     RS_NumVal(math_func(v.numval)));                                              \
-    } else {                                                                                       \
-      RSFieldMap_Set(&res->fields, pc->alias ? pc->alias : pc->properties->keys[0], RS_NullVal()); \
-    }                                                                                              \
-    return RS_RESULT_OK;                                                                           \
+#define NUMERIC_PROJECTION_WRAPPER(f, math_func)                                         \
+  static int f(ResultProcessorCtx *ctx, SearchResult *res) {                             \
+    ProjectorCtx *pc = ctx->privdata;                                                    \
+    /* this will return EOF if needed */                                                 \
+    ResultProcessor_ReadOrEOF(ctx->upstream, res, 0);                                    \
+    RSValue v = SearchResult_GetValue(res, QueryProcessingCtx_GetSortingTable(ctx->qxc), \
+                                      pc->properties->keys[0]);                          \
+    if (v.t == RSValue_Number) {                                                         \
+      RSFieldMap_Set(&res->fields, pc->alias ? pc->alias : pc->properties->keys[0],      \
+                     RS_NumVal(math_func(v.numval)));                                    \
+    } else {                                                                             \
+      double d;                                                                          \
+      /* Try to parse as number                           */                             \
+      if (RSValue_ToNumber(&v, &d)) {                                                    \
+        RSFieldMap_Set(&res->fields, pc->alias ? pc->alias : pc->properties->keys[0],    \
+                       RS_NumVal(d));                                                    \
+      } else {                                                                           \
+        RSFieldMap_Set(&res->fields, pc->alias ? pc->alias : pc->properties->keys[0],    \
+                       RS_NullVal());                                                    \
+      }                                                                                  \
+    }                                                                                    \
+    return RS_RESULT_OK;                                                                 \
   }
 
 #define GENERIC_PROJECTOR_FACTORY(f, next_func)                                                \
@@ -43,3 +51,83 @@ GENERIC_PROJECTOR_FACTORY(NewLogArgs, log_Next);
 
 NUMERIC_PROJECTION_WRAPPER(log2_Next, log2);
 GENERIC_PROJECTOR_FACTORY(NewLog2Args, log2_Next);
+
+typedef struct {
+  union {
+    RSValue val;
+    const char *prop;
+  };
+  int isValue;
+} valueOrProp;
+
+typedef struct {
+  int len;
+  valueOrProp params[];
+} dynamicExpr;
+
+static inline RSValue getVaueOrProp(SearchResult *r, valueOrProp *vp, RSSortingTable *tbl) {
+  if (vp->isValue) {
+    return vp->val;
+  }
+  return SearchResult_GetValue(r, tbl, vp->prop);
+}
+
+static int add_Next(ResultProcessorCtx *ctx, SearchResult *res) {
+  ResultProcessor_ReadOrEOF(ctx->upstream, res, 0);
+
+  ProjectorCtx *pc = ctx->privdata;
+  dynamicExpr *dx = pc->privdata;
+
+  double sum = 0;
+  int ok = 1;
+  for (int i = 0; i < dx->len; i++) {
+    RSValue v = getVaueOrProp(res, &dx->params[i],
+                              ctx->qxc->sctx->spec ? ctx->qxc->sctx->spec->sortables : NULL);
+    if (v.t == RSValue_Number) {
+      sum += v.numval;
+    } else {
+      double d = 0;
+      if (RSValue_ToNumber(&v, &d)) {
+        sum += d;
+      } else {
+        ok = 0;
+        break;
+      }
+    }
+  }
+
+  RSFieldMap_Set(&res->fields, pc->alias ? pc->alias : "sum", ok ? RS_NumVal(sum) : RS_NullVal());
+  return RS_RESULT_OK;
+}
+
+ResultProcessor *NewAddProjection(ResultProcessor *upstream, const char *alias, CmdArg *args,
+                                  char **err) {
+
+  if (CMDARG_ARRLEN(args) < 1) {
+    RETURN_ERROR(err, "Invalid or missing arguments for projection ADD%s", "");
+  }
+
+  dynamicExpr *dx = malloc(sizeof(dynamicExpr) + CMDARG_ARRLEN(args) * sizeof(valueOrProp));
+  dx->len = CMDARG_ARRLEN(args);
+  for (size_t i = 0; i < dx->len; i++) {
+    const char *p = CMDARG_STRPTR(CMDARG_ARRELEM(args, i));
+    if (*p == '@') {
+      dx->params[i].isValue = 0;
+      dx->params[i].prop = RSKEY(p);
+    } else {
+      if (!RSValue_ParseNumber(p, CMDARG_STRLEN(CMDARG_ARRELEM(args, i)), &dx->params[i].val)) {
+        RETURN_ERROR(err, "Could not parse argument %s", p);
+      }
+      dx->params[i].isValue = 1;
+    }
+  }
+
+  ProjectorCtx *ctx = malloc(sizeof(ProjectorCtx));
+  ctx->alias = alias;
+  ctx->privdata = dx;
+  ctx->properties = NULL;
+  ResultProcessor *proc = NewResultProcessor(upstream, ctx);
+  proc->Next = add_Next;
+  proc->Free = ProjectorCtx_GenericFree;
+  return proc;
+}
