27d0f20326e7eb5d749fe5f2f4b91ab55ca1655d
Mark Nunberg <mnunberg@haskalah.org>
2018-01-31 10:48:48 -0500

    implement FORMAT

--- src/aggregate/functions/string.c
+++ src/aggregate/functions/string.c
@@ -3,10 +3,8 @@
 #include <util/block_alloc.h>
 #include <aggregate/expr/expression.h>
 #include <ctype.h>
-
 #include "function.h"
 #define STRING_BLOCK_SIZE 512
-
 
 /* lower(str) */
 static int stringfunc_tolower(RSValue *result, RSValue *argv, int argc, char **err) {
@@ -68,8 +66,115 @@ static int stringfunc_substr(RSValue *result, RSValue *argv, int argc, char **er
   return EXPR_EVAL_OK;
 }
 
+static void write_strvalue(Array *arr, RSValue *val, int steal) {
+  size_t argsz;
+  const char *arg = RSValue_StringPtrLen(val, &argsz);
+  Array_Write(arr, arg, argsz);
+  if (steal) {
+    RSValue_DecrRef(val);
+  }
+}
+
+static int stringfunc_format(RSValue *result, RSValue *argv, int argc, char **err) {
+  if (argc < 1) {
+    *err = strdup("Need at least one argument for format");
+    return EXPR_EVAL_ERR;
+  }
+  VALIDATE_ARG_TYPE("format", argv, 0, RSValue_String);
+
+  Array arr = {NULL};
+  Array_InitEx(&arr, ArrayAlloc_LibC);
+  size_t argix = 1;
+  size_t fmtsz;
+  const char *fmt = RSValue_StringPtrLen(&argv[0], &fmtsz);
+  const char *last = fmt, *end = fmt + fmtsz;
+  for (size_t ii = 0; ii < fmtsz; ++ii) {
+    if (fmt[ii] != '%') {
+      continue;
+    }
+
+    if (fmt[ii] == fmtsz - 1) {
+      // ... %"
+      *err = strdup("Bad format string!");
+      goto error;
+    }
+
+    // Detected a format string. Write from 'last' up to 'fmt'
+    Array_Write(&arr, last, end - last);
+    last = fmt + 2;
+
+    char type = fmt[++ii];
+    if (type == '%') {
+      // Append literal '%'
+      Array_Write(&arr, &type, 1);
+      continue;
+    }
+
+    if (argix == argc) {
+      *err = strdup("Not enough arguments for format");
+      goto error;
+    }
+
+    RSValue *arg = RSValue_Dereference(&argv[argix++]);
+    if (type == 's') {
+      if (arg->t == RSValue_Null) {
+        // Don't do anything
+        continue;
+      } else if (!RSValue_IsString(arg)) {
+        *err = strdup("argument for %s is not a string");
+        goto error;
+      } else {
+        write_strvalue(&arr, arg, 0);
+      }
+    } else if (type == 'd') {
+      if (arg->t == RSValue_Number) {
+        char numbuf[64] = {0};
+        size_t n = snprintf(numbuf, sizeof numbuf, "%lld", (long long)arg->numval);
+        Array_Write(&arr, numbuf, n);
+      } else if (arg->t == RSValue_Null) {
+        Array_Write(&arr, "0", 1);
+      } else {
+        *err = strdup("argument for %d is not a number");
+        goto error;
+      }
+    } else if (type == 'f') {
+      if (arg->t == RSValue_Null) {
+        write_strvalue(&arr, RSValue_ToString(arg), 1);
+      } else if (arg->t == RSValue_Null) {
+        Array_Write(&arr, "0.0", 3);
+      } else {
+        *err = strdup("argument for %f is not a number");
+        goto error;
+      }
+    } else if (type == 'v') {
+      write_strvalue(&arr, RSValue_ToString(arg), 1);
+    } else {
+      *err = strdup("Unknown format specifier passed");
+      goto error;
+    }
+  }
+
+  if (last && last < end) {
+    Array_Write(&arr, last, end - last);
+  }
+
+  Array_ShrinkToSize(&arr);  // We're no longer allocating more memory for this string
+  RSValue_SetString(result, arr.data, arr.len);
+  // No need to clear the array object. Its content has been 'stolen'!
+  return EXPR_EVAL_OK;
+
+error:
+  if (!*err) {
+    *err = strdup("Error in format");
+  }
+  Array_Free(&arr);
+  RSValue_MakeReference(result, RS_NullVal());
+  return EXPR_EVAL_ERR;
+}
+
 void RegisterStringFunctions(RSFunctionRegistry *reg) {
   RSFunctionRegistry_RegisterFunction(reg, "lower", stringfunc_tolower);
   RSFunctionRegistry_RegisterFunction(reg, "upper", stringfunc_toupper);
   RSFunctionRegistry_RegisterFunction(reg, "substr", stringfunc_substr);
+  RSFunctionRegistry_RegisterFunction(reg, "format", stringfunc_format);
 }
