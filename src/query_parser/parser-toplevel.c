2afb289aa749ce72d2654748e76fb4344bf8451d
Mark Nunberg <mnunberg@haskalah.org>
2018-06-14 09:24:10 -0400

    Use default lemon template file rather than modified version
    Fix symbol collision by using preprocessor defines rather than modifying
    different template file.
    
    This way we are not locked into a specific Lemon version

---
+++ src/query_parser/parser-toplevel.c
@@ -0,0 +1,8 @@
+#define Parse RSQuery_Parse
+#define ParseTrace RSQuery_ParseTrace
+#define ParseAlloc RSQuery_ParseAlloc
+#define ParseFree RSQuery_ParseFree
+#define ParseInit RSQuery_ParseInit
+#define ParseFinalize RSQuery_ParseFinalize
+#define ParseStackPeak RSQuery_ParseStackPeak
+#include "parser.c.inc"
