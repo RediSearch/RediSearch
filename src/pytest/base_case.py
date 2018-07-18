6a59ea51bfbd8ce8d34c20aa88bb525f02c60fe5
Mark Nunberg <mnunberg@haskalah.org>
2018-07-18 16:16:37 -0400

    Additional helper methods for base case

--- src/pytest/base_case.py
+++ src/pytest/base_case.py
@@ -1,8 +1,27 @@
 from rmtest import BaseModuleTestCase
+import redis
 
 
-class BaseSearchTestCase(BaseModuleTestCase):
+class FTBaseCaseMethods(object):
+    def ftcreate(self, idx, *args):
+        return self.execute_command('ft.create', idx, *args)
+
     def search(self, idx, *args):
         if not isinstance(idx, basestring):
             raise Exception("OOPS!")
         return self.cmd('ft.search', idx, *args)
+
+    def ftexists(self, idx, doc):
+        try:
+            self.ftget(idx, doc)
+            return True
+
+        except redis.ResponseError:
+            return False
+
+    def ftget(self, idx, doc):
+        return self.execute_command('ft.get', idx, doc)
+
+
+class BaseSearchTestCase(BaseModuleTestCase, FTBaseCaseMethods):
+    pass
