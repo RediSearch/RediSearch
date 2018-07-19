10aae4feddf0f5dadb84081733a7a784db98092b
Mark Nunberg <mnunberg@haskalah.org>
2018-07-19 10:36:31 -0400

    Remove safemode test. This can be tested through other means

--- src/pytest/test_safemode.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from test import SearchTestCase
-
-
-class SafemodeTestCase(SearchTestCase):
-    @classmethod
-    def get_module_args(cls):
-        return super(SafemodeTestCase, cls).get_module_args() + ['SAFEMODE']
