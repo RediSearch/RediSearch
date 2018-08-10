92d1cc722fc9f2442fbe8621c6a7f0d5b505c06c
Mark Nunberg <mnunberg@haskalah.org>
2018-08-10 01:51:27 -0400

    Add new argument validation library
    This uses a cursor/iterative/scanning API rather than declarative "parse
    this magic argument string" syntax.

---
+++ src/rmutil/args.c
@@ -0,0 +1,125 @@
+#include "args.h"
+#include "redismodule.h"
+#include <float.h>
+#include <math.h>
+#include <string.h>
+
+int AC_Advance(ArgsCursor *ac) {
+  if (ac->offset == ac->argc) {
+    return AC_ERR_NOARG;
+  } else {
+    ac->offset++;
+  }
+  return 0;
+}
+
+#define MAYBE_ADVANCE()            \
+  if (!(flags & AC_F_NOADVANCE)) { \
+    AC_Advance(ac);                \
+  }
+
+static int tryReadAsDouble(ArgsCursor *ac, long long *ll, int flags) {
+  double dTmp = 0.0;
+  if (AC_GetDouble(ac, &dTmp, flags | AC_F_NOADVANCE)) {
+    return AC_ERR_PARSE;
+  }
+  if (flags & AC_F_COALESCE) {
+    *ll = dTmp;
+    return 0;
+  }
+
+  if ((double)(long long)dTmp != dTmp) {
+    return AC_ERR_PARSE;
+  } else {
+    *ll = dTmp;
+    return 0;
+  }
+}
+
+int AC_GetLongLong(ArgsCursor *ac, long long *ll, int flags) {
+  if (ac->offset == ac->argc) {
+    return AC_ERR_NOARG;
+  }
+  if (ac->type == AC_TYPE_RSTRING) {
+    if (RedisModule_StringToLongLong(AC_CURRENT(ac), ll) == REDISMODULE_ERR &&
+        tryReadAsDouble(ac, ll, flags)) {
+    }
+  } else {
+    char *endptr = AC_CURRENT(ac);
+    *ll = strtoll(AC_CURRENT(ac), &endptr, 10);
+    if (*endptr != '\0' || *ll == LLONG_MIN || *ll == LLONG_MAX) {
+      if (tryReadAsDouble(ac, ll, flags) != 0) {
+        return AC_ERR_PARSE;
+      }
+    }
+  }
+  if ((flags & AC_F_GE0) && *ll < 0) {
+    return AC_ERR_ELIMIT;
+  }
+  // Do validation
+  if ((flags & AC_F_GE1) && *ll < 1) {
+    return AC_ERR_ELIMIT;
+  }
+  MAYBE_ADVANCE();
+  return 0;
+}
+
+#define GEN_AC_FUNC(name, T, minVal, maxVal, isUnsigned)      \
+  int name(ArgsCursor *ac, T *p, int flags) {                 \
+    if (isUnsigned) {                                         \
+      flags |= AC_F_GE0;                                      \
+    }                                                         \
+    long long ll;                                             \
+    int rv = AC_GetLongLong(ac, &ll, flags | AC_F_NOADVANCE); \
+    if (rv) {                                                 \
+      return rv;                                              \
+    }                                                         \
+    if (ll > maxVal || ll < minVal) {                         \
+      return AC_ERR_ELIMIT;                                   \
+    }                                                         \
+    *p = ll;                                                  \
+    MAYBE_ADVANCE();                                          \
+    return 0;                                                 \
+  }
+
+GEN_AC_FUNC(AC_GetUnsignedLongLong, unsigned long long, 0, LLONG_MAX, 1)
+GEN_AC_FUNC(AC_GetUnsigned, unsigned, 0, UINT_MAX, 1)
+GEN_AC_FUNC(AC_GetInt, int, INT_MIN, INT_MAX, 0)
+
+int AC_GetDouble(ArgsCursor *ac, double *d, int flags) {
+  if (ac->type == AC_TYPE_RSTRING) {
+    if (RedisModule_StringToDouble(ac->objs[ac->offset], d) != REDISMODULE_OK) {
+      return AC_ERR_PARSE;
+    }
+  } else {
+    char *endptr = AC_CURRENT(ac);
+    *d = strtod(AC_CURRENT(ac), &endptr);
+    if (*endptr != '\0' || *d == HUGE_VAL || *d == -HUGE_VAL) {
+      return AC_ERR_PARSE;
+    }
+  }
+  if ((flags & AC_F_GE0) && *d < 0.0) {
+    return AC_ERR_ELIMIT;
+  }
+  if ((flags & AC_F_GE1) && *d < 1.0) {
+    return AC_ERR_ELIMIT;
+  }
+  MAYBE_ADVANCE();
+  return 0;
+}
+
+int AC_GetString(ArgsCursor *ac, const char **s, size_t *n, int flags) {
+  if (ac->offset == ac->argc) {
+    return AC_ERR_NOARG;
+  }
+  if (ac->type == AC_TYPE_RSTRING) {
+    *s = RedisModule_StringPtrLen(AC_CURRENT(ac), n);
+  } else {
+    *s = AC_CURRENT(ac);
+    if (n) {
+      *n = strlen(*s);
+    }
+  }
+  MAYBE_ADVANCE();
+  return 0;
+}
---
+++ src/rmutil/args.h
@@ -0,0 +1,71 @@
+#ifndef RMUTIL_ARGS_H
+#define RMUTIL_ARGS_H
+
+#include <stdlib.h>
+#include <limits.h>
+#include <stdint.h>
+#include "redismodule.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define AC_TYPE_RSTRING 1
+#define AC_TYPE_CHAR 2
+
+/**
+ * The cursor model simply reads through the current argument list, advancing
+ * the 'offset' position as required. No tricky declarative syntax, and allows
+ * for finer grained error handling.
+ */
+typedef struct {
+  void **objs;
+  int type;
+  size_t argc;
+  size_t offset;
+} ArgsCursor;
+
+static inline void ArgsCursor_InitCString(ArgsCursor *cursor, const char **argv, int argc) {
+  cursor->objs = (void **)argv;
+  cursor->type = AC_TYPE_CHAR;
+  cursor->offset = 0;
+  cursor->argc = argc;
+}
+
+static inline void ArgsCursor_InitRString(ArgsCursor *cursor, const RedisModuleString **argv,
+                                          int argc) {
+  cursor->objs = (void **)argv;
+  cursor->type = AC_TYPE_RSTRING;
+  cursor->offset = 0;
+  cursor->argc = argc;
+}
+
+#define AC_OK 0          // if (!AC_...)
+#define AC_ERR_PARSE 1   // Couldn't parse string as integer
+#define AC_ERR_NOARG 2   // No such argument
+#define AC_ERR_ELIMIT 3  // Exceeded limitations of type
+
+// These flags can be AND'd with the original type
+#define AC_F_GE1 0x100        // Must be >= 1 (no zero or negative)
+#define AC_F_GE0 0x200        // Must be >= 0 (no negative)
+#define AC_F_NOADVANCE 0x400  // Don't advance cursor position
+#define AC_F_COALESCE 0x800   // Coalesce non-integral input
+
+// These functions return AC_OK or an error code on error. Note that the
+// output value is not guaranteed to remain untouched in the case of an error
+int AC_GetString(ArgsCursor *ac, const char **s, size_t *n, int flags);
+int AC_GetLongLong(ArgsCursor *ac, long long *ll, int flags);
+int AC_GetUnsignedLongLong(ArgsCursor *ac, unsigned long long *ull, int flags);
+int AC_GetUnsigned(ArgsCursor *ac, unsigned *u, int flags);
+int AC_GetInt(ArgsCursor *ac, int *i, int flags);
+int AC_GetDouble(ArgsCursor *ac, double *d, int flags);
+
+int AC_Advance(ArgsCursor *ac);
+
+#define AC_CURRENT(ac) ((ac)->objs[(ac)->offset])
+#define AC_Clear(ac)  // NOOP
+
+#ifdef __cplusplus
+}
+#endif
+#endif
