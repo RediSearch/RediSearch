77192806b5fb4952d540e5eb4e6c602303df40ad
Mark Nunberg <mnunberg@haskalah.org>
2018-08-20 20:11:08 -0400

    Allow to override DIST_SUFFIX

--- .circleci/ci_package.sh
+++ .circleci/ci_package.sh
@@ -15,38 +15,40 @@ fi
 # Install my fork of RAMP- need to merge it first
 pip install git+https://github.com/mnunberg/RAMP@print_version --upgrade
 
-if [ -e /etc/redhat-release ]; then
-    DIST_SUFFIX=-rha
-elif [ -e /etc/debian_version ]; then
-    DIST_SUFFIX=""
-else
-    DIST_SUFFIX="" # WAT?
+if [ -z "$DIST_SUFFIX" ]; then
+    if [ -e /etc/redhat-release ]; then
+        DIST_SUFFIX=-rha
+    elif [ -e /etc/debian_version ]; then
+        DIST_SUFFIX=""
+    else
+        DIST_SUFFIX="" # WAT?
+    fi
 fi
 
 
 function do_build {
-    mode=$1
-    case "$mode" in
-        'release')
+mode=$1
+case "$mode" in
+    'release')
         format={os}$DIST_SUFFIX-{architecture}.{semantic_version}
         subdir=release
         ;;
-        'snapshot')
+    'snapshot')
         format={os}$DIST_SUFFIX-{architecture}.$CIRCLE_BRANCH-snapshot
         subdir=snapshot
         ;;
-        *)
+    *)
         echo 'No such mode!'
         exit 1
-    esac
-    distdir=$DISTDIR/$subdir
-    mkdir -p $distdir
-    outname_base=$distdir/$PACKAGE_NAME.$format
-    echo "Old outname base: $outname_base"
-    outname_base=$(ramp pack -m ramp.yml -o $outname_base --print-only $MODULE_SO)
-    echo "Output is $outname_base"
-    ramp pack -m ramp.yml -o $outname_base.zip $MODULE_SO
-    return 0
+esac
+distdir=$DISTDIR/$subdir
+mkdir -p $distdir
+outname_base=$distdir/$PACKAGE_NAME.$format
+echo "Old outname base: $outname_base"
+outname_base=$(ramp pack -m ramp.yml -o $outname_base --print-only $MODULE_SO)
+echo "Output is $outname_base"
+ramp pack -m ramp.yml -o $outname_base.zip $MODULE_SO
+return 0
 }
 
 do_build release
