4f3d19d351d86adc702b36fb7b6a4afe79140b07
Mark Nunberg <mnunberg@haskalah.org>
2018-11-07 13:40:50 -0500

    FNV functions: Input buffer is const

--- src/util/fnv.h
+++ src/util/fnv.h
@@ -6,8 +6,8 @@
 
 #define FNV_32_PRIME ((Fnv32_t)0x01000193)
 
-uint32_t rs_fnv_32a_buf(void *buf, size_t len, uint32_t hval);
+uint32_t rs_fnv_32a_buf(const void *buf, size_t len, uint32_t hval);
 
-uint64_t fnv_64a_buf(void *buf, size_t len, uint64_t hval);
+uint64_t fnv_64a_buf(const void *buf, size_t len, uint64_t hval);
 
 #endif
