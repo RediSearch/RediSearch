84102b430bc01229480b802d101f6eb25f1c8d6d
Meir Shpilraien <meir@redislabs.com>
2018-11-12 09:45:19 +0200

    added pytests

---
+++ src/pytest/test_expire.py
@@ -0,0 +1,35 @@
+import time
+from RLTest import Env
+
+
+def testExpire():
+    env = Env()
+    env.cmd('ft.create', 'idx', 'TEMPORARY', '4', 'SCHEMA', 'test', 'TEXT', 'SORTABLE')
+    ttl = env.cmd('ttl', 'idx:idx')
+    env.assertTrue(ttl > 2)
+
+    while ttl > 2:
+        ttl = env.cmd('ttl', 'idx:idx')
+        time.sleep(1)
+    env.cmd('ft.add', 'idx', 'doc1', '1.0', 'FIELDS', 'test', 'this is a simple test')
+    ttl = env.cmd('ttl', 'idx:idx')
+    env.assertTrue(ttl > 2)
+
+    while ttl > 2:
+        ttl = env.cmd('ttl', 'idx:idx')
+        time.sleep(1)
+    env.cmd('ft.search', 'idx', 'simple')
+    ttl = env.cmd('ttl', 'idx:idx')
+    env.assertTrue(ttl > 2)
+
+    while ttl > 2:
+        ttl = env.cmd('ttl', 'idx:idx')
+        time.sleep(1)
+    env.cmd('ft.aggregate', 'idx', 'simple', 'LOAD', '1', '@test')
+    ttl = env.cmd('ttl', 'idx:idx')
+    env.assertTrue(ttl > 2)
+
+    while ttl != -2:
+        ttl = env.cmd('ttl', 'idx:idx')
+        time.sleep(1)
+    env.expect('keys', '*').equal([])
