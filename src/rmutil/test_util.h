a9ee3db9f40a97ea73f6a38a2cacc5af971a60cb
Mark Nunberg <mnunberg@haskalah.org>
2018-11-13 22:56:44 -0500

    Remove nonstandard use of `__STRING` (#517)

--- src/rmutil/test_util.h
+++ src/rmutil/test_util.h
@@ -8,16 +8,16 @@
 
 
 #define RMUtil_Test(f) \
-                if (argc < 2 || RMUtil_ArgExists(__STRING(f), argv, argc, 1)) { \
+                if (argc < 2 || RMUtil_ArgExists(#f, argv, argc, 1)) { \
                     int rc = f(ctx); \
                     if (rc != REDISMODULE_OK) { \
-                        RedisModule_ReplyWithError(ctx, "Test " __STRING(f) " FAILED"); \
+                        RedisModule_ReplyWithError(ctx, "Test " #f " FAILED"); \
                         return REDISMODULE_ERR;\
                     }\
                 }
            
                 
-#define RMUtil_Assert(expr) if (!(expr)) { fprintf (stderr, "Assertion '%s' Failed\n", __STRING(expr)); return REDISMODULE_ERR; }
+#define RMUtil_Assert(expr) if (!(expr)) { fprintf (stderr, "Assertion '%s' Failed\n", #expr); return REDISMODULE_ERR; }
 
 #define RMUtil_AssertReplyEquals(rep, cstr) RMUtil_Assert( \
             RMUtil_StringEquals(RedisModule_CreateStringFromCallReply(rep), RedisModule_CreateString(ctx, cstr, strlen(cstr))) \
