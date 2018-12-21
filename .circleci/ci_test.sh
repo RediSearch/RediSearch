148284ea223378a5bdc491520ddeaccd2230111b
Mark Nunberg <mnunberg@haskalah.org>
2018-12-21 05:20:20 -0500

    typo

--- .circleci/ci_test.sh
+++ .circleci/ci_test.sh
@@ -4,7 +4,7 @@ set -x
 
 pip install wheel
 pip install setuptools --upgrade
-pip install git+https://github.com/RedisLabsModules/RLTest.git@mark-fixest
+pip install git+https://github.com/RedisLabsModules/RLTest.git@mark-fixes
 
 PROJECT_DIR=$PWD
 cd $PROJECT_DIR/$BUILD_DIR
