196919862d220e688f6a3c2f1e3feba38441495c
Mark Nunberg <mnunberg@haskalah.org>
2019-01-29 07:28:50 -0500

    Forgot HLL_SUM reducer

--- src/aggregate/reducer.c
+++ src/aggregate/reducer.c
@@ -43,7 +43,7 @@ ReducerFactory RDCR_GetFactory(const char *name) {
   X(RDCRFirstValue_New, "FIRST_VALUE")             \
   X(RDCRRandomSample_New, "RANDOM_SAMPLE")         \
   X(RDCRHLL_New, "HLL")                            \
-  X(RDCRHLLSum_New, "HLLSUM")
+  X(RDCRHLLSum_New, "HLL_SUM")
 
 void RDCR_RegisterBuiltins(void) {
 #define X(fn, n) RDCR_RegisterFactory(n, fn);
