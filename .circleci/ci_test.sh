af9b72f6be75934d5b3889ea6a435ea315698574
Meir Shpilraien <meir@redislabs.com>
2019-02-04 13:16:57 +0200

    fixed circle tests

--- .circleci/ci_test.sh
+++ .circleci/ci_test.sh
@@ -5,6 +5,7 @@ set -x
 pip install wheel
 pip install setuptools --upgrade
 pip install git+https://github.com/RedisLabsModules/RLTest.git
+pip install redis-py-cluster
 
 PROJECT_DIR=$PWD
 cd $PROJECT_DIR/$BUILD_DIR
