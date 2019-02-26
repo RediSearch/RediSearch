589a71cda1530249cbf73d940fa16e7086e9b03f
Meir Shpilraien (Spielrein) <meir@redislabs.com>
2019-02-26 15:49:16 +0200

    use wait4 instead of wait3 to check when the gc fork process terminates (#618)
    * use wait4 instead of wait3 to check when the gc fork process terminates
    
    * set testGCIntegrationWithRedisFork test to run with forkgc only
    
    * review fixes

--- src/config.h
+++ src/config.h
@@ -77,6 +77,7 @@ typedef struct {
 
   GCPolicy gcPolicy;
   GCPolicy forkGcRunIntervalSec;
+  size_t forkGcSleepBeforeExit;
 
   // Chained configuration data
   void *chainedConfig;
@@ -155,6 +156,7 @@ sds RSConfig_GetInfoString(const RSConfig *config);
     .indexPoolSize = CONCURRENT_INDEX_POOL_DEFAULT_SIZE, .poolSizeNoAuto = 0,                   \
     .gcScanSize = GC_SCANSIZE, .minPhoneticTermLen = DEFAULT_MIN_PHONETIC_TERM_LEN,             \
     .gcPolicy = GCPolicy_Default, .forkGcRunIntervalSec = DEFAULT_FORK_GC_RUN_INTERVAL,         \
+    .forkGcSleepBeforeExit = 0,                                                                 \
   }
 
 #endif
