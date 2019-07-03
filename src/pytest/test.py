1c89606d69687f2d74b23ab4f895b7550ace1ef7
Mark Nunberg <mnunberg@haskalah.org>
2019-07-03 15:19:30 -0400

    Fix crash on conversion error when freeing other indexed fields
    This happens because the other indexed fields are not zero-initialized
    and we have no way of knowing whether they were indexed or not.

--- src/pytest/test.py
+++ src/pytest/test.py
@@ -2105,6 +2105,20 @@ def testUnknownSymbolErrorOnConditionalAdd(env):
     env.expect('ft.add idx doc1 1.0 REPLACE PARTIAL IF @f1<awfwaf FIELDS f1 foo f2 1 f3 boo').ok()
     env.expect('ft.add idx doc1 1.0 REPLACE PARTIAL IF @f1<awfwaf FIELDS f1 foo f2 1 f3 boo').error().contains('Unknown symbol')
 
+
+def testIssue736(env):
+    # 1. create the schema, we need a tag field
+    env.cmd('ft.create', 'idx', 'schema', 't1', 'text', 'n2', 'numeric', 't2', 'tag')
+    # 2. create a single document to initialize at least one RSAddDocumentCtx
+    env.cmd('ft.add', 'idx', 'doc1', 1, 'fields', 't1', 'hello', 't2', 'foo, bar')
+    # 3. create a second document with many filler fields to force a realloc:
+    extra_fields = []
+    for x in range(20):
+        extra_fields += ['nidx_fld{}'.format(x), 'val{}'.format(x)]
+    extra_fields += ['n2', 'not-a-number', 't2', 'random, junk']
+    with env.assertResponseError():
+        env.cmd('ft.add', 'idx', 'doc2', 1, 'fields', *extra_fields)
+
 def grouper(iterable, n, fillvalue=None):
     "Collect data into fixed-length chunks or blocks"
     from itertools import izip_longest
