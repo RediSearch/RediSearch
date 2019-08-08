852d215425dc45ac43aa6993bcab3cd227342336
Mark Nunberg <mnunberg@haskalah.org>
2019-08-07 22:17:37 -0400

    Updgrade to new rltest version.. also fix concurrency issue in rdb test

--- .circleci/ci_get_deps.sh
+++ .circleci/ci_get_deps.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 set -x
 ./srcutil/get_gtest.sh
+python ./src/pytest/test_rdb_compatibility.py
+
 pip install wheel
 pip install setuptools --upgrade
-pip install git+https://github.com/RedisLabsModules/RLTest.git
+pip install git+https://github.com/RedisLabsModules/RLTest.git@concurrent
 pip install redis-py-cluster
