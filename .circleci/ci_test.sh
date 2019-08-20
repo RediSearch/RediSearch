ce9ec946e760b53e6cdd88bcf0bbf6b9f7a3e84b
meir@redislabs.com <meir@redislabs.com>
2019-08-20 10:46:59 +0300

    set redis_py_cluster to version 1.3.6

--- .circleci/ci_test.sh
+++ .circleci/ci_test.sh
@@ -5,7 +5,7 @@ set -x
 pip install wheel
 pip install setuptools --upgrade
 pip install git+https://github.com/RedisLabsModules/RLTest.git
-pip install redis-py-cluster
+pip install redis-py-cluster==1.3.6
 
 PROJECT_DIR=$PWD
 cd $PROJECT_DIR/$BUILD_DIR
