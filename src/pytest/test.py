ebc28dd1f1a331b5909e012b729fe98b3e198fb2
meir@redislabs.com <meir@redislabs.com>
2019-08-27 15:31:07 +0300

    fix #866, now it will not be possible to delete unexists documents

--- src/pytest/test.py
+++ src/pytest/test.py
@@ -2217,6 +2217,12 @@ def testIssue862(env):
     rv = env.cmd("FT.SEARCH", "idx", "foo", 'WITHSORTKEYS')
     env.assertEqual([1L, 'doc1', None, ['test', 'foo']], rv)
 
+def testIssue_866(env):
+    env.expect('ft.sugadd', 'sug', 'test123', '1').equal(1)
+    env.expect('ft.sugadd', 'sug', 'test456', '1').equal(2)
+    env.expect('ft.sugdel', 'sug', 'test').equal(0)
+    env.expect('ft.sugget', 'sug', '').equal(['test123', 'test456'])
+
 def grouper(iterable, n, fillvalue=None):
     "Collect data into fixed-length chunks or blocks"
     from itertools import izip_longest
