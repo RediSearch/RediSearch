ea2ee03553ed806f1e97e5574ada70a02450c320
meir@redislabs.com <meir@redislabs.com>
2019-09-09 12:40:51 +0300

    fix fork gc not exits on terminate

--- src/fork_gc.c
+++ src/fork_gc.c
@@ -799,7 +799,7 @@ static int ForkGc_PeriodicCallback(RedisModuleCtx *ctx, void *privdata) {
     if (!ForkGc_IsForkApiExists()) {
 #ifdef __linux__
       // set the parrent death signal to SIGTERM
-      int r = prctl(PR_SET_PDEATHSIG, SIGTERM);
+      int r = prctl(PR_SET_PDEATHSIG, SIGKILL);
       if (r == -1) {
         exit(1);
       }
