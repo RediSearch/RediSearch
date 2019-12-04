fa354aca1acb154fc6408e4183540eccd7a1f606
Ariel <ashtul@gmail.com>
2019-12-04 11:17:52 +0200

    cleaned and move into function

--- src/pytest/test_replicate.py
+++ src/pytest/test_replicate.py
@@ -4,26 +4,54 @@ import os
 import os.path
 from RLTest import Env
 import time
+import random
+
+class TimeoutException(Exception):
+  pass
+
+class TimeLimit(object):
+    """
+    A context manager that fires a TimeExpired exception if it does not
+    return within the specified amount of time.
+    """
+    def __init__(self, timeout):
+        self.timeout = timeout
+    def __enter__(self):
+        signal.signal(signal.SIGALRM, self.handler)
+        signal.setitimer(signal.ITIMER_REAL, self.timeout, 0)
+    def __exit__(self, exc_type, exc_value, traceback):
+        signal.setitimer(signal.ITIMER_REAL, 0)
+        signal.signal(signal.SIGALRM, signal.SIG_DFL)
+    def handler(self, signum, frame):
+        raise TimeoutException()
+
+def checkSlaveSynced(env, slaveConn, command, expected_result, time_out=5):
+  try:
+    with TimeLimit(time_out):
+      res = slaveConn.execute_command(*command)
+      while res != expected_result:
+        res = slaveConn.execute_command(*command)
+  except TimeoutException:
+    env.assertTrue(False, message='Failed waiting for command to be executed on slave')
+  except Exception as e:
+    env.assertTrue(False, message=e.message)
 
 def testDelReplicate():
   env = Env(useSlaves=True)
   master = env.getConnection()
   slave = env.getSlaveConnection() 
-
+  master.execute_command('FLUSHALL')
   env.assertContains("PONG", master.execute_command("ping"))
   env.assertContains("PONG", slave.execute_command("ping"))  
   env.assertOk(master.execute_command('ft.create', 'idx', 'schema', 'f', 'text'))
 
+  checkSlaveSynced(env, slave, ('exists', 'idx:idx'), 1)
+
   for i in range(10):
     master.execute_command('ft.add', 'idx', 'doc%d' % i, 1.0, 'fields',
                                       'f', 'hello world')
-  # Ensure slave is updated
-  master.execute_command('set foo bar')
-  end = time.time() + 5
-  while time.time() < end:
-    if slave.execute_command('exists foo'):
-      break
-    time.sleep(0.01)
+
+  checkSlaveSynced(env, slave, ('ft.get', 'idx', 'doc0'), ['f', 'hello world'])
 
   for i in range(10):
     # checking for insertion
@@ -36,13 +64,7 @@ def testDelReplicate():
     env.assertEqual(1, master.execute_command(
           'ft.del', 'idx', 'doc%d' % i, 'DD'))
   
-  # Ensure slave is updated
-  master.execute_command('del foo')
-  end = time.time() + 5
-  while time.time() < end:
-    if not slave.execute_command('exists foo'):
-      break
-    time.sleep(0.01)
+  ensureMasterSlaveSync(env, slave, ('ft.get', 'idx', 'doc0'), None)
 
   for i in range(10):
     # checking for deletion
