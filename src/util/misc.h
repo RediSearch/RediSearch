aeb496f6745d8f3e0274c5b8544d64115ae61fc9
Mark Nunberg <mnunberg@haskalah.org>
2020-04-14 20:26:41 -0400

    Add some utility macros for free/retain

--- src/util/misc.h
+++ src/util/misc.h
@@ -2,10 +2,26 @@
 #define RS_MISC_H
 
 #include "redismodule.h"
+#include "module.h"
 
 /**
  * This handler crashes
  */
 void GenericAofRewrite_DisabledHandler(RedisModuleIO *aof, RedisModuleString *key, void *value);
+
+#define RM_XFreeString(s)                        \
+  do {                                           \
+    if (s) {                                     \
+      RedisModule_FreeString(RSDummyContext, s); \
+    }                                            \
+  } while (0);
+
+#define RM_XRetainString(s)                        \
+  ({                                               \
+    if (s) {                                       \
+      RedisModule_RetainString(RSDummyContext, s); \
+    }                                              \
+    s;                                             \
+  })
 
 #endif
