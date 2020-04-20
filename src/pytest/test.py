b508f326db5b7eb781ceae47e4b0c66f4f1d6319
meir@redislabs.com <meir@redislabs.com>
2020-04-20 13:46:12 +0300

    fix tests on coordinator

--- src/pytest/test.py
+++ src/pytest/test.py
@@ -2984,6 +2984,7 @@ def testIssue1169(env):
     env.expect('FT.AGGREGATE idx foo GROUPBY 1 @txt1 REDUCE FIRST_VALUE 1 @txt2 as test').equal([1L, ['txt1', 'foo', 'test', None]])
 
 def testIssue1184(env):
+    env.skipOnCluster()
     field_types = ['TEXT', 'NUMERIC', 'TAG']
 
     for ft in field_types:
