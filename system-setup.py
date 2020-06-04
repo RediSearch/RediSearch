cfe84c244d50a7ebeeceef85197439d95de82bbc
rafie <rafi@redislabs.com>
2020-06-04 20:37:59 +0300

    fixes 1

--- system-setup.py
+++ system-setup.py
@@ -22,13 +22,21 @@ class RediSearchSetup(paella.Setup):
 
     def debian_compat(self):
         self.install("libatomic1")
-        self.install("build-essential")
+        self.install("build-essential cmake")
         self.install("python-psutil")
 
     def redhat_compat(self):
+        self.install("redhat-lsb-core")
         self.install("libatomic")
         self.group_install("'Development Tools'")
-        self.install("redhat-lsb-core")
+        self.install("cmake3")
+        self.run("ln -s `command -v cmake3` /usr/local/bin/cmake")
+
+        self.install("centos-release-scl")
+        self.install("devtoolset-8")
+        self.run("cp /opt/rh/devtoolset-8/enable /etc/profile.d/scl-devtoolset-8.sh")
+        paella.mkdir_p("%s/profile.d" % ROOT)
+        self.run("cp /opt/rh/devtoolset-8/enable %s/profile.d/scl-devtoolset-8.sh" % ROOT)
 
         # uninstall and install psutil (order is important), otherwise RLTest fails
         self.run("pip uninstall -y psutil || true")
@@ -37,11 +45,15 @@ class RediSearchSetup(paella.Setup):
     def fedora(self):
         self.install("libatomic")
         self.group_install("'Development Tools'")
+        self.install("cmake")
+        self.run("ln -s `command -v cmake3` /usr/local/bin/cmake")
 
     def macosx(self):
         if sh('xcode-select -p') == '':
             fatal("Xcode tools are not installed. Please run xcode-select --install.")
 
+        self.install_gnu_utils()
+        self.install("cmake")
     def common_last(self):
         self.run("pip uninstall -y -q redis redis-py-cluster ramp-packer RLTest rmtest semantic-version || true")
         # redis-py-cluster should be installed from git due to redis-py dependency
@@ -49,6 +61,8 @@ class RediSearchSetup(paella.Setup):
         self.pip_install("--no-cache-dir git+https://github.com/RedisLabsModules/RLTest.git@master")
         self.pip_install("--no-cache-dir git+https://github.com/RedisLabs/RAMP@master")
         self.pip_install("pudb")
+
+        self.pip3_install("-r %s/readies/paella/requirements.txt" % HERE)
 
 #----------------------------------------------------------------------------------------------
 
