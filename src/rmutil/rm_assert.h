121a1e89f5dae8c531f091909a272e839a60e3cc
ariel shtul <ashtul@gmail.com>
2020-06-10 10:29:31 +0300

    update rm_assert

--- src/rmutil/rm_assert.h
+++ src/rmutil/rm_assert.h
@@ -2,8 +2,6 @@
 #define __REDISEARCH_ASSERT__
 
 #include <redismodule.h>
-#include <assert.h>
-#include "module.h"
 #ifdef NDEBUG
 
 #define RS_LOG_ASSERT(ctx, condition, fmt, ...)    (__ASSERT_VOID_CAST (0))
@@ -13,10 +11,9 @@
 
 #define RS_LOG_ASSERT_FMT(condition, fmt, ...)                                          \
     if (!(condition)) {                                                                 \
-        RedisModuleCtx* assertCtx = RSDummyContext;                                     \
-        RedisModule_Log(assertCtx, "warning", "(%s) failed on %s:%s, Line %d - " fmt,   \
-                #condition, __FILE__, __func__, __LINE__, __VA_ARGS__);                 \
-        *((char *)NULL) = 0; /* Crashes server and create a crash report*/              \
+        RedisModuleCtx* assertCtx = RedisModule_GetThreadSafeContext(NULL);             \
+        RedisModule_Log(assertCtx, "warning", fmt, __VA_ARGS__);                        \
+        RedisModule_Assert(condition); /* Crashes server and create a crash report*/    \
     } 
 
 #define RS_LOG_ASSERT(condition, str)  RS_LOG_ASSERT_FMT(condition, str "%s", "")
