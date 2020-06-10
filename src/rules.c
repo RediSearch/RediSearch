4f92b28458d6a8050a0ea5950f7f07434138ce0d
ariel shtul <ashtul@gmail.com>
2020-06-10 09:03:38 +0300

    WIP

--- src/rules.c
+++ src/rules.c
@@ -1,46 +1,28 @@
 #include "rules.h"
-#include "aggregate/expr/expression.h"
 
-IndexSpec *SchemaRules_g;
-
-SchemaRule *Rule_Create(ruleSettings *rulesopts, QueryError *status) {
+int Rule_EvalExpression(IndexSpec *spec, ruleSettings *rulesopts, QueryError *status) {
     RSExpr *e = ExprAST_Parse(rulesopts->expr, strlen(rulesopts->expr), status);
     if (!e) {
-      return NULL;
+      return REDISMODULE_ERR;
     }
 
     SchemaRule *rule = rm_calloc(1, sizeof(*rule));
+    rule->spec = spec;
     rule->expression = e;
     rule->setting.expr = rm_strdup(rulesopts->expr);
 
-    if (rulesopts->score) {
+    if (rule->setting.score) {
       rule->setting.score = rm_strdup(rulesopts->score);
     }
     
-    if (rulesopts->lang) {
+    if (rule->setting.lang) {
       rule->setting.lang = rm_strdup(rulesopts->lang);
     }
 
-    if (rulesopts->payload) {
+    if (rule->setting.payload) {
       rule->setting.payload = rm_strdup(rulesopts->payload);
     }
 
-    return rule;
-}
-
-void Rule_free(SchemaRule *rule) {
-  ExprAST_Free(rule->expression);
-  rm_free(rule->setting.expr);
-
-  if (rule->setting.score) {
-    rm_free(rule->setting.score);
-  }  
-  if (rule->setting.lang) {
-    rm_free(rule->setting.lang);
-  }
-  if (rule->setting.payload) {
-    rm_free(rule->setting.payload);
-  }
-  rm_free(rule);
-  // TODO: Remove from global list
+    SchemaRules_g = array_ensure_append(SchemaRules_g, &rule, 1, SchemaRule);
+    return REDISMODULE_OK;
 }
