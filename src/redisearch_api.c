972f3d971b87194e08286db66c67d297cda65986
meir@redislabs.com <meir@redislabs.com>
2020-07-12 10:59:17 +0300

    update forkgc deletedDocsFromLastRun from llapi

--- src/redisearch_api.c
+++ src/redisearch_api.c
@@ -154,6 +154,9 @@ int RediSearch_DeleteDocument(IndexSpec* sp, const void* docKey, size_t len) {
     if (DocTable_Delete(&sp->docs, docKey, len)) {
       // Delete returns true/false, not RM_{OK,ERR}
       sp->stats.numDocuments--;
+      if (sp->gc) {
+        GCContext_OnDelete(sp->gc);
+      }
     } else {
       rc = REDISMODULE_ERR;
     }
