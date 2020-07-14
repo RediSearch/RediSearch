b6f0c26351db22319f2a7d50a62fcf26fc816dcc
ariel shtul <ashtul@gmail.com>
2020-07-14 11:19:49 +0300

    fix test

--- src/pytest/test_issues.py
+++ src/pytest/test_issues.py
@@ -1,5 +1,5 @@
 def test_1282(env):
-  env.expect('FT.CREATE idx SCHEMA txt1 TEXT').equal('OK')
+  env.expect('FT.CREATE idx ON HASH SCHEMA txt1 TEXT').equal('OK')
   env.expect('FT.ADD idx doc1 1.0 FIELDS txt1 foo').equal('OK')
 
   # optional search for new word would crash server
