8a28d5c40848d9c3f69c5be7ecc85d51e35f7619
Ariel Shtul <ashtul@gmail.com>
2020-07-28 09:36:43 +0300

    fix -20* query (#1374)

--- src/pytest/test_issues.py
+++ src/pytest/test_issues.py
@@ -5,3 +5,8 @@ def test_1282(env):
   # optional search for new word would crash server
   env.expect('FT.SEARCH idx', '~foo').equal([1L, 'doc1', ['txt1', 'foo']])
   env.expect('FT.SEARCH idx', '~bar ~foo').equal([1L, 'doc1', ['txt1', 'foo']])
+
+def test_1304(env):
+  env.expect('FT.CREATE idx SCHEMA txt1 TEXT').equal('OK')
+  env.expect('FT.EXPLAIN idx -20*').equal('PREFIX{-20*}\n')
+  env.expect('FT.EXPLAIN idx -\\20*').equal('NOT{\n  PREFIX{20*}\n}\n')
