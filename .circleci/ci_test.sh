84daed585d1bf7804ed46415aa5e80b5ebcd4b28
rafie <rafi@redislabs.com>
2020-08-16 13:29:22 +0300

    Install RLTest after redis-py-cluster

--- .circleci/ci_test.sh
+++ .circleci/ci_test.sh
@@ -5,8 +5,8 @@ set -x
 python -m pip install --upgrade pip==19.3.1
 pip install wheel
 pip install setuptools --upgrade
-pip install git+https://github.com/RedisLabsModules/RLTest.git
 pip install redis-py-cluster
+pip install git+https://github.com/RedisLabsModules/RLTest.git
 
 PROJECT_DIR=$PWD
 cd $PROJECT_DIR/$BUILD_DIR
