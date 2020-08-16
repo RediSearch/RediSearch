dcbbefc2f545fcb43e4dccd7fb92a4480295ec43
Ariel Shtul <ashtul@gmail.com>
2020-08-16 10:00:11 +0300

    Fix issue 1414 remove hard limit on ft.search (#1416)
    * Fix issue 1414 remove hard limit on ft.search
    
    * add docs
    
    * print out unlimited

--- src/pytest/test_issues.py
+++ src/pytest/test_issues.py
@@ -10,3 +10,11 @@ def test_1304(env):
   env.expect('FT.CREATE idx SCHEMA txt1 TEXT').equal('OK')
   env.expect('FT.EXPLAIN idx -20*').equal('PREFIX{-20*}\n')
   env.expect('FT.EXPLAIN idx -\\20*').equal('NOT{\n  PREFIX{20*}\n}\n')
+
+def test_1414(env):
+  env.expect('FT.CREATE idx SCHEMA txt1 TEXT').equal('OK')
+  env.expect('ft.add idx doc 1 fields foo hello bar world').ok()
+  env.expect('ft.search idx * limit 0 1234567').error().contains('LIMIT exceeds maximum of 1000000') 
+  env.expect('FT.CONFIG set MAXSEARCHRESULTS -1').equal('OK')
+  env.expect('ft.search idx * limit 0 1234567').equal([1L, 'doc', ['foo', 'hello', 'bar', 'world']]) 
+  
