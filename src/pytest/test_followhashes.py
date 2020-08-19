ea60737e79ba7c90078ed27044ff0345ca1269ae
ariel shtul <ashtul@gmail.com>
2020-08-19 10:41:14 +0300

    wait for index

--- src/pytest/test_followhashes.py
+++ src/pytest/test_followhashes.py
@@ -492,13 +492,17 @@ def testNoInitialScan(env):
     
     #Regular
     env.expect('FT.CREATE idx SCHEMA test TEXT').equal('OK')
+    waitForIndex(env, 'idx')
     env.expect('FT.SEARCH idx hello').equal([1L, 'a', ['test', 'hello', 'text', 'world']])
     #NoInitialIndex
     env.expect('FT.CREATE idx_no_scan NOINITIALSCAN SCHEMA test TEXT').equal('OK')
+    waitForIndex(env, 'idx_no_scan')
     env.expect('FT.SEARCH idx_no_scan hello').equal([0L])
     # Temporary
     env.expect('FT.CREATE temp_idx TEMPORARY 10 SCHEMA test TEXT').equal('OK')
+    waitForIndex(env, 'temp_idx')
     env.expect('FT.SEARCH temp_idx hello').equal([1L, 'a', ['test', 'hello', 'text', 'world']])
     # Temporary & NoInitialIndex
     env.expect('FT.CREATE temp_idx_no_scan NOINITIALSCAN TEMPORARY 10 SCHEMA test TEXT').equal('OK')
+    waitForIndex(env, 'temp_idx_no_scan')
     env.expect('FT.SEARCH temp_idx_no_scan hello').equal([0L])
