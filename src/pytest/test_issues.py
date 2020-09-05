aadaa175d72369338263916b643dfb2cc32b61b5
ariel shtul <ashtul@gmail.com>
2020-09-05 21:10:56 +0300

    test !term

--- src/pytest/test_issues.py
+++ src/pytest/test_issues.py
@@ -1,3 +1,5 @@
+from common import getConnectionByEnv, waitForIndex, sortedResults, toSortedFlatList
+
 def test_1282(env):
   env.expect('FT.CREATE idx ON HASH SCHEMA txt1 TEXT').equal('OK')
   env.expect('FT.ADD idx doc1 1.0 FIELDS txt1 foo').equal('OK')
@@ -19,3 +21,9 @@ def test_1414(env):
   env.expect('FT.CONFIG set MAXSEARCHRESULTS -1').equal('OK')
   env.expect('ft.search idx * limit 0 1234567').equal([1L, 'doc', ['foo', 'hello', 'bar', 'world']]) 
   
+def testNotOnly(env):
+  conn = getConnectionByEnv(env)
+  conn.execute_command('FT.CREATE', 'idx', 'SCHEMA', 'txt1', 'TEXT')
+  conn.execute_command('HSET', 'a', 'txt1', 'hello', 'txt2', 'world')
+  conn.execute_command('HSET', 'b', 'txt1', 'world', 'txt2', 'hello')
+  env.expect('ft.search idx !world').equal([1L, 'b', ['txt1', 'world', 'txt2', 'hello']])
