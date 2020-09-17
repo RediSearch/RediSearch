2f5ec54f76384f4b575f175d43a103de9f1b94f9
Ariel Shtul <ashtul@gmail.com>
2020-09-17 18:14:07 +0300

    tweak ft.info and docs (#1525)
    (cherry picked from commit 353da92d1e1cddc517411cd43a1dced21a6fb488)

---
+++ src/pytest/test_info.py
@@ -0,0 +1,53 @@
+from common import getConnectionByEnv, waitForIndex, sortedResults, toSortedFlatList
+from RLTest import Env
+from time import sleep
+
+
+def ft_info_to_dict(env, idx):
+  res = env.execute_command('ft.info', idx)
+  return {res[i]: res[i + 1] for i in range(0, len(res), 2)}
+
+# The output for this test can be used for recreating documentation for `FT.INFO`
+def testInfo(env):
+  env.skip()
+  count = 345678
+  conn = env.getConnection()
+  pl = conn.pipeline()
+
+  idx = 'wikipedia'
+
+  for i in range(count):
+    geo = '1.23456,1.' + str(i / float(count))
+    pl.execute_command('HSET', 'doc%d' % i, 'title', 'hello%d' % i,
+                                            'body', '%dhello%dworld%dhow%dare%dyou%dtoday%d' % (i, i, i, i, i, i, i),
+                                            'n', i / 17.0,
+                                            'geo', geo)
+    if i % 10000 == 0:
+      pl.execute()
+  pl.execute()
+
+  env.expect('FT.CREATE', idx, 'STOPWORDS', 3, 'TLV', 'summer', '2020', 
+                               'SCHEMA', 'title', 'TEXT', 'SORTABLE',                                          
+                                         'body', 'TEXT',                                         
+                                         'id', 'NUMERIC',                                         
+                                         'subject location', 'GEO').ok()
+
+  waitForIndex(env, idx)
+
+  for i in range(count):
+    pl.execute_command('DEL', 'doc%d' % i)
+    if i % 10000 == 0:
+      pl.execute()
+      conn.execute_command('FT.DEBUG', 'GC_FORCEINVOKE', idx)    
+  pl.execute()
+
+  #  GC stats
+  for i in range(25):
+    conn.execute_command('FT.DEBUG', 'GC_FORCEINVOKE', idx)    
+
+  # cursor stats
+  #query = ['FT.AGGREGATE', idx, '*', 'WITHCURSOR']
+  #res = env.cmd(*query)  
+  #env.cmd('FT.CURSOR', 'READ', idx, str(res[1]))
+  
+  #print info
