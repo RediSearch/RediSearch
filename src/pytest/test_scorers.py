81804bef4de2c86999db4347b6c07a29f31a7b3c
Ariel Shtul <ashtul@gmail.com>
2020-09-17 14:52:50 +0300

    explicit error for EXPLAINSCORE w/o WITHSCORES (#1550)
    * explicit error for EXPLAINSCORE w/o WITHSCORES
    
    * per review
    
    (cherry picked from commit 21b3d7f6a778cde71637757de92bd5f5bb4d226f)

--- src/pytest/test_scorers.py
+++ src/pytest/test_scorers.py
@@ -157,3 +157,8 @@ def testDisMaxScorerExplanation(env):
             ['DISMAX 10.00 = Weight 1.00 * Frequency 10', 'DISMAX 10.00 = Weight 1.00 * Frequency 10']])
     env.assertEqual(res[8][1], ['20.00 = Weight 1.00 * children DISMAX 20.00',
             ['DISMAX 10.00 = Weight 1.00 * Frequency 10', 'DISMAX 10.00 = Weight 1.00 * Frequency 10']])
+
+def testScoreError(env):
+    env.expect('ft.create idx ON HASH schema title text').ok()
+    env.expect('ft.add idx doc1 0.01 fields title hello').ok()
+    env.expect('ft.search idx hello EXPLAINSCORE').error().contains('EXPLAINSCORE must be accompanied with WITHSCORES')
