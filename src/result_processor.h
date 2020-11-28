306ff42b6c21af4a2fe005e14d14506f5c1a298a
ariel shtul <ashtul@gmail.com>
2020-11-28 10:28:02 +0200

    fix struct timespec

--- src/result_processor.h
+++ src/result_processor.h
@@ -238,8 +238,8 @@ static inline int TimedOut(struct timespec timeout) {
 
 static inline void updateTimeout(struct timespec *timeout, int32_t durationNS) {
   struct timespec now;
-  struct timespec duration = {.tv_nsec = ((durationNS % 1000) * 1000000),
-                             .tv_sec = durationNS / 1000 };
+  struct timespec duration = { .tv_sec = durationNS / 1000,
+                              .tv_nsec = ((durationNS % 1000) * 1000000) };
   clock_gettime(CLOCK_MONOTONIC_RAW, &now);
   timeradd(&now, &duration, timeout);
   //printf("sec %ld ms %ld\n", now.tv_sec, now.tv_nsec);
