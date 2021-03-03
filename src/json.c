3431e69742ed13fba8c596421d7d7cbf8ae7eba2
oshadmi <omer.shadmi@redislabs.com>
2021-03-03 14:24:53 +0200

    WIP (register for rejson callbacks)

---
+++ src/json.c
@@ -0,0 +1,39 @@
+#include "json.h"
+#include <string.h>
+
+int JSONKeyChangeHandler(RedisModuleKey *key) {
+  printf("==> in JSONKeyChangeHandler\n");
+  return 1;
+}
+
+void ModuleChangeHandler(struct RedisModuleCtx *ctx, RedisModuleEvent e, uint64_t sub, void *data) {
+
+  REDISMODULE_NOT_USED(e);
+  RedisModuleModuleChange *ei = data;
+  if (sub == REDISMODULE_SUBEVENT_MODULE_UNLOADED) {    
+    // If RedisJSON module is loaded after RediSearch
+    // Need to get the API exported by RedisJSON and use it to register for events
+    if (strcmp(ei->module_name, "ReJSON") == 0) {
+        printf("detected %p unloading %s", ctx, ei->module_name);
+        if (RegisterJSONCallbacks(ctx, 0)) {
+            //TODO: Once registered we can unsubscribe from ServerEvent RedisModuleEvent_ModuleChange
+            // Unless we want to hanle ReJSON module unload
+        }
+    }
+  }
+}
+
+int RegisterJSONCallbacks(RedisModuleCtx *ctx, int subscribeToModuleChange) {
+  RedisJSON_GetApiV1 get_api_func_ptr = RedisModule_GetSharedAPI(ctx, "RedisJSON_GetApiV1");
+  if (get_api_func_ptr) {
+    RedisJSONAPI_V1 *rejson_api = get_api_func_ptr(ctx);
+    if (rejson_api) {
+      rejson_api->register_callback_key_change(JSONKeyChangeHandler);
+      return 1;
+    }
+  } else if (subscribeToModuleChange) {
+    RedisModule_SubscribeToServerEvent(ctx,
+        RedisModuleEvent_ModuleChange, ModuleChangeHandler);
+  }
+  return 0;
+}
---
+++ src/rejson_api.h
@@ -0,0 +1,24 @@
+#ifndef SRC_REJSON_API_H_
+#define SRC_REJSON_API_H_
+
+#include "redismodule.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef int (*RedisModuleAPICallbackKeyChange)(RedisModuleKey* key);
+typedef int (*RedisModuleAPICallbackEvent)(int event);
+typedef struct RedisJSONAPI_V1 {
+    void (*free_string)(const char* string);
+    int (*register_callback_key_change)(RedisModuleAPICallbackKeyChange callback);
+    int (*register_callback_event)(RedisModuleAPICallbackEvent callback);
+    int (*get_json_path)(RedisModuleKey* key, const char* path);
+} RedisJSONAPI_V1;
+
+typedef RedisJSONAPI_V1* (*RedisJSON_GetApiV1)(RedisModuleCtx *ctx);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* SRC_REJSON_API_H_ */
