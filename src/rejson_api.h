145ad219969391f42ecbf91564ddb8eb7d951b5e
oshadmi <omer.shadmi@redislabs.com>
2021-03-19 15:54:52 +0200

    WIP initial API fine tuning

--- src/rejson_api.h
+++ src/rejson_api.h
@@ -10,24 +10,40 @@ extern "C" {
 typedef enum JSONType {
     JSONType_String = 0,
     JSONType_Int = 1,
-    JSONType_Float = 2,
+    JSONType_Double = 2,
     JSONType_Bool = 3,
     JSONType_Object = 4,
     JSONType_Array = 5,
     JSONType_Null = 6,
-    JSONType_Err = 7,
     JSONType__EOF
 } JSONType;
 
 typedef const void *RedisJSONKey;
-typedef const void *RedisJSONPath;
+typedef const void *RedisJSON;
 
 typedef struct RedisJSONAPI_V1 {
+    /* RedisJSONKey functions */
     RedisJSONKey (*openKey)(struct RedisModuleCtx* ctx, RedisModuleString* key_name);
-    RedisJSONPath (*getPath)(RedisJSONKey opened_key, const char* path);
-    int (*getInfo)(RedisJSONPath, const char **name, int *type, size_t *items);
-    void (*closeKey)(RedisJSONKey);
-    void (*closePath)(RedisJSONPath);
+    void (*closeKey)(RedisJSONKey key);
+    /* RedisJSON functions
+     * Return NULL if path does not exist
+     * `count` can be NULL and return 0 for for non array/object
+     **/
+    RedisJSON (*getPath)(RedisJSONKey key, const char *path,
+                            JSONType *type, size_t *count);
+    RedisJSON (*getAt)(RedisJSON jsonIn, size_t index,
+                            JSONType *type, size_t *count);
+    void (*closeJSON)(RedisJSON json);
+    /* RedisJSON value functions
+     * Return REDISMODULE_OK if RedisJSON is of the correct JSONType,
+     * else REDISMODULE_ERR is returned
+     **/
+    int (*getInt)(const RedisJSON *path, int *integer);
+    int (*getDouble)(const RedisJSON *path, double *dbl);
+    int (*getBoolean)(const RedisJSON *path, int *boolean);
+    int (*getString)(const RedisJSON *path, char **str, size_t *len);
+
+    void (*replyWithJSON)(const RedisJSON *json);
 } RedisJSONAPI_V1;
 
 RedisJSONAPI_V1 *japi;
