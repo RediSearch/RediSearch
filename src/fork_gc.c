f520c8bed147cea4aaf36c8b48dfac3f2e7b68d2
Rafi Einstein <rafi@redislabs.com>
2021-04-18 10:12:12 +0300

    Do not abort GC fork on broken pipe (#1936)
    (cherry picked from commit c705aa20d4960bd3a1bd70ba84df1980d016409e)

--- src/fork_gc.c
+++ src/fork_gc.c
@@ -83,12 +83,10 @@ static void FGC_sendFixed(ForkGC *fgc, const void *buff, size_t len) {
   RS_LOG_ASSERT(len > 0, "buffer length cannot be 0");
   ssize_t size = write(fgc->pipefd[GC_WRITERFD], buff, len);
   if (size != len) {
-    if (size == -1) {
-      perror("write()");
-      abort();
-    } else {
-      RS_LOG_ASSERT(size == len, "buffer failed to write");
-    }
+    perror("broken pipe, exiting GC fork: write() failed");
+    // just exit, do not abort(), which will trigger a watchdog on RLEC, causing adverse effects
+    RedisModule_Log(NULL, "warning", "GC fork: broken pipe, exiting");
+    exit(1);
   }
 }
 
