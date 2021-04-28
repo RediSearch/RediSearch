e001d0acf1852812b6beca4e081a27247fee97ad
Rafi Einstein <rafi@redislabs.com>
2021-04-28 17:13:16 +0300

    RedisModules.h update + small cmake fix (#1977)
    (cherry picked from commit 45a8a34e5c7c37502b44244f81abe2d323062607)

--- .circleci/ci_build.sh
+++ .circleci/ci_build.sh
@@ -4,7 +4,7 @@ set -e
 # set -x
 
 HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-ROOT=$(realpath $HERE/..)
+ROOT=$(cd $HERE/.. && pwd)
 cd $ROOT
 
 PROJECT_DIR=$PWD
@@ -14,7 +14,8 @@ cd $BUILD_DIR
 cmake $PROJECT_DIR \
     -DCMAKE_BUILD_TYPE=RelWithDebInfo \
     -DRS_RUN_TESTS=1 \
-    -DRS_VERBOSE_TESTS=1 ${extra_args} \
+    -RS_VERBOSE_TESTS=$VERBOSE \
+    {extra_args} \
     ../
 
 if [[ -z $CI_CONCURRENCY ]]; then
