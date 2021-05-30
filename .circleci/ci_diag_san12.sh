58eb036cb4c68cbf71d0972b32524c8d18ec5b10
rafie <rafi@redislabs.com>
2021-05-30 15:51:32 +0300

    Fixes before merge (4)

--- .circleci/ci_diag_san12.sh
+++ .circleci/ci_diag_san12.sh
@@ -12,36 +12,7 @@ cd $ROOT
 
 SAN_PREFIX=/opt/llvm-project/build-msan
 
-# extra_flags=""
-
-echo "fun:THPIsEnabled" >> /build/redis.blacklist
-if [[ $ASAN == 1 ]]; then
-    # mode=asan
-	SAN_MODE=address
-    # extra_flags="-DUSE_ASAN=ON"
-    $READIES/bin/getredis --force -v 6.0 --no-run --suffix asan --clang-asan --clang-san-blacklist /build/redis.blacklist
-elif [[ $MSAN == 1 ]]; then
-    # mode=msan
-	SAN_MODE=memory
-    # extra_flags="-DUSE_MSAN=ON -DMSAN_PREFIX=${SAN_PREFIX}"
-    $READIES/bin/getredis --force -v 6.0  --no-run --suffix msan --clang-msan --llvm-dir /opt/llvm-project/build-msan --clang-san-blacklist /build/redis.blacklist
-else
-    echo "Should define either ASAN=1 or MSAN=1"
-    exit 1
-fi
-
-# mkdir -p build-${mode}
-# cd build-${mode}
-
-# cmake -DCMAKE_BUILD_TYPE=DEBUG \
-#     -DCMAKE_C_COMPILER=clang \
-#     -DCMAKE_CXX_COMPILER=clang++ \
-#     -DRS_RUN_TESTS=ON \
-#     $extra_flags \
-#     ..
-
-# COMPAT_DIR="$ROOT/build-${mode}" make -C $ROOT SAN=${SAN_MODE}
-make -C $ROOT build SAN=${SAN_MODE}
+make build SAN=${SAN_MODE}
 
 if [[ -z $CI_CONCURRENCY ]]; then
 	CI_CONCURRENCY=$($ROOT/deps/readies/bin/nproc)
@@ -49,20 +20,6 @@ fi
 if [[ $CI_CONCURRENCY > 20 ]]; then
 	CI_CONCURRENCY=20
 fi
-
-# make -j$CI_CONCURRENCY
-
-export REDIS_SERVER=redis-server-${mode}
-cat >rltest.config <<EOF
---no-output-catch
---exit-on-failure
---check-exitcode
---unix
-EOF
-export CONFIG_FILE="$PWD/rltest.config"
-
-export ASAN_OPTIONS=detect_odr_violation=0
-export RS_GLOBAL_DTORS=1
 
 cd $ROOT/deps
 git clone --recursive https://github.com/RedisJSON/RedisJSON.git
@@ -76,5 +33,4 @@ make SAN=${SAN_MODE}
 export REJSON_PATH=$ROOT/deps/RedisJSON/target/x86_64-unknown-linux-gnu/debug/rejson.so
 
 cd $ROOT
-COMPAT_DIR="$ROOT/build-${mode}" make -C $ROOT test SAN=${SAN} CTEST_ARGS="--output-on-failure"  CTEST_PARALLEL=${CI_CONCURRENCY}
-# ctest --output-on-failure -j$CI_CONCURRENCY
+make test SAN=${SAN} CTEST_ARGS="--output-on-failure"  CTEST_PARALLEL=${CI_CONCURRENCY}
