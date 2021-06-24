f995e0424d08b0f8a1d2fb46d6a28dc0b98839d9
ariel shtul <ashtul@gmail.com>
2021-06-24 13:38:47 +0300

    On CRDT, do not replicate

--- src/redis_index.c
+++ src/redis_index.c
@@ -464,7 +464,11 @@ int Redis_DeleteKey(RedisModuleCtx *ctx, RedisModuleString *s) {
 
 int Redis_DeleteKeyC(RedisModuleCtx *ctx, char *cstr) {
   RedisModuleCallReply *rep;
-  rep = RedisModule_Call(ctx, "DEL", "c!", cstr);
+  if (!isCrdt) {
+    rep = RedisModule_Call(ctx, "DEL", "c!", cstr);
+  } else {
+    rep = RedisModule_Call(ctx, "DEL", "c", cstr);
+  }
   RedisModule_Assert(RedisModule_CallReplyType(rep) == REDISMODULE_REPLY_INTEGER);
   long long res = RedisModule_CallReplyInteger(rep);
   RedisModule_FreeCallReply(rep);
