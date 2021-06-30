ae437b1f590748f8b9cab82bd560679347442bce
meir@redislabs.com <meir@redislabs.com>
2021-06-30 10:06:13 +0300

    fix tests

--- tests/pytests/test.py
+++ tests/pytests/test.py
@@ -3315,12 +3315,16 @@ def testSchemaWithAs_Duplicates(env):
 def testMod1407(env):
     conn = getConnectionByEnv(env)
 
-    env.expect('FT.CREATE', 'idx', 'SCHEMA', 'limit', 'TEXT', 'f', 'TEXT').ok()
+    env.expect('FT.CREATE', 'idx', 'SCHEMA', 'limit', 'TEXT', 'LimitationTypeID', 'TAG', 'LimitationTypeDesc', 'TEXT').ok()
     
-    conn.execute_command('HSET', 'doc1', 'limit', 'foo1', 'f', 'boo1')
-    conn.execute_command('HSET', 'doc2', 'limit', 'foo2', 'f', 'boo2')
+    conn.execute_command('HSET', 'doc1', 'limit', 'foo1', 'LimitationTypeID', 'boo1', 'LimitationTypeDesc', 'doo1')
+    conn.execute_command('HSET', 'doc2', 'limit', 'foo2', 'LimitationTypeID', 'boo2', 'LimitationTypeDesc', 'doo2')
 
-    env.expect('FT.AGGREGATE', 'idx', '*', 'SORTBY', '3', '@limit', '@f', 'ASC').equal([2L, ['limit', 'foo1', 'f', 'boo1'], ['limit', 'foo2', 'f', 'boo2']])
+    env.expect('FT.AGGREGATE', 'idx', '*', 'SORTBY', '3', '@limit', '@LimitationTypeID', 'ASC').equal([2L, ['limit', 'foo1', 'LimitationTypeID', 'boo1'], ['limit', 'foo2', 'LimitationTypeID', 'boo2']])
 
     # make sure the crashed query is not crashing anymore
-    env.expect('FT.AGGREGATE', 'SRC_Limit_790153', '*', 'GROUPBY', '2', 'LLimitationTypeID', 'LLimitationTypeDesc', 'REDUCE', 'COUNT', '0').equal([0L])
+    env.expect('FT.AGGREGATE', 'idx', '*', 'GROUPBY', '2', 'LLimitationTypeID', 'LLimitationTypeDesc', 'REDUCE', 'COUNT', '0')
+
+    # make sure correct query not crashing and return the right results
+    env.expect('FT.AGGREGATE', 'idx', '*', 'GROUPBY', '2', '@LimitationTypeID', '@LimitationTypeDesc', 'REDUCE', 'COUNT', '0').equal([2L, ['LimitationTypeID', 'boo2', 'LimitationTypeDesc', 'doo2', '__generated_aliascount', '1'], ['LimitationTypeID', 'boo1', 'LimitationTypeDesc', 'doo1', '__generated_aliascount', '1']])
+
