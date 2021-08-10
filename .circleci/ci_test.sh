e92452d321470925e430c3802b02dbc3a7c6eea2
meir@redislabs.com <meir@redislabs.com>
2021-08-10 14:28:32 +0300

    test fixes

--- .circleci/ci_test.sh
+++ .circleci/ci_test.sh
@@ -15,9 +15,8 @@ fi
 
 BRANCH=master $ROOT/sbin/get-redisjson
 COMPAT_DIR="$ROOT/$BUILD_DIR" make -C $ROOT test CTEST_ARGS="-V"
-platform=$($ROOT/deps/readies/bin/platform -t)
 
-REJSON_PATH=$ROOT/bin/$platform/RedisJSON/rejson.so EXISTING_ENV=1 $ROOT/tests/pytests/runtests.sh $ROOT/$BUILD_DIR/redisearch.so
+REJSON=1 EXISTING_ENV=1 $ROOT/tests/pytests/runtests.sh $ROOT/$BUILD_DIR/redisearch.so
 
 if [[ ! -z $USE_COVERAGE ]]; then
     ./lcov-capture.sh $ROOT/coverage
