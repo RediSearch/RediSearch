b6181ccc2d4748cda8a592dd3fc2f2c91633aa0a
Omer Shadmi <76992134+oshadmi@users.noreply.github.com>
2021-08-19 12:43:38 +0300

    Avoid unneeded code (small performance fix) (#2190)
    * Avoid unneeded code
    
    * Fix per code review by Ariel

--- src/notifications.c
+++ src/notifications.c
@@ -10,6 +10,7 @@ extern RedisModuleCtx *RSDummyContext;
 RedisModuleString **hashFields = NULL;
 
 typedef enum {
+  _null_cmd,
   hset_cmd,
   hmset_cmd,
   hsetnx_cmd,
@@ -106,6 +107,7 @@ int HashNotificationCallback(RedisModuleCtx *ctx, int type, const char *event,
     else CHECK_AND_CACHE_EVENT(rename_from)
     else CHECK_AND_CACHE_EVENT(rename_to)
     else CHECK_AND_CACHE_EVENT(loaded)
+    else redisCommand = _null_cmd;
   }
 
   switch (redisCommand) {
