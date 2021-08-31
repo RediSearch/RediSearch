6e24887d89ab4e6adab173d3675d4d361c89c2dd
ariel shtul <ashtul@gmail.com>
2021-08-31 17:39:01 +0300

    more tests

--- tests/pytests/test_vecsim.py
+++ tests/pytests/test_vecsim.py
@@ -49,73 +49,85 @@ def test_1st(env):
 
 def test_escape(env):
     conn = getConnectionByEnv(env)
-    env.expect('FT.CREATE idx SCHEMA v VECTOR INT32 2 L2 HNSW').ok()
-    conn.execute_command('HSET', 'a', 'v', '////////')
-    conn.execute_command('HSET', 'b', 'v', '++++++++')
-    conn.execute_command('HSET', 'c', 'v', 'abcdefgh')
-    conn.execute_command('HSET', 'd', 'v', 'aacdefgh')
-    conn.execute_command('HSET', 'e', 'v', 'aaadefgh')
 
-    messages = ['\+\+\+\+\+\+\+\+', '\/\/\/\/\/\/\/\/', 'abcdefgh', 'aacdefgh', 'aaadefgh']
-    for message in messages:
-        res = env.cmd('FT.SEARCH', 'idx', '@v:[' + message + ' TOPK 1]')
-        env.assertEqual(res[2][1], message.replace('\\', ''))
+    vecsim_type = ['BF', 'HNSW']
+    for vs_type in vecsim_type:
+        conn.execute_command('FT.CREATE idx SCHEMA v VECTOR INT32 2 L2 ' + vs_type)
+        conn.execute_command('HSET', 'a', 'v', '////////')
+        conn.execute_command('HSET', 'b', 'v', '++++++++')
+        conn.execute_command('HSET', 'c', 'v', 'abcdefgh')
+        conn.execute_command('HSET', 'd', 'v', 'aacdefgh')
+        conn.execute_command('HSET', 'e', 'v', 'aaadefgh')
 
-        message_bytes = message.encode('ascii')
-        base64_bytes = base64.b64encode(message_bytes)
-        base64_message = base64_bytes.decode('ascii')
-        print message_bytes
-        print base64_bytes
-        print base64_message
+        messages = ['\+\+\+\+\+\+\+\+', '\/\/\/\/\/\/\/\/', 'abcdefgh', 'aacdefgh', 'aaadefgh']
+        for message in messages:
+            res = env.cmd('FT.SEARCH', 'idx', '@v:[' + message + ' TOPK 1]')
+            env.assertEqual(res[2][1], message.replace('\\', ''))
 
-        # RANGE uses topk but translate to base64 before
-        res = env.cmd('FT.SEARCH', 'idx', '@v:[' + base64_message + ' RANGE 1]')
-        env.assertEqual(res[2][1], message.replace('\\', ''))
+            message_bytes = message.encode('ascii')
+            base64_bytes = base64.b64encode(message_bytes)
+            base64_message = base64_bytes.decode('ascii')
+            print message_bytes
+            print base64_bytes
+            print base64_message
+
+            # RANGE uses topk but translate to base64 before
+            res = env.cmd('FT.SEARCH', 'idx', '@v:[' + base64_message + ' RANGE 1]')
+            env.assertEqual(res[2][1], message.replace('\\', ''))
+
+        conn.execute_command('FT.DROPINDEX idx DD')
 
 def testDel(env):
     conn = getConnectionByEnv(env)
-    conn.execute_command('FT.CREATE', 'idx', 'SCHEMA', 'v', 'VECTOR', 'INT32', '2', 'L2', 'HNSW')
-    conn.execute_command('HSET', 'a', 'v', 'abcdefgh')
-    conn.execute_command('HSET', 'b', 'v', 'abcdefgg')
-    conn.execute_command('HSET', 'c', 'v', 'aacdefgh')
-    conn.execute_command('HSET', 'd', 'v', 'azcdefgh')
+    vecsim_type = ['BF', 'HNSW']
+    for vs_type in vecsim_type:
+        conn.execute_command('FT.CREATE', 'idx', 'SCHEMA', 'v', 'VECTOR', 'INT32', '2', 'L2', vs_type)
+        conn.execute_command('HSET', 'a', 'v', 'abcdefgh')
+        conn.execute_command('HSET', 'b', 'v', 'abcdefgg')
+        conn.execute_command('HSET', 'c', 'v', 'aacdefgh')
+        conn.execute_command('HSET', 'd', 'v', 'azcdefgh')
 
-    env.expect('FT.SEARCH', 'idx', '@v:[abcdefgh TOPK 1]').equal([1L, 'a', ['v', 'abcdefgh']])
-    env.expect('FT.SEARCH', 'idx', '@v:[abcdefgg TOPK 1]').equal([1L, 'b', ['v', 'abcdefgg']])
-    env.expect('FT.SEARCH', 'idx', '@v:[aacdefgh TOPK 1]').equal([1L, 'c', ['v', 'aacdefgh']])
-    env.expect('FT.SEARCH', 'idx', '@v:[azcdefgh TOPK 1]').equal([1L, 'd', ['v', 'azcdefgh']])
+        env.expect('FT.SEARCH', 'idx', '@v:[abcdefgh TOPK 1]').equal([1L, 'a', ['v', 'abcdefgh']])
+        env.expect('FT.SEARCH', 'idx', '@v:[abcdefgg TOPK 1]').equal([1L, 'b', ['v', 'abcdefgg']])
+        env.expect('FT.SEARCH', 'idx', '@v:[aacdefgh TOPK 1]').equal([1L, 'c', ['v', 'aacdefgh']])
+        env.expect('FT.SEARCH', 'idx', '@v:[azcdefgh TOPK 1]').equal([1L, 'd', ['v', 'azcdefgh']])
 
-    env.expect('FT.SEARCH', 'idx', '@v:[abcdefgh TOPK 1]', 'SORTBY', 'v', 'ASC')   \
-        .equal([1L, 'a', ['v', 'abcdefgh']])
-    env.expect('FT.SEARCH', 'idx', '@v:[abcdefgh TOPK 2]', 'SORTBY', 'v', 'ASC')   \
-        .equal([2L, 'a', ['v', 'abcdefgh'], 'c', ['v', 'aacdefgh']])
-    env.expect('FT.SEARCH', 'idx', '@v:[abcdefgh TOPK 3]', 'SORTBY', 'v', 'ASC')   \
-        .equal([3L, 'a', ['v', 'abcdefgh'], 'c', ['v', 'aacdefgh'], 'd', ['v', 'azcdefgh']])
-    env.expect('FT.SEARCH', 'idx', '@v:[abcdefgh TOPK 4]', 'SORTBY', 'v', 'ASC')   \
-        .equal([4L, 'a', ['v', 'abcdefgh'], 'c', ['v', 'aacdefgh'], 'd', ['v', 'azcdefgh'], 'b', ['v', 'abcdefgg']])
-    env.expect('DEL', 'a').equal(1)
-    
-    env.expect('FT.SEARCH', 'idx', '@v:[abcdefgh TOPK 1]', 'SORTBY', 'v', 'ASC')    \
-        .equal([1L, 'c', ['v', 'aacdefgh']])
-    env.expect('FT.SEARCH', 'idx', '@v:[abcdefgh TOPK 2]', 'SORTBY', 'v', 'ASC')    \
-        .equal([2L, 'c', ['v', 'aacdefgh'], 'd', ['v', 'azcdefgh']])
-    env.expect('FT.SEARCH', 'idx', '@v:[abcdefgh TOPK 3]', 'SORTBY', 'v', 'ASC')    \
-        .equal([3L, 'c', ['v', 'aacdefgh'], 'd', ['v', 'azcdefgh'], 'b', ['v', 'abcdefgg']])
+        env.expect('FT.SEARCH', 'idx', '@v:[abcdefgh TOPK 1]', 'SORTBY', 'v', 'ASC')   \
+            .equal([1L, 'a', ['v', 'abcdefgh']])
+        env.expect('FT.SEARCH', 'idx', '@v:[abcdefgh TOPK 2]', 'SORTBY', 'v', 'ASC')   \
+            .equal([2L, 'a', ['v', 'abcdefgh'], 'c', ['v', 'aacdefgh']])
+        env.expect('FT.SEARCH', 'idx', '@v:[abcdefgh TOPK 3]', 'SORTBY', 'v', 'ASC')   \
+            .equal([3L, 'a', ['v', 'abcdefgh'], 'c', ['v', 'aacdefgh'], 'd', ['v', 'azcdefgh']])
+        env.expect('FT.SEARCH', 'idx', '@v:[abcdefgh TOPK 4]', 'SORTBY', 'v', 'ASC')   \
+            .equal([4L, 'a', ['v', 'abcdefgh'], 'c', ['v', 'aacdefgh'], 'd', ['v', 'azcdefgh'], 'b', ['v', 'abcdefgg']])
+        env.expect('DEL', 'a').equal(1)
+        
+        env.expect('FT.SEARCH', 'idx', '@v:[abcdefgh TOPK 1]', 'SORTBY', 'v', 'ASC')    \
+            .equal([1L, 'c', ['v', 'aacdefgh']])
+        env.expect('FT.SEARCH', 'idx', '@v:[abcdefgh TOPK 2]', 'SORTBY', 'v', 'ASC')    \
+            .equal([2L, 'c', ['v', 'aacdefgh'], 'd', ['v', 'azcdefgh']])
+        env.expect('FT.SEARCH', 'idx', '@v:[abcdefgh TOPK 3]', 'SORTBY', 'v', 'ASC')    \
+            .equal([3L, 'c', ['v', 'aacdefgh'], 'd', ['v', 'azcdefgh'], 'b', ['v', 'abcdefgg']])
 
-    '''
-    This test returns 4 results instead of the expected 3. The HNSW library return the additional results.
-    env.expect('FT.SEARCH', 'idx', '@v:[abcdefgh TOPK 4]').equal([3L, 'b', ['v', 'abcdefgg'], 'c', ['v', 'aacdefgh'], 'd', ['v', 'azcdefgh']])
-    '''
+        '''
+        This test returns 4 results instead of the expected 3. The HNSW library return the additional results.
+        env.expect('FT.SEARCH', 'idx', '@v:[abcdefgh TOPK 4]').equal([3L, 'b', ['v', 'abcdefgg'], 'c', ['v', 'aacdefgh'], 'd', ['v', 'azcdefgh']])
+        '''
+
+        conn.execute_command('FT.DROPINDEX idx DD')
 
 def test_query_empty(env):
     env.skip()
     conn = getConnectionByEnv(env)
-    conn.execute_command('FT.CREATE', 'idx', 'SCHEMA', 'v', 'VECTOR', 'INT32', '2', 'L2', 'HNSW')
-    env.expect('FT.SEARCH', 'idx', '@v:[abcdefgh TOPK 1]').equal([0L])
-    conn.execute_command('HSET', 'a', 'v', 'redislab')
-    env.expect('FT.SEARCH', 'idx', '@v:[abcdefgh TOPK 1]').equal([1L, 'a', ['v', 'redislab']])
-    conn.execute_command('DEL', 'a')
-    env.expect('FT.SEARCH', 'idx', '@v:[abcdefgh TOPK 1]').equal([0L])
+    vecsim_type = ['BF', 'HNSW']
+    for vs_type in vecsim_type:
+        conn.execute_command('FT.CREATE', 'idx', 'SCHEMA', 'v', 'VECTOR', 'INT32', '2', 'L2', vs_type)
+        env.expect('FT.SEARCH', 'idx', '@v:[abcdefgh TOPK 1]').equal([0L])
+        conn.execute_command('HSET', 'a', 'v', 'redislab')
+        env.expect('FT.SEARCH', 'idx', '@v:[abcdefgh TOPK 1]').equal([1L, 'a', ['v', 'redislab']])
+        conn.execute_command('DEL', 'a')
+        env.expect('FT.SEARCH', 'idx', '@v:[abcdefgh TOPK 1]').equal([0L])
+        conn.execute_command('FT.DROPINDEX idx DD')
 
 def del_insert(env):
     conn = getConnectionByEnv(env)
