a41ce7dcf733efc9861a7a043284a5974c2a1973
Chayim I. Kirshen <c@kirshen.com>
2021-09-09 12:23:40 +0300

    path at end to debug tests

--- .circleci/ci_build.sh
+++ .circleci/ci_build.sh
@@ -2,7 +2,7 @@
 
 set -e
 echo $PATH
-export PATH=/usr/local/bin:$PATH
+export PATH=$PATH:/usr/local/bin
 
 HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 ROOT=$(cd $HERE/.. && pwd)
