89c5d30e91acf4b47a6e49c5930b75ba18d0bdb0
ariel shtul <ashtul@gmail.com>
2021-09-14 13:27:36 +0300

    [FEATURE] Add LOAD ALL for FT.AGGREGATE

--- src/aggregate/aggregate.h
+++ src/aggregate/aggregate.h
@@ -47,6 +47,9 @@ typedef enum {
   QEXEC_F_PROFILE = 0x8000,
   QEXEC_F_PROFILE_LIMITED = 0x10000,
 
+  /* FT.AGGREGATE load all fields */
+  QEXEC_AGG_LOAD_ALL = 0x20000,
+
 } QEFlags;
 
 #define IsCount(r) ((r)->reqflags & QEXEC_F_NOROWS)
