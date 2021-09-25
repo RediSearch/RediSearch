6b8d70ab79db2a13b89d22cba07d0d5cabee85f7
rafie <rafi@redislabs.com>
2021-09-26 01:41:33 +0300

    diag 1

--- tests/pytests/test_vecsim.py
+++ tests/pytests/test_vecsim.py
@@ -78,6 +78,7 @@ def test_escape(env):
         conn.execute_command('FT.DROPINDEX idx DD')
 
 def testDel(env):
+    env.skip() # @@diag
     conn = getConnectionByEnv(env)
     vecsim_type = ['BF', 'HNSW']
     for vs_type in vecsim_type:
