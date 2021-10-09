1c244a509b7964183a117edeedb5113a41683885
rafie <rafi@redislabs.com>
2021-10-09 20:17:40 +0300

    fixes 5

--- src/module.c
+++ src/module.c
@@ -1056,7 +1056,11 @@ void __attribute__((destructor)) RediSearch_CleanupModule(void) {
     IndexAlias_DestroyGlobal(&AliasTable_g);
     freeGlobalAddStrings();
     SchemaPrefixes_Free(ScemaPrefixes_g);
-    
+
+    if (legacySpecDict)
+      dictRelease(legacySpecDict);
+      legacySpecDict = NULL;
+    }
     Indexes_Free(specDict_g);
     dictRelease(specDict_g);
     specDict_g = NULL;
