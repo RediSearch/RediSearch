f039b43441d85eacffec9d4a21614cdcbdfabe5d
rafie <rafi@redislabs.com>
2021-10-10 19:32:19 +0300

    [vecsim] Sanitizer updates

--- .circleci/ci_prepare_benchmark.sh
+++ .circleci/ci_prepare_benchmark.sh
@@ -9,17 +9,16 @@ READIES="$ROOT/deps/readies"
 cd $ROOT
 ENABLE_SYSTEM_SETUP=${ENABLE_SYSTEM_SETUP:-"0"}
 
-if [ "${ENABLE_SYSTEM_SETUP}" = "1" ]; then
+if [[ $ENABLE_SYSTEM_SETUP == 1 ]]; then
     ./.circleci/ci_get_deps.sh
 fi
-# 
 
 cd $ROOT/deps
 rm -rf $ROOT/deps/RedisJSON
-git clone --recursive https://github.com/RedisJSON/RedisJSON.git
+git clone --quiet --recursive https://github.com/RedisJSON/RedisJSON.git
 cd RedisJSON
 git checkout master
-if [ "${ENABLE_SYSTEM_SETUP}" = "1" ]; then
+if [[ $ENABLE_SYSTEM_SETUP == 1 ]]; then
     ./deps/readies/bin/getpy3
     ./system-setup.py
     source /etc/profile.d/rust.sh
