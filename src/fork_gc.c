13a8b33cc2066b5dce49e2d16f6a6ee55880da2e
ariel shtul <ashtul@gmail.com>
2021-10-13 10:03:17 +0300

    per review

--- src/fork_gc.c
+++ src/fork_gc.c
@@ -493,8 +493,8 @@ static void FGC_childCollectTags(ForkGC *gc, RedisSearchCtx *sctx) {
                              .uniqueId = tagIdx->uniqueId};
 
       TrieMapIterator *iter = TrieMap_Iterate(tagIdx->values, "", 0);
-      char *ptr = NULL;
-      tm_len_t len = 0;
+      char *ptr;
+      tm_len_t len;
       InvertedIndex *value;
       while (TrieMapIterator_Next(iter, &ptr, &len, (void **)&value)) {
         header.curPtr = value;
