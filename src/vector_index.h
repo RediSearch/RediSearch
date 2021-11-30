f9ff02efc89ea79affa67bc0617e589a2b0a2407
Ariel Shtul <ashtul@gmail.com>
2021-11-30 10:48:20 +0200

    fix 2
    (cherry picked from commit 5802d253fccc0aad1c08ca1c3ae2deeb2129b9b3)

--- src/vector_index.h
+++ src/vector_index.h
@@ -63,4 +63,4 @@ const char *VecSimMetric_ToString(VecSimMetric metric);
 const char *VecSimAlgorithm_ToString(VecSimAlgo algo);
 
 void VecSim_RdbSave(RedisModuleIO *rdb, VecSimParams *vecsimParams);
-int VecSim_RdbLoad(RedisModuleIO *rdb, VecSimParams *vecsimParams, int encver);
+int VecSim_RdbLoad(RedisModuleIO *rdb, VecSimParams *vecsimParams);
