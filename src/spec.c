6a2e67cae4223c4ccaed3c46f7ce0b2fd548469b
Ariel Shtul <ashtul@gmail.com>
2021-12-06 12:48:56 +0200

    [BUG] init spec w/o fields parameters

--- src/spec.c
+++ src/spec.c
@@ -411,6 +411,11 @@ int IndexSpec_CreateTextId(const IndexSpec *sp) {
  */
 static int IndexSpec_AddFieldsInternal(IndexSpec *sp, ArgsCursor *ac, QueryError *status,
                                        int isNew) {
+  if (ac->offset == ac->argc) {
+    QueryError_SetErrorFmt(status, QUERY_EPARSEARGS, "Please provided parameters for fields");
+    return 0;
+  }
+
   if (sp->spcache) {
     IndexSpecCache_Decref(sp->spcache);
     sp->spcache = NULL;
