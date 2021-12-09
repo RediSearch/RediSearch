8b311e54e847dc4ab92b07fb3e9b7dd104772515
rafie <rafi@redislabs.com>
2021-12-09 12:20:51 +0200

    fixes 9

--- build/vecsim/vecsim.cmake
+++ build/vecsim/vecsim.cmake
@@ -14,18 +14,22 @@ set(RS_VECSIM_SO_FLAGS "-lstdc++")
 include (ExternalProject)
 
 if (USE_ASAN)
-	set(VECSIM_SAN_ARGS -DUSE_ASAN=ON)
+	set(VECSIM_SAN_ARGS -DUSE_ASAN:bool=ON)
 elseif(USE_MSAN)
-	set(VECSIM_SAN_ARGS -DUSE_MSAN=ON)
+	set(VECSIM_SAN_ARGS -DUSE_MSAN:bool=ON)
 endif()
+
+message("RediSearch/VecSim dir: ${root}/deps/VectorSimilarity/src")
 
 ExternalProject_Add (VectorSimilarity
 	SOURCE_DIR      ${root}/deps/VectorSimilarity/src
 	PREFIX          ${binroot}/VectorSimilarity
 	CMAKE_ARGS      -DCMAKE_INSTALL_PREFIX:PATH=${binroot}/VectorSimilarity -Dbinroot:string=${binroot}
-	CMAKE_ARGS      -DCMAKE_BUILD_TYPE:string=${CMAKE_BUILD_TYPE} -DVECSIM_STATIC:BOOL=ON
+	CMAKE_ARGS      -DCMAKE_BUILD_TYPE:string=${CMAKE_BUILD_TYPE} -DVECSIM_STATIC:bool=ON
+	CMAKE_ARGS      -DOS:string=${OS} -DARCH:string=${ARCH} -DOSNICK:string=${OSNICK}
 	CMAKE_ARGS      -DVECSIM_ARCH:string=${VECSIM_ARCH}
-	CMAKE_ARGS      ${VECSIM_SAN_ARGS} -DBUILD_TESTS=OFF
+	CMAKE_ARGS      -DBUILD_TESTS:bool=OFF
+	CMAKE_ARGS      ${VECSIM_SAN_ARGS}
 )
 
 ExternalProject_Get_Property (VectorSimilarity install_dir)
