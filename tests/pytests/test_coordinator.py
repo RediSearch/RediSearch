370849d1acd01589157cdc59e397a3d7bda9cc36
Ariel Shtul <ashtul@gmail.com>
2022-01-31 20:35:53 +0200

    per review

--- tests/pytests/test_coordinator.py
+++ tests/pytests/test_coordinator.py
@@ -14,6 +14,11 @@ def testInfo(env):
     env.assertLess(float(idx_info['doc_table_size_mb']), 1)
     env.assertLess(float(idx_info['sortable_values_size_mb']), 1)
     env.assertLess(float(idx_info['key_table_size_mb']), 1)
+    env.assertGreater(float(idx_info['inverted_sz_mb']), 0)
+    env.assertGreater(float(idx_info['offset_vectors_sz_mb']), 0)
+    env.assertGreater(float(idx_info['doc_table_size_mb']), 0)
+    env.assertGreater(float(idx_info['sortable_values_size_mb']), 0)
+    env.assertGreater(float(idx_info['key_table_size_mb']), 0)
 
 
     
