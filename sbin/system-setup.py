91edaf7af2983d1ed8e9f5b927a1fb1ff108641e
rafie <rafi@redislabs.com>
2022-02-01 21:52:15 +0200

    Testing RLTest cluster fix

--- sbin/system-setup.py
+++ sbin/system-setup.py
@@ -72,7 +72,7 @@ class RediSearchSetup(paella.Setup):
     def common_last(self):
         self.run("{PYTHON} {READIES}/bin/getcmake --usr".format(PYTHON=self.python, READIES=READIES),
                  sudo=self.os != 'macos')
-        self.run("{PYTHON} {READIES}/bin/getrmpytools --reinstall".format(PYTHON=self.python, READIES=READIES))
+        self.run("{PYTHON} {READIES}/bin/getrmpytools --reinstall --rltest-branch ariel_slave-init-params".format(PYTHON=self.python, READIES=READIES))
         if self.dist != "arch":
             self.install("lcov")
         else:
