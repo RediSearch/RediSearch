fd4631609266e9744813ce727d75c0fb5958edf8
Ariel Shtul <ashtul@gmail.com>
2022-02-07 13:31:51 +0200

    change profile test to TOPK 3

--- tests/pytests/test_profile.py
+++ tests/pytests/test_profile.py
@@ -212,15 +212,17 @@ def testProfileVector(env):
   conn.execute_command('hset', '4', 'v', 'bbaabbaa', 't', "hello world")
   conn.execute_command('hset', '5', 'v', 'aaaabbbb', 't', "hello world")
 
-  actual_res = conn.execute_command('ft.profile', 'idx', 'search', 'query', '*=>[TOP_K 2 @v $vec]', 'PARAMS', '2', 'vec', 'aaaaaaaa', 'nocontent')
-  expected_iterators_res = ['Iterators profile', ['Type', 'VECTOR', 'Counter', 2L]]
-  expected_vecsim_rp_res = ['Type', 'Vector Similarity Scores Loader', 'Counter', 2L]
+  actual_res = conn.execute_command('ft.profile', 'idx', 'search', 'query', '*=>[TOP_K 3 @v $vec]', 'PARAMS', '2', 'vec', 'aaaaaaaa', 'nocontent')
+  expected_iterators_res = ['Iterators profile', ['Type', 'VECTOR', 'Counter', 3L]]
+  expected_vecsim_rp_res = ['Type', 'Vector Similarity Scores Loader', 'Counter', 3L]
+  env.assertEqual(actual_res[0], [3L, '1', '2', '4'])
   env.assertEqual(actual_res[1][3], expected_iterators_res)
   env.assertEqual(actual_res[1][4][3], expected_vecsim_rp_res)
 
   # Test with hybrid query
   actual_res = conn.execute_command('ft.profile', 'idx', 'search', 'query', '(@t:hello world)=>[TOP_K 3 @v $vec]', 'PARAMS', '2', 'vec', 'aaaaaaaa', 'nocontent')
   expected_iterators_res = ['Iterators profile', ['Type', 'VECTOR', 'Counter', 2L, 'Child iterator', ['Type', 'INTERSECT', 'Counter', 4L, 'Child iterators', ['Type', 'TEXT', 'Term', 'world', 'Counter', 4L, 'Size', 2L], ['Type', 'TEXT', 'Term', 'hello', 'Counter', 4L, 'Size', 5L]]]]
+  expected_vecsim_rp_res = ['Type', 'Vector Similarity Scores Loader', 'Counter', 2L]
   env.assertEqual(actual_res[0], [2L, '4', '5'])
   env.assertEqual(actual_res[1][3], expected_iterators_res)
   env.assertEqual(actual_res[1][4][3], expected_vecsim_rp_res)
