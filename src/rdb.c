e36a2bd4e3fddef7511a70c38b9f1ebfa575e7c0
Ariel Shtul <ariel.shtul@redislabs.com>
2022-02-10 14:42:03 +0200

    [BUG] Free prefix and cursor efficiantly on  Indexes_Free (#2560)
    * [PERFORMANCE] free all prefixes as batch
    
    * test
    
    * Free cursorList as well
    
    * Fix leak
    
    * clean
    
    * clean 2
    
    * fix

--- src/rdb.c
+++ src/rdb.c
@@ -32,12 +32,28 @@ void Restore_Globals() {
   AliasTable_g_bkup = NULL;
 }
 
+
+
 void Discard_Globals_Backup() {
-  Indexes_Free(specDict_g_bkup);
+  // This is a temporary fix until we change functions to get pointer to lists
+  // save global to temp
+  dict *specDict_g_temp = specDict_g;
+  TrieMap *ScemaPrefixes_g_temp = ScemaPrefixes_g;
+  AliasTable *AliasTable_g_temp = AliasTable_g;
+  // set backup as globals
+  specDict_g = specDict_g_bkup;
+  ScemaPrefixes_g = ScemaPrefixes_g_bkup;
+  AliasTable_g = &AliasTable_g_bkup;
+  // clear data
+  Indexes_Free(specDict_g);
+  SchemaPrefixes_Free(ScemaPrefixes_g);
+  IndexAlias_DestroyGlobal(&AliasTable_g);
+  // restore global from temp
+  specDict_g = specDict_g_temp;
+  ScemaPrefixes_g = ScemaPrefixes_g_temp;
+  AliasTable_g = AliasTable_g_temp;
+  // nullify backup
   specDict_g_bkup = NULL;
-
-  SchemaPrefixes_Free(ScemaPrefixes_g_bkup);
   ScemaPrefixes_g_bkup = NULL;
-
-  IndexAlias_DestroyGlobal(&AliasTable_g_bkup);
+  AliasTable_g_bkup = NULL;
 }
