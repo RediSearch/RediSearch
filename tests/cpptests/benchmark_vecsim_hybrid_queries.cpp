19d5e2880418f8a9f72ccc21d8cccd38b244c622
GuyAv46 <47632673+GuyAv46@users.noreply.github.com>
2022-03-14 18:44:08 +0200

    Vecsim norm opt (#2647)
    * normalizing query vector if needed,
      using new VecSimIndex_GetDistanceFrom that doesn't normalize the vector
    
    * added test (credit @alonre24)
    
    * refactor test - don't test vector scores, only for expected ids
    
    * Change test so it will pass with coordinator as well
    
    Co-authored-by: alon <alonreshef24@gmail.com>

--- tests/cpptests/benchmark_vecsim_hybrid_queries.cpp
+++ tests/cpptests/benchmark_vecsim_hybrid_queries.cpp
@@ -69,8 +69,11 @@ void run_hybrid_benchmark(VecSimIndex *index, size_t max_id, size_t d, std::mt19
 
       float query[NUM_ITERATIONS][d];
       KNNVectorQuery top_k_query = {.vector = NULL, .vecLen = d, .k = k, .order = BY_SCORE};
-      VecSimQueryParams queryParams;
+      VecSimQueryParams queryParams = {.hnswRuntimeParams = {.efRuntime = 0}};
       HybridIteratorParams hParams = {.index = index,
+                                      .dim = d,
+                                      .elementType = VecSimType_FLOAT32,
+                                      .spaceMetric = VecSimMetric_L2,
                                       .query = top_k_query,
                                       .qParams = queryParams,
                                       .vectorScoreField = (char *)"__v_score",
