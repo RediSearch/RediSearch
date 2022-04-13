4e28f532092757aa56c899815761741609b482fe
rafie <rafi@redislabs.com>
2022-04-13 22:57:24 +0300

    fixes 2

--- tests/pytests/test_aggregate.py
+++ tests/pytests/test_aggregate.py
@@ -675,7 +675,7 @@ def testMaxAggResults(env):
 
 def testAggregateWithLimit0(env):
     conn = getConnectionByEnv(env)
-    conn.execute_command('ft.create', 'idx', 'SCHEMA', 't', 'TEXT')
+    env.execute_command('ft.create', 'idx', 'SCHEMA', 't', 'TEXT')
     conn.execute_command('hset', 'doc1', 't', 'foo')
     # limit 0 0 on aggregate should return no results
     env.expect('ft.aggregate', 'idx', '*', 'LIMIT', '0', '0').equal([0])
