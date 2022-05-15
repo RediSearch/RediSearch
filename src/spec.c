687801ecd07cdeb285b34a68f83e2d8e6598e8c8
DvirDukhan <dvir@redis.com>
2022-05-15 13:42:09 +0300

    fail on ">=". added information about available memory

--- src/spec.c
+++ src/spec.c
@@ -389,8 +389,9 @@ static int parseVectorField_validate_hnsw(VecSimParams *params, QueryError *stat
   if (params->hnswParams.blockSize == 0) {
     params->hnswParams.blockSize = MIN(DEFAULT_BLOCK_SIZE, maxBlockSize);
   }
-  if (VecSimIndex_EstimateInitialSize(params) > memoryLimit - used_memory) {
-    QueryError_SetErrorFmt(status, QUERY_ELIMIT, "Vector index size exceeded server limit (%zuB) with the given parameters", memoryLimit);
+  size_t index_size_estimation = VecSimIndex_EstimateInitialSize(params);
+  if (index_size_estimation >= memoryLimit - used_memory) {
+    QueryError_SetErrorFmt(status, QUERY_ELIMIT, "Vector index size exceeded server limit (%zuB) with the given parameters. Requested: %zu, available: %zu", memoryLimit, index_size_estimation, memoryLimit - used_memory);
     return 0;
   } else if (params->hnswParams.blockSize  > maxBlockSize) {
     // TODO: uncomment when BLOCK_SIZE is added to FT.CREATE on HNSW
@@ -411,8 +412,8 @@ static int parseVectorField_validate_flat(VecSimParams *params, QueryError *stat
   // Calculating index size estimation, after first vector block was allocated.
   size_t index_size_estimation = VecSimIndex_EstimateInitialSize(params);
   index_size_estimation += elementSize * params->bfParams.blockSize;
-  if (index_size_estimation > memoryLimit - used_memory) {
-    QueryError_SetErrorFmt(status, QUERY_ELIMIT, "Vector index size exceeded server limit (%zuB) with the given parameters", memoryLimit);
+  if (index_size_estimation >= memoryLimit - used_memory) {
+    QueryError_SetErrorFmt(status, QUERY_ELIMIT, "Vector index size exceeded server limit (%zuB) with the given parameters. Requested: %zu, available: %zu", memoryLimit, index_size_estimation, memoryLimit - used_memory);
     return 0;
   } else if (params->bfParams.blockSize > maxBlockSize) {
     QueryError_SetErrorFmt(status, QUERY_ELIMIT, "Vector index block size %zu exceeded server limit (%zu with the given parameters)", params->bfParams.blockSize, maxBlockSize);
