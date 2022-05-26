4e0caa47b381656d133f8d65ea87cfaab57a16c0
Ariel Shtul <ashtul@gmail.com>
2022-05-26 14:12:34 +0300

    add SYNONYM_PREFIX_CHAR

--- src/indexer.c
+++ src/indexer.c
@@ -235,7 +235,8 @@ static void writeCurEntries(DocumentIndexer *indexer, RSAddDocumentCtx *aCtx, Re
     }
     
     if (spec->suffixMask & entry->fieldMask && entry->term[0] != STEM_PREFIX
-                                            && entry->term[0] != PHONETIC_PREFIX) {
+                                            && entry->term[0] != PHONETIC_PREFIX
+                                            && entry->term[0] != SYNONYM_PREFIX_CHAR) {
       addSuffixTrie(spec->suffix, entry->term, entry->len);
     }
 
