2a45ba5da892bed38a604e0b708e49a5a9cee2b6
Ariel Shtul <ashtul@gmail.com>
2022-06-07 08:57:12 +0300

    No need to replicate RM_CALL("DEL")

--- src/redis_index.c
+++ src/redis_index.c
@@ -467,11 +467,7 @@ int Redis_DeleteKey(RedisModuleCtx *ctx, RedisModuleString *s) {
 
 int Redis_DeleteKeyC(RedisModuleCtx *ctx, char *cstr) {
   RedisModuleCallReply *rep;
-  if (!isCrdt) {
-    rep = RedisModule_Call(ctx, "DEL", "c!", cstr);
-  } else {
-    rep = RedisModule_Call(ctx, "DEL", "c", cstr);
-  }
+  rep = RedisModule_Call(ctx, "DEL", "c", cstr);
   RedisModule_Assert(RedisModule_CallReplyType(rep) == REDISMODULE_REPLY_INTEGER);
   long long res = RedisModule_CallReplyInteger(rep);
   RedisModule_FreeCallReply(rep);
