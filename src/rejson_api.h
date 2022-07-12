ca5a6615b5e9293d36cac99fd6c83ed12cc443ce
oshadmi <omer.shadmi@redislabs.com>
2022-07-12 15:28:37 +0300

    Remove duplicated entries from JSON API V2

--- src/rejson_api.h
+++ src/rejson_api.h
@@ -73,48 +73,6 @@ typedef struct RedisJSONAPI_V1 {
 } RedisJSONAPI_V1;
 
 typedef struct RedisJSONAPI_V2 {
-  /* RedisJSON functions */
-  RedisJSON (*openKey)(RedisModuleCtx *ctx, RedisModuleString *key_name);
-  RedisJSON (*openKeyFromStr)(RedisModuleCtx *ctx, const char *path);
-
-  JSONResultsIterator (*get)(RedisJSON json, const char *path);
-  
-  RedisJSON (*next)(JSONResultsIterator iter);
-  size_t (*len)(JSONResultsIterator iter);
-  void (*freeIter)(JSONResultsIterator iter);
-
-  RedisJSON (*getAt)(RedisJSON json, size_t index);
-
-  /* RedisJSON value functions
-   * Return REDISMODULE_OK if RedisJSON is of the correct JSONType,
-   * else REDISMODULE_ERR is returned
-   * */
-
-  // Return the length of Object/Array
-  int (*getLen)(RedisJSON json, size_t *count);
-
-  // Return the JSONType
-  JSONType (*getType)(RedisJSON json);
-
-  // Return int value from a Numeric field
-  int (*getInt)(RedisJSON json, long long *integer);
-
-  // Return double value from a Numeric field
-  int (*getDouble)(RedisJSON json, double *dbl);
-
-  // Return 0 or 1 as int value from a Bool field
-  int (*getBoolean)(RedisJSON json, int *boolean);
-
-  // Return a Read-Only String value from a String field
-  int (*getString)(RedisJSON json, const char **str, size_t *len);
-
-  // Return JSON String representation (for any JSONType)
-  // The caller gains ownership of `str`
-  int (*getJSON)(RedisJSON json, RedisModuleCtx *ctx, RedisModuleString **str);
-
-  // Return 1 if type of key is JSON
-  int (*isJSON)(RedisModuleKey *redis_key);
-
   // Return a parsed JSONPath
   // Return NULL if failed to parse, and the error message in `err_msg`
   // The caller gains ownership of `err_msg`
