73e985f38942c2e651561496b91a10cc878e0409
Avital Fine <98389525+Avital-Fine@users.noreply.github.com>
2022-07-13 13:05:24 +0300

    Add RediSearch_MemUsage to API (#2883)
    (cherry picked from commit 719b5d733179fbe0913d5defdc61719e5d79f892)

--- src/redisearch_api.h
+++ src/redisearch_api.h
@@ -335,6 +335,8 @@ MODULE_API_FUNC(double, RediSearch_ResultsIteratorGetScore)(const RSResultsItera
 
 MODULE_API_FUNC(void, RediSearch_IndexOptionsSetGCPolicy)(RSIndexOptions* options, int policy);
 
+MODULE_API_FUNC(size_t, RediSearch_MemUsage)(RSIndex* sp);
+
 /**
  * Return an info struct
  * @param sp the index
@@ -395,6 +397,7 @@ MODULE_API_FUNC(void, RediSearch_IndexInfoFree)(RSIdxInfo *info);
   X(IterateQueryWithDialect)         \
   X(ResultsIteratorGetScore)         \
   X(IndexOptionsSetGCPolicy)         \
+  X(MemUsage)                        \
   X(IndexInfo)                       \
   X(IndexInfoFree)                   \
   X(SetCriteriaTesterThreshold)
