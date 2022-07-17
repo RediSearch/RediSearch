06363b94e585baf7fe4706f06f185a9493399a2d
Ariel Shtul <ashtul@gmail.com>
2022-07-17 20:20:16 +0300

    add comments

--- deps/wildcard/wildcard.h
+++ deps/wildcard/wildcard.h
@@ -28,8 +28,18 @@ typedef enum {
   NO_MATCH = 2,
 } match_t;
 
+/* Check string vs pattern for a match.
+ * Return FULL_MATCH for a match.
+ * Return PARTIAL_MATCH if there is no match so far but a match is possible with additional characters
+ * Return NO_MATCH if match is no possible. */
 match_t Wildcard_MatchChar(const char *pattern, size_t p_len, const char *str, size_t str_len);
 match_t Wildcard_MatchRune(const rune *pattern, size_t p_len, const rune *str, size_t str_len);
+
+/* Moves '?' before '*' and removes multiple '*' */
 size_t Wildcard_TrimPattern(char *pattern, size_t p_len);
+
+/* Removes '\\' */
 size_t Wildcard_RemoveEscape(char *str, size_t len);
+
+/* Breaks wildcard at '*'s */
 int Wildcard_StarBreak(const char *str, size_t len, size_t *tokenIdx, size_t *tokenLen);
