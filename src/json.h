602ae4ba4c97ab95770b6245bc1a6a12dff9f010
Omer Shadmi <76992134+oshadmi@users.noreply.github.com>
2022-07-27 20:39:12 +0300

    MOD-2972: Initial support for multi-value text (#2819)
    * WIP initial support for multi-value text
    
    * Add test for non-text values
    
    * WIP sorting
    
    * WIP Add JSON API V2 and avoid SORTABLE with multi value
    
    * add tests and avoid failing SORTABLE
    
    * check undefined ordering
    
    * Add FT config option MULTI_TEXT_OFFSET_DELTA
    
    * cleanups
    
    * code review with Ariel (1)
    
    * More fixes and code review with Ariel
    
    * Fix from review: rename config
    
    * [skip ci] Fix from review: refactor querying jsonpath info (1)
    
    * Fix from review: refactor querying jsonpath info (2)
    
    * Rename test file and fix log message typo
    
    * Add test for Boolean TAG
    
    * Fail index creation on an invalid jsonpath
    
    * Fix order in testMultiTagBool
    
    * Remove duplicated entries from JSON API V2
    
    * Make JSON API versions incremental
    
    * Make JSON API versions incremental (2)
    
    * API cleanup
    
    * Rename API pathIsStatic to pathIsSingle
    
    * update readies
    
    * Add alternative expected depending on ReJSON version
    
    * Fixes from Meir's review (1)
    
    * update readies and increase no_output_timeout
    
    * Add score to see ordering in failed test
    
    * Fix test for coordinator
    
    * Fix test for coordinator
    
    * Fix test for coordinator (2)
    
    * Fix tests (order and moduleArgs config param)
    
    * Iterate a json array without parsing a jsonpath
    
    * Skip score test in cluster
    
    * Fixes from Meir's review (2) - single Env per test, run on coord
    
    * Fixes from Meir's review (2) - revert timeout
    
    * Try skip MULTI_TEXT_SLOP config tests on cluster
    
    * Fix MULTI_TEXT_SLOP config tests on cluster
    
    * Fix MULTI_TEXT_SLOP config tests on cluster (2)
    
    * update readies

--- src/json.h
+++ src/json.h
@@ -8,7 +8,8 @@
 extern "C" {
 #endif
 
-extern RedisJSONAPI_V1 *japi;
+extern RedisJSONAPI *japi;
+extern int japi_ver;
 // extern RedisModuleCtx *RSDummyContext;
 
 #define JSON_ROOT "$"
@@ -18,11 +19,16 @@ struct DocumentField;
 int GetJSONAPIs(RedisModuleCtx *ctx, int subscribeToModuleChange);
 
 /* Creates a Redis Module String from JSONType string, int, double, bool */
-int JSON_LoadDocumentField(JSONResultsIterator jsonIter, size_t len,
-                              FieldSpec *fs, struct DocumentField *df);
+int JSON_LoadDocumentField(JSONResultsIterator jsonIter, size_t len, FieldSpec *fs,
+                           struct DocumentField *df);
 
 /* Checks if JSONType fits the FieldType */
 int FieldSpec_CheckJsonType(FieldType fieldType, JSONType type);
+
+JSONPath pathParse(const char *path, RedisModuleString **err_msg);
+void pathFree(JSONPath jsonpath);
+int pathIsSingle(JSONPath jsonpath);
+int pathHasDefinedOrder(JSONPath jsonpath);
 
 #ifdef __cplusplus
 }
