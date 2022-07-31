2d73f274ca96077d020499e9c2ead7c5fbec3e43
Ariel Shtul <ashtul@gmail.com>
2022-07-31 20:03:18 +0300

    Fix for spellcheck

--- src/trie/trie.c
+++ src/trie/trie.c
@@ -595,7 +595,7 @@ TrieIterator *TrieNode_Iterate(TrieNode *n, StepFilter f, StackPopCallback pf, v
   TrieIterator *it = rm_calloc(1, sizeof(TrieIterator));
   it->filter = f;
   it->popCallback = pf;
-  it->minScore = 0;
+  it->minScore = -1;    // terms from dictionary which are not in term trie get a valid score -1
   it->ctx = ctx;
   __ti_Push(it, n, 0);
 
