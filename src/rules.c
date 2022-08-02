a32d9b606455106454a79f8d185fe8652349b224
Ariel Shtul <ashtul@gmail.com>
2022-08-02 11:52:49 +0300

    Scan only modified index

--- src/rules.c
+++ src/rules.c
@@ -458,6 +458,49 @@ void SchemaRule_RdbSave(SchemaRule *rule, RedisModuleIO *rdb) {
   RedisModule_SaveUnsigned(rdb, rule->lang_default);
 }
 
+bool SchemaRule_ShouldIndex(struct IndexSpec *sp, RedisModuleString *keyname, DocumentType type) {
+  // check type
+  if (type != sp->rule->type) {
+    return false;
+  }
+
+  const char *keyCstr = RedisModule_StringPtrLen(keyname, NULL);
+
+  // check prefixes
+  bool match = false;
+  const char **prefixes = sp->rule->prefixes;
+  for (int i = 0; i < array_len(prefixes); ++i) {
+    if (!strncmp(keyCstr, prefixes[i], strlen(prefixes[i]))) {
+      match = true;
+      break;
+    }
+  }
+  if (!match) {
+    return false;
+  }
+
+  // check filters
+  int ret = true;
+  SchemaRule *rule = sp->rule;
+  if (rule->filter_exp) {
+    EvalCtx *r = NULL;     
+    // load hash only if required
+    r = EvalCtx_Create();
+
+    RLookup_LoadRuleFields(RSDummyContext, &r->lk, &r->row, rule, keyCstr);
+ 
+    if (EvalCtx_EvalExpr(r, rule->filter_exp) != EXPR_EVAL_OK ||
+        !RSValue_BoolTest(&r->res)) {
+      ret = false;
+    }
+    QueryError_ClearError(r->ee.err);
+    EvalCtx_Destroy(r);
+  }
+
+  return ret;
+}
+
+
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
 void SchemaPrefixes_Create() {
