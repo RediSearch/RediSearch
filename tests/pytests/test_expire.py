10738ea83efb3e5adbb170f91a7d7ccbf3b62616
Ariel Shtul <ashtul@gmail.com>
2022-08-08 11:39:52 +0300

    test EXPLAINSCORE

--- tests/pytests/test_expire.py
+++ tests/pytests/test_expire.py
@@ -59,3 +59,29 @@ def testExpireDocs(env):
 
     # only 1 doc is left
     env.expect('FT.SEARCH', 'idx', '*').equal([1, 'doc2', ['t', 'bar']])
+
+
+    # test with SCOREEXPLAIN and EXPLAINSCORE - make sure all memory is released
+    conn.execute_command('HSET', 'doc1', 't', 'foo')
+
+    # both docs exist
+    res = [2, 'doc2', ['1', ['Final TFIDF : words TFIDF 1.00 * document score 1.00 / norm 1 / slop 1',
+                                ['(TFIDF 1.00 = Weight 1.00 * Frequency 1)']]], ['t', 'bar'],
+              'doc1', ['1', ['Final TFIDF : words TFIDF 1.00 * document score 1.00 / norm 1 / slop 1',
+                                ['(TFIDF 1.00 = Weight 1.00 * Frequency 1)']]], ['t', 'foo']]
+    env.expect('FT.SEARCH', 'idx', '*', 'WITHSCORES', 'EXPLAINSCORE').equal(res)
+
+    conn.execute_command('PEXPIRE', 'doc1', 1)
+    time.sleep(0.01)
+
+    # both docs exist but doc1 fail to load field since they were expired passively
+    res = [2, 'doc2', ['1', ['Final TFIDF : words TFIDF 1.00 * document score 1.00 / norm 1 / slop 1',
+                                ['(TFIDF 1.00 = Weight 1.00 * Frequency 1)']]], ['t', 'bar'],
+              'doc1', ['1', ['Final TFIDF : words TFIDF 1.00 * document score 1.00 / norm 1 / slop 1',
+                                ['(TFIDF 1.00 = Weight 1.00 * Frequency 1)']]], None]
+    env.expect('FT.SEARCH', 'idx', '*', 'WITHSCORES', 'EXPLAINSCORE').equal(res)
+
+    # only 1 doc is left
+    res = [1, 'doc2', ['1', ['Final TFIDF : words TFIDF 1.00 * document score 1.00 / norm 1 / slop 1',
+                                ['(TFIDF 1.00 = Weight 1.00 * Frequency 1)']]], ['t', 'bar']]
+    env.expect('FT.SEARCH', 'idx', '*', 'WITHSCORES', 'EXPLAINSCORE').equal(res)
