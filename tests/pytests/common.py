c66f2d8ae99258daa43bcfd49a40820173d9f390
rafie <rafi@redislabs.com>
2022-09-18 10:14:09 +0300

    rafi: fixes in tests dirs

---
+++ tests/pytests/common.py
@@ -0,0 +1,231 @@
+
+from collections import Iterable
+import time
+from packaging import version
+from functools import wraps
+import signal
+import platform
+import itertools
+from redis.client import NEVER_DECODE
+import RLTest
+
+from includes import *
+
+
+class TimeLimit(object):
+    """
+    A context manager that fires a TimeExpired exception if it does not
+    return within the specified amount of time.
+    """
+
+    def __init__(self, timeout):
+        self.timeout = timeout
+
+    def __enter__(self):
+        signal.signal(signal.SIGALRM, self.handler)
+        signal.setitimer(signal.ITIMER_REAL, self.timeout, 0)
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        signal.setitimer(signal.ITIMER_REAL, 0)
+        signal.signal(signal.SIGALRM, signal.SIG_DFL)
+
+    def handler(self, signum, frame):
+        raise Exception('timeout')
+
+
+def getConnectionByEnv(env):
+    conn = None
+    if env.env == 'oss-cluster':
+        conn = env.envRunner.getClusterConnection()
+    else:
+        conn = env.getConnection()
+    return conn
+
+def waitForIndex(env, idx):
+    waitForRdbSaveToFinish(env)
+    while True:
+        res = env.execute_command('ft.info', idx)
+        if int(res[res.index('indexing') + 1]) == 0:
+            break
+        time.sleep(0.1)
+
+def py2sorted(x):
+    it = iter(x)
+    groups = [[next(it)]]
+    for item in it:
+        for group in groups:
+            try:
+                item < group[0]  # exception if not comparable
+                group.append(item)
+                break
+            except TypeError:
+                continue
+        else:  # did not break, make new group
+            groups.append([item])
+    # print(groups)  # for debugging
+    return list(itertools.chain.from_iterable(sorted(group) for group in groups))
+
+def toSortedFlatList(res):
+    if isinstance(res, str):
+        return [res]
+    if isinstance(res, Iterable):
+        finalList = []
+        for e in res:
+            finalList += toSortedFlatList(e)
+
+        return py2sorted(finalList)
+    return [res]
+
+def assertInfoField(env, idx, field, expected):
+    if not env.isCluster():
+        res = env.cmd('ft.info', idx)
+        d = {res[i]: res[i + 1] for i in range(0, len(res), 2)}
+        env.assertEqual(d[field], expected)
+
+def sortedResults(res):
+    n = res[0]
+    res = res[1:]
+
+    y = []
+    data = []
+    for x in res:
+        y.append(x)
+        if len(y) == 2:
+            data.append(y)
+            y = []
+
+    data = py2sorted(data)
+    res = [n] + [item for sublist in data for item in sublist]
+    return res
+
+def slice_at(v, val):
+    try:
+        i = v.index(val)
+        return v[i+1:]
+    except:
+        return []
+
+def numver_to_version(numver):
+    v = numver
+    v = "%d.%d.%d" % (int(v/10000), int(v/100)%100, v%100)
+    return version.parse(v)
+
+def arch_int_bits():
+  arch = platform.machine()
+  if arch == 'x86_64':
+    return 128
+  elif arch == 'aarch64':
+    return 64
+  else:
+    return 64
+
+module_ver = None
+def module_version_at_least(env, ver):
+    global module_ver
+    if module_ver is None:
+        v = env.execute_command('MODULE LIST')[0][3]
+        module_ver = numver_to_version(v)
+    if not isinstance(ver, version.Version):
+        ver = version.parse(ver)
+    return module_ver >= ver
+
+def module_version_less_than(env, ver):
+    return not module_version_at_least(env, ver)
+
+server_ver = None
+def server_version_at_least(env, ver):
+    global server_ver
+    if server_ver is None:
+        v = env.execute_command('INFO')['redis_version']
+        server_ver = version.parse(v)
+    if not isinstance(ver, version.Version):
+        ver = version.parse(ver)
+    return server_ver >= ver
+
+def server_version_less_than(env, ver):
+    return not server_version_at_least(env, ver)
+
+def index_info(env, idx):
+    res = env.cmd('FT.INFO', idx)
+    res = {res[i]: res[i + 1] for i in range(0, len(res), 2)}
+    return res
+
+def skipOnExistingEnv(env):
+    if 'existing' in env.env:
+        env.skip()
+
+def SkipOnNonCluster(env):
+    if not env.isCluster():
+        env.skip()
+
+def skipOnCrdtEnv(env):
+    if len([a for a in env.cmd('module', 'list') if a[1] == 'crdt']) > 0:
+        env.skip()
+
+def waitForRdbSaveToFinish(env):
+    # info command does not take a key therefore a cluster env is no good here
+    if env is RLTest.Env or env is RLTest.StandardEnv:
+        conn = env.getConnection()
+    else:
+        # probably not an Env but a Connection
+        conn = env
+    while True:
+        if not conn.execute_command('info', 'Persistence')['rdb_bgsave_in_progress']:
+            break
+
+def countKeys(env, pattern='*'):
+    if not env.is_cluster():
+        return len(env.keys(pattern))
+    keys = 0
+    for shard in range(0, env.shardsCount):
+        conn = env.getConnection(shard)
+        keys += len(conn.keys(pattern))
+    return keys
+
+def collectKeys(env, pattern='*'):
+    if not env.is_cluster():
+        return sorted(env.keys(pattern))
+    keys = []
+    for shard in range(0, env.shardsCount):
+        conn = env.getConnection(shard)
+        keys.extend(conn.keys(pattern))
+    return sorted(keys)
+
+def forceInvokeGC(env, idx):
+    waitForRdbSaveToFinish(env)
+    env.cmd('ft.debug', 'GC_FORCEINVOKE', idx)
+
+def skip(f, on_cluster=False):
+    @wraps(f)
+    def wrapper(env, *args, **kwargs):
+        if not on_cluster or env.isCluster():
+            env.skip()
+            return
+        return f(env, *args, **kwargs)
+    return wrapper
+
+def no_msan(f):
+    @wraps(f)
+    def wrapper(env, *args, **kwargs):
+        if SANITIZER == 'memory':
+            fname = f.func_name
+            env.debugPrint("skipping {} due to memory sanitizer".format(fname), force=True)
+            env.skip()
+            return
+        return f(env, *args, **kwargs)
+    return wrapper
+
+def unstable(f):
+    @wraps(f)
+    def wrapper(env, *args, **kwargs):
+        if ONLY_STABLE:
+            fname = f.func_name
+            env.debugPrint("skipping {} because it is unstable".format(fname), force=True)
+            env.skip()
+            return
+        return f(env, *args, **kwargs)
+    return wrapper
+
+def to_dict(res):
+    d = {res[i]: res[i + 1] for i in range(0, len(res), 2)}
+    return d
