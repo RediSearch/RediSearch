4221cc5fc1ea4db6ba8b6f1dbe7a389d3e017405
rafie <rafi@redislabs.com>
2022-10-16 04:15:24 +0300

    fixes 16

--- src/module-init/module-init.c
+++ src/module-init/module-init.c
@@ -47,7 +47,7 @@ static int validateAofSettings(RedisModuleCtx *ctx) {
     return rc;
   }
 
-  // Can't exexcute commands on the loading context, so make a new one
+  // Can't execute commands on the loading context, so make a new one
   RedisModuleCtx *confCtx = RedisModule_GetThreadSafeContext(NULL);
   RedisModuleCallReply *reply =
       RedisModule_Call(confCtx, "CONFIG", "cc", "GET", "aof-use-rdb-preamble");
