8fbceba624ce6e40048671c3d2986516e710d416
rafie <rafi@redislabs.com>
2022-11-15 10:38:37 +0200

    License change

--- src/score_explain.h
+++ src/score_explain.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright Redis Ltd. 2016 - present
+ * Licensed under your choice of the Redis Source Available License 2.0 (RSALv2) or
+ * the Server Side Public License v1 (SSPLv1).
+ */
+
 #ifndef RS_SCORE_EXPLAIN_H_
 #define RS_SCORE_EXPLAIN_H_
 #include "redismodule.h"
