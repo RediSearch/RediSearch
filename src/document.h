0878c8d5b5b4e9be55ec0ab8d90af468b889f127
Rafi Einstein <rafi@redislabs.com>
2022-11-15 14:32:56 +0200

    License change (#3226) (#3228)
    * License change
    * Added lexer/parser sources
    * Typo in LICENSE.txt
    * More referrals to old license
    * License hyperlinks in README.md
    * VectorSimilarity v0.6.0 (after license change)
    
    (cherry picked from commit e24ab70d5d33b15b07f6246c674368e02b026782)

--- src/document.h
+++ src/document.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright Redis Ltd. 2016 - present
+ * Licensed under your choice of the Redis Source Available License 2.0 (RSALv2) or
+ * the Server Side Public License v1 (SSPLv1).
+ */
+
 #ifndef __RS_DOCUMENT_H__
 #define __RS_DOCUMENT_H__
 #include <pthread.h>
