25a47b5a3e4739d015a8392f935237eec93e7cbe
Rafi Einstein <rafi@redislabs.com>
2022-11-15 13:36:29 +0200

    License change (#3226)
    * License change
    * Added lexer/parser sources
    * Typo in LICENSE.txt
    * More referrals to old license
    * License hyperlinks in README.md
    * VectorSimilarity v0.6.0 (after license change)
    
    (cherry picked from commit e24ab70d5d33b15b07f6246c674368e02b026782)

--- coord/src/cluster_spell_check.h
+++ coord/src/cluster_spell_check.h
@@ -1,4 +1,10 @@
 /*
+ * Copyright Redis Ltd. 2016 - present
+ * Licensed under your choice of the Redis Source Available License 2.0 (RSALv2) or
+ * the Server Side Public License v1 (SSPLv1).
+ */
+
+/*
  * cluster_spell_check.h
  *
  *  Created on: Jul 29, 2018
--- tests/ctests/test_cntokenize.c
+++ tests/ctests/test_cntokenize.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright Redis Ltd. 2016 - present
+ * Licensed under your choice of the Redis Source Available License 2.0 (RSALv2) or
+ * the Server Side Public License v1 (SSPLv1).
+ */
+
 // TODO: We might not need all these includes
 #include "test_util.h"
 #include "time_sample.h"
