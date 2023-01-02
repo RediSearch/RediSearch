273b478584cc271c4c6bae952fbeafd3f60bfbb9
GuyAv46 <47632673+GuyAv46@users.noreply.github.com>
2023-01-02 17:20:42 +0200

    Multi thread configuration flags - [MOD-4565, MOD-4566] (#3300)
    * added configuration flags
    
    * added a test
    
    * review fixes

--- tests/pytests/test_config.py
+++ tests/pytests/test_config.py
@@ -28,6 +28,8 @@ def testGetConfigOptions(env):
     assert env.expect('ft.config', 'get', 'TIMEOUT').res[0][0] =='TIMEOUT'
     assert env.expect('ft.config', 'get', 'INDEX_THREADS').res[0][0] =='INDEX_THREADS'
     assert env.expect('ft.config', 'get', 'SEARCH_THREADS').res[0][0] =='SEARCH_THREADS'
+    assert env.expect('ft.config', 'get', 'WORKER_THREADS').res[0][0] =='WORKER_THREADS'
+    assert env.expect('ft.config', 'get', 'ENABLE_THREADS').res[0][0] =='ENABLE_THREADS'
     assert env.expect('ft.config', 'get', 'FRISOINI').res[0][0] =='FRISOINI'
     assert env.expect('ft.config', 'get', 'MAXSEARCHRESULTS').res[0][0] =='MAXSEARCHRESULTS'
     assert env.expect('ft.config', 'get', 'MAXAGGREGATERESULTS').res[0][0] =='MAXAGGREGATERESULTS'
@@ -65,6 +67,7 @@ def testSetConfigOptions(env):
     env.expect('ft.config', 'set', 'TIMEOUT', 1).equal('OK')
     env.expect('ft.config', 'set', 'INDEX_THREADS', 1).equal('Not modifiable at runtime')
     env.expect('ft.config', 'set', 'SEARCH_THREADS', 1).equal('Not modifiable at runtime')
+    env.expect('ft.config', 'set', 'WORKER_THREADS', 1).equal('Not modifiable at runtime')
     env.expect('ft.config', 'set', 'FRISOINI', 1).equal('Not modifiable at runtime')
     env.expect('ft.config', 'set', 'ON_TIMEOUT', 1).equal('Success (not an error)')
     env.expect('ft.config', 'set', 'GCSCANSIZE', 1).equal('OK')
@@ -103,6 +106,8 @@ def testAllConfig(env):
     env.assertIn(res_dict['TIMEOUT'][0], ['500', '0'])
     env.assertEqual(res_dict['INDEX_THREADS'][0], '8')
     env.assertEqual(res_dict['SEARCH_THREADS'][0], '20')
+    env.assertEqual(res_dict['WORKER_THREADS'][0], '0')
+    env.assertEqual(res_dict['ENABLE_THREADS'][0], 'false')
     env.assertEqual(res_dict['FRISOINI'][0], None)
     env.assertEqual(res_dict['ON_TIMEOUT'][0], 'return')
     env.assertEqual(res_dict['GCSCANSIZE'][0], '100')
@@ -144,6 +149,7 @@ def testInitConfig(env):
     test_arg_num('MAXPREFIXEXPANSIONS', 5)
     test_arg_num('INDEX_THREADS', 3)
     test_arg_num('SEARCH_THREADS', 3)
+    test_arg_num('WORKER_THREADS', 3)
     test_arg_num('GCSCANSIZE', 3)
     test_arg_num('MIN_PHONETIC_TERM_LEN', 3)
     test_arg_num('FORK_GC_RUN_INTERVAL', 3)
@@ -178,6 +184,7 @@ def testInitConfig(env):
         env.stop()
 
     test_arg_str('GC_POLICY', 'fork')
+    test_arg_str('ENABLE_THREADS', 'true')
     test_arg_str('GC_POLICY', 'default', 'fork')
     test_arg_str('ON_TIMEOUT', 'fail')
     test_arg_str('TIMEOUT', '0', '0')
@@ -204,6 +211,7 @@ def testImmutable(env):
     env.expect('ft.config', 'set', 'MAXDOCTABLESIZE').error().contains('Not modifiable at runtime')
     env.expect('ft.config', 'set', 'INDEX_THREADS').error().contains('Not modifiable at runtime')
     env.expect('ft.config', 'set', 'SEARCH_THREADS').error().contains('Not modifiable at runtime')
+    env.expect('ft.config', 'set', 'WORKER_THREADS').error().contains('Not modifiable at runtime')
     env.expect('ft.config', 'set', 'FRISOINI').error().contains('Not modifiable at runtime')
     env.expect('ft.config', 'set', 'GC_POLICY').error().contains('Not modifiable at runtime')
     env.expect('ft.config', 'set', 'NO_MEM_POOLS').error().contains('Not modifiable at runtime')
