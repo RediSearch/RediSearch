e32004f32c33c33a87cd48a4ae3f61bf50753757
meir <meir@redis.com>
2023-01-16 13:51:32 +0200

    Performance optimization, add first char of each children into the father.

--- src/redisearch_rs/trie_rs/src/ordered_u8_map.rs
+++ src/redisearch_rs/trie_rs/src/ordered_u8_map.rs
@@ -8,7 +8,7 @@ pub trait FirstCharGetter {
 pub struct OrderedU8Map<T: FirstCharGetter> {
     // the Option<Box<...>> is used to improve memory usage, most of the data are leaves with no children so we only store 8 bytes
     // for those instead of 16 bytes if we use LowMemoryVec directly
-    map: Option<Box<LowMemoryVec<T>>>,
+    map: Option<Box<LowMemoryVec<(u8, T)>>>,
 }
 
 impl<T: FirstCharGetter> OrderedU8Map<T> {
@@ -26,35 +26,35 @@ impl<T: FirstCharGetter> OrderedU8Map<T> {
 
     pub(crate) fn get(&self, key: u8) -> Option<&T> {
         let m = self.map.as_ref()?;
-        let index = m.binary_search_by_key(&key, |e| e.get_first_char()).ok()?;
-        m.get(index as u32)
+        let index = m.binary_search_by_key(&key, |e| e.0).ok()?;
+        Some(&m.get(index as u32)?.1)
     }
 
     pub(crate) fn get_mut(&mut self, key: u8) -> Option<&mut T> {
         let m = self.map.as_mut()?;
-        let index = m.binary_search_by_key(&key, |e| e.get_first_char()).ok()?;
-        m.get_mut(index as u32)
+        let index = m.binary_search_by_key(&key, |e| e.0).ok()?;
+        Some(&mut m.get_mut(index as u32)?.1)
     }
 
     pub(crate) fn get_or_create<F: FnOnce() -> T>(&mut self, key: u8, create: F) -> &mut T {
         let m = self.map.get_or_insert(Box::new(LowMemoryVec::new()));
-        let index = match m.binary_search_by_key(&key, |e| e.get_first_char()) {
+        let index = match m.binary_search_by_key(&key, |e| e.0) {
             Ok(index) => index,
             Err(index) => {
                 let val = create();
-                m.insert(index as u32, val);
+                m.insert(index as u32, (key, val));
                 index
             }
         };
-        &mut m[index]
+        &mut m[index].1
     }
 
     pub(crate) fn insert(&mut self, key: u8, val: T) -> bool {
         let m = self.map.get_or_insert(Box::new(LowMemoryVec::new()));
-        match m.binary_search_by_key(&key, |e| e.get_first_char()) {
+        match m.binary_search_by_key(&key, |e| e.0) {
             Ok(_index) => false,
             Err(index) => {
-                m.insert(index as u32, val);
+                m.insert(index as u32, (key, val));
                 true
             }
         }
@@ -62,9 +62,9 @@ impl<T: FirstCharGetter> OrderedU8Map<T> {
 
     pub(crate) fn remove(&mut self, key: u8) -> Option<T> {
         let m = self.map.as_mut()?;
-        let index = m.binary_search_by_key(&key, |e| e.get_first_char()).ok()?;
+        let index = m.binary_search_by_key(&key, |e| e.0).ok()?;
         let res = m.remove(index as u32);
-        Some(res)
+        Some(res.1)
     }
 
     pub(crate) fn values(&self) -> OrderedU8ValuesIterator<'_, T> {
@@ -96,7 +96,7 @@ impl<T: FirstCharGetter> Iterator for OrderedU8MapIterator<T> {
 
     fn next(&mut self) -> Option<Self::Item> {
         let res = self.map.map.as_mut()?.pop()?;
-        Some((res.get_first_char(), res))
+        Some(res)
     }
 }
 
@@ -117,6 +117,6 @@ impl<'map, T: FirstCharGetter> Iterator for OrderedU8ValuesIterator<'map, T> {
     fn next(&mut self) -> Option<Self::Item> {
         let res = self.map.map.as_ref()?.get(self.index as u32)?;
         self.index += 1;
-        Some(res)
+        Some(&res.1)
     }
 }
