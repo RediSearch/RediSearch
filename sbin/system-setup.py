31be212b796652a02ad28795fc4e23f806b38cdb
rafie <rafi@redislabs.com>
2023-01-24 14:25:31 +0200

    fixes 49

--- sbin/system-setup.py
+++ sbin/system-setup.py
@@ -71,7 +71,7 @@ class RediSearchSetup(paella.Setup):
     def common_last(self):
         self.run("{PYTHON} {READIES}/bin/getcmake --usr".format(PYTHON=self.python, READIES=READIES),
                  sudo=self.os != 'macos')
-        self.run("{PYTHON} {READIES}/bin/getrmpytools --reinstall --modern --rltest-version=github:master".format(PYTHON=self.python, READIES=READIES))
+        self.run("{PYTHON} {READIES}/bin/getrmpytools --reinstall --modern".format(PYTHON=self.python, READIES=READIES))
         if self.dist != "arch":
             self.install("lcov")
         else:
