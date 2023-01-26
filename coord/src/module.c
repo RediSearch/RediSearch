f2727687e3e680822f155713b96f73f17c1dee79
Rafi Einstein <rafi@redislabs.com>
2023-01-26 15:28:35 +0200

    MOD-4560: Updated libuv (v1.44.2), also fixing crash during cluster failover (#3354)

--- coord/src/module.c
+++ coord/src/module.c
@@ -31,6 +31,8 @@
 #include "cluster_spell_check.h"
 #include "profile.h"
 
+#include "libuv/include/uv.h"
+
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
@@ -2058,6 +2060,7 @@ RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
   }
 
   setHiredisAllocators();
+  uv_replace_allocator(rm_malloc, rm_realloc, rm_calloc, rm_free);
 
   if (!RSDummyContext) {
     if (RedisModule_GetDetachedThreadSafeContext) {
