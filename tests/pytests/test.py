362c2ed1d929631eddd07e911d539adfc4afe673
meir <meir@redis.com>
2023-01-29 15:27:55 +0200

    Update test to run on single shard and on cluster

--- tests/pytests/test.py
+++ tests/pytests/test.py
@@ -3615,10 +3615,6 @@ def test_missing_schema(env):
     env.expect('FT.SEARCH', 'idx2', '*').error().equal('idx2: no such index')
 
 def test_cluster_set(env):
-    # if not env.isCluster():
-    #     # this test is only relevant on cluster
-    #     env.skip()
-
     def verify_address(addr):
         try:
             with TimeLimit(10):
