0398dce099b372efd4e0ffa698255171a9d75457
Rafi Einstein <rafi@redislabs.com>
2023-01-30 14:12:47 +0200

    MOD-4296: Handling division by zero in expressions (#3357) (#3359)
    (cherry picked from commit eaaf404183f4edf449863936181e3c806b0231d0)

--- tests/pytests/test_issues.py
+++ tests/pytests/test_issues.py
@@ -626,3 +626,9 @@ def test_mod_4207(env):
 
   env.expect('FT.SEARCH', 'idx1', '*', 'NOCONTENT').equal([3, 'address:1', 'address:2', 'address:4'])
   env.expect('FT.SEARCH', 'idx2', '*', 'NOCONTENT').equal([3, 'address:1', 'address:2', 'address:3'])
+
+def test_mod4296_badexpr(env):
+  env.expect('FT.CREATE', 'idx', 'SCHEMA', 't', 'TEXT').equal('OK')
+  env.expect('HSET', 'doc', 't', 'foo').equal(1)
+  env.expect('FT.AGGREGATE', 'idx', 'foo', 'LOAD', 1, '@t', 'APPLY', '1%0', 'as', 'foo').equal([1, ['t', 'foo', 'foo', 'nan']])
+  env.expect('FT.AGGREGATE', 'idx', 'foo', 'LOAD', 1, '@t', 'APPLY', '1/0', 'as', 'foo').equal([1, ['t', 'foo', 'foo', 'nan']])
