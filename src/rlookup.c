06dcc109c5c42e3c115e5115a3352bf40e9b7289
meiravgri <meirav.grimberg@redis.com>
2023-02-02 13:16:27 +0000

    removed index lock in load docuemnt

--- src/rlookup.c
+++ src/rlookup.c
@@ -690,34 +690,22 @@ int RLookup_LoadDocument(RLookup *it, RLookupRow *dst, RLookupLoadOptions *optio
     dst->sv = options->dmd->sortVector;
   }
 
-  // TODO: Move the locking after enabling concurent reads and writes.
-  // If we are running in multi threaded mode we need to lock the GIL
-  RedisModuleCtx *ctx = options->sctx->redisCtx;
-  RedisModuleBlockedClient *bc = RedisModule_GetBlockedClientHandle(ctx);
-  if (bc) {
-  // Lock the GIL
-    RedisModule_ThreadSafeContextLock(ctx);
-  }
   if (options->mode & RLOOKUP_LOAD_ALLKEYS) {
     if (options->dmd->type == DocumentType_Hash) {
       rv = RLookup_HGETALL(it, dst, options);
     } else if (options->dmd->type == DocumentType_Json) {
       rv = RLookup_JSON_GetAll(it, dst, options);
-    }
+    }  
   } else {
     rv = loadIndividualKeys(it, dst, options);
   }
   // if loading the document failed b/c it does not exist, delete the document from DocTable
   // this will mark doc as deleted and reply with `(nil)`
   if (rv != REDISMODULE_OK) {
-    // TODO: multithreaded: make sure we are not taking the write lock here while we are in read lock
+    RedisModuleCtx *ctx = options->sctx->redisCtx;
     RedisModuleString *rmstr = DMD_CreateKeyString(options->dmd, ctx);
     IndexSpec_DeleteDoc(options->sctx->spec, ctx, rmstr);
     RedisModule_FreeString(ctx, rmstr);
-  }
-  if (bc) {
-  // Unlock the GIL
-    RedisModule_ThreadSafeContextUnlock(ctx);
   }
   return rv;
 }
