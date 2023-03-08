2533efd1cf85801c78afcdfad6d53b51d7ddcd64
Ephraim Feldblum <ephraim.feldblum@redis.com>
2023-03-08 14:26:51 +0200

    license

--- src/geometry/rtdoc.cpp
+++ src/geometry/rtdoc.cpp
@@ -1,7 +1,13 @@
+/*
+ * Copyright Redis Ltd. 2016 - present
+ * Licensed under your choice of the Redis Source Available License 2.0 (RSALv2) or
+ * the Server Side Public License v1 (SSPLv1).
+ */
+
 
 #include "rtdoc.hpp"
 
-extern "C" RTDoc *From_WKT(const char *wkt, size_t len, t_docId id) {
+RTDoc *From_WKT(const char *wkt, size_t len, t_docId id) {
   try {
     return new RTDoc{std::string_view{wkt, len}, id};
   } catch (...) {
