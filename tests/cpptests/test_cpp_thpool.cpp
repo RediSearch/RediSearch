9bf5474257e26571ac84c01364ca669d5dda9290
DvirDukhan <dvir@redis.com>
2023-03-09 16:29:17 +0200

    tests for now

---
+++ tests/cpptests/test_cpp_thpool.cpp
@@ -0,0 +1,121 @@
+#include "gtest/gtest.h"
+#include "deps/thpool/thpool.h"
+#include <chrono>
+#include <thread>
+
+class ThpoolTest : public ::testing::Test {
+
+
+    public:
+    threadpool pool;
+        virtual void SetUp() {
+            this->pool = thpool_init(1);
+        }
+
+        virtual void TearDown() {
+            thpool_destroy(this->pool);
+        }
+};
+
+struct test_struct {
+    std::chrono::time_point<std::chrono::high_resolution_clock> *arr; // Pointer to the array of timestamps
+    int index;                                                        // Index of the timestamp in the array
+};
+
+/* The purpose of the function is to sleep for 100ms and then set the timestamp
+ * in the test_struct. 
+*/
+void sleep_and_set(test_struct *ts) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    ts->arr[ts->index] = std::chrono::high_resolution_clock::now();
+}
+
+
+/* The purpose of the test is to check that tasks with the same priority are handled 
+ * in FIFO manner. The test adds 10 tasks with low priority and checks that the
+ * tasks are handled in the order they were added.
+ */
+TEST_F(ThpoolTest, AllLowPriority) {
+    int array_len = 10;
+    std::chrono::time_point<std::chrono::high_resolution_clock> arr[array_len];
+    test_struct ts[array_len];
+    for (int i = 0; i < array_len; i++) {
+        ts[i].arr = arr;
+        ts[i].index = i;
+        thpool_add_work(this->pool, (void (*)(void *))sleep_and_set, (void *)&ts[i], THPOOL_PRIORITY_LOW);
+    }
+    thpool_wait(this->pool);
+    for (int i = 0; i < array_len-1; i++) {
+        ASSERT_LT(arr[i],  arr[i+1]);
+    }  
+}
+
+/* The purpose of the test is to check that tasks with the same priority are handled 
+ * in FIFO manner. The test adds 10 tasks with low priority and checks that the
+ * tasks are handled in the order they were added.
+ */
+TEST_F(ThpoolTest, AllHighPriority) {
+    int array_len = 10;
+    std::chrono::time_point<std::chrono::high_resolution_clock> arr[array_len];
+    test_struct ts[array_len];
+    for (int i = 0; i < array_len; i++) {
+        ts[i].arr = arr;
+        ts[i].index = i;
+        thpool_add_work(this->pool, (void (*)(void *))sleep_and_set, (void *)&ts[i], THPOOL_PRIORITY_HIGH);
+    }
+    thpool_wait(this->pool);
+    for (int i = 0; i < array_len-1; i++) {
+        ASSERT_LT(arr[i],  arr[i+1]);
+    } 
+}
+
+/* The purpose of the test is to check that tasks with different priorities are handled 
+ * in FIFO manner. The test adds 2 tasks with high priority and 1 task with low priority between them
+ * and checks that the high priority tasks are handled before the low priority task, since the ratio between
+ * handling high priority tasks and low priority tasks is 2:1.
+ */
+TEST_F(ThpoolTest, HighLowHighTest) {
+    int high_priority_tasks = 2;
+    std::chrono::time_point<std::chrono::high_resolution_clock> arr[high_priority_tasks];
+    std::chrono::time_point<std::chrono::high_resolution_clock> low_priority_timestamp;
+    // Initialize the test_struct array
+    test_struct ts[high_priority_tasks + 1] = {0};
+    for (int i = 0; i < high_priority_tasks; i++) {
+        ts[i].arr = arr;
+        ts[i].index = i;
+    }
+    // The low priority task is added in the middle, but it should run after the high priority tasks
+    ts[high_priority_tasks].arr = &low_priority_timestamp;
+    thpool_add_work(this->pool, (void (*)(void *))sleep_and_set, (void *)&ts[0], THPOOL_PRIORITY_HIGH);
+    thpool_add_work(this->pool, (void (*)(void *))sleep_and_set, (void *)&ts[2], THPOOL_PRIORITY_LOW);
+    thpool_add_work(this->pool, (void (*)(void *))sleep_and_set, (void *)&ts[1], THPOOL_PRIORITY_HIGH);
+
+    thpool_wait(this->pool);
+    for (int i = 0; i < high_priority_tasks; i++) {
+        ASSERT_LT(arr[i],  low_priority_timestamp);
+    } 
+}
+
+
+// TEST_F(ThpoolTest, LowLowHighTest) {
+//     int low_priority_tasks = 2;
+//     std::chrono::time_point<std::chrono::high_resolution_clock> arr[low_priority_tasks];
+//     std::chrono::time_point<std::chrono::high_resolution_clock> high_priority_timestamp;
+//     // Initialize the test_struct array
+//     test_struct ts[low_priority_tasks + 1] = {0};
+//     for (int i = 0; i < low_priority_tasks; i++) {
+//         ts[i].arr = arr;
+//         ts[i].index = i;
+//     }
+//     // The high priority task is added in the middle, so it should run in the middle.
+//     ts[low_priority_tasks].arr = &high_priority_timestamp;
+//     thpool_add_work(this->pool, (void (*)(void *))sleep_and_set, (void *)&ts[0], THPOOL_PRIORITY_LOW);
+//     thpool_add_work(this->pool, (void (*)(void *))sleep_and_set, (void *)&ts[1], THPOOL_PRIORITY_LOW);
+//     thpool_add_work(this->pool, (void (*)(void *))sleep_and_set, (void *)&ts[2], THPOOL_PRIORITY_HIGH);
+
+//     thpool_wait(this->pool);
+//     ASSERT_LT(arr[0],  high_priority_timestamp);
+//     ASSERT_LT(high_priority_timestamp, arr[1]);
+
+// }
+
