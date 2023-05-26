07591968ada16948a35da1228a5dcba9a0e8fa4f
alon <alonreshef24@gmail.com>
2023-05-26 18:16:28 +0300

    make tests shorter

--- tests/pytests/test_multithread.py
+++ tests/pytests/test_multithread.py
@@ -247,7 +247,7 @@ def test_reload_index_while_indexing():
     env = Env(enableDebugCommand=True, moduleArgs='WORKER_THREADS 2 ENABLE_THREADS TRUE DEFAULT_DIALECT 2')
     conn = getConnectionByEnv(env)
     n_shards = env.shardsCount
-    n_vectors = 50000 * n_shards
+    n_vectors = 5000 * n_shards
     dim = 64
     # Load random vectors into redis.
     env.expect('FT.CREATE', 'idx', 'SCHEMA', 'vector', 'VECTOR', 'HNSW', '8', 'TYPE', 'FLOAT32', 'M', '64',
@@ -267,7 +267,7 @@ def test_delete_index_while_indexing():
     env = Env(enableDebugCommand=True, moduleArgs='WORKER_THREADS 2 ENABLE_THREADS TRUE DEFAULT_DIALECT 2')
     conn = getConnectionByEnv(env)
     n_shards = env.shardsCount
-    n_vectors = 50000 * n_shards
+    n_vectors = 5000 * n_shards
     dim = 64
     # Load random vectors into redis.
     env.expect('FT.CREATE', 'idx', 'SCHEMA', 'vector', 'VECTOR', 'HNSW', '8', 'TYPE', 'FLOAT32', 'M', '64',
@@ -285,7 +285,7 @@ def test_workers_priority_queue():
     env = Env(enableDebugCommand=True, moduleArgs='WORKER_THREADS 2 ENABLE_THREADS TRUE DEFAULT_DIALECT 2')
     conn = getConnectionByEnv(env)
     n_shards = env.shardsCount
-    n_vectors = 50000 * n_shards
+    n_vectors = 10000 * n_shards
     dim = 64
 
     # Load random vectors into redis, save the last one to use as query vector later on.
@@ -326,7 +326,7 @@ def test_async_updates_sanity():
     env = Env(enableDebugCommand=True, moduleArgs='WORKER_THREADS 2 ENABLE_THREADS TRUE DEFAULT_DIALECT 2')
     conn = getConnectionByEnv(env)
     n_shards = env.shardsCount
-    n_vectors = 5000 * n_shards
+    n_vectors = 1000 * n_shards
     dim = 32
     block_size = 1024
 
