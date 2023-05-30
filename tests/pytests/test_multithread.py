241c5bf48c65a41c9fecda6b7bd40eab0d45737b
GuyAv46 <guy.avimor@gmail.com>
2023-05-30 12:11:13 +0300

    added aggregate test

--- tests/pytests/test_multithread.py
+++ tests/pytests/test_multithread.py
@@ -104,6 +104,8 @@ def test_pipeline(env):
         return ['Type', 'Unlocker', 'Counter', docs_count if not paged else reply_length]
     def loader(paged= False):
         return ['Type', 'Loader', 'Counter', docs_count if not paged else reply_length]
+    def apply(paged= False):
+        return ['Type', 'Projector - Operator ^', 'Counter', docs_count if not paged else reply_length]
     pager = ['Type', 'Pager/Limiter', 'Counter', reply_length]
     highlighter = ['Type', 'Highlighter', 'Counter', docs_count]
 
@@ -119,10 +121,14 @@ def test_pipeline(env):
     env.assertEqual(conn.execute_command('HSET', 'doc2', nonsortable_field_name, '101', sortable_UNF_field_name, '201'), 2)
     env.assertEqual(conn.execute_command('HSET', 'doc3', nonsortable_field_name, '100', sortable_UNF_field_name, '200'), 2)
 
-    ft_profile_cmd = ['FT.PROFILE', 'idx', 'SEARCH', 'LIMITED', 'QUERY']
+    ft_profile_search_cmd = ['FT.PROFILE', 'idx', 'SEARCH', 'LIMITED', 'QUERY']
+    ft_profile_aggregate_cmd = ft_profile_search_cmd.copy()
+    ft_profile_aggregate_cmd[2] = 'AGGREGATE'
     search_UNF_sortable = '@n_sortable:[200 202]'
-    sortby_UNF_sortable = ['SORTBY', sortable_UNF_field_name]
-    sortby_not_sortable = ['SORTBY', nonsortable_field_name]
+    sortby_UNF_sortable_s = ['SORTBY', sortable_UNF_field_name]
+    sortby_not_sortable_s = ['SORTBY', nonsortable_field_name]
+    sortby_UNF_sortable_a = ['SORTBY', 1, '@' + sortable_UNF_field_name]
+    sortby_not_sortable_a = ['SORTBY', 1, '@' + nonsortable_field_name]
 
     ############################### Sorter WITH NO access to redis ###############################
 
@@ -130,45 +136,45 @@ def test_pipeline(env):
         expected pipeline: root(<-scorer)<-sorter  '''
     expected_pipeline = ['Result processors profile', root, sorter()]
     #sortby SORTABLE NOCONTENT
-    res = conn.execute_command(*ft_profile_cmd, search_UNF_sortable, *sortby_UNF_sortable, 'NOCONTENT')
+    res = conn.execute_command(*ft_profile_search_cmd, search_UNF_sortable, *sortby_UNF_sortable_s, 'NOCONTENT')
     env.assertEqual(get_pipeline(res), expected_pipeline)
 
     #sortby a (SORTABLE UNF) return a
-    res = conn.execute_command(*ft_profile_cmd, search_UNF_sortable, *sortby_UNF_sortable, 'RETURN', 1, sortable_UNF_field_name)
+    res = conn.execute_command(*ft_profile_search_cmd, search_UNF_sortable, *sortby_UNF_sortable_s, 'RETURN', 1, sortable_UNF_field_name)
     env.assertEqual(get_pipeline(res), expected_pipeline)
 
     #no sortby NOCONTENT
     expected_pipeline = ['Result processors profile', root, scorer, sorter()]
-    res = conn.execute_command(*ft_profile_cmd, search_UNF_sortable, 'NOCONTENT')
+    res = conn.execute_command(*ft_profile_search_cmd, search_UNF_sortable, 'NOCONTENT')
     env.assertEqual(get_pipeline(res), expected_pipeline)
 
     ''' case 2: with loader (sortby SORTABLE (loadall)/ sortby SORTABLE a return b, no sortby (loadall) )
         expected pipeline: root<-(<-scorer)sorter<-buffer-locker<-loader<-unlocker  '''
     expected_pipeline = ['Result processors profile', root, sorter(), buffer_locker(), loader(), unlocker()]
     #sortby SORTABLE (loadall)
-    res = conn.execute_command(*ft_profile_cmd, search_UNF_sortable, *sortby_UNF_sortable)
+    res = conn.execute_command(*ft_profile_search_cmd, search_UNF_sortable, *sortby_UNF_sortable_s)
     env.assertEqual(get_pipeline(res), expected_pipeline)
 
     #sortby a (SORTABLE UNF) return b
-    res = conn.execute_command(*ft_profile_cmd, search_UNF_sortable, *sortby_UNF_sortable, 'RETURN', 1, notindexed_field_name)
+    res = conn.execute_command(*ft_profile_search_cmd, search_UNF_sortable, *sortby_UNF_sortable_s, 'RETURN', 1, notindexed_field_name)
     env.assertEqual(get_pipeline(res), expected_pipeline)
 
     #no sortby (loadall)
     expected_pipeline = ['Result processors profile', root, scorer, sorter(), buffer_locker(), loader(), unlocker()]
-    res = conn.execute_command(*ft_profile_cmd, search_UNF_sortable)
+    res = conn.execute_command(*ft_profile_search_cmd, search_UNF_sortable)
     env.assertEqual(get_pipeline(res), expected_pipeline)
 
     '''case 3: with pager, no loader (sortby a (SORTABLE UNF) LIMIT 1 1 NOCONTENT)
         expected pipeline: root<-sorter<-pager '''
     paged = True
     expected_pipeline = ['Result processors profile', root, sorter(paged), pager]
-    res = conn.execute_command(*ft_profile_cmd, search_UNF_sortable, *sortby_UNF_sortable, 'LIMIT', 1, 1, 'NOCONTENT')
+    res = conn.execute_command(*ft_profile_search_cmd, search_UNF_sortable, *sortby_UNF_sortable_s, 'LIMIT', 1, 1, 'NOCONTENT')
     env.assertEqual(get_pipeline(res), expected_pipeline)
 
     '''case 4: with pager, with loader (sortby a (SORTABLE UNF) LIMIT 1 1 (loadall))
         expected pipeline: root<-sorter<-pager<-buffer-locker<-loader<-unlocker'''
     expected_pipeline = ['Result processors profile', root, sorter(paged), pager, buffer_locker(paged), loader(paged), unlocker(paged)]
-    res = conn.execute_command(*ft_profile_cmd, search_UNF_sortable, *sortby_UNF_sortable, 'LIMIT', 1, 1)
+    res = conn.execute_command(*ft_profile_search_cmd, search_UNF_sortable, *sortby_UNF_sortable_s, 'LIMIT', 1, 1)
     env.assertEqual(get_pipeline(res), expected_pipeline)
 
     ############################### Sorter WITH access to redis: ###############################
@@ -177,36 +183,158 @@ def test_pipeline(env):
         expected pipeline: root<-buffer-locker<-loader<-unlocker<-sorter '''
     expected_pipeline = ['Result processors profile', root, buffer_locker(), loader(), unlocker(), sorter()]
     #sortby NOT SORTABLE NOCONTENT
-    res = conn.execute_command(*ft_profile_cmd, search_UNF_sortable, *sortby_not_sortable, 'NOCONTENT')
+    res = conn.execute_command(*ft_profile_search_cmd, search_UNF_sortable, *sortby_not_sortable_s, 'NOCONTENT')
     env.assertEqual(get_pipeline(res), expected_pipeline)
 
     #sortby a (NOTSORTABLE) return a
-    res = conn.execute_command(*ft_profile_cmd, search_UNF_sortable, *sortby_not_sortable, 'RETURN', 1, nonsortable_field_name)
+    res = conn.execute_command(*ft_profile_search_cmd, search_UNF_sortable, *sortby_not_sortable_s, 'RETURN', 1, nonsortable_field_name)
     env.assertEqual(get_pipeline(res), expected_pipeline)
 
     ''' case 2: with loader
         expected pipeline: root<-buffer-locker<-loader<-sorter<-loader<-unlocker'''
     expected_pipeline = ['Result processors profile', root, buffer_locker(), loader(), sorter(), loader(), unlocker()]
     #sortby NOT SORTABLE (loadall)
-    res = conn.execute_command(*ft_profile_cmd, search_UNF_sortable, *sortby_not_sortable)
+    res = conn.execute_command(*ft_profile_search_cmd, search_UNF_sortable, *sortby_not_sortable_s)
     env.assertEqual(get_pipeline(res), expected_pipeline)
 
     # sortby NOTSORTABLE a return b
-    res = conn.execute_command(*ft_profile_cmd, search_UNF_sortable, *sortby_not_sortable, 'RETURN', 1, notindexed_field_name)
+    res = conn.execute_command(*ft_profile_search_cmd, search_UNF_sortable, *sortby_not_sortable_s, 'RETURN', 1, notindexed_field_name)
     env.assertEqual(get_pipeline(res), expected_pipeline)
 
     '''case 3: with pager, no loader
         expected pipeline: root<-buffer-locker<-loader<-unlocker<-sorter<-pager'''
     expected_pipeline = ['Result processors profile', root, buffer_locker(), loader(), unlocker(), sorter(paged), pager]
     #(sortby NOTSORTABLE LIMIT 1 1 NOCONTENT)
-    res = conn.execute_command(*ft_profile_cmd, search_UNF_sortable, *sortby_not_sortable, 'LIMIT', 1, 1, 'NOCONTENT')
+    res = conn.execute_command(*ft_profile_search_cmd, search_UNF_sortable, *sortby_not_sortable_s, 'LIMIT', 1, 1, 'NOCONTENT')
     env.assertEqual(get_pipeline(res), expected_pipeline)
 
     '''case 4: with pager, with loader
         expected pipeline: root<-buffer-locker<-loader<-sorter<-pager<-loader<-unlocker '''
     expected_pipeline = ['Result processors profile', root, buffer_locker(), loader(), sorter(paged), pager, loader(paged), unlocker(paged)]
     # (sortby NOTSORTABLE LIMIT 1 1 (loadall))
-    res = conn.execute_command(*ft_profile_cmd, search_UNF_sortable, *sortby_not_sortable, 'LIMIT', 1, 1)
+    res = conn.execute_command(*ft_profile_search_cmd, search_UNF_sortable, *sortby_not_sortable_s, 'LIMIT', 1, 1)
+    env.assertEqual(get_pipeline(res), expected_pipeline)
+
+    # AGGREGATE pipeline
+    ############################### Sorter WITH NO access to redis ###############################
+
+    ''' case 1: no loader (sortby SORTABLE NOCONTENT/sortby a (SORTABLE UNF) return a, no sortby NOCONTENT)
+        expected pipeline: root<-sorter  '''
+    expected_pipeline = ['Result processors profile', root, sorter()]
+    #sortby SORTABLE NOCONTENT
+    res = conn.execute_command(*ft_profile_aggregate_cmd, search_UNF_sortable, *sortby_UNF_sortable_a, 'NOCONTENT')
+    env.assertEqual(get_pipeline(res), expected_pipeline)
+
+    #sortby a (SORTABLE UNF) return a
+    res = conn.execute_command(*ft_profile_aggregate_cmd, search_UNF_sortable, *sortby_UNF_sortable_a, 'LOAD', 1, sortable_UNF_field_name)
+    env.assertEqual(get_pipeline(res), expected_pipeline)
+
+    #no sortby NOCONTENT
+    expected_pipeline = ['Result processors profile', root]
+    res = conn.execute_command(*ft_profile_aggregate_cmd, search_UNF_sortable, 'NOCONTENT')
+    env.assertEqual(get_pipeline(res), expected_pipeline)
+
+    ''' case 2: with loader (sortby SORTABLE loadall / sortby SORTABLE a return b, no sortby loadall )
+        expected pipeline: root<-sorter<-buffer-locker<-loader<-unlocker  '''
+    expected_pipeline = ['Result processors profile', root, sorter(), buffer_locker(), loader(), unlocker()]
+    #sortby SORTABLE (loadall)
+    res = conn.execute_command(*ft_profile_aggregate_cmd, search_UNF_sortable, *sortby_UNF_sortable_a, 'LOAD', '*')
+    env.assertEqual(get_pipeline(res), expected_pipeline)
+
+    #sortby a (SORTABLE UNF) return b
+    res = conn.execute_command(*ft_profile_aggregate_cmd, search_UNF_sortable, *sortby_UNF_sortable_a, 'LOAD', 1, notindexed_field_name)
+    env.assertEqual(get_pipeline(res), expected_pipeline)
+
+    #no sortby
+    expected_pipeline = ['Result processors profile', root]
+    res = conn.execute_command(*ft_profile_aggregate_cmd, search_UNF_sortable)
+    env.assertEqual(get_pipeline(res), expected_pipeline)
+    #no sortby (loadall)
+    expected_pipeline = ['Result processors profile', root, buffer_locker(), loader(), unlocker()]
+    res = conn.execute_command(*ft_profile_aggregate_cmd, search_UNF_sortable, 'LOAD', '*')
+    env.assertEqual(get_pipeline(res), expected_pipeline)
+
+    '''case 3: with pager, no loader (sortby a (SORTABLE UNF) LIMIT 1 1 NOCONTENT)
+        expected pipeline: root<-sorter<-pager '''
+    paged = True
+    expected_pipeline = ['Result processors profile', root, sorter(paged), pager]
+    res = conn.execute_command(*ft_profile_aggregate_cmd, search_UNF_sortable, *sortby_UNF_sortable_a, 'LIMIT', 1, 1, 'NOCONTENT')
+    env.assertEqual(get_pipeline(res), expected_pipeline)
+
+    '''case 4: with pager, with no loader (sortby a (SORTABLE UNF) LIMIT 1 1)
+        expected pipeline: root<-sorter<-pager'''
+    expected_pipeline = ['Result processors profile', root, sorter(paged), pager]
+    res = conn.execute_command(*ft_profile_aggregate_cmd, search_UNF_sortable, *sortby_UNF_sortable_a, 'LIMIT', 1, 1)
+    env.assertEqual(get_pipeline(res), expected_pipeline)
+
+    '''case 5: with pager, with loader (sortby a (SORTABLE UNF) LIMIT 1 1 loadall)
+        expected pipeline: root<-sorter<-pager<-buffer-locker<-loader<-unlocker'''
+    expected_pipeline = ['Result processors profile', root, sorter(paged), pager, buffer_locker(paged), loader(paged), unlocker(paged)]
+    res = conn.execute_command(*ft_profile_aggregate_cmd, search_UNF_sortable, *sortby_UNF_sortable_a, 'LIMIT', 1, 1, 'LOAD', '*')
+    env.assertEqual(get_pipeline(res), expected_pipeline)
+
+    '''case 6: with loader and applier (sortby a (SORTABLE UNF) LOAD 1 @n apply @n^2)
+        expected pipeline: root<-sorter<-buffer-locker<-loader<-apply<-unlocker'''
+    expected_pipeline = ['Result processors profile', root, sorter(), buffer_locker(), loader(), apply(), unlocker()]
+    res = conn.execute_command(*ft_profile_aggregate_cmd, search_UNF_sortable, *sortby_UNF_sortable_a, 'LOAD', 1, nonsortable_field_name, 'APPLY', '@' + nonsortable_field_name + '^2')
+    env.assertEqual(get_pipeline(res), expected_pipeline)
+
+    '''case 7: with applier, sorter and loader (sortby n apply @n^2)
+        expected pipeline: root<-buffer-locker<-loader<-unlocker<-sorter<-apply'''
+    expected_pipeline = ['Result processors profile', root, buffer_locker(), loader(), unlocker(), sorter(), apply()]
+    res = conn.execute_command(*ft_profile_aggregate_cmd, search_UNF_sortable, *sortby_not_sortable_a, 'APPLY', '@' + nonsortable_field_name + '^2')
+    env.assertEqual(get_pipeline(res), expected_pipeline)
+
+    ############################### Sorter WITH access to redis: ###############################
+
+    ''' case 1: no loader
+        expected pipeline: root<-buffer-locker<-loader<-unlocker<-sorter '''
+    expected_pipeline = ['Result processors profile', root, buffer_locker(), loader(), unlocker(), sorter()]
+    #sortby NOT SORTABLE NOCONTENT
+    res = conn.execute_command(*ft_profile_aggregate_cmd, search_UNF_sortable, *sortby_not_sortable_a, 'NOCONTENT')
+    env.assertEqual(get_pipeline(res), expected_pipeline)
+
+    #sortby a (NOTSORTABLE) return a
+    res = conn.execute_command(*ft_profile_aggregate_cmd, search_UNF_sortable, *sortby_not_sortable_a, 'LOAD', 1, nonsortable_field_name)
+    env.assertEqual(get_pipeline(res), expected_pipeline)
+
+    ''' case 2: with loader
+        expected pipeline: root<-buffer-locker<-loader<-unlocker<-sorter'''
+    expected_pipeline = ['Result processors profile', root, buffer_locker(), loader(), unlocker(), sorter()]
+    #sortby NOT SORTABLE
+    res = conn.execute_command(*ft_profile_aggregate_cmd, search_UNF_sortable, *sortby_not_sortable_a)
+    env.assertEqual(get_pipeline(res), expected_pipeline)
+
+    ''' case 2.1: with loader
+        expected pipeline: root<-buffer-locker<-loader<-sorter<-loader<-unlocker'''
+    expected_pipeline = ['Result processors profile', root, buffer_locker(), loader(), sorter(), loader(), unlocker()]
+    #sortby NOT SORTABLE loadall
+    res = conn.execute_command(*ft_profile_aggregate_cmd, search_UNF_sortable, *sortby_not_sortable_a, 'LOAD', '*')
+    env.assertEqual(get_pipeline(res), expected_pipeline)
+
+    # sortby NOTSORTABLE a return b
+    res = conn.execute_command(*ft_profile_aggregate_cmd, search_UNF_sortable, *sortby_not_sortable_a, 'LOAD', 1, notindexed_field_name)
+    env.assertEqual(get_pipeline(res), expected_pipeline)
+
+    '''case 3: with pager, no loader
+        expected pipeline: root<-buffer-locker<-loader<-unlocker<-sorter<-pager'''
+    expected_pipeline = ['Result processors profile', root, buffer_locker(), loader(), unlocker(), sorter(paged), pager]
+    #(sortby NOTSORTABLE LIMIT 1 1 NOCONTENT)
+    res = conn.execute_command(*ft_profile_aggregate_cmd, search_UNF_sortable, *sortby_not_sortable_a, 'LIMIT', 1, 1, 'NOCONTENT')
+    env.assertEqual(get_pipeline(res), expected_pipeline)
+
+    '''case 4: with pager, with loader
+        expected pipeline: root<-buffer-locker<-loader<-unlocker<-sorter<-pager '''
+    expected_pipeline = ['Result processors profile', root, buffer_locker(), loader(), unlocker(), sorter(paged), pager]
+    # (sortby NOTSORTABLE LIMIT 1 1)
+    res = conn.execute_command(*ft_profile_aggregate_cmd, search_UNF_sortable, *sortby_not_sortable_a, 'LIMIT', 1, 1)
+    env.assertEqual(get_pipeline(res), expected_pipeline)
+
+    '''case 4.1: with pager, with loader
+        expected pipeline: root<-buffer-locker<-loader<-sorter<-pager<-loader<-unlocker '''
+    expected_pipeline = ['Result processors profile', root, buffer_locker(), loader(), sorter(paged), pager, loader(paged), unlocker(paged)]
+    # (sortby NOTSORTABLE LIMIT 1 1 loadall)
+    res = conn.execute_command(*ft_profile_aggregate_cmd, search_UNF_sortable, *sortby_not_sortable_a, 'LIMIT', 1, 1, 'LOAD', '*')
     env.assertEqual(get_pipeline(res), expected_pipeline)
 
     ############################### Additional rp ###############################
@@ -214,7 +342,7 @@ def test_pipeline(env):
     ''' case 1: highlighter (last to access redis is not endProc)
         expected pipeline: root<-sorter<-buffer-locker<-loader<-unlocker<-highlighter '''
     expected_pipeline = ['Result processors profile', root, sorter(), buffer_locker(), loader(), unlocker(), highlighter]
-    res = conn.execute_command(*ft_profile_cmd, '*', *sortby_UNF_sortable, 'HIGHLIGHT')
+    res = conn.execute_command(*ft_profile_search_cmd, '*', *sortby_UNF_sortable_s, 'HIGHLIGHT')
     env.assertEqual(get_pipeline(res), expected_pipeline)
 
 
@@ -231,7 +359,7 @@ def test_pipeline(env):
 
     metric = ['Type', 'Metrics Applier', 'Counter', docs_count]
 
-    res = conn.execute_command(*ft_profile_cmd, '*=>[KNN 3 @v $vec]',
+    res = conn.execute_command(*ft_profile_search_cmd, '*=>[KNN 3 @v $vec]',
                                'SORTBY', '__v_score', 'PARAMS', '2', 'vec', 'aaaaaaaa')
 
     expected_pipeline = ['Result processors profile', root, metric, sorter(), buffer_locker(), loader(), unlocker()]
