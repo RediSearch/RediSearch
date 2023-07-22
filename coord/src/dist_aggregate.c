915632f07768bd84a7db7993afedab2a62c16507
oshadmi <omer.shadmi@redislabs.com>
2023-07-23 00:03:23 +0300

    Init sctx for AGGREGATE to avoid crash

--- coord/src/dist_aggregate.c
+++ coord/src/dist_aggregate.c
@@ -614,15 +614,17 @@ void RSExecDistAggregate(RedisModuleCtx *ctx, RedisModuleString **argv, int argc
 
   if (IsProfile(r)) r->parseTime = clock() - r->initClock;
 
+  // With cursor, we rely on the existing mechanism of AREQ to free the ctx object when the cursor is exhausted
+  r->sctx = rm_new(RedisSearchCtx);
+  *r->sctx = SEARCH_CTX_STATIC(ctx, NULL);
+  r->sctx->apiVersion = dialect;
+  // Value of r->sctx->expanded will be prcossed from shards reply
+
   if (r->reqflags & QEXEC_F_IS_CURSOR) {
     // Keep the original concurrent context
     ConcurrentCmdCtx_KeepRedisCtx(cmdCtx);
 
-    // We rely on the existing mechanism of AREQ to free the ctx object when the cursor is exhausted
-    r->sctx = rm_new(RedisSearchCtx);
-    *r->sctx = SEARCH_CTX_STATIC(ctx, NULL);
     StrongRef dummy_spec_ref = {.rm = NULL};
-
     rc = AREQ_StartCursor(r, reply, dummy_spec_ref, &status);
 
     if (rc != REDISMODULE_OK) {
