9db441d38369ce7c47ef2652a885622d9f1a07b1
oshadmi <omer.shadmi@redislabs.com>
2023-07-24 20:04:27 +0300

    Avoid bad fix

--- src/aggregate/aggregate_request.c
+++ src/aggregate/aggregate_request.c
@@ -190,7 +190,6 @@ int parseValueFormat(uint32_t *flags, ArgsCursor *ac, QueryError *status) {
 }
  
 int SetValueFormat(bool is_resp3, bool is_json, uint32_t *flags, QueryError *status) {
-  *flags &= ~QEXEC_FORMAT_EXPAND;
   if (*flags & QEXEC_FORMAT_DEFAULT) {
     if (is_json && is_resp3) {
       *flags |= QEXEC_FORMAT_EXPAND;
