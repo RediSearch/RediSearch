94353dba2db2e70066b4a6bdec0c228f96d5f4d8
GuyAv46 <guy.avimor@gmail.com>
2023-08-28 18:18:50 +0300

    attempt

--- .install/ubuntu_22.04.sh
+++ .install/ubuntu_22.04.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 export DEBIAN_FRONTEND=noninteractive
-apt-get update -qq
-apt-get install -yqq git wget build-essential valgrind lcov
+apt update -qq
+apt install -yqq git wget build-essential valgrind lcov
 source install_cmake.sh
