59caec088f1f51f4af4591d8069de0f3803b6d2f
GuyAv46 <guy.avimor@gmail.com>
2023-09-04 13:17:22 +0300

    some updates

--- .install/ubuntu_20.04.sh
+++ .install/ubuntu_20.04.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 export DEBIAN_FRONTEND=noninteractive
-apt-get update -qq
-apt-get install -yqq wget make clang-format gcc valgrind python3-pip lcov git
+apt update -qq
+apt install -yqq wget make clang-format gcc valgrind python3-pip lcov git
 source install_cmake.sh
