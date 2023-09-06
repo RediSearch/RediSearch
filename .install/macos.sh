c7748e521dd7e268c8547007469b4af83cf17ae3
GuyAv46 <guy.avimor@gmail.com>
2023-09-06 15:14:31 +0300

    a

--- .install/macos.sh
+++ .install/macos.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 brew update
-brew install make build-essential g++ python3-dev autotools-dev libicu-dev libbz2-dev libboost-all-dev
+brew install make
+brew install build-essential
+brew install g++
+brew install python3-dev
+brew install autotools-dev
+brew install libicu-dev
+brew install libbz2-dev
+brew install libboost-all-dev
 ADD_GNUBIN_TO_PATH='export PATH="/usr/local/opt/make/libexec/gnubin:$PATH"'
 echo $ADD_GNUBIN_TO_PATH >> ~/.bashrc
 echo $ADD_GNUBIN_TO_PATH >> ~/.zshrc
