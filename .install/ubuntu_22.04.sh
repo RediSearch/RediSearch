f53361e56096057617f8dff769ea5c3fb32ba271
GuyAv46 <guy.avimor@gmail.com>
2023-09-06 16:22:47 +0300

    a

--- .install/ubuntu_22.04.sh
+++ .install/ubuntu_22.04.sh
@@ -2,5 +2,5 @@
 set -e
 export DEBIAN_FRONTEND=noninteractive
 apt update -qq
-apt install -yqq git wget build-essential lcov
+apt install -yqq git wget build-essential lcov clang
 source install_cmake.sh
