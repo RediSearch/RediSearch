b40defce336fb8140c039c8e2efc5a8544c6a90d
raz-mon <razmonsonego2@gmail.com>
2023-09-07 12:13:52 +0300

    add necessary case

--- src/json.c
+++ src/json.c
@@ -561,7 +561,6 @@ int JSON_LoadDocumentField(JSONResultsIterator jsonIter, size_t len,
     if (jsonIterToValue(ctx, jsonIter, APIVERSION_RETURN_MULTI_CMP_FIRST, &rsv) == REDISMODULE_OK) {
       df->multisv = rsv;
     } else {
-      japi->freeIter(jsonIter);
       rv = REDISMODULE_ERR;
     }
   } else if (rv == REDISMODULE_OK) {
