c3b06c967b7645be3f859d911f33041787c6056d
nafraf <nafraf@gmail.com>
2023-10-09 16:14:40 -0500

    print file permissions

--- .install/mariner2.sh
+++ .install/mariner2.sh
@@ -5,10 +5,13 @@ tdnf install -q -y build-essential git wget ca-certificates tar openssl-devel \
 
 pip install -q --upgrade setuptools
 pip install -q --upgrade pip
+pwd
+ls -alh tests/pytest/requirements.txt
+whoami
 pip install -q -r tests/pytest/requirements.txt
 
 # These packages are needed to build the package
-pip install -q addict toml jinja2 ramp-packer conan
+pip install -q addict toml jinja2 ramp-packer
 
 # List installed packages
 pip list
