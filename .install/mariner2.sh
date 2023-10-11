f3d58a6a8df6f27aa19659060b6ae729d52ce54c
nafraf <nafraf@gmail.com>
2023-10-11 09:24:59 -0500

    Install rsync

--- .install/mariner2.sh
+++ .install/mariner2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 tdnf install -q -y build-essential git wget ca-certificates tar openssl-devel \
-    cmake python3 python3-pip which unzip
+    cmake python3 python3-pip which unzip rsync
 git config --global --add safe.directory $PWD
 
 pip install -q --upgrade setuptools
