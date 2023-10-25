cdf77c4dfc39bc597daa3224e895b67da2d162e8
GuyAv46 <guy.avimor@gmail.com>
2023-10-25 14:06:21 +0300

    wip

--- .install/centos_linux_7.sh
+++ .install/centos_linux_7.sh
@@ -2,5 +2,8 @@
 MODE=$1 # whether to install using sudo or not
 set -e
 export DEBIAN_FRONTEND=noninteractive
-$MODE yum install -y "Development Tools" wget git
+ARCH=$(uname -m)
+$MODE yum install -y https://packages.endpointdev.com/rhel/7/os/${ARCH}/endpoint-repo.${ARCH}.rpm
+$MODE yum groupinstall -y "Development Tools"
+$MODE yum install -y wget git
 source install_cmake.sh $MODE
