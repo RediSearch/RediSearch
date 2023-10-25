e92c0a1f54fb807e69f026a69073779fdf456ac7
GuyAv46 <guy.avimor@gmail.com>
2023-10-25 15:38:25 +0300

    bullseye script

---
+++ .install/debian_gnu_linux_11.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+export DEBIAN_FRONTEND=noninteractive
+MODE=$1 # whether to install using sudo or not
+
+$MODE apt update -qq
+$MODE apt install -yqq git wget build-essential lcov
+source install_cmake.sh $MODE
