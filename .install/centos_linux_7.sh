1c8c1ae7ed446abfe807cf2dd1f8203226755418
GuyAv46 <guy.avimor@gmail.com>
2023-10-25 13:54:59 +0300

    fixes

--- .install/centos_linux_7.sh
+++ .install/centos_linux_7.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 MODE=$1 # whether to install using sudo or not
 set -e
+export DEBIAN_FRONTEND=noninteractive
 $MODE yum install -y "Development Tools" wget git
-source /opt/rh/gcc-toolset-10/enable
 source install_cmake.sh $MODE
