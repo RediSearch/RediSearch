2d36bca5fcd6d3375843ab25cd57c393a794af9d
GuyAv46 <guy.avimor@gmail.com>
2023-10-29 15:07:25 +0200

    amazon linux gcc 10 install

--- .install/amazon_linux_2.sh
+++ .install/amazon_linux_2.sh
@@ -5,5 +5,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 $MODE yum update -y
 $MODE yum groupinstall -y "Development Tools"
-$MODE yum install -y wget git openssl-devel openssl
+$MODE yum remove -y gcc # remove gcc 7
+$MODE yum install -y wget git openssl-devel openssl gcc10 gcc10-c++
+$MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc10-gcc 60 --slave /usr/bin/g++ g++ /usr/bin/gcc10-g++
 source install_cmake.sh $MODE
