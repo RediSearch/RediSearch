984610299f54db8e1d9d7dd69262467530d78fb8
GuyAv46 <guy.avimor@gmail.com>
2023-10-29 19:44:37 +0200

    BUILD_TLS=yes

--- .install/macos.sh
+++ .install/macos.sh
@@ -2,7 +2,4 @@
 export HOMEBREW_NO_AUTO_UPDATE=1
 brew update
 brew install make openssl
-# For compilers to find openssl@1.1 you may need to set:
-# export LDFLAGS="-L/usr/local/opt/openssl@1.1/lib"
-# export CPPFLAGS="-I/usr/local/opt/openssl@1.1/include"
 source install_cmake.sh
