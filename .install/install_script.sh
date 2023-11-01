96d83cc4df4c4c85fdbf8b01b4d3f323c9168862
GuyAv46 <47632673+GuyAv46@users.noreply.github.com>
2023-11-01 15:28:54 +0200

    Refactor dependencies - take 2 (#3984)
    * renaming flows
    
    * rename file
    
    * some cleanup
    
    * attempt
    
    * attempt
    
    * workaround of current limitation
    
    * revert some unnecessary changes
    
    * another attempt
    
    * disable M1
    
    * attempt
    
    * attempt
    
    * attempt
    
    * minor fix
    
    * attempt to fix 2
    
    * fix attempt 3
    
    * fix attempt 4
    
    * setup changes
    
    * wip
    
    * disable arm until we have credentials
    
    * disabled some of the configuration until setup is successful
    
    * wip
    
    * test
    
    * wip
    
    * fix test
    
    * fix test 2
    
    * test fix
    
    * fix test 4
    
    * fix test 5
    
    * fix test 6
    
    * test fix 7
    
    * fix test? 8
    
    * fix test 9
    
    * fix test 10
    
    * fix test 11
    
    * fix test 12
    
    * test
    
    * test
    
    * fix test 1
    
    * test
    
    * fixes and remove temp flow
    
    * disabled again some configuration (temp)
    
    * fix amazonlinux
    
    * macos cleanup
    
    * cleanup
    
    * test
    
    * fix 1
    
    * fix 2
    
    * fix 2
    
    * fix 4
    
    * fix 5
    
    * test
    
    * fix test 1
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * fix?
    
    * fix? 2
    
    * test
    
    * test
    
    * test fix
    
    * test fix 2
    
    * test
    
    * test
    
    * test
    
    * fix test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * fix
    
    * improve text
    
    * test
    
    * debug job
    
    * more debug
    
    * more debug log
    
    * fix mac and attempt for linux
    
    * fixes
    
    * remove temp flow
    
    * cleanup
    
    * temporary print system info
    
    * added focal and lots of empty scripts
    
    * implement some scripts
    
    * some changes
    
    * wip scripts
    
    * fixes
    
    * wip
    
    * wip
    
    * amazon linux script
    
    * wip
    
    * wip
    
    * alt
    
    * get conan
    
    * no conan yet
    
    * attempt
    
    * cleanup
    
    * readies setup
    
    * rename AWS secrets
    
    * uncomment arm flow
    
    * bullseye script
    
    * replace GH token
    
    * fix arch checking
    
    * update secret names
    
    * get gcc 10 for ubuntu 18.04
    
    * minor fixes and improvements for common-flow
    
    * macos improvement (to be verified)
    
    * openssl dependencies install (tls)
    
    * amazon linux gcc 10 install
    
    * attempt to fix centos7 on ARM
    
    * split platforms to ones that supports x86 and arm
    
    * disable passing configs
    
    * disable M1 (requires enterprise account)
    
    * BUILD_TLS=yes
    
    * coverage flow refresh
    
    * cleanups
    
    * attempt to fail san again
    
    * simplified error checking
    
    * made prettier
    
    * remove unnecessary parameter
    
    * update template secret names
    
    * attempt to unify common flows
    
    * temporarily run merge flow
    
    * try nothing value
    
    * reverted temporary changes
    
    * renaming arm64 to aarch64
    
    * minor python script modifications
    
    * testing "set-safe-directory"
    
    * remove unnecessary "set-safe-directory"

---
+++ .install/install_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+OS_TYPE=$(uname -s)
+MODE=$1 # whether to install using sudo or not
+
+if [[ $OS_TYPE = 'Darwin' ]]
+then
+    OS='macos'
+else
+    VERSION=$(grep '^VERSION_ID=' /etc/os-release | sed 's/"//g')
+    VERSION=${VERSION#"VERSION_ID="}
+    OS_NAME=$(grep '^NAME=' /etc/os-release | sed 's/"//g')
+    OS_NAME=${OS_NAME#"NAME="}
+    OS=${OS_NAME,,}_${VERSION}
+    OS=$(echo $OS | sed 's/[/ ]/_/g') # replace spaces and slashes with underscores
+fi
+echo $OS
+
+source ${OS}.sh $MODE
