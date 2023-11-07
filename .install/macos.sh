72040f2db204856494c78238f23ebd2e455f74bd
GuyAv46 <guy.avimor@gmail.com>
2023-11-07 16:22:46 +0200

    wip

--- .install/macos.sh
+++ .install/macos.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 export HOMEBREW_NO_AUTO_UPDATE=1
 brew update
-brew_install() {
-  if brew ls --versions "$1" >/dev/null; then
-    echo "Package '$1' is already installed"
-  else
-    HOMEBREW_NO_AUTO_UPDATE=1 brew install "$1"
-  fi
-}
-brew_install make
-brew_install openssl
-brew_install python3
-brew_install cmake
+brew install make openssl python3
+source install_cmake.sh
 
 GNUBINS=$(find -L /usr/local/opt -type d -name gnubin | tr ' ' ':')
+echo "path:"
+echo $PATH
+echo "gnubins:"
+echo $GNUBINS
 echo "export PATH=$GNUBINS:$PATH" >> ~/.bashrc
 echo "export PATH=$GNUBINS:$PATH" >> ~/.zshrc
