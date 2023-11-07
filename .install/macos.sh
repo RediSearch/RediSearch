d32ef796ca765e73a0bbc129521c2920b10c215b
GuyAv46 <guy.avimor@gmail.com>
2023-11-07 16:17:02 +0200

    attempt to fix

--- .install/macos.sh
+++ .install/macos.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 export HOMEBREW_NO_AUTO_UPDATE=1
 brew update
-brew install make openssl python3
-source install_cmake.sh
+brew_install() {
+  if brew ls --versions "$1" >/dev/null; then
+    echo "Package '$1' is already installed"
+  else
+    HOMEBREW_NO_AUTO_UPDATE=1 brew install "$1"
+  fi
+}
+brew_install make
+brew_install openssl
+brew_install python3
+brew_install cmake
+
+GNUBINS=$(find -L /usr/local/opt -type d -name gnubin | tr ' ' ':')
+echo "export PATH=$GNUBINS:$PATH" >> ~/.bashrc
+echo "export PATH=$GNUBINS:$PATH" >> ~/.zshrc
