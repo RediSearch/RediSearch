ef04fd4d935fb361ee9a9e34fa40de8f8ded91ca
GuyAv46 <47632673+GuyAv46@users.noreply.github.com>
2023-11-07 18:34:37 +0200

    Fix merge queue validation 2 (#4024)
    * test1
    
    * test2
    
    * test3
    
    * test4
    
    * test5
    
    * test6
    
    * test7
    
    * test8
    
    * modified PR validation to never skip
    
    * attempt to support some docs only PRs
    
    * wip
    
    * wip
    
    * replace `always()` with `!cancelled()`
    
    * test cancelling
    
    * fix
    
    * test cancel 2
    
    * attempt to fix macos
    
    * test
    
    * attempt to fix
    
    * wip
    
    * alternative
    
    * refresh
    
    * last try
    
    * reverts
    
    * fix macos script
    
    * remove python3 for now
    
    * reverts and disable test

--- .install/macos.sh
+++ .install/macos.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 export HOMEBREW_NO_AUTO_UPDATE=1
+GNUBIN=$(brew --prefix)/opt/make/libexec/gnubin
+
 brew update
-brew install make openssl
+brew install make
+
+echo "export PATH=$GNUBIN:$PATH" >> ~/.bashrc
+echo "export PATH=$GNUBIN:$PATH" >> ~/.zshrc
+
+brew install openssl
 source install_cmake.sh
