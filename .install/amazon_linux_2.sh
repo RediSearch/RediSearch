b5963e17f1d3c4f2e2cdddeb15bd3f678ce4ff5f
Meir Shpilraien <meir@redis.com>
2023-11-21 08:29:14 +0200

    MOD-6012 use openssl11 on Amazon Linux 2
    (cherry picked from commit 23460cfde5a6c58fb3763ebe680ddca9ea75e6f7)

--- .install/amazon_linux_2.sh
+++ .install/amazon_linux_2.sh
@@ -6,6 +6,10 @@ export DEBIAN_FRONTEND=noninteractive
 $MODE yum update -y
 $MODE yum groupinstall -y "Development Tools"
 $MODE yum remove -y gcc # remove gcc 7
-$MODE yum install -y wget git openssl-devel openssl gcc10 gcc10-c++
+$MODE yum install -y wget git openssl11 openssl11-devel gcc10 gcc10-c++
 $MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc10-gcc 60 --slave /usr/bin/g++ g++ /usr/bin/gcc10-g++
+# make openssl11 the default openssl devel package
+$MODE ln -s /usr/lib64/pkgconfig/libssl11.pc /usr/lib64/pkgconfig/libssl.pc
+$MODE ln -s /usr/lib64/pkgconfig/libcrypto11.pc /usr/lib64/pkgconfig/libcrypto.pc
+$MODE ln -s /usr/lib64/pkgconfig/openssl11.pc /usr/lib64/pkgconfig/openssl.pc
 source install_cmake.sh $MODE
