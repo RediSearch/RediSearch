f2c4acc7e397fadf52d2d97d981fb0c3d9d89aba
Meir Shpilraien <meir@redis.com>
2023-11-22 09:30:30 +0200

    Fix setup steps
    (cherry picked from commit a05582172b3080353d7865e72e7bc2f4dd816af2)

--- .install/amazon_linux_2.sh
+++ .install/amazon_linux_2.sh
@@ -6,10 +6,15 @@ export DEBIAN_FRONTEND=noninteractive
 $MODE yum update -y
 $MODE yum groupinstall -y "Development Tools"
 $MODE yum remove -y gcc # remove gcc 7
-$MODE yum install -y wget git openssl11 openssl11-devel gcc10 gcc10-c++
+$MODE yum install -y wget git openssl openssl-devel gcc10 gcc10-c++
 $MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc10-gcc 60 --slave /usr/bin/g++ g++ /usr/bin/gcc10-g++
-# make openssl11 the default openssl devel package
-$MODE ln -s /usr/lib64/pkgconfig/libssl11.pc /usr/lib64/pkgconfig/libssl.pc
-$MODE ln -s /usr/lib64/pkgconfig/libcrypto11.pc /usr/lib64/pkgconfig/libcrypto.pc
-$MODE ln -s /usr/lib64/pkgconfig/openssl11.pc /usr/lib64/pkgconfig/openssl.pc
+# Install 'openss11' and make it the default so we will use it when linking.
+# Currently it is commented out and handled on 'sbin/setup'. This is because
+# 'sbin/setup' installs 'openssl-devel' which conflicts with 'openssl11-devel'.
+# So by if we install 'openssl11-devel' here we will cause 'sbin/setup' to fail.
+# to avoid 
+#$MODE yum install -y openssl11 openssl11-devel
+#$MODE ln -s /usr/lib64/pkgconfig/libssl11.pc /usr/lib64/pkgconfig/libssl.pc
+#$MODE ln -s /usr/lib64/pkgconfig/libcrypto11.pc /usr/lib64/pkgconfig/libcrypto.pc
+#$MODE ln -s /usr/lib64/pkgconfig/openssl11.pc /usr/lib64/pkgconfig/openssl.pc
 source install_cmake.sh $MODE
