71fc0a0ada9dfd18cfffa88e4db7aa35950e8bd8
Ephraim Feldblum <ephraim.feldblum@redis.com>
2023-11-27 03:46:52 +0200

    finally managed to remove code duplication. unfortunately required a macro. still worth it

--- src/geometry/rtree.hpp
+++ src/geometry/rtree.hpp
@@ -80,8 +80,8 @@ class RTree {
 
   template <typename Predicate, typename Filter>
   [[nodiscard]] auto apply_predicate(Predicate&& p, Filter&& f) const -> query_results;
-  [[nodiscard]] auto generate_predicate(doc_type const& query_doc, QueryType query_type,
-                                        geom_type const& query_geom) const -> query_results;
+  [[nodiscard]] auto generate_predicate(QueryType query_type, geom_type const& query_geom) const
+      -> query_results;
 };
 
 }  // namespace GeoShape
