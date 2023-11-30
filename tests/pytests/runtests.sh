67a0dda862d0ce03067a4c01343432dd1c418d0a
GuyAv46 <guy.avimor@gmail.com>
2023-11-30 09:31:15 +0200

    fixing test flow

--- tests/pytests/runtests.sh
+++ tests/pytests/runtests.sh
@@ -152,7 +152,7 @@ setup_rltest() {
 		fi
 	fi
 
-	RLTEST_ARGS+=" --enable-debug-command"
+	RLTEST_ARGS+=" --allow-unsafe"  # allow redis use debug and module command and change protected configs
 
 	LOG_LEVEL=${LOG_LEVEL:-debug}
 	RLTEST_ARGS+=" --log-level $LOG_LEVEL"
