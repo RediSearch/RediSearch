0e114a01338f866d5517a7159ef8d63fbc7ecc8a
GuyAv46 <guy.avimor@gmail.com>
2023-12-06 09:11:46 +0200

    another attempt

--- .install/install_boost.sh
+++ .install/install_boost.sh
@@ -6,10 +6,14 @@ MODE=$2 # whether to install using sudo or not
 
 BOOST_DIR="boost_${VERSION//./_}"
 
-wget https://boostorg.jfrog.io/artifactory/main/release/${VERSION}/source/${BOOST_DIR}.tar.gz
+# Download and extract boost if not found (cached)
+if [[ ! -d ${BOOST_DIR} ]]; then
+    wget https://boostorg.jfrog.io/artifactory/main/release/${VERSION}/source/${BOOST_DIR}.tar.gz
+    tar -xzf ${BOOST_DIR}.tar.gz
+fi
 
-tar -xzf ${BOOST_DIR}.tar.gz
 cd ${BOOST_DIR}
 
+# Build and install boost. Should be fast if cached
 ./bootstrap.sh --prefix=/usr
 $MODE ./b2 install
