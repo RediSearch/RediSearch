aaeb346021db764cb7317b904bf86efa7040bd9f
alonre24 <alonreshef24@gmail.com>
2023-12-07 12:17:44 +0200

    Fix flaky test_mod5778_add_new_shard_to_cluster - take2  (#4168)
    * Test that topology updated in new shard, allow it to happen within 10 secs and extend timeout message
    
    * Update tests/pytests/test_issues.py
    
    Co-authored-by: meiravgri <109056284+meiravgri@users.noreply.github.com>
    
    * Update tests/pytests/test_issues.py
    
    Co-authored-by: meiravgri <109056284+meiravgri@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: meiravgri <109056284+meiravgri@users.noreply.github.com>

--- tests/pytests/test_issues.py
+++ tests/pytests/test_issues.py
@@ -882,8 +882,12 @@ def test_mod5778_add_new_shard_to_cluster(env):
     env.assertEqual(shard_with_slot_0, expected)
 
     expected = {'primary': ('127.0.0.1', new_instance_port), 'replicas': []}  # the expected reply from cluster_slots()
-    res = new_instance_conn.cluster_slots(cluster.ClusterNode('127.0.0.1', new_instance_port))
-    env.assertEqual(len(res), len(env.envRunner.shards) + 1)
+    with TimeLimit(10, 'waiting for new shard to acknowledge the topology change'):
+        # Wait until the new instance node is updated that the slot had moved
+        while True:
+            res = new_instance_conn.cluster_slots(cluster.ClusterNode('127.0.0.1', new_instance_port))
+            if len(res) == len(env.envRunner.shards) + 1:
+                break
     env.assertEqual(res[(0, 0)], expected)
 
     # cleanup
