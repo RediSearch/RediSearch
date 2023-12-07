8402c0c15eb62822432a55912516ed87d27e45f1
alonre24 <alonreshef24@gmail.com>
2023-12-07 10:52:55 +0200

    Update tests/pytests/test_issues.py
    Co-authored-by: meiravgri <109056284+meiravgri@users.noreply.github.com>
    (cherry picked from commit 8e2fd362b5125e1e488b858334f44441d4b638d4)

--- tests/pytests/test_issues.py
+++ tests/pytests/test_issues.py
@@ -882,7 +882,6 @@ def test_mod5778_add_new_shard_to_cluster(env):
     env.assertEqual(shard_with_slot_0, expected)
 
     expected = {'primary': ('127.0.0.1', new_instance_port), 'replicas': []}  # the expected reply from cluster_slots()
-    res = new_instance_conn.cluster_slots(cluster.ClusterNode('127.0.0.1', new_instance_port))
     with TimeLimit(10, 'waiting for new shard to acknowledge the topology change'):
         # Wait until the new instance node is updated that the slot had moved
         while True:
