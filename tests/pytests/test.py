06c0b250211f027004bcdb42a9ddd6d082c30c98
Raz Monsonego <74051729+raz-mon@users.noreply.github.com>
2023-12-12 19:40:02 +0200

    MOD-6184: Return timeout errors from `FT.PROFILE` (#4165) (#4187)
    * wip
    
    * fix test
    
    * Add a timeout section to profile response
    
    * fix tests to take the Timed-out value in FT.PROFILE into account
    
    * expect string instead of value
    
    * refactor Timed-out key to Warning, and do not fail on timeout, no matter the timeout policy
    
    * remove comment
    
    * change condition order
    
    * add test for coordinator
    
    * fix skip test on standalone
    
    * skip on non-cluster
    
    * fix
    
    * small fixes
    
    * fix test name
    
    * add comment

--- tests/pytests/test.py
+++ tests/pytests/test.py
@@ -3671,9 +3671,9 @@ def test_mod_4200(env):
 def test_RED_86036(env):
     env.cmd('FT.CREATE', 'idx', 'SCHEMA', 't', 'TEXT')
     for i in range(1000):
-        env.execute_command('hset', 'doc%d' % i, 't', 'foo')
-    res = env.execute_command('FT.PROFILE', 'idx', 'search', 'query', '*', 'INKEYS', '2', 'doc0', 'doc999')
-    res = res[1][3][1][7] # get the list iterator profile
+        env.cmd('hset', 'doc%d' % i, 't', 'foo')
+    res = env.cmd('FT.PROFILE', 'idx', 'search', 'query', '*', 'INKEYS', '2', 'doc0', 'doc999')
+    res = res[1][4][1][7] # get the list iterator profile
     env.assertEqual(res[1], 'ID-LIST')
     env.assertLess(res[5], 3)
 
