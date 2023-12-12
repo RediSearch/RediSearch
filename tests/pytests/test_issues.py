d22ecf90ec3159394ddcacf699d2cd204f2de204
alon <alonreshef24@gmail.com>
2023-12-12 18:59:20 +0200

    Small fix
    (cherry picked from commit 848089811f1261b809c147fcfc659363ad486e46)

--- tests/pytests/test_issues.py
+++ tests/pytests/test_issues.py
@@ -899,7 +899,7 @@ def mod5778_add_new_shard_to_cluster(env: Env):
                 new_instance_conn = RedisCluster(**kwargs)
                 break
             except exceptions.RedisClusterException and IndexError:
-                pass
+                pass  # these two exceptions indicate that the new shard still waking up
     new_instance_conn = RedisCluster(**kwargs)
     env.assertTrue(new_instance_conn.ping()) # make sure the new instance is alive
 
