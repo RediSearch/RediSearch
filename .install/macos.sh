43689d616536cab227d6234faa90668ca4f80414
meiravgri <meirav.grimberg@redis.com>
2023-12-13 15:39:51 +0000

    skip readies with macos12
    cleanup macos.sh
    
    (cherry picked from commit 9d6526dd8a32bf3343f69f802239f5aab70a6479)

--- .install/macos.sh
+++ .install/macos.sh
@@ -3,13 +3,11 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 GNUBIN=$(brew --prefix)/opt/make/libexec/gnubin
 LLVM=/usr/local/opt/llvm@16/bin
 
-
 brew update
 brew install make
 brew install llvm@16
 
 echo "export PATH=$LLVM:$GNUBIN:$PATH" >> ~/.bashrc
-#echo "export PATH=$LLVM:$GNUBIN:$PATH" >> ~/.bash_profile
 echo "export PATH=$LLVM:$GNUBIN:$PATH" >> ~/.zshrc
 
 brew install openssl
@@ -17,4 +15,3 @@ source install_cmake.sh
 
 pip install -q -r ../tests/pytests/requirements.txt
 pip install -q -r ../tests/pytests/requirements.macos.txt
-#echo 'export PATH="/usr/local/opt/llvm@16/bin:$PATH"' >> /Users/runner/.bash_profile
