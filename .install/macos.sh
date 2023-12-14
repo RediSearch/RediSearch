b4b1956da62d29199a708c7c06f6e0fad1a69417
meiravgri <meirav.grimberg@redis.com>
2023-12-14 13:42:21 +0000

    install coreutils on mac
    (cherry picked from commit 4549ee426a7ce2180292a5daf88946c5753b1cb8)

--- .install/macos.sh
+++ .install/macos.sh
@@ -3,10 +3,12 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 BREW_PREFIX=$(brew --prefix)
 GNUBIN=$BREW_PREFIX/opt/make/libexec/gnubin
 LLVM=$BREW_PREFIX/opt/llvm@16/bin
+COREUTILS=$BREW_PREFIX/opt/coreutils/libexec/gnubin
 
 brew update
 brew install make
 brew install llvm@16
+brew install coreutils
 
 echo "export PATH=$LLVM:$GNUBIN:$PATH" >> ~/.bashrc
 echo "export PATH=$LLVM:$GNUBIN:$PATH" >> ~/.zshrc
