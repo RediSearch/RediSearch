b759003445decf88adefd14c1fb100cf529d9f16
GuyAv46 <47632673+GuyAv46@users.noreply.github.com>
2023-12-14 18:22:23 +0200

    [2.6] MOD-6273 Remove readies from macos setup (#4206)
    * try reomve readies
    
    (cherry picked from commit d2f73289e7c9708b49314c4736c15c39b5d08edf)
    
    * install req mac
    
    (cherry picked from commit 9a63138a7e0decf910e19947eea100ca6885d767)
    
    * install python 3.10
    
    (cherry picked from commit 1b237349416154e52d1beddc797e1a49b1c20c12)
    
    * fix
    
    (cherry picked from commit 1b3c89e4af8d167a22596124db164415de63f0ed)
    
    * fix
    
    (cherry picked from commit a1794c543f0bd7d9fa6171416430ecc70d184257)
    
    * manually install llvm16
    
    (cherry picked from commit f52d9538ad9d909622257672f032217ad25c1d63)
    
    * try export to bash_profile
    
    (cherry picked from commit 8a20637342210cf283a3f83b54b5e3a0b526f78c)
    
    * remove bash_profile
    change LLVM to the recommanded
    
    (cherry picked from commit c5085255e94de80d3f977f0b3aca74e69b009473)
    
    * skip readies with macos12
    
    cleanup macos.sh
    
    (cherry picked from commit 9d6526dd8a32bf3343f69f802239f5aab70a6479)
    
    * run everything in PR
    run coordinator on temp
    
    (cherry picked from commit e2d7c9407739439c4b36cfbd5d7b3448c440a1f6)
    
    * skip mac
    
    (cherry picked from commit 53ae8b70ad1dd632972ec1478717f332fc8fd6ea)
    
    * fix condition
    
    (cherry picked from commit 4ac6b269567f600bb55209f34380a35373abd7bc)
    
    * remove python installation from mariner
    add BREW_PREFIX to macos.sh
    
    (cherry picked from commit 2e70cf8e6fcec8e6ec43c7d2cf31c3aaf702c37e)
    
    * Install Python Dependencies
    
    (cherry picked from commit 429e677ef3cd5195075d2b644466b7a94ad6a75b)
    
    * instal python only on mac for now
    
    update build artifcats
    revert changes in install mariner
    clean macos install
    
    (cherry picked from commit cbbf04ce1a6217c396ede9288ace4a7699876b5d)
    
    * try installing llvm with actions to check if it is faster
    
    (cherry picked from commit 42266d0e3f770c6d05b4c3d18e85749259d301e3)
    
    * fix llvm version
    
    (cherry picked from commit da1655727a50ed43ff578ae150c260c7d3387dae)
    
    * remove instal llvm with actions
    
    (cherry picked from commit 8553973e697bb82e6ccb99b7dfe581a0d6bbf542)
    
    * uncomment ignore
    
    (cherry picked from commit f19dc136e89a48f8c524b674055b7382a6110d27)
    
    * add build artifacts requirments
    install requirments for build package
    
    (cherry picked from commit 6e1724dd9c639e3b3924b8ab8359d00bc3be1876)
    
    * fix name
    
    (cherry picked from commit 3b109e6fb69f15c927f77969b170379eeb138456)
    
    * build_package_requirments only in mac and mariner
    
    (cherry picked from commit 41c1a1990f65332b09aad2144b9793c3fb37f33a)
    
    * install coreutils on mac
    
    (cherry picked from commit 4549ee426a7ce2180292a5daf88946c5753b1cb8)
    
    * add COREUTILS path to mac
    
    (cherry picked from commit 31ae6cfcf943d4b33b057483844d5917c22c27ae)
    
    * try du #2
    
    (cherry picked from commit 32a1632082af59017cf467503846521fe97c40d4)
    
    * check again
    
    (cherry picked from commit 8a3a0232acb988fc72d77af176fbfad4d4b725c3)
    
    * clean ups
    
    (cherry picked from commit 8e5deba76da291dd634f2055335fd6c7a9e780ba)
    
    ---------
    
    Co-authored-by: meiravgri <meirav.grimberg@redis.com>

--- .install/macos.sh
+++ .install/macos.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 export HOMEBREW_NO_AUTO_UPDATE=1
-GNUBIN=$(brew --prefix)/opt/make/libexec/gnubin
+BREW_PREFIX=$(brew --prefix)
+GNUBIN=$BREW_PREFIX/opt/make/libexec/gnubin
+LLVM=$BREW_PREFIX/opt/llvm@16/bin
+COREUTILS=$BREW_PREFIX/opt/coreutils/libexec/gnubin
 
 brew update
+brew install coreutils
 brew install make
+brew install llvm@16
 
-echo "export PATH=$GNUBIN:$PATH" >> ~/.bashrc
-echo "export PATH=$GNUBIN:$PATH" >> ~/.zshrc
+echo "export PATH=$COREUTILS:$LLVM:$GNUBIN:$PATH" >> ~/.bashrc
+echo "export PATH=$COREUTILS:$LLVM:$GNUBIN:$PATH" >> ~/.zshrc
+source ~/.bashrc
+source ~/.zshrc
 
 brew install openssl
 source install_cmake.sh
+
+pip install -q -r ../tests/pytests/requirements.txt
+pip install -q -r ../tests/pytests/requirements.macos.txt
+
+# These packages are needed to build the package
+# TODO: move to upload artifacts flow
+pip install -q -r build_package_requirments.txt
+
+# List installed packages
+pip list
