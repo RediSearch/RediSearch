a55eaba5f4f47d7a85ee2cd9c6d2efa1b4103848
nafraf <nafraf@gmail.com>
2023-12-19 12:07:40 -0500

    test_stats: Test memory after drop text and tag

--- tests/pytests/test_stats.py
+++ tests/pytests/test_stats.py
@@ -19,7 +19,7 @@ def ft_debug_to_dict(env, idx, n):
 def check_empty(env, idx, memory_consumption):
     d = ft_info_to_dict(env, idx)
     env.assertEqual(float(d['num_records']), 0)
-    env.assertGreaterEqual(memory_consumption, float(d['inverted_sz_mb']))
+    env.assertEqual(memory_consumption, float(d['inverted_sz_mb']))
 
 def check_not_empty(env, idx):
     d = ft_info_to_dict(env, idx)
@@ -55,14 +55,13 @@ def runTestWithSeed(env, s=None):
     env.expect('FT.SEARCH idx * LIMIT 0 0').equal([count])
     for i in range(count):
         env.expect('FT.SEARCH', 'idx', '@n:[%d,%d]' % (i, i))#.equal([1, 'doc%d' % i, ['n', str(i)]])
-    #check_not_empty(env, idx)
+    check_not_empty(env, idx)
 
     for i in range(cleaning_loops):
         check_not_empty(env, idx)
         for ii in range(loop_count):
             conn.execute_command('DEL', 'doc%d' % int(loop_count * i + ii))
-        for jj in range(10):
-            forceInvokeGC(env, 'idx')
+        forceInvokeGC(env, 'idx')
 
     for i in range(count):
         env.expect('FT.SEARCH', 'idx', '@n:[%d,%d]' % (i, i))#.equal([0])
@@ -102,8 +101,7 @@ def runTestWithSeed(env, s=None):
         check_not_empty(env, idx)
         for ii in range(loop_count):
             conn.execute_command('DEL', 'doc%d' % int(loop_count * i + ii))
-        for jj in range(10):
-            forceInvokeGC(env, 'idx')
+        forceInvokeGC(env, 'idx')
     check_empty(env, idx, expected_empty_inverted_sz_mb)
 
 @skip(cluster=True)
@@ -115,6 +113,85 @@ def testRandom(env):
     runTestWithSeed(env, 2)
 
     runTestWithSeed(env)
+
+@skip(cluster=True)
+def testMemoryAfterDrop(env):
+
+    if env.cmd('FT.CONFIG', 'GET', 'GC_POLICY')[0][1] != 'fork':
+        env.skip()
+
+    idx_count = 100
+    doc_count = 50
+    divide_by = 1000000   # ensure limits of geo are not exceeded
+    pl = env.getConnection().pipeline()
+
+    env.cmd('FLUSHALL')
+    env.cmd('ft.config', 'set', 'FORK_GC_CLEAN_THRESHOLD', 0)
+
+    for i in range(idx_count):
+        env.expect('FT.CREATE', 'idx%d' % i, 'PREFIX', 1, '%ddoc' % i, 'SCHEMA', 't', 'TEXT', 'n', 'NUMERIC', 'tg', 'TAG', 'g', 'GEO').ok()
+
+    for i in range(idx_count):
+        geo = '1.23456,' + str(float(i) / divide_by)
+        for j in range(doc_count):
+            pl.execute_command('HSET', '%ddoc%d' % (i, j), 't', '%dhello%d' % (i, j), 'tg', '%dworld%d' % (i, j), 'n', i, 'g', geo)
+        pl.execute()
+        d = ft_info_to_dict(env, 'idx%d' % i)
+        env.assertEqual(d['num_docs'], str(doc_count))
+
+    for i in range(idx_count):
+        for j in range(doc_count):
+            pl.execute_command('DEL', '%ddoc%d' % (i, j))
+        pl.execute()
+        d = ft_info_to_dict(env, 'idx%d' % i)
+        env.assertEqual(d['num_docs'], '0')
+        for _ in range(10):
+            forceInvokeGC(env, 'idx%d' % i)
+
+    for i in range(idx_count):
+        check_empty(env, 'idx%d' % i, 102 / (1024 * 1024))
+
+@skip(cluster=True)
+def testIssue1497(env):
+
+    if env.cmd('FT.CONFIG', 'GET', 'GC_POLICY')[0][1] != 'fork':
+        env.skip()
+
+    count = 110
+    divide_by = 1000000   # ensure limits of geo are not exceeded
+    number_of_fields = 4  # one of every type
+
+    env.cmd('FLUSHALL')
+    waitForRdbSaveToFinish(env)
+    env.cmd('ft.config', 'set', 'FORK_GC_CLEAN_THRESHOLD', 0)
+    env.expect('FT.CREATE', 'idx', 'SCHEMA', 't', 'TEXT', 'n', 'NUMERIC', 'tg', 'TAG', 'g', 'GEO').ok()
+
+    res = env.cmd('ft.info', 'idx')
+    d = {res[i]: res[i + 1] for i in range(0, len(res), 2)}
+    env.assertEqual(d['inverted_sz_mb'], '0')
+    env.assertEqual(d['num_records'], '0')
+    for i in range(count):
+        geo = '1.23456,' + str(float(i) / divide_by)
+        env.expect('HSET', 'doc%d' % i, 't', 'hello%d' % i, 'tg', 'world%d' % i, 'n', i * 1.01, 'g', geo)
+    res = env.cmd('FT.SEARCH idx *')
+    check_not_empty(env, 'idx')
+    env.assertEqual(res[0], count)
+
+    res = env.cmd('ft.info', 'idx')
+    d = {res[i]: res[i + 1] for i in range(0, len(res), 2)}
+    env.assertGreater(d['inverted_sz_mb'], '0')
+    env.assertGreaterEqual(int(d['num_records']), count * number_of_fields)
+    for i in range(count):
+        env.expect('DEL', 'doc%d' % i)
+
+    for _ in range(50):
+        forceInvokeGC(env, 'idx')
+
+    res = env.cmd('ft.info', 'idx')
+    d = {res[i]: res[i + 1] for i in range(0, len(res), 2)}
+    env.assertEqual(d['inverted_sz_mb'], '0')
+    env.assertEqual(d['num_records'], '0')
+    check_empty(env, 'idx')
 
 @skip(cluster=True)
 def testMemoryAfterDrop_numeric(env):
@@ -147,8 +224,7 @@ def testMemoryAfterDrop_numeric(env):
         pl.execute()
         d = ft_info_to_dict(env, 'idx%d' % i)
         env.assertEqual(d['num_docs'], '0')
-        for _ in range(10):
-            forceInvokeGC(env, 'idx%d' % i)
+        forceInvokeGC(env, 'idx%d' % i)
 
     expected_inverted_sz_mb = 102 / (1024 * 1024)
     for i in range(idx_count):
@@ -185,10 +261,79 @@ def testMemoryAfterDrop_geo(env):
         pl.execute()
         d = ft_info_to_dict(env, 'idx%d' % i)
         env.assertEqual(d['num_docs'], '0')
-        for _ in range(10):
-            forceInvokeGC(env, 'idx%d' % i)
+        forceInvokeGC(env, 'idx%d' % i)
 
     expected_inverted_sz_mb = 102 / (1024 * 1024)
+    for i in range(idx_count):
+        check_empty(env, 'idx%d' % i, expected_inverted_sz_mb)
+
+@skip(cluster=True)
+def testMemoryAfterDrop_text(env):
+
+    if env.cmd('FT.CONFIG', 'GET', 'GC_POLICY')[0][1] != 'fork':
+        env.skip()
+
+    idx_count = 1
+    doc_count = 1
+    pl = env.getConnection().pipeline()
+
+    env.execute_command('FLUSHALL')
+    env.execute_command('ft.config', 'set', 'FORK_GC_CLEAN_THRESHOLD', 0)
+
+    for i in range(idx_count):
+        env.expect('FT.CREATE', 'idx%d' % i, 'PREFIX', 1, '%ddoc' % i, 'SCHEMA', 't', 'TEXT').ok()
+
+    for i in range(idx_count):
+        for j in range(doc_count):
+            pl.execute_command('HSET', '%ddoc%d' % (i, j), 't', '%dhello%d' % (i, j))
+        pl.execute()
+        d = ft_info_to_dict(env, 'idx%d' % i)
+        env.assertEqual(d['num_docs'], str(doc_count))
+
+    for i in range(idx_count):
+        for j in range(doc_count):
+            pl.execute_command('DEL', '%ddoc%d' % (i, j))
+        pl.execute()
+        d = ft_info_to_dict(env, 'idx%d' % i)
+        env.assertEqual(d['num_docs'], '0')
+        forceInvokeGC(env, 'idx%d' % i)
+
+    expected_inverted_sz_mb = 0
+    for i in range(idx_count):
+        check_empty(env, 'idx%d' % i, expected_inverted_sz_mb)
+
+@skip(cluster=True)
+def testMemoryAfterDrop_tag(env):
+
+    if env.cmd('FT.CONFIG', 'GET', 'GC_POLICY')[0][1] != 'fork':
+        env.skip()
+
+    idx_count = 1
+    doc_count = 2
+    pl = env.getConnection().pipeline()
+
+    env.execute_command('FLUSHALL')
+    env.execute_command('ft.config', 'set', 'FORK_GC_CLEAN_THRESHOLD', 0)
+
+    for i in range(idx_count):
+        env.expect('FT.CREATE', 'idx%d' % i, 'PREFIX', 1, '%ddoc' % i, 'SCHEMA', 'tg', 'TAG').ok()
+
+    for i in range(idx_count):
+        for j in range(doc_count):
+            pl.execute_command('HSET', '%ddoc%d' % (i, j), 'tg', '%dworld%d' % (i, j))
+        pl.execute()
+        d = ft_info_to_dict(env, 'idx%d' % i)
+        env.assertEqual(d['num_docs'], str(doc_count))
+
+    for i in range(idx_count):
+        for j in range(doc_count):
+            pl.execute_command('DEL', '%ddoc%d' % (i, j))
+        pl.execute()
+        d = ft_info_to_dict(env, 'idx%d' % i)
+        env.assertEqual(d['num_docs'], '0')
+        forceInvokeGC(env, 'idx%d' % i)
+
+    expected_inverted_sz_mb = 75 / (1024 * 1024)
     for i in range(idx_count):
         check_empty(env, 'idx%d' % i, expected_inverted_sz_mb)
 
