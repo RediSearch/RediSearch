a5bfab7980c58d7d38e78445c4689dd9f005c080
GuyAv46 <guy.avimor@gmail.com>
2023-12-20 12:29:52 +0200

    import fix

--- tests/pytests/test_info.py
+++ tests/pytests/test_info.py
@@ -1,4 +1,4 @@
-from common import waitForIndex, forceInvokeGC, skip, to_list, index_info
+from common import waitForIndex, forceInvokeGC, skip, to_list, index_info, Env
 
 # The output for this test can be used for recreating documentation for `FT.INFO`
 @skip()
