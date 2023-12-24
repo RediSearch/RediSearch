5b68e924a8785d199acfa6fb0112d5e13ed34d9e
meiravgri <meirav.grimberg@redis.com>
2023-12-24 14:45:59 +0000

    install python 3
    (cherry picked from commit 0900ea4f2a0dc5521af61388bbd9c5301b48b000)

--- .install/ubuntu_22.04.sh
+++ .install/ubuntu_22.04.sh
@@ -4,5 +4,5 @@ export DEBIAN_FRONTEND=noninteractive
 MODE=$1 # whether to install using sudo or not
 
 $MODE apt update -qq
-$MODE apt install -yqq git wget build-essential lcov openssl libssl-dev
+$MODE apt install -yqq git wget build-essential lcov openssl libssl-dev python3
 source install_cmake.sh $MODE
