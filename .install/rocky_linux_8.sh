ec62b566ea0012f78f7a697765ab6364259e5ae1
meiravgri <meirav.grimberg@redis.com>
2023-12-24 15:42:08 +0000

    ubuntu18, rock8: get python 3.10
    (cherry picked from commit 9eaf93e50cec64c886aceb232cce9ed0f653b490)

--- .install/rocky_linux_8.sh
+++ .install/rocky_linux_8.sh
@@ -2,7 +2,16 @@
 MODE=$1 # whether to install using sudo or not
 set -e
 
-$MODE yum update -y
-$MODE yum install -y gcc-toolset-10-gcc gcc-toolset-10-gcc-c++ make wget git openssl openssl-devel python3
+$MODE dnf update -y
+$MODE dnf install -y gcc-toolset-10-gcc gcc-toolset-10-gcc-c++ make wget git openssl openssl-devel
+
 source /opt/rh/gcc-toolset-10/enable
 source install_cmake.sh $MODE
+
+# Install python3.10 (latest version suggested by the default package manager is python3.6)
+$MODE wget https://www.python.org/ftp/python/3.10.0/Python-3.10.0.tar.xz
+$MODE -xf Python-3.10.0.tar.xz
+cd Python-3.10.0
+$MODE ./configure --enable-optimizations
+$MODE make -j 2
+$MODE make altinstall
