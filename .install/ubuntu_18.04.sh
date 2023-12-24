8e1afb23fb45863b353ff1738f95576a8c00a682
meiravgri <meirav.grimberg@redis.com>
2023-12-24 15:19:42 +0000

    try using ppa
    (cherry picked from commit ca751e497b8e8e21c6189f046b65f8e0f48531d8)

--- .install/ubuntu_18.04.sh
+++ .install/ubuntu_18.04.sh
@@ -10,6 +10,8 @@ $MODE apt install -yqq software-properties-common
 $MODE add-apt-repository ppa:ubuntu-toolchain-r/test -y
 $MODE add-apt-repository ppa:git-core/ppa -y
 $MODE apt update
-$MODE apt install -yqq git wget make gcc-10 g++-10 openssl libssl-dev python3-venv python3
+$MODE apt-get install -yqq git wget make gcc-10 g++-10 openssl libssl-dev
+$MODE add-apt-repository ppa:deadsnakes/ppa -y
+$MODE apt install python3 python3-venv -y
 $MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 60 --slave /usr/bin/g++ g++ /usr/bin/g++-10
 source install_cmake.sh $MODE
