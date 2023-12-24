2a2b7a4eef485dbd29dfd7869923f49c3e00b453
meiravgri <meirav.grimberg@redis.com>
2023-12-24 15:42:08 +0000

    ubuntu18, rock8: get python 3.10
    (cherry picked from commit 9eaf93e50cec64c886aceb232cce9ed0f653b490)

--- .install/ubuntu_18.04.sh
+++ .install/ubuntu_18.04.sh
@@ -11,7 +11,14 @@ $MODE add-apt-repository ppa:ubuntu-toolchain-r/test -y
 $MODE add-apt-repository ppa:git-core/ppa -y
 $MODE apt update
 $MODE apt-get install -yqq git wget make gcc-10 g++-10 openssl libssl-dev
+
+# Install Python 3.10
 $MODE add-apt-repository ppa:deadsnakes/ppa -y
-$MODE apt install python3 python3-venv -y
+$MODE apt install python3.10 python3.10-venv -y
+# Set python3 to point to python3.10
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 2
+sudo update-alternatives  --set python /usr/bin/python3.10
+
 $MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 60 --slave /usr/bin/g++ g++ /usr/bin/g++-10
 source install_cmake.sh $MODE
