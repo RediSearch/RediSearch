978c7d05aaf851164fc46d1160d762aca9091045
meiravgri <meirav.grimberg@redis.com>
2023-12-24 14:45:59 +0000

    install python 3

--- .install/rocky_linux_9.sh
+++ .install/rocky_linux_9.sh
@@ -3,7 +3,5 @@ MODE=$1 # whether to install using sudo or not
 set -e
 export DEBIAN_FRONTEND=noninteractive
 $MODE dnf update -y
-$MODE dnf install -y gcc gcc-c++ make wget git openssl openssl-devel
+$MODE dnf install -y gcc gcc-c++ make wget git openssl openssl-devel python3
 source install_cmake.sh $MODE
-
-echo INSTALLER="dnf" >> $GITHUB_ENV
