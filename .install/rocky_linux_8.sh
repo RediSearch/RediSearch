980c74a794738be31258469ac3e588b2ae2ac514
meiravgri <meirav.grimberg@redis.com>
2023-12-24 14:58:52 +0000

    ubuntu: add python3-venv centos: which rocky: yum update amzn: install openssl11
    (cherry picked from commit 36140744637b80a8f40baee640b994dc87ce76cb)

--- .install/rocky_linux_8.sh
+++ .install/rocky_linux_8.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 MODE=$1 # whether to install using sudo or not
 set -e
+
+$MODE yum update -y
 $MODE yum install -y gcc-toolset-10-gcc gcc-toolset-10-gcc-c++ make wget git openssl openssl-devel python3
 source /opt/rh/gcc-toolset-10/enable
 source install_cmake.sh $MODE
