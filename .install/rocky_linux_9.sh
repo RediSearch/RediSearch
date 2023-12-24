914a6fab873624c968017827652977b7dddfdb4a
meiravgri <meirav.grimberg@redis.com>
2023-12-24 12:50:19 +0000

    try installing puython manually
    (cherry picked from commit 909fe5f43b3be1c5340004d9407a7553d21191db)

--- .install/rocky_linux_9.sh
+++ .install/rocky_linux_9.sh
@@ -5,3 +5,5 @@ export DEBIAN_FRONTEND=noninteractive
 $MODE dnf update -y
 $MODE dnf install -y gcc gcc-c++ make wget git openssl openssl-devel
 source install_cmake.sh $MODE
+
+echo INSTALLER="dnf" >> $GITHUB_ENV
