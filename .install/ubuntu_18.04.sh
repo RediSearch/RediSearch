219ac7b556fdd3547c15bf738b6bfc04042367e6
meiravgri <meirav.grimberg@redis.com>
2023-12-25 15:00:37 +0000

    ring back add-apt-repository ppa:git-core/ppa -y

--- .install/ubuntu_18.04.sh
+++ .install/ubuntu_18.04.sh
@@ -9,12 +9,13 @@ $MODE apt dist-upgrade -yqq
 $MODE apt install -yqq software-properties-common
 
 $MODE add-apt-repository ppa:ubuntu-toolchain-r/test -y
-$MODE add-apt-repository ppa:deadsnakes/ppa -y
+$MODE add-apt-repository ppa:git-core/ppa -y
 $MODE apt update
 $MODE apt-get install -yqq git wget make gcc-10 g++-10 openssl libssl-dev
 $MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 60 --slave /usr/bin/g++ g++ /usr/bin/g++-10
 
 # Install Python 3.7
+#$MODE add-apt-repository ppa:deadsnakes/ppa -y
 $MODE apt-get update
 $MODE apt-get install python3.7 python3.7-venv python3-venv -y
 
@@ -23,7 +24,6 @@ echo "which python3.6: $(which python3.6)"
 echo "which python3.7: $(which python3.7)"
 
 # Set python3 to point to python3.7
-#$MODE update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
 $MODE update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 2
 $MODE update-alternatives --set python3 /usr/bin/python3.7
 
