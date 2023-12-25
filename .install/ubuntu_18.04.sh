3184fd63c3e3e6efc9ea510107adbb84b40e28e5
meiravgri <meirav.grimberg@redis.com>
2023-12-25 14:27:09 +0000

    bring back add-apt-repository ppa:git-core/ppa -y
    (cherry picked from commit 33197d429e195cde76b8802202162656a09417e2)

--- .install/ubuntu_18.04.sh
+++ .install/ubuntu_18.04.sh
@@ -9,7 +9,7 @@ $MODE apt dist-upgrade -yqq
 $MODE apt install -yqq software-properties-common
 
 $MODE add-apt-repository ppa:ubuntu-toolchain-r/test -y
-#$MODE add-apt-repository ppa:git-core/ppa -y
+$MODE add-apt-repository ppa:git-core/ppa -y
 $MODE apt update
 $MODE apt-get install -yqq git wget make gcc-10 g++-10 openssl libssl-dev
 $MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 60 --slave /usr/bin/g++ g++ /usr/bin/g++-10
