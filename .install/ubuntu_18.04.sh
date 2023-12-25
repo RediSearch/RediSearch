ee849d42edb6beb5ad57693a149fe068f25ef711
meiravgri <meirav.grimberg@redis.com>
2023-12-25 14:42:59 +0000

    bring back deadsnakes
    (cherry picked from commit b925cf672132ffc2305a933251773a3e60bc3075)

--- .install/ubuntu_18.04.sh
+++ .install/ubuntu_18.04.sh
@@ -9,12 +9,12 @@ $MODE apt dist-upgrade -yqq
 $MODE apt install -yqq software-properties-common
 
 $MODE add-apt-repository ppa:ubuntu-toolchain-r/test -y
+$MODE add-apt-repository ppa:deadsnakes/ppa -y
 $MODE apt update
 $MODE apt-get install -yqq git wget make gcc-10 g++-10 openssl libssl-dev
 $MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 60 --slave /usr/bin/g++ g++ /usr/bin/g++-10
 
 # Install Python 3.7
-#$MODE add-apt-repository ppa:deadsnakes/ppa -y
 $MODE apt-get update
 $MODE apt-get install python3.7 python3.7-venv python3-venv -y
 
