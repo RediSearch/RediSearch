b95a476ba98879d0de7c541a7e347fcc161d09ed
meiravgri <meirav.grimberg@redis.com>
2023-12-26 10:16:12 +0000

    install also python3.7-dev
    run also rocky8
    
    (cherry picked from commit b594785cbc5ad2e46f76d3a045865203270c314f)

--- .install/ubuntu_18.04.sh
+++ .install/ubuntu_18.04.sh
@@ -15,22 +15,11 @@ $MODE apt-get install -yqq build-essential git wget make gcc-10 g++-10 openssl l
 $MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 60 --slave /usr/bin/g++ g++ /usr/bin/g++-10
 
 # Install Python 3.7
-#$MODE add-apt-repository ppa:deadsnakes/ppa -y
-$MODE apt-get install python3.7 python3.7-venv python3-venv python3-dev -y
-
-echo "which python3: $(which python3)"
-echo "which python3.6: $(which python3.6)"
-echo "which python3.7: $(which python3.7)"
+$MODE apt-get install python3.7 python3.7-venv python3-venv python3.7-dev python3-dev -y
 
 # Set python3 to point to python3.7
 $MODE update-alternatives --install /usr/bin/python python /usr/bin/python3.7 2 --slave /usr/bin/python3 python3 /usr/bin/python3.7
 
-# $MODE update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 2
-# $MODE update-alternatives --set python3 /usr/bin/python3.7
-# $MODE update-alternatives --install /usr/bin/python python /usr/bin/python3.7 2
-# $MODE update-alternatives --set python /usr/bin/python3.7
-echo "after alter python: $(python --version)"
-echo "after alter python3: $(python3 --version)"
 
 #$MODE apt-get install python3-venv -y
 
