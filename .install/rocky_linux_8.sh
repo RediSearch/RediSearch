b594785cbc5ad2e46f76d3a045865203270c314f
meiravgri <meirav.grimberg@redis.com>
2023-12-26 10:16:12 +0000

    install also python3.7-dev
    run also rocky8

--- .install/rocky_linux_8.sh
+++ .install/rocky_linux_8.sh
@@ -4,15 +4,13 @@ set -e
 
 $MODE dnf update -y
 $MODE dnf install -y gcc-toolset-10-gcc gcc-toolset-10-gcc-c++ make wget git openssl openssl-devel \
-    bzip2-devel libffi-devel zlib-devel tar xz which
+    bzip2-devel libffi-devel zlib-devel tar xz which find
 
 source /opt/rh/gcc-toolset-10/enable
 source install_cmake.sh $MODE
 
 # Install python3.10 (latest version suggested by the default package manager is python3.6)
-$MODE wget https://www.python.org/ftp/python/3.10.0/Python-3.10.0.tar.xz
-$MODE tar -xf Python-3.10.0.tar.xz
-cd Python-3.10.0
-$MODE ./configure --enable-optimizations
-$MODE make -j 2
-$MODE make altinstall
+$MODE dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
+$MODE dnf config-manager --set-enabled powertools
+$MODE dnf install epel-release
+$MODE dnf install python38 -y
