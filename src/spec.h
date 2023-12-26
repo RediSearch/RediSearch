ac3c751ae7480b6fd5ad73cb0cbfe1a4dfc57b28
GuyAv46 <guy.avimor@gmail.com>
2023-12-26 11:00:50 +0200

    fix alignment

--- src/spec.h
+++ src/spec.h
@@ -259,8 +259,8 @@ typedef struct IndexSpec {
   FieldSpec *fields;              // Fields in the index schema
   int numFields;                  // Number of fields
 
-  IndexStats stats;               // Statistics of memory used and quantities
   IndexFlags flags;               // Flags
+  IndexStats stats;               // Statistics of memory used and quantities
 
   Trie *terms;                    // Trie of all terms. Used for GC and fuzzy queries
   Trie *suffix;                   // Trie of suffix tokens of terms. Used for contains queries
