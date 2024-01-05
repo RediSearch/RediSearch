7bbae0f22fef4fc46fac9b0fb21b41f8084fcb72
Ephraim Feldblum <ephraim.feldblum@redis.com>
2024-01-05 02:36:01 +0200

    const
    (cherry picked from commit 3dbc625e3756f8783a12899736d1df1144ad3606)

--- src/geometry/query_iterator.cpp
+++ src/geometry/query_iterator.cpp
@@ -35,7 +35,7 @@ int QueryIterator::skip_to(t_docId docId, RSIndexResult *&hit) {
     return INDEXREAD_EOF;
   }
 
-  auto it = std::ranges::lower_bound(iter_.cbegin() + index_, iter_.cend(), docId);
+  const auto it = std::ranges::lower_bound(iter_.cbegin() + index_, iter_.cend(), docId);
   index_ = std::ranges::distance(iter_.cbegin(), it + 1);
   if (!has_next()) {
     abort();
@@ -82,7 +82,7 @@ int QIter_HasNext(void *ctx) {
 }
 void QIter_Free(IndexIterator *self) {
   using alloc_type = Allocator::TrackingAllocator<QueryIterator>;
-  auto qi = static_cast<QueryIterator *>(self->ctx);
+  const auto qi = static_cast<QueryIterator * const>(self->ctx);
   auto alloc = alloc_type{qi->iter_.get_allocator()};
   IndexResult_Free(self->current);
   std::allocator_traits<alloc_type>::destroy(alloc, qi);
@@ -100,24 +100,21 @@ void QIter_Rewind(void *ctx) {
 }  // anonymous namespace
 
 IndexIterator QueryIterator::init_base(QueryIterator *ctx) {
-  auto ii = IndexIterator{
-      .isValid = 1,
-      .ctx = ctx,
-      .current = NewVirtualResult(0),
-      .mode = MODE_SORTED,
-      .type = ID_LIST_ITERATOR,
-      .NumEstimated = QIter_Len,
-      .GetCriteriaTester = nullptr,
-      .Read = QIter_Read,
-      .SkipTo = QIter_SkipTo,
-      .LastDocId = QIter_LastDocId,
-      .HasNext = QIter_HasNext,
-      .Free = QIter_Free,
-      .Len = QIter_Len,
-      .Abort = QIter_Abort,
-      .Rewind = QIter_Rewind,
+  return IndexIterator{
+    .isValid = 1,
+    .ctx = ctx,
+    .current = NewVirtualResult(0),
+    .type = ID_LIST_ITERATOR,
+    .NumEstimated = QIter_Len,
+    .Read = QIter_Read,
+    .SkipTo = QIter_SkipTo,
+    .LastDocId = QIter_LastDocId,
+    .HasNext = QIter_HasNext,
+    .Free = QIter_Free,
+    .Len = QIter_Len,
+    .Abort = QIter_Abort,
+    .Rewind = QIter_Rewind,
   };
-  return ii;
 }
 }  // namespace GeoShape
 }  // namespace RediSearch
