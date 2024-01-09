54cc1d2e8be8a35f88bc46c3d965d431a860de1c
GuyAv46 <47632673+GuyAv46@users.noreply.github.com>
2024-01-09 16:25:12 +0200

    [2.8] improve GHA workflows - [MOD-6404] (#4328)
    * improve GHA workflows - [MOD-6404] (#4253)
    
    * improve flows
    
    * build artifacts improvements
    
    * fix pr-validation
    
    * make release flow version bump PR trigger CI
    
    * change condition from contains "bug" to "not contains "feature"
    
    * fix get tag logic
    
    * temp test
    
    * temp test
    
    * test
    
    * last test
    
    * remove tests
    
    * copy labels to backport PRs
    
    * simplified and improved task-test validation
    
    * update upload-artifact version to v4
    
    * remove condition
    
    * fix RLTest flags condition
    
    * TEST enable macos parallelism
    
    * specified log names by OS, arch, redis ver
    
    * TEMP install specific RLTest
    
    * fix names to valid ones
    
    * normalize name
    
    * set an option to use upload v3 if node20 is not supported
    
    * reverted requirements and macos parallelizm
    
    * convert script to bash
    
    * fix condition
    
    * macos improvement (#4318)
    
    * attempt for macos
    
    * enable test
    
    * move flag setting
    
    * disabling flag from script
    
    * returned the env var export
    
    * cleanups
    
    * improve comment
    
    * remove macos matrix (we have parallelism now)
    
    * fix backport regex
    
    * review changes
    
    * fix condition
    
    * GHA - fix shell options (#4332)

--- tests/pytests/runtests.sh
+++ tests/pytests/runtests.sh
@@ -165,7 +165,7 @@ setup_rltest() {
 	if [[ $RLTEST_DEBUG == 1 ]]; then
 		RLTEST_ARGS+=" --debug-print"
 	fi
-	if [[ -n $RLTEST_LOG && $RLTEST_LOG != 1 ]]; then
+	if [[ -n $RLTEST_LOG && $RLTEST_LOG != 1 && -z $RLTEST_PARALLEL_ARG ]]; then
 		RLTEST_ARGS+=" -s"
 	fi
 	if [[ $RLTEST_CONSOLE == 1 ]]; then
@@ -516,6 +516,9 @@ OS=$($READIES/bin/platform --os)
 ARCH=$($READIES/bin/platform --arch)
 OSNICK=$($READIES/bin/platform --osnick)
 
+# RLTest uses `fork` which might fail on macOS with the following variable set
+[[ $OS == macos ]] && export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+
 #---------------------------------------------------------------------------------- Tests scope
 
 [[ $COORD == 1 ]] && COORD=oss
@@ -576,9 +579,6 @@ STATFILE=${STATFILE:-$ROOT/bin/artifacts/tests/status}
 #---------------------------------------------------------------------------------- Parallelism
 
 PARALLEL=${PARALLEL:-1}
-
-# due to Python "Can't pickle local object" problem in RLTest
-[[ $OS == macos ]] && PARALLEL=0
 
 [[ $EXT == 1 || $EXT == run || $BB == 1 || $GDB == 1 ]] && PARALLEL=0
 
