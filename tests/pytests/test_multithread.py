a0d56cfdead9e523140fd6ce6d0539bbf8496f9b
meiravgri <meirav.grimberg@redis.com>
2024-01-10 12:50:32 +0000

    revert skip test_invalid_mt_config_combinations

--- tests/pytests/test_multithread.py
+++ tests/pytests/test_multithread.py
@@ -61,8 +61,7 @@ def testMultipleBlocksBuffer():
 # We test it only for redis versions > 7.2.3 due to a bug in redis - onFLush callback is called even though module loading
 # failed, causing access to an invalid memory that was freed (module context) - see
 # https://github.com/redis/redis/issues/12808.
-#@skip(cluster=True, noWorkers=True)
-@skip()
+@skip(cluster=True, noWorkers=True)
 def test_invalid_mt_config_combinations(env):
     if server_version_less_than(env, "7.2.4"):
         env.skip()
