6e68dae230c7fa0ae3e95ba2b1288d6dfb00115f
GuyAv46 <guy.avimor@gmail.com>
2024-01-28 12:25:15 +0200

    make it static
    (cherry picked from commit 2c7d875d0a821b3c9eacc516a18497e78387ae93)

--- src/aggregate/functions/date.c
+++ src/aggregate/functions/date.c
@@ -64,7 +64,7 @@ https://gmbabar.wordpress.com/2010/12/01/mktime-slow-use-custom-function/ */
 // https://godbolt.org/z/qscb5d9dT
 // https://quick-bench.com/q/oTV4_9uVqPTcrj2fpDEvbbMzZ48
 // https://quick-bench.com/q/2Bc8WY1Ys0vmbp-HPagWFxu81jI
-time_t fast_timegm(const struct tm *ltm) {
+static time_t fast_timegm(const struct tm *ltm) {
   long years = ltm->tm_year - 70; // tm->tm_year is from 1900, epoch is from 1970.
   long leaps = (years + 1) / 4;   // number of leap years from 1970, not including the current year.
                                   // correct until 2100.
