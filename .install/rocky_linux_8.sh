3745a7ed1680f9370283ddea427c24af27a44bcd
meiravgri <meirav.grimberg@redis.com>
2024-01-31 12:35:07 +0000

    rocky8 repair installation gcc11

--- .install/rocky_linux_8.sh
+++ .install/rocky_linux_8.sh
@@ -3,10 +3,19 @@ MODE=$1 # whether to install using sudo or not
 set -e
 
 $MODE dnf update -y
-$MODE dnf install -y gcc gcc-c++ gcc-toolset-10-gcc gcc-toolset-10-gcc-c++ make wget git openssl openssl-devel \
+
+# Install epel
+$MODE dnf groupinstall "Development Tools" -y
+$MODE dnf config-manager --set-enabled powertools
+$MODE dnf install epel-release
+
+
+$MODE dnf install -y gcc-toolset-11-gcc gcc-toolset-11-gcc-c++ make wget git openssl openssl-devel \
     bzip2-devel libffi-devel zlib-devel tar xz which
 
-source /opt/rh/gcc-toolset-10/enable
+source /opt/rh/gcc-toolset-11/enable
+echo "gcc version: $(gcc --version)"
+
 source install_cmake.sh $MODE
 
 # Install python3.10 (latest version suggested by the default package manager is python3.6)
