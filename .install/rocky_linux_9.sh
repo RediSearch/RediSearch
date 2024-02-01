96388615daddc676ecc446aeecb317a1a4479c30
meiravgri <meirav.grimberg@redis.com>
2024-02-01 13:28:53 +0000

    install gcc13
    (cherry picked from commit 44c607818080fa97d4e8d04103d18396ff5a7c16)

--- .install/rocky_linux_9.sh
+++ .install/rocky_linux_9.sh
@@ -6,9 +6,9 @@ $MODE dnf update -y
 
 $MODE dnf groupinstall "Development Tools" -yqq
 
-$MODE dnf install -y gcc-toolset-12-gcc gcc-toolset-12-gcc-c++ make wget git openssl openssl-devel python3 which
+$MODE dnf install -y gcc-toolset-13-gcc gcc-toolset-13-gcc-c++ make wget git openssl openssl-devel python3 which
 
-cp /opt/rh/gcc-toolset-12/enable /etc/profile.d/gcc-toolset-12.sh
+cp /opt/rh/gcc-toolset-13/enable /etc/profile.d/gcc-toolset-13.sh
 bash
 
 source install_cmake.sh $MODE
