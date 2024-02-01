6d60836d246be50697bd2e29b52dd10698772286
meiravgri <meirav.grimberg@redis.com>
2024-02-01 12:15:57 +0000

    install dev tools

--- .install/rocky_linux_9.sh
+++ .install/rocky_linux_9.sh
@@ -3,9 +3,12 @@ MODE=$1 # whether to install using sudo or not
 set -e
 export DEBIAN_FRONTEND=noninteractive
 $MODE dnf update -y
+
+$MODE dnf groupinstall "Development Tools" -yqq
+
 $MODE dnf install -y gcc-toolset-12-gcc gcc-toolset-12-gcc-c++ make wget git openssl openssl-devel python3 which
 
-cp /opt/rh/gcc-toolset-12/enable /etc/profile.d/gcc-toolset-11.sh
+cp /opt/rh/gcc-toolset-12/enable /etc/profile.d/gcc-toolset-12.sh
 bash
 
 source install_cmake.sh $MODE
