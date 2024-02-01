549a2890c1693361400afb17941d0d554470d2cc
meiravgri <meirav.grimberg@redis.com>
2024-02-01 14:01:25 +0000

    install unzip
    (cherry picked from commit 560dca78632c07207c8742f0359ce369b7725dbb)

--- .install/rocky_linux_9.sh
+++ .install/rocky_linux_9.sh
@@ -4,7 +4,8 @@ set -e
 export DEBIAN_FRONTEND=noninteractive
 $MODE dnf update -y
 
-$MODE dnf install -y gcc-toolset-13-gcc gcc-toolset-13-gcc-c++ make wget git openssl openssl-devel python3 which rsync
+$MODE dnf install -y gcc-toolset-13-gcc gcc-toolset-13-gcc-c++ make wget git openssl openssl-devel python3 which \
+    rsync unzip
 
 cp /opt/rh/gcc-toolset-13/enable /etc/profile.d/gcc-toolset-13.sh
 bash
