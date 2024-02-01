46e9e59ad04803a0f46f8ac01a17919dd8189f45
meiravgri <meirav.grimberg@redis.com>
2024-02-01 15:16:55 +0000

    install gcc10

--- .install/ubuntu_20.04.sh
+++ .install/ubuntu_20.04.sh
@@ -4,5 +4,11 @@ export DEBIAN_FRONTEND=noninteractive
 MODE=$1 # whether to install using sudo or not
 
 $MODE apt update -qq
-$MODE apt install -yqq wget make clang-format gcc python3 python3-venv python3-pip lcov git openssl libssl-dev
+$MODE apt upgrade -yqq
+
+$MODE apt install -yqq wget make clang-format gcc python3 python3-venv python3-pip lcov git openssl libssl-dev \
+    build-essential gcc-10 g++-10
+
+$MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 60 --slave /usr/bin/g++ g++ /usr/bin/g++-10
+
 source install_cmake.sh $MODE
