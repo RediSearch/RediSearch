a6ceaa7e37293e833eafa0007cd96048cdfdeb3e
meiravgri <meirav.grimberg@redis.com>
2024-02-01 15:46:47 +0000

    install rsync unzip

--- .install/ubuntu_20.04.sh
+++ .install/ubuntu_20.04.sh
@@ -7,7 +7,7 @@ $MODE apt update -qq
 $MODE apt upgrade -yqq
 
 $MODE apt install -yqq wget make clang-format gcc python3 python3-venv python3-pip lcov git openssl libssl-dev \
-    build-essential gcc-10 g++-10
+    unzip rsync build-essential gcc-10 g++-10
 
 $MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 60 --slave /usr/bin/g++ g++ /usr/bin/g++-10
 
