34477bf1626a1e7b206b5da5d2e3a0ecd34c34ac
meiravgri <meirav.grimberg@redis.com>
2024-02-01 12:01:31 +0000

    install gcc-toolset-12

--- .install/rocky_linux_9.sh
+++ .install/rocky_linux_9.sh
@@ -3,5 +3,9 @@ MODE=$1 # whether to install using sudo or not
 set -e
 export DEBIAN_FRONTEND=noninteractive
 $MODE dnf update -y
-$MODE dnf install -y gcc gcc-c++ make wget git openssl openssl-devel python3 which
+$MODE dnf install -y gcc-toolset-12-gcc gcc-toolset-12-gcc-c++ make wget git openssl openssl-devel python3 which
+
+cp /opt/rh/gcc-toolset-11/enable /etc/profile.d/gcc-toolset-11.sh
+bash
+
 source install_cmake.sh $MODE
