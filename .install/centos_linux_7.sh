b305f9c7ba378790ab5c077a4729b058fde84b4b
meiravgri <meirav.grimberg@redis.com>
2024-02-04 12:57:23 +0000

    install script for cenos7 upgrade RLTest min version to 0.7.11

--- .install/centos_linux_7.sh
+++ .install/centos_linux_7.sh
@@ -2,9 +2,25 @@
 MODE=$1 # whether to install using sudo or not
 set -e
 export DEBIAN_FRONTEND=noninteractive
-ARCH=$([[ $(uname -m) == x86_64 ]] && echo x86_64 || echo noarch)
+#ARCH=$([[ $(uname -m) == x86_64 ]] && echo x86_64 || echo noarch)
 $MODE yum update -y
-$MODE yum install -y https://packages.endpointdev.com/rhel/7/os/${ARCH}/endpoint-repo.${ARCH}.rpm
-$MODE yum groupinstall -y "Development Tools"
-$MODE yum install -y wget git openssl-devel openssl python3 which
+#$MODE yum install -y https://packages.endpointdev.com/rhel/7/os/${ARCH}/endpoint-repo.${ARCH}.rpm
+#$MODE yum groupinstall -y "Development Tools"
+
+
+$MODE  yum install openssl-devel openssl bzip2-devel libffi-devel wget which git -y
+
+$MODE yum install centos-release-scl
+$MODE yum install devtoolset-10-gcc devtoolset-10-gcc-c++
+cp /opt/rh/devtoolset-10/enable /etc/profile.d/scl-devtoolset-10.sh
+bash
+wget https://www.python.org/ftp/python/3.9.6/Python-3.9.6.tgz
+tar -xvf Python-3.9.6.tgz
+cd Python-3.9.6
+./configure --enable-optimizations
+$MODE make altinstall
+update-alternatives --install /usr/bin/python3 python3 `which python3.9` 2
+# gcc
+
+
 source install_cmake.sh $MODE
