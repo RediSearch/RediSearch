8e8cb01f27720c7861251dfd9f5b19448b1b65d6
meiravgri <meirav.grimberg@redis.com>
2024-02-06 13:23:39 +0000

    jammy install rsync coomon:fix path to profiled

--- .install/ubuntu_22.04.sh
+++ .install/ubuntu_22.04.sh
@@ -4,5 +4,5 @@ export DEBIAN_FRONTEND=noninteractive
 MODE=$1 # whether to install using sudo or not
 
 $MODE apt update -qq
-$MODE apt install -yqq git wget build-essential lcov openssl libssl-dev python3 python3-venv python3-dev unzip
+$MODE apt install -yqq git wget build-essential lcov openssl libssl-dev python3 python3-venv python3-dev unzip rsync
 source install_cmake.sh $MODE
