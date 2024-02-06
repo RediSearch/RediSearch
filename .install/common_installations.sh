4af34594b47464a3819f0391e6911a63f2fedb16
meiravgri <meirav.grimberg@redis.com>
2024-02-06 12:55:51 +0000

    get profile d depending on the os
    (cherry picked from commit 4cc120356a6849a822dfbd26068ee14f3a39db49)

--- .install/common_installations.sh
+++ .install/common_installations.sh
@@ -3,8 +3,19 @@ set -e
 OS_TYPE=$(uname -s)
 MODE=$1 # whether to install using sudo or not
 
+get_profile_d() {
+	local d
+	if [[ OS_TYPE == Darwin ]]; then
+		d="$HOME/.profile.d"
+	else
+		d="/etc/profile.d"
+	fi
+	echo "$d"
+}
+
+
 python3 -m venv venv
-$MODE cp $PWD/venv/bin/activate /etc/profile.d/activate_venv.sh
+$MODE cp $PWD/venv/bin/activate $(get_profile_d)/activate_venv.sh
 source venv/bin/activate
 
 pip install --upgrade pip
@@ -13,12 +24,6 @@ echo "pip version: $(pip --version)"
 echo "pip path: $(which pip)"
 
 pip install -q -r tests/pytests/requirements.txt
-# if [[ $OS_TYPE = 'Darwin' ]]
-# then
-#     pip install -q -r tests/pytests/requirements.macos.txt
-# else
-#     pip install -q -r tests/pytests/requirements.linux.txt
-# fi
 
 # These packages are needed to build the package
 # TODO: move to upload artifacts flow
