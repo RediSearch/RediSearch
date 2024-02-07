b9c91d235c516f189d5cf466fb250eb2deaafa18
meiravgri <meirav.grimberg@redis.com>
2024-02-07 09:13:01 +0000

    ubuntu18 with apt instead of apt-get
    (cherry picked from commit 8b46657cff835ea6a9d1cc9f697cc75da5e5be42)

--- .install/ubuntu_18.04.sh
+++ .install/ubuntu_18.04.sh
@@ -12,11 +12,11 @@ $MODE apt install -yqq software-properties-common unzip rsync
 $MODE add-apt-repository ppa:ubuntu-toolchain-r/test -y
 $MODE add-apt-repository ppa:git-core/ppa -y
 $MODE apt update
-$MODE apt-get install -yqq build-essential git wget make gcc-10 g++-10 openssl libssl-dev
+$MODE apt install -yqq build-essential git wget make gcc-10 g++-10 openssl libssl-dev
 $MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 60 --slave /usr/bin/g++ g++ /usr/bin/g++-10
 
 # Install Python 3.8
-$MODE apt -y install python3.8 python3.8-venv  python3.8-dev python3-venv python3-dev
+$MODE apt -y install python3.8 python3.8-venv python3.8-dev python3-venv python3-dev
 
 # Set python3 to point to python3.8
 $MODE update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2
