71a4b21dccf748374e9844446db4818428d8f0be
meiravgri <meirav.grimberg@redis.com>
2024-02-07 08:49:33 +0000

    remove aws cli from mariner

--- .install/common_base_linux_mariner_2.0.sh
+++ .install/common_base_linux_mariner_2.0.sh
@@ -7,11 +7,3 @@ $MODE tdnf install -q -y build-essential git wget ca-certificates tar unzip rsyn
                          openssl-devel openssl python3 python3-pip which
 
 source install_cmake.sh $MODE
-
-# Install aws-cli for uploading artifacts to s3
-curdir="$PWD"
-cd /tmp/
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip -q awscliv2.zip
-$MODE ./aws/install
-cd $curdir
