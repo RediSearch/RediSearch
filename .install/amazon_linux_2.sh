6269a39ce93eb40dc0e001b909c70696d4c50785
meiravgri <meirav.grimberg@redis.com>
2024-02-07 13:20:57 +0000

    review fixes:
    codecov: remove Get Installation Mode, always use sudo
    
    remove bash from os scripts
    add missing $MODE

--- .install/amazon_linux_2.sh
+++ .install/amazon_linux_2.sh
@@ -4,6 +4,7 @@ set -e
 export DEBIAN_FRONTEND=noninteractive
 
 $MODE yum update -y
+# Install the RPM package that provides the Software Collections (SCL) required for devtoolset-11
 $MODE yum install -y http://mirror.centos.org/centos/7/extras/x86_64/Packages/centos-release-scl-rh-2-3.el7.centos.noarch.rpm
 
 $MODE yum install -y wget git which devtoolset-11-gcc devtoolset-11-gcc-c++ devtoolset-11-make rsync python3 unzip
@@ -11,8 +12,7 @@ $MODE yum install -y wget git which devtoolset-11-gcc devtoolset-11-gcc-c++ devt
 source /opt/rh/devtoolset-11/enable
 
 cp /opt/rh/devtoolset-11/enable /etc/profile.d/scl-devtoolset-11.sh
-bash
 
 $MODE yum install -y openssl11 openssl11-devel
-ln -s `which openssl11` /usr/bin/openssl
+$MODE ln -s `which openssl11` /usr/bin/openssl
 source install_cmake.sh $MODE
