1cdfbd1d08e2ab07cace4e94422a0945b2b9f7c1
meiravgri <109056284+meiravgri@users.noreply.github.com>
2024-02-13 10:23:47 +0200

    [2.6] remove macos and linux requirments from system setup (#4449)
    * remove macos requirments from system setup as it no longer exists
    
    (cherry picked from commit 23b334875d60948e0c0c6d3bbf320f2076ecb6c6)
    
    * revert yaml
    
    (cherry picked from commit 8f76c2537e9620b9811c9a25ce31654b8305343c)
    
    * revert merged yaml
    
    * revert spaces in yaml

--- sbin/system-setup.py
+++ sbin/system-setup.py
@@ -74,11 +74,7 @@ class RediSearchSetup(paella.Setup):
         self.install_gnu_utils()
         self.install("pkg-config")
         self.install("libtool m4 automake")
-        self.pip_install(f"-r {ROOT}/tests/pytests/requirements.macos.txt")
         # self.run(f"{self.python} {READIES}/bin/getredis -v 6 --force")
-
-    def linux_last(self):
-        self.pip_install(f"-r {ROOT}/tests/pytests/requirements.linux.txt")
 
     def common_last(self):
         self.run(f"{self.python} {READIES}/bin/getcmake --usr", sudo=self.os != 'macos')
