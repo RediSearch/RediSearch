a6ce1102cfde68904c5ddbd0d1eea549ef481bd1
Ephraim Feldblum <ephraim.feldblum@redis.com>
2024-02-20 13:45:40 +0200

    remove logging
    (cherry picked from commit 0d5ba23f702572f0053d93adbe051a34457713cc)

--- src/query.c
+++ src/query.c
@@ -327,7 +327,6 @@ static enum QueryType parseGeometryPredicate(const char *predicate, size_t len) 
   int cmp = ((len << CHAR_BIT) | toupper(predicate[len-1]));    // for all ascii chars, 'a' | 0x20 == 'A'
 #define CASE(s) (((sizeof(s)-1) << CHAR_BIT) | s[sizeof(s)-2])  // two bytes: len | last char
 #define COND(s) ((cmp == CASE(s)) && !strncasecmp(predicate, s, len))
-  RedisModule_Log(NULL, "WARNING", "%d, %.*s", len, len, predicate);
   if COND("WITHIN") {  // 0x06'4E
     return WITHIN;
   }
