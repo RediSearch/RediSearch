9db0f29297f24c256ed564b8cd146ec43c1a5b93
Ephraim Feldblum <ephraim.feldblum@redis.com>
2024-02-21 14:46:02 +0200

    passing more descriptive error messages out
    (cherry picked from commit e36eb4bf8acd95c42f4abf8fe92b8c8026230f92)

--- src/geometry/rtree.cpp
+++ src/geometry/rtree.cpp
@@ -120,8 +120,9 @@ auto from_wkt(std::string_view wkt) -> geom_type<cs> {
           throw std::runtime_error{"attempting to create empty geometry"};
         }
         // TODO: GEOMETRY - add flag to allow user to ascertain validity of input
-        if (bg::correct(geom); !bg::is_valid(geom)) {
-          throw std::runtime_error{"invalid geometry"};
+        bg::correct(geom);
+        if (std::string error; !bg::is_valid(geom, error)) {
+          throw std::runtime_error{"invalid geometry: " + error};
         }
       },
       geom);
