d52e6340d38cf5a6335014c19b621967640a1d64
nafraf <nafraf@gmail.com>
2024-03-10 17:51:25 -0500

    Fix FT.EXPLAIN

--- src/query.c
+++ src/query.c
@@ -1823,7 +1823,7 @@ static sds QueryNode_DumpSds(sds s, const IndexSpec *spec, const QueryNode *qs, 
       return s;
     case QN_WILDCARD_QUERY:
       s = sdscatprintf(s, "WILDCARD{%s}\n", qs->verb.tok.str);
-      break;
+      return s;
     case QN_NULL:
       s = sdscat(s, "<empty>");
       break;
