f0275005dad1ff8349506039b24f474b8b879e59
meiravgri <meirav.grimberg@redis.com>
2024-03-19 15:28:59 +0000

    install with mode

--- .install/install_boost.sh
+++ .install/install_boost.sh
@@ -2,6 +2,7 @@
 
 set -e
 VERSION=$1
+MODE=$2
 BOOST_DIR="boost_${VERSION//./_}"
 
 wget https://boostorg.jfrog.io/artifactory/main/release/${VERSION}/source/${BOOST_DIR}.tar.gz
@@ -12,4 +13,4 @@ cd ${BOOST_DIR}
 
 
 ./bootstrap.sh --prefix=/usr
-./b2 install
+$MODE ./b2 install
