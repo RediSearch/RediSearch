d8325307c2d1021775b94e00b9a5b2f9e04f641a
meiravgri <meirav.grimberg@redis.com>
2024-03-19 17:07:54 +0000

    update boost cache path
    (cherry picked from commit 6bd80952e6a8aae45004efc3a7d50c9f5f88af93)

--- .install/install_boost.sh
+++ .install/install_boost.sh
@@ -3,11 +3,13 @@
 set -e
 VERSION=$1
 MODE=$2
-BOOST_DIR="boost_${VERSION//./_}"
+BOOST_NAME="boost_${VERSION//./_}"
+BOOST_DIR="boost" # here we search for the boost cached installation if exists. Do not change this value
 
-wget https://boostorg.jfrog.io/artifactory/main/release/${VERSION}/source/${BOOST_DIR}.tar.gz
+wget https://boostorg.jfrog.io/artifactory/main/release/${VERSION}/source/${BOOST_NAME}.tar.gz
 
-tar -xzf ${BOOST_DIR}.tar.gz
+tar -xzf ${BOOST_NAME}.tar.gz
+mv ${BOOST_NAME} ${BOOST_DIR}
 cd ${BOOST_DIR}
 echo "BOOST_DIR=$PWD" >> $GITHUB_ENV
 echo "export BOOST_DIR = $BOOST_DIR"
