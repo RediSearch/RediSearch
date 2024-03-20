766945118c4fe3e0c308b84a095ca1ba8421bc63
meiravgri <meirav.grimberg@redis.com>
2024-03-20 15:24:00 +0000

    try returning venv
    (cherry picked from commit e4b38f091ae49e38b9b292bde14ed9b5ec333f10)

--- .install/common_installations.sh
+++ .install/common_installations.sh
@@ -3,7 +3,18 @@ set -e
 OS_TYPE=$(uname -s)
 MODE=$1 # whether to install using sudo or not
 
+activate_venv() {
+	echo "copy ativation script to shell config"
+	if [[ $OS_TYPE == Darwin ]]; then
+		echo "source venv/bin/activate" >> ~/.bashrc
+		echo "source venv/bin/activate" >> ~/.zshrc
+	else
+		echo "source $PWD/venv/bin/activate" >> ~/.bash_profile
+	fi
+}
+
 python3 -m venv venv
+activate_venv
 source venv/bin/activate
 
 pip install --upgrade pip
