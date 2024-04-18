079cfcdb9609aa47f6ec63af9d8254d95b390873
GuyAv46 <guy.avimor@gmail.com>
2024-04-18 16:42:29 +0300

    add support for fp16

--- src/vector_index.h
+++ src/vector_index.h
@@ -12,9 +12,11 @@
 #include "query_ctx.h"
 
 // TODO: delete when aligned with VecSim
+#define VecSimType_FLOAT16 4
 #define VecSimType_BFLOAT16 5
 
 #define VECSIM_TYPE_BFLOAT16 "BFLOAT16"
+#define VECSIM_TYPE_FLOAT16 "FLOAT16"
 #define VECSIM_TYPE_FLOAT32 "FLOAT32"
 #define VECSIM_TYPE_FLOAT64 "FLOAT64"
 #define VECSIM_TYPE_INT32 "INT32"
