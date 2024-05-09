b6cfedb56e3b5cd61c3de53e9b6d89db7f102b7c
raz-mon <razmonsonego2@gmail.com>
2024-05-09 13:35:28 +0300

    Enhance test

--- tests/pytests/test_missing.py
+++ tests/pytests/test_missing.py
@@ -28,11 +28,15 @@ def testMissingInfo():
     res = env.cmd('FT.INFO', 'idx')
 
     # The fields' section is in different places for cluster and standalone builds
+    n_found = 0
     if env.isCluster():
         fields = res[5]
         for field in fields:
-            env.assertEquals(field[-1], "ISMISSING")
+            env.assertEqual(field[-1], "ISMISSING")
+            n_found += 1
     else:
         fields = res[7]
         for field in fields:
-            env.assertEquals(field[-1], "ISMISSING")
+            env.assertEqual(field[-1], "ISMISSING")
+            n_found += 1
+    env.assertEqual(n_found, 2)
