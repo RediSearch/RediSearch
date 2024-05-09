14b34ed628033714c2287b3f3ab8bdbba814cb65
Raz Monsonego <74051729+raz-mon@users.noreply.github.com>
2024-05-09 18:42:14 +0300

    MOD-6533, MOD-6581: Support MISSING indexing syntax (#4635)

---
+++ tests/pytests/test_missing.py
@@ -0,0 +1,42 @@
+from common import *
+
+def testMissingValidations():
+    """Tests the validations for missing values indexing"""
+
+    env = Env(moduleArgs="DEFAULT_DIALECT 2")
+
+    # Validate successful index creation with the `ISMISSING` keyword for TAG,
+    # TEXT fields
+    env.expect('FT.CREATE', 'idx1', 'SCHEMA', 'ta', 'TAG', 'ISMISSING', 'te', 'TEXT', 'ISMISSING').ok()
+
+    # Same with SORTABLE, WITHSUFFIXTRIE
+    env.expect('FT.CREATE', 'idx2', 'SCHEMA',
+               'ta', 'TAG', 'ISMISSING', 'WITHSUFFIXTRIE', 'SORTABLE',
+               'te', 'TEXT', 'ISMISSING', 'WITHSUFFIXTRIE', 'SORTABLE'
+               ).ok()
+
+def testMissingInfo():
+    """Tests that we get the `ISMISSING` keyword in the INFO response for fields
+    that index missing values."""
+
+    env = Env(moduleArgs="DEFAULT_DIALECT 2")
+
+    # Create an index with TAG and TEXT fields that index empty fields.
+    env.expect('FT.CREATE', 'idx', 'SCHEMA', 'ta', 'TAG', 'ISMISSING', 'te', 'TEXT', 'ISMISSING').ok()
+
+    # Validate `INFO` response
+    res = env.cmd('FT.INFO', 'idx')
+
+    # The fields' section is in different places for cluster and standalone builds
+    n_found = 0
+    if env.isCluster():
+        fields = res[5]
+        for field in fields:
+            env.assertEqual(field[-1], "ISMISSING")
+            n_found += 1
+    else:
+        fields = res[7]
+        for field in fields:
+            env.assertEqual(field[-1], "ISMISSING")
+            n_found += 1
+    env.assertEqual(n_found, 2)
