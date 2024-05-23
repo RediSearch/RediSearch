e99e1d36486031604240b82f5c1673d881019e9b
GuyAv46 <guy.avimor@gmail.com>
2024-05-23 17:51:29 +0300

    fix build for arm ubuntu

--- .install/ubuntu_22.04.sh
+++ .install/ubuntu_22.04.sh
@@ -4,5 +4,5 @@ export DEBIAN_FRONTEND=noninteractive
 MODE=$1 # whether to install using sudo or not
 
 $MODE apt update -qq
-$MODE apt install -yqq git wget build-essential lcov openssl libssl-dev python3 python3-venv python3-dev unzip rsync
+$MODE apt install -yqq git wget build-essential lcov openssl libssl-dev python3 python3-venv python3-dev unzip rsync pkg-config
 source install_cmake.sh $MODE
