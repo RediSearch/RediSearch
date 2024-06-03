71ad0b8ab93540ed4b5d6902998f4fd56129852a
meiravgri <meirav.grimberg@redis.com>
2024-06-03 06:46:24 +0000

    software-properties-common needed to get the ppa is broken on ubuntu16.04 Add the ppa manually

--- .install/ubuntu_16.04.sh
+++ .install/ubuntu_16.04.sh
@@ -24,11 +24,13 @@ pyenv global $PYTHON_VER
 
 # install gcc and git
 cd -
-$MODE apt install -yqq software-properties-common
-$MODE add-apt-repository ppa:ubuntu-toolchain-r/test -y
-$MODE add-apt-repository ppa:git-core/ppa -y
-$MODE apt update
+# software-properties-common needed to get the ppa is broken on ubuntu16.04
+# Add the ppa manually
+cd /etc/apt/sources.list.d
+echo "deb http://ppa.launchpad.net/ubuntu-toolchain-r/test/ubuntu xenial main" | $MODE tee ubuntu-toolchain-r-test.list
+$MODE apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 60C317803A41BA51845E371A1E9377A2BA9EF27F
+$MODE apt-get update
 $MODE apt install -yqq gcc-9 g++-9 git
 $MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-9
-
+cd -
 source install_cmake.sh $MODE
