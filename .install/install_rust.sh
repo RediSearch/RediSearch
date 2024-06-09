a749299eb0b4e4a6452e30ff25a733142bedb0c7
DvirDukhan <dvir@redis.com>
2024-06-09 22:06:08 +0300

    wip

--- .install/install_rust.sh
+++ .install/install_rust.sh
@@ -4,10 +4,11 @@ OS_TYPE=$(uname -s)
 
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+. "$HOME/.cargo/env"  
 rustup update 
 rustup update nightly
 # for RedisJSON build with addess santizer
-ustup component add rust-src --toolchain nightly
+rustup component add rust-src --toolchain nightly
 # if [[ $OS_TYPE = 'Darwin' ]]
 # then
 #     rustup component add rust-src --toolchain nightly-aarch64-apple-darwin
