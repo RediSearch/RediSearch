224c12d9f3e50acf411f764082c017eb37bbb598
DvirDukhan <dvir@redis.com>
2024-06-10 09:39:17 +0300

    added mode for llvm install

--- .install/install_llvm.sh
+++ .install/install_llvm.sh
@@ -2,11 +2,15 @@
 
 OS_TYPE=$(uname -s)
 
+MODE=$1 
+
+
+
 if [[ $OS_TYPE == Darwin ]]; then
     brew install llvm@17
 else
+    $MODE apt install lsb-release wget software-properties-common gnupg
     wget https://apt.llvm.org/llvm.sh
     chmod +x llvm.sh
-    apt install lsb-release wget software-properties-common gnupg
-    ./llvm.sh 17
+    $MODE ./llvm.sh 17
 fi
