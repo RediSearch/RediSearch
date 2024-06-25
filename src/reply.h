0ac01dfbd07d54852bef8fc8a016169a078c6597
GuyAv46 <47632673+GuyAv46@users.noreply.github.com>
2024-06-25 16:12:52 +0300

    [2.8] Fix crucial simple string misuse - [MOD-7258] (#4792)
    Fix crucial simple string misuse - [MOD-7258] (#4771)
    
    * implement checking and escaping unsafe strings for simple-string reply
    
    * reply the correct way for debug commands
    
    * use new functions
    
    * more fixes
    
    * fix resp3 required field name reply
    
    * make clusterinfo safe
    
    * tidy up
    
    * added test
    
    (cherry picked from commit e76120804615a225d6a3c1c9cb35d137e2f5ab24)

--- src/reply.h
+++ src/reply.h
@@ -47,8 +47,10 @@ int RedisModule_EndReply(RedisModule_Reply *reply);
 int RedisModule_Reply_LongLong(RedisModule_Reply *reply, long long val);
 int RedisModule_Reply_Double(RedisModule_Reply *reply, double val);
 int RedisModule_Reply_SimpleString(RedisModule_Reply *reply, const char *val);
+int RedisModule_Reply_CString(RedisModule_Reply *reply, const char *val);
 int RedisModule_Reply_StringBuffer(RedisModule_Reply *reply, const char *val, size_t len);
 int RedisModule_Reply_Stringf(RedisModule_Reply *reply, const char *fmt, ...);
+int RedisModule_Reply_SimpleStringf(RedisModule_Reply *reply, const char *fmt, ...);
 int RedisModule_Reply_String(RedisModule_Reply *reply, const RedisModuleString *val);
 int RedisModule_Reply_Null(RedisModule_Reply *reply);
 int RedisModule_Reply_Error(RedisModule_Reply *reply, const char *error);
@@ -65,6 +67,7 @@ int RedisModule_ReplyKV_LongLong(RedisModule_Reply *reply, const char *key, long
 int RedisModule_ReplyKV_Double(RedisModule_Reply *reply, const char *key, double val);
 int RedisModule_ReplyKV_SimpleString(RedisModule_Reply *reply, const char *key, const char *val);
 int RedisModule_ReplyKV_StringBuffer(RedisModule_Reply *reply, const char *key, const char *val, size_t len);
+int RedisModule_ReplyKV_SimpleStringf(RedisModule_Reply *reply, const char *key, const char *fmt, ...);
 int RedisModule_ReplyKV_String(RedisModule_Reply *reply, const char *key, const RedisModuleString *val);
 int RedisModule_ReplyKV_Null(RedisModule_Reply *reply, const char *key);
 int RedisModule_ReplyKV_Array(RedisModule_Reply *reply, const char *key);
@@ -73,5 +76,20 @@ int RedisModule_ReplyKV_Map(RedisModule_Reply *reply, const char *key);
 int RedisModule_ReplyKVorV_SimpleString(RedisModule_Reply *reply, const char *key, const char *val);
 
 void print_reply(RedisModule_Reply *reply);
+
+/*
+ * This function is a workaround helper for replying with a string that may contain
+ * newlines or other characters that are not safe for RESP Simple Strings.
+ * Should be removed once we can replace all SimpleString replies with BulkString replies.
+ */
+static inline bool isUnsafeForSimpleString(const char *str) {
+  return strpbrk(str, "\r\n") != NULL;
+}
+/*
+ * This function is a workaround helper for replying with a string that may contain
+ * newlines or other characters that are not safe for RESP Simple Strings.
+ * Should be removed once we can replace all SimpleString replies with BulkString replies.
+ */
+char *escapeSimpleString(const char *str);
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
