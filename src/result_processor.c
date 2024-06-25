4e20fcde2b96098bd0a2f0cb01a6d39fec21791b
meiravgri <meirav.grimberg@redis.com>
2024-06-25 04:57:45 +0000

    mark result as expired in following accesses

--- src/result_processor.c
+++ src/result_processor.c
@@ -624,8 +624,10 @@ typedef struct {
 } RPLoader;
 
 static void rpLoader_loadDocument(RPLoader *self, SearchResult *r) {
-  // If the document was modified or deleted, we don't load it
+  // If the document was modified or deleted, we don't load it, and we need to mark
+  // the result as expired.
   if ((r->dmd->flags & Document_FailedToOpen) || (r->dmd->flags & Document_Deleted)) {
+    r->flags |= Result_ExpiredDoc;
     return;
   }
 
