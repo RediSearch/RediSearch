153d8cc90a0f7b1f1b7758bffd7f66b6b587ec89
GuyAv46 <guy.avimor@gmail.com>
2024-07-02 18:13:09 +0300

    fix installation

--- .install/amazon_linux_2.sh
+++ .install/amazon_linux_2.sh
@@ -7,6 +7,9 @@ $MODE yum update -y
 # Install the RPM package that provides the Software Collections (SCL) required for devtoolset-11
 $MODE yum install -y https://vault.centos.org/centos/7/extras/x86_64/Packages/centos-release-scl-rh-2-3.el7.centos.noarch.rpm
 
+$MODE sed -i 's/mirrorlist=/#mirrorlist=/g' /etc/yum.repos.d/CentOS-SCLo-scl-rh.repo
+$MODE sed -i 's/#baseurl=http:\/\/mirror.centos.org/baseurl=http:\/\/mirror.nsc.liu.se\/centos-store/g' /etc/yum.repos.d/CentOS-SCLo-scl-rh.repo
+
 $MODE yum install -y wget git which devtoolset-11-gcc devtoolset-11-gcc-c++ devtoolset-11-make rsync python3 unzip
 
 source /opt/rh/devtoolset-11/enable
