2c360ca1346ba2e8efc951d42eb9bc7b16fd8c97
nafraf <nafraf@gmail.com>
2024-07-04 18:20:58 -0500

    Patch VectorSimilarity to compile it with GCC 12

--- .install/debian_gnu_linux_12.sh
+++ .install/debian_gnu_linux_12.sh
@@ -8,23 +8,12 @@ $MODE apt update -qq
 $MODE apt install -yqq git wget build-essential lcov openssl libssl-dev \
         python3 python3-venv python3-dev rsync unzip
 
-# Install GCC 13 on x86_64 because GCC 12 fails compilating VectorSimilarity
+# Patch VectorSimilarity because GCC 12 fails to compile it
 # https://gcc.gnu.org/bugzilla/show_bug.cgi?id=105593
 if [[ $ARCH = 'x86_64' ]]
 then
-    # Download and compile GCC 13
-    cd /tmp
-    wget https://ftp.gnu.org/gnu/gcc/gcc-13.3.0/gcc-13.3.0.tar.gz
-    tar -xf gcc-13.3.0.tar.gz
-    cd gcc-13.3.0
-    $MODE ./contrib/download_prerequisites
-    ./configure --disable-multilib --enable-languages=c,c++
-    make -j$(nproc)
-    $MODE make install
-    # Update alternatives
-    $MODE update-alternatives --install /usr/bin/gcc gcc /usr/local/bin/gcc 60 \
-        --slave /usr/bin/g++ g++ /usr/local/bin/g++
-    cd $OLDPWD
+        sed -i 's/set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Werror -Wall")/set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Werror -Wall -Wno-error=uninitialized")/g' \
+                ../deps/VectorSimilarity/src/VecSim/spaces/CMakeLists.txt
 fi
 
 source install_cmake.sh $MODE
