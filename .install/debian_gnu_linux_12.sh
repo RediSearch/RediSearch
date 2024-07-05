75252030ad51f1069ac1a2c7dd0148e516094c5d
nafraf <nafraf@gmail.com>
2024-07-05 14:57:31 -0500

    Install gcc-11

--- .install/debian_gnu_linux_12.sh
+++ .install/debian_gnu_linux_12.sh
@@ -1,11 +1,22 @@
+
 #!/bin/bash
 ARCH=$(uname -m)
-set -e
 export DEBIAN_FRONTEND=noninteractive
 MODE=$1 # whether to install using sudo or not
 
 $MODE apt update -qq
 $MODE apt install -yqq git wget build-essential lcov openssl libssl-dev \
         python3 python3-venv python3-dev rsync unzip
+
+# Install gcc-11 and g++-11 because gcc-12.2 fails to compile VectorSimilarity
+# https://gcc.gnu.org/bugzilla/show_bug.cgi?id=105593
+if [[ $ARCH = 'x86_64' ]]
+then
+        $MODE apt install -yqq gcc-11 g++-11
+        # Update alternatives
+        $MODE update-alternatives \
+                --install /usr/bin/gcc-11 gcc /usr/local/bin/gcc 60 \
+                --slave /usr/bin/g++-11 g++ /usr/local/bin/g++
+fi
 
 source install_cmake.sh $MODE
