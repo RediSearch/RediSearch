95f65b93307ef2c5bb58000fd2056a46045ef160
nafraf <nafraf@gmail.com>
2024-07-05 17:59:23 -0500

    Support amazonlinux2 arm64

--- .install/amazon_linux_2.sh
+++ .install/amazon_linux_2.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
+ARCH=$(uname -m)
 MODE=$1 # whether to install using sudo or not
 set -e
 export DEBIAN_FRONTEND=noninteractive
 
 $MODE yum update -y
-# Install the RPM package that provides the Software Collections (SCL) required for devtoolset-11
-$MODE yum install -y https://vault.centos.org/centos/7/extras/x86_64/Packages/centos-release-scl-rh-2-3.el7.centos.noarch.rpm
 
-# http://mirror.centos.org/centos/7/ is deprecated, so we changed the above link to `https://vault.centos.org`,
-# and we have to change the baseurl in the repo file to the working mirror (from mirror.centos.org to vault.centos.org)
-$MODE sed -i 's/mirrorlist=/#mirrorlist=/g' /etc/yum.repos.d/CentOS-SCLo-scl-rh.repo                        # Disable mirrorlist
-$MODE sed -i 's/#baseurl=http:\/\/mirror/baseurl=http:\/\/vault/g' /etc/yum.repos.d/CentOS-SCLo-scl-rh.repo # Enable a working baseurl
+if [[ $ARCH = 'x86_64' ]]
+then
+    # Install the RPM package that provides the Software Collections (SCL) required for devtoolset-11
+    $MODE yum install -y https://vault.centos.org/centos/7/extras/x86_64/Packages/centos-release-scl-rh-2-3.el7.centos.noarch.rpm
 
-$MODE yum install -y wget git which devtoolset-11-gcc devtoolset-11-gcc-c++ devtoolset-11-make rsync python3 unzip
+    # http://mirror.centos.org/centos/7/ is deprecated, so we changed the above link to `https://vault.centos.org`,
+    # and we have to change the baseurl in the repo file to the working mirror (from mirror.centos.org to vault.centos.org)
+    $MODE sed -i 's/mirrorlist=/#mirrorlist=/g' /etc/yum.repos.d/CentOS-SCLo-scl-rh.repo                        # Disable mirrorlist
+    $MODE sed -i 's/#baseurl=http:\/\/mirror/baseurl=http:\/\/vault/g' /etc/yum.repos.d/CentOS-SCLo-scl-rh.repo # Enable a working baseurl
 
-source /opt/rh/devtoolset-11/enable
+    $MODE yum install -y wget git which devtoolset-11-gcc devtoolset-11-gcc-c++ devtoolset-11-make rsync python3 unzip
 
-cp /opt/rh/devtoolset-11/enable /etc/profile.d/scl-devtoolset-11.sh
+    source /opt/rh/devtoolset-11/enable
+
+    cp /opt/rh/devtoolset-11/enable /etc/profile.d/scl-devtoolset-11.sh
+else
+    $MODE yum install -y wget git which gcc gcc-c++ make rsync python3 python3-devel unzip
+fi
 
 $MODE yum install -y openssl11 openssl11-devel
 $MODE ln -s `which openssl11` /usr/bin/openssl
