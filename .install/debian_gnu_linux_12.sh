9c92d8f95d74e2e4f396e285d63f26d7ecb58ee7
nafraf <nafraf@gmail.com>
2024-07-04 19:03:29 -0500

    Remove VectorSimilarity patch

--- .install/debian_gnu_linux_12.sh
+++ .install/debian_gnu_linux_12.sh
@@ -8,12 +8,4 @@ $MODE apt update -qq
 $MODE apt install -yqq git wget build-essential lcov openssl libssl-dev \
         python3 python3-venv python3-dev rsync unzip
 
-# Patch VectorSimilarity because GCC 12 fails to compile it
-# https://gcc.gnu.org/bugzilla/show_bug.cgi?id=105593
-if [[ $ARCH = 'x86_64' ]]
-then
-        sed -i 's/set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Werror -Wall")/set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Werror -Wall -Wno-error=uninitialized")/g' \
-                ../deps/VectorSimilarity/src/VecSim/spaces/CMakeLists.txt
-fi
-
 source install_cmake.sh $MODE
