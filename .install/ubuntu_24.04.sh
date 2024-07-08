a5f082d304df1e5fc0827d723527b44adb73c020
nafraf <nafraf@gmail.com>
2024-07-08 14:49:39 -0500

    build redisjson module if not found in S3

--- .install/ubuntu_24.04.sh
+++ .install/ubuntu_24.04.sh
@@ -4,5 +4,6 @@ export DEBIAN_FRONTEND=noninteractive
 MODE=$1 # whether to install using sudo or not
 
 $MODE apt update -qq
-$MODE apt install -yqq git wget build-essential lcov openssl libssl-dev python3 python3-venv python3-dev unzip rsync
+$MODE apt install -yqq git wget build-essential lcov openssl libssl-dev \
+    python3 python3-venv python3-dev unzip rsync clang
 source install_cmake.sh $MODE
