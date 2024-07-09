5227129412714345f73561dc09dba330b4ec39fd
meiravgri <meirav.grimberg@redis.com>
2024-07-09 10:06:18 +0000

    add sudo to update-alternatives

--- .install/ubuntu_22.04.sh
+++ .install/ubuntu_22.04.sh
@@ -7,5 +7,5 @@ $MODE apt update -qq
 $MODE apt install -yqq gcc-12 g++-12 git wget build-essential lcov openssl libssl-dev python3 python3-venv python3-dev unzip rsync
 $MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-12 60 --slave /usr/bin/g++ g++ /usr/bin/g++-12
 # align gcov version with gcc version
-update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-12 60
+$MODE update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-12 60
 source install_cmake.sh $MODE
