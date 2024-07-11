0ea3338daf565b58ad382cd7360b8926f465d614
nafraf <nafraf@gmail.com>
2024-07-11 15:50:23 -0500

    Check __GLIBC__

--- .install/alpine_linux_3.19.sh
+++ .install/alpine_linux_3.19.sh
@@ -4,9 +4,9 @@ set -e
 
 $MODE apk update
 
-$MODE apk add --no-cache gcc g++ make linux-headers openblas-dev xsimd curl \
-    wget git python3 python3-dev py3-pip openssl openssl-dev tar xz which \
-    rsync clang musl-dev
+$MODE apk add --no-cache build-base gcc g++ make linux-headers openblas-dev \
+    xsimd curl wget git python3 python3-dev py3-pip openssl openssl-dev \
+    tar xz which rsync clang 
 
 $MODE apk add cmake --no-cache
 #source install_cmake.sh $MODE
