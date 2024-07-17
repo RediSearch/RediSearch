977d6a26136b7fb2e4d9f1c07c9fc900bafb8580
jonathan keinan <jonathan.keinan@redis.com>
2024-07-17 11:58:53 +0300

    * release iterator

--- src/indexer.c
+++ src/indexer.c
@@ -328,6 +328,7 @@ static void writeMissingFieldDocs(RSAddDocumentCtx *aCtx, RedisSearchCtx *sctx, 
     RSIndexResult rec = {.type = RSResultType_Virtual, .docId = docId, .offsetsSz = 0, .freq = 0};
     InvertedIndex_WriteEntryGeneric(iiMissingDocs, enc, docId, &rec);
   }
+  dictReleaseIterator(iter);
   dictRelease(df_fields_dict);
 }
 
