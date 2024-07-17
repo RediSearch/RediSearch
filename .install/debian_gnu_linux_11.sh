de9c00b160e35108b58fe6563b0bbe53d6a32414
nafraf <nafraf@gmail.com>
2024-07-16 21:30:31 -0500

    Support debian:bullseye arm64

--- .install/debian_gnu_linux_11.sh
+++ .install/debian_gnu_linux_11.sh
@@ -4,5 +4,6 @@ export DEBIAN_FRONTEND=noninteractive
 MODE=$1 # whether to install using sudo or not
 
 $MODE apt update -qq
-$MODE apt install -yqq git wget build-essential lcov openssl libssl-dev python3 python3-venv rsync unzip
+$MODE apt install -yqq git wget build-essential lcov openssl libssl-dev \
+        python3 python3-venv python3-dev rsync unzip
 source install_cmake.sh $MODE
