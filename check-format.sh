94665a6f774222e7a8fab4c290091dff2e0bc0f6
DvirDukhan <dvir@redis.com>
2024-07-29 14:52:05 +0300

    check format

---
+++ check-format.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+CLANG_FMT_SRCS=$(find ./src/ \( -name '*.c' -o -name '*.cc' -o -name '*.cpp' -o -name '*.h' -o -name '*.hh' -o -name '*.hpp' \))
+CLANG_FMT_SRCS+=$(find ./coord/ \( -name '*.c' -o -name '*.cc' -o -name '*.cpp' -o -name '*.h' -o -name '*.hh' -o -name '*.hpp' \))
+CLANG_FMT_TESTS="$(find ./tests/ -type d \( -path ./tests/unit/build \) -prune -false -o  \( -name '*.c' -o -name '*.cc' -o -name '*.cpp' -o -name '*.h' -o -name '*.hh' -o -name '*.hpp' \))"
+
+if [[ $FIX == 1 ]]; then
+    clang-format --verbose -style=file -i $CLANG_FMT_SRCS $CLANG_FMT_TESTS
+else
+    clang-format -style=file -Werror --dry-run $CLANG_FMT_SRCS $CLANG_FMT_TESTS
+fi
