ac4f9f75c0f26b4fb2abf60ebbd57ddfccf064d9
GuyAv46 <47632673+GuyAv46@users.noreply.github.com>
2024-08-02 17:14:45 +0300

    [2.8] VERBATIM is not passed to the shards on FT.AGGREGATE - [MOD-7463] (#4896)
    VERBATIM is not passed to the shards on FT.AGGREGATE - [MOD-7463] (#4894)
    
    * add failing test
    
    * fix bug
    
    (cherry picked from commit 440d83750f700eaddd5255764524429ce3c50d3a)

--- coord/src/dist_aggregate.c
+++ coord/src/dist_aggregate.c
@@ -550,6 +550,10 @@ static void buildMRCommand(RedisModuleString **argv, int argc, int profileArgs,
     array_append(tmparr, RedisModule_StringPtrLen(argv[argOffset + 3 + 1 + profileArgs], NULL));  // the format
   }
 
+  if (RMUtil_ArgIndex("VERBATIM", argv + 3 + profileArgs, argc - 3 - profileArgs) != -1) {
+    array_append(tmparr, "VERBATIM");
+  }
+
   for (size_t ii = 0; ii < us->nserialized; ++ii) {
     array_append(tmparr, us->serialized[ii]);
   }
