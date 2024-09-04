6956aaf604a6556abb178f16b31792477deea3bc
GuyAv46 <guy.avimor@gmail.com>
2024-09-04 11:31:00 +0200

    use the right log

--- src/spec.c
+++ src/spec.c
@@ -2699,8 +2699,8 @@ int Indexes_RdbLoad(RedisModuleIO *rdb, int encver, int when) {
   QueryError status = {0};
   for (size_t i = 0; i < nIndexes; ++i) {
     if (IndexSpec_CreateFromRdb(ctx, rdb, encver, &status) != REDISMODULE_OK) {
-      RedisModule_Log(ctx, "warning", "RDB Load: %s",
-                      status.detail ? status.detail : "general failure");
+      RedisModule_LogIOError(rdb, "warning", "RDB Load: %s",
+                             status.detail ? status.detail : "general failure");
       return REDISMODULE_ERR;
     }
   }
