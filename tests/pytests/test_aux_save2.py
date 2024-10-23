7e061ee83d9f1e2ccf26723d36f891bc87270b4d
nafraf <nafraf@gmail.com>
2024-10-23 14:29:32 -0500

    Test loading rdb with aux data

--- tests/pytests/test_aux_save2.py
+++ tests/pytests/test_aux_save2.py
@@ -2,7 +2,7 @@ from common import *
 from RLTest import Env
 
 @skip(cluster=True, no_json=True, asan=True)
-def test_aux_save2(env: Env):
+def test_loading_rdb_without_aux_data(env: Env):
     env.expect('FT.CREATE', 'idx', 'SCHEMA', 't', 'TEXT').equal('OK')
     env.expect('HSET', 'doc1', 't', 'hello').equal(1)
     env.expect('FT.SEARCH', 'idx', 'hello', 'NOCONTENT').equal([1, 'doc1'])
@@ -21,5 +21,32 @@ def test_aux_save2(env: Env):
     # does not contains module aux data
     env.start()
     env.expect('HGET', 'doc1', 't').equal('hello')
+
+@skip(cluster=True, no_json=True, asan=True)
+def test_loading_rdb_with_aux_data(env: Env):
+    env.expect('FT.CREATE', 'idx', 'SCHEMA', 't', 'TEXT').equal('OK')
+    env.expect('HSET', 'doc1', 't', 'hello').equal(1)
+    env.expect('FT.SEARCH', 'idx', 'hello', 'NOCONTENT').equal([1, 'doc1'])
+    # Save state to RDB
+    env.stop()
+    # Restart without modules. 
+    env.assertEqual(len(env.envRunner.modulePath), 2)
+    env.assertEqual(len(env.envRunner.moduleArgs), 2)
+    env.envRunner.modulePath.pop()
+    env.envRunner.moduleArgs.pop()
+    env.envRunner.modulePath.pop()
+    env.envRunner.moduleArgs.pop()
+    env.envRunner.masterCmdArgs = env.envRunner.createCmdArgs('master')
+    # Attempt to load RDB should fail because the RDB contains module aux data
+    try:
+        env.start()
+    except Exception as e:
+        expected_msg = 'Redis server is dead'
+        env.assertContains(expected_msg, str(e))
+        if expected_msg not in str(e):
+            raise e
+    finally:
+        env.assertFalse(env.isUp()) 
+
 
 
