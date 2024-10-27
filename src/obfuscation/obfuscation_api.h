364ee9cd0641185704a5f6b89c42b2aa4d942ea4
jonathan keinan <jonathan.keinan@redis.com>
2024-10-27 16:46:04 +0200

    * add copyright

--- src/obfuscation/obfuscation_api.h
+++ src/obfuscation/obfuscation_api.h
@@ -1,6 +1,8 @@
-//
-// Created by jonathan on 9/18/24.
-//
+/*
+* Copyright Redis Ltd. 2016 - present
+ * Licensed under your choice of the Redis Source Available License 2.0 (RSALv2) or
+ * the Server Side Public License v1 (SSPLv1).
+ */
 
 #ifndef OBFUSCATION_API_H
 #define OBFUSCATION_API_H
