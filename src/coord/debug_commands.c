8cffbb6171a1c1665f8a07c217e7dd6688188536
GuyAv46 <47632673+GuyAv46@users.noreply.github.com>
2024-10-31 09:52:02 +0200

    Remove an unnecessary assertion - [MOD-8008] (#5138)
    * remove unneeded assertion
    
    * added a test

--- src/coord/debug_commands.c
+++ src/coord/debug_commands.c
@@ -5,6 +5,7 @@
  */
 
 #include "coord/rmr/rmr.h"
+#include "coord/rmr/rq.h"
 #include "debug_commands.h"
 #include "debug_command_names.h"
 #include "coord/rmr/redis_cluster.h"
@@ -34,10 +35,17 @@ DEBUG_COMMAND(resumeTopologyUpdater) {
   }
 }
 
+DEBUG_COMMAND(clearTopology) {
+  if (argc != 2) return RedisModule_WrongArity(ctx);
+  RQ_Debug_ClearPendingTopo();
+  return RedisModule_ReplyWithSimpleString(ctx, "OK");
+}
+
 DebugCommandType coordCommands[] = {
   {"SHARD_CONNECTION_STATES", shardConnectionStates},
   {"PAUSE_TOPOLOGY_UPDATER", pauseTopologyUpdater},
   {"RESUME_TOPOLOGY_UPDATER", resumeTopologyUpdater},
+  {"CLEAR_PENDING_TOPOLOGY", clearTopology},
   {NULL, NULL}
 };
 // Make sure the two arrays are of the same size (don't forget to update `debug_command_names.h`)
