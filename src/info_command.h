41b53c55f0c4cbffe29848065ecdda8bc1c4f454
GuyAv46 <47632673+GuyAv46@users.noreply.github.com>
2024-11-06 17:00:04 +0200

    Add Active Indices Metrics - [MOD-7952] (#5160)
    * old IndexStats cleanup
    
    * count read and write operations
    
    * added count of active indices to the info command
    
    * add total active writes and total active queries gauges
    
    * rename variables

--- src/info_command.h
+++ src/info_command.h
@@ -36,6 +36,13 @@ typedef struct TotalSpecsInfo {
   // Indexing Errors
   size_t indexing_failures;      // Total count of indexing errors
   size_t max_indexing_failures;  // Maximum number of indexing errors among all specs
+
+  // Index
+  size_t num_active_indexes;           // Number of active indexes
+  size_t num_active_indexes_querying;  // Number of active read indexes
+  size_t num_active_indexes_indexing;  // Number of active write indexes
+  size_t total_active_writes;          // Total number of active writes
+  size_t total_active_queries;         // Total number of active queries (reads)
 } TotalSpecsInfo;
 
 int IndexInfoCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc);
