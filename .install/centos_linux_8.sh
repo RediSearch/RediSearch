14c3400017982c5ab0a552e1b4cf358cf416e39f
kei-nan <jonathan.keinan@redis.com>
2024-11-06 17:28:01 +0200

    MOD-8061: Backport Workflow Changes From 2.6 (#5140)
    * * initial commit
    
    * * partial copy failing flow tests from 2.6
    
    * * add .install folder from 2.6
    
    * update readies submodule commit
    
    * - copy includes.py from 2.6
    
    * - update sbin folder from 2.6
    
    * - update runtests.sh from 2.6
    
    * - add requirements.txt from 2.6
    
    * * change back to json 2.2
    
    * * another set of changes to support redis json so location
    
    * * try reverting some of the changes
    
    * * patch pudb version
    
    * * try and use lower redis and rltest packages, revert runtests.sh
    
    * * move to macos 12
    
    * * disable build-macos flow
    
    * * disable macos from merge queue
    
    * * add centos:8 to add support for rhel8
    
    * * add centos:8 support - part 2
    
    * * add centos:8 support - part 3
    
    * * add centos:8 support - part 4
    
    * * add centos:8 support - part 5
    
    * * add centos:8 support - part 6
    
    * * add centos:8 support - part 7

---
+++ .install/centos_linux_8.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+MODE=$1 # whether to install using sudo or not
+set -e
+export DEBIAN_FRONTEND=noninteractive
+ARCH=$([[ $(uname -m) == x86_64 ]] && echo x86_64 || echo noarch)
+
+# http://mirror.centos.org/centos/7/ is deprecated, so we have to disable mirrorlists
+# and change the baseurl in the repo file to the working mirror (from mirror.centos.org to vault.centos.org)
+set_all_baseurls() {
+    for file in /etc/yum.repos.d/*.repo; do
+        $MODE sed -i 's/^mirrorlist=/#mirrorlist=/g' $file
+        $MODE sed -i 's/^#[[:space:]]*baseurl=http:\/\/mirror/baseurl=http:\/\/vault/g' $file
+    done
+}
+
+set_all_baseurls # set the baseurls to the working mirror before installing basic packages
+$MODE yum update -y
+$MODE yum install -y https://packages.endpointdev.com/rhel/8/main/${ARCH}/endpoint-repo.noarch.rpm
+$MODE yum groupinstall -y "Development Tools"
+
+set_all_baseurls # set the baseurls again before installing devtoolset-11 (some new repos were added)
+$MODE yum -y install openssl-devel openssl bzip2-devel libffi-devel wget which git sqlite sqlite-devel\
+    gcc-toolset-11-gcc gcc-toolset-11-gcc-c++ gcc-toolset-11-make gcc-toolset-11-libatomic-devel rsync
+
+source /opt/rh/gcc-toolset-11/enable
+
+cp /opt/rh/gcc-toolset-11/enable /etc/profile.d/scl-gcc-toolset-11.sh
+
+# get a newer libstc++ library. The one that comes with the gcc version (6.0.19) will fail with
+# the error: `/usr/lib64/libstdc++.so.6: version `GLIBCXX_3.4.20' not found`
+wget https://redismodules.s3.amazonaws.com/gnu/libstdc%2B%2B.so.6.0.25-linux-x64.tgz
+file_name=$(tar -xvf libstdc++.so.6.0.25-linux-x64.tgz)
+$MODE mv $file_name /usr/lib64/
+cd /usr/lib64
+$MODE rm -f libstdc++.so.6  # remove the old symlink
+$MODE ln -s libstdc++.so.6.0.25 libstdc++.so.6
+cd -
+
+wget https://www.python.org/ftp/python/3.9.6/Python-3.9.6.tgz
+tar -xvf Python-3.9.6.tgz
+cd Python-3.9.6
+./configure --enable-optimizations
+$MODE make altinstall
+update-alternatives --install /usr/bin/python3 python3 `which python3.9` 2
+
+cd -
+source install_cmake.sh $MODE
---
+++ .install/ubuntu_16.04.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+export DEBIAN_FRONTEND=noninteractive
+MODE=$1 # whether to install using sudo or not
+
+$MODE apt update -qq
+$MODE apt upgrade -yqq
+$MODE apt dist-upgrade -yqq
+
+# Install python using pyenv
+PYTHON_VER=3.9
+$MODE apt install -yqq git make build-essential libssl-dev zlib1g-dev unzip rsync \
+				libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+				libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+cd ~/.pyenv && src/configure && make -C src
+export PYENV_ROOT=$HOME/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
+source ~/.bash_profile
+pyenv install $PYTHON_VER
+pyenv global $PYTHON_VER
+
+# install gcc and git
+cd -
+# software-properties-common needed to get the ppa is broken on ubuntu16.04
+# Add the ppa manually
+cd /etc/apt/sources.list.d
+# ppa to install gcc-9
+echo "deb http://ppa.launchpad.net/ubuntu-toolchain-r/test/ubuntu xenial main" | $MODE tee ubuntu-toolchain-r-test.list
+$MODE apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 60C317803A41BA51845E371A1E9377A2BA9EF27F
+# ppa to install git
+echo "deb http://ppa.launchpad.net/git-core/ppa/ubuntu xenial main" | $MODE tee git-core-test.list
+$MODE apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E1DD270288B4E6030699E45FA1715D88E1DF1F24
+$MODE apt-get update
+$MODE apt install -yqq gcc-9 g++-9 git
+$MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-9
+cd -
+source install_cmake.sh $MODE
