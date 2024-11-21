357d356db01fac23d522344bfb0855cb8a38f4d2
meiravgri <109056284+meiravgri@users.noreply.github.com>
2024-11-21 08:24:52 +0200

    MOD-8097, MOD-8114 Fix memory counting (#5204)
    * expose openNumericKeysDict, requires a write/read argument.
    
    replaces all OpenNumericIndex calls.
    
    * fork gc -initialiize index pointer only once
    
    rename createIndex to createPopulateTermsInvIndex
    
    add function to tests/index_utils
    
    generalize FGCTest suit
    add inheriting suite for tags
    
    some cosmetics in cpp_range
    
    ExtTest.testDynamicLoading: log when assert fails (the test halts upon assertion and the log will be nerver pronted)
    
    in IndexTest and QueryTest use IndexSpec_RemoveFromGlobals instead of StrongRef_Release to ensure the spec is properly cleaned
    otherwise some structs like the global prefix trie nodes, potentially accessed by following tests, might hold a copy of the spec refrence that was already released and cuase a crash.
    
    * add inverted index total size to the numeric tree root
    
    update in gc and in addition
    add cpp_range test and cpp_forkgc test
    
    * FIX:
    decrease gc->stats.totalCollected by bytesAdded to not count bytes added by us
    
    remove reseting currNode->range->invertedIndexSize in applyNumIdx called upon each node recieved from the child process
    instead, we will count the memory released when trimming the empty leaves
    
    call IndexSpec_TotalMemUsage from RediSearch_MemUsage instead of code duplication
    
    add IndexSpec_collect_numeric_overhead that counts the memory of NumericRangeTree in a given  spec
    call this function to add it to IndexSpec_TotalMemUsage
    
    update LLApiTest testInfo* accordingly
    
    * fixes
    
    * fix new cpp forkgc test
    
    * fix unsafe read
    
    * fix status check
    
    * numeric tree debug commands - if tree was not intizlied yet, reply as if it was an empty tree
    
    * fix reset range->invertedIndexSize in applyNumIdx when index is empty.
    it will be counted when the range is deleted
    
    * add initial inverted index size if numeric to test
    
    don't intialize the numeric inverted index in the child if it's empty
    
    * fix gc warning
    
    * little fix
    
    * changes rt->invertedIndexSize to rt->invertedIndexesSize
    
    fix IndexSpec_collect_numeric_overhead and add test
    
    fix sending NULL to FGC_sendFixed
    
    * CalculateNumericInvertedIndexMemory fix identation and break when failing
    
    * rename openNumericKeysDict variable write to initialize and change type to bool
    
    * change OPEN_INDEX_READ to DONT_CREATE_INDEX
    and OPEN_INDEX_WRITE to CREATE_INDEX
    
    add coments to tests
    
    * use Likst instead of list to support older python versopns
    
    * remove type from function args
    
    * fix
    
    ---------
    
    Co-authored-by: GuyAv46 <guy.avimor@gmail.com>

--- src/numeric_index.c
+++ src/numeric_index.c
@@ -387,6 +387,7 @@ NumericRangeTree *NewNumericRangeTree() {
 
   // updated value since splitCard should be >NR_CARD_CHECK
   ret->root = NewLeafNode(2, 16);
+  ret->invertedIndexesSize = ret->root->range->invertedIndexSize;
   ret->numEntries = 0;
   ret->numRanges = 1;
   ret->revisionId = 0;
@@ -415,6 +416,7 @@ NRN_AddRv NumericRangeTree_Add(NumericRangeTree *t, t_docId docId, double value,
   }
   t->numRanges += rv.numRanges;
   t->numEntries++;
+  t->invertedIndexesSize += rv.sz;
 
   return rv;
 }
@@ -494,6 +496,7 @@ NRN_AddRv NumericRangeTree_TrimEmptyLeaves(NumericRangeTree *t) {
     t->revisionId++;
     t->numRanges += rv.numRanges;
     t->emptyLeaves = 0;
+    t->invertedIndexesSize += rv.sz;
   }
   return rv;
 }
@@ -574,13 +577,13 @@ RedisModuleString *fmtRedisNumericIndexKey(const RedisSearchCtx *ctx, const char
                                         field);
 }
 
-static NumericRangeTree *openNumericKeysDict(IndexSpec* spec, RedisModuleString *keyName,
-                                             int write) {
+NumericRangeTree *openNumericKeysDict(IndexSpec* spec, RedisModuleString *keyName,
+                                             bool create_if_missing) {
   KeysDictValue *kdv = dictFetchValue(spec->keysDict, keyName);
   if (kdv) {
     return kdv->p;
   }
-  if (!write) {
+  if (!create_if_missing) {
     return NULL;
   }
   kdv = rm_calloc(1, sizeof(*kdv));
@@ -608,7 +611,7 @@ struct indexIterator *NewNumericFilterIterator(const RedisSearchCtx *ctx, const 
 
     t = RedisModule_ModuleTypeGetValue(key);
   } else {
-    t = openNumericKeysDict(ctx->spec, s, 0);
+    t = openNumericKeysDict(ctx->spec, s, DONT_CREATE_INDEX);
   }
 
   if (!t) {
@@ -629,10 +632,6 @@ struct indexIterator *NewNumericFilterIterator(const RedisSearchCtx *ctx, const 
     ConcurrentSearch_AddKey(csx, NumericRangeIterator_OnReopen, uc, rm_free);
   }
   return it;
-}
-
-NumericRangeTree *OpenNumericIndex(const RedisSearchCtx *ctx, RedisModuleString *keyName) {
-  return openNumericKeysDict(ctx->spec, keyName, 1);
 }
 
 void __numericIndex_memUsageCallback(NumericRangeNode *n, void *ctx) {
@@ -820,7 +819,7 @@ void NumericRangeIterator_OnReopen(void *privdata) {
   IndexIterator *it = nu->it;
 
   RedisModuleString *numField = IndexSpec_GetFormattedKeyByName(sp, nu->fieldName, INDEXFLD_T_NUMERIC);
-  NumericRangeTree *rt = openNumericKeysDict(sp, numField, 0);
+  NumericRangeTree *rt = openNumericKeysDict(sp, numField, DONT_CREATE_INDEX);
 
   if (!rt || rt->revisionId != nu->lastRevId) {
     // The numeric tree was either completely deleted or a node was splitted or removed.
