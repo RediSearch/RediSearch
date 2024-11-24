4ad00e898ef2c29ea37e89e60527d3418a86aa79
Raz Monsonego <74051729+raz-mon@users.noreply.github.com>
2024-11-24 19:30:29 +0200

    MOD-8129: Fix indexed_percent info stat (#5238)
    Fix indexed_percent info stat

--- src/spec.h
+++ src/spec.h
@@ -634,10 +634,10 @@ typedef struct IndexesScanner {
   bool cancelled;
   WeakRef spec_ref;
   char *spec_name;
-  size_t scannedKeys, totalKeys;
+  size_t scannedKeys;
 } IndexesScanner;
 
-double IndexesScanner_IndexedPercent(IndexesScanner *scanner, IndexSpec *sp);
+double IndexesScanner_IndexedPercent(RedisModuleCtx *ctx, IndexesScanner *scanner, IndexSpec *sp);
 
 /**
  * @return the overhead used by the TAG fields in `sp`, i.e., the size of the
