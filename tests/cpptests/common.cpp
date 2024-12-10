5f3ce6476900f9574a12118d338d7ac64aee49b4
meiravgri <109056284+meiravgri@users.noreply.github.com>
2024-12-10 07:56:33 +0200

    [2.8][MOD-8115] Free spec resources in the main thread (#5324) (#5330)
    [MOD-8115] Free spec resources in the main thread (#5324)
    
    * free spec resources in the main thread.
    
    * improve cpp gc tests suite:
    allow multiple gc runs in each test
    
    move args to test suite members
    use pthread_join instead of pthread_detach
    
    loop to enable multiple gc interval taken from https://github.com/RediSearch/RediSearch/pull/5049
    
    (cherry picked from commit fc508014c80a3dd7b88340c59d79b53a34008bf2)

--- tests/cpptests/common.cpp
+++ tests/cpptests/common.cpp
@@ -26,6 +26,7 @@ class MyEnvironment : public ::testing::Environment {
     const char *arguments[] = {"SAFEMODE", "NOGC"};
     // No arguments..
     RMCK_Bootstrap(my_OnLoad, arguments, 2);
+    RSGlobalConfig.freeResourcesThread = false;
   }
 
   virtual void TearDown() {
