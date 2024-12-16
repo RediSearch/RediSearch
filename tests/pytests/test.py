9f6cea82c078c69fc0f7e17a366602eb05650649
itzik.vaknin@redis.com <itzikvaknin6@gmail.com>
2024-12-16 17:21:54 +0200

    Remove blank lines Add .contains() to error assertion

--- tests/pytests/test.py
+++ tests/pytests/test.py
@@ -2611,14 +2611,12 @@ def testAlias(env):
     r = env.cmd('ft.search', 'temp', 'foo')
     env.assertEqual([1, 'doc3', ['t1', 'foo']], r)
 
-
 def testAliasIndexConflict(env):
-    env.cmd('ft.create', 'idx', 'ON', 'HASH', 'PREFIX', 1, 'doc1', 'schema', 't1', 'text')
-    env.cmd('ft.create', 'tmp_index_name', 'ON', 'HASH', 'PREFIX', 1, 'doc2', 'schema', 't1', 'text')
-    env.expect('ft.aliasAdd', 'tmp_index_name', 'idx').error()
+    env.cmd('ft.create', 'idx', 'ON', 'HASH', 'schema', 't1', 'text')
+    env.cmd('ft.create', 'tmp_index_name', 'ON', 'HASH', 'schema', 't1', 'text')
+    env.expect('ft.aliasAdd', 'tmp_index_name', 'idx').error().contains('Alias conflicts with an existing index name')
     env.cmd('ft.drop', 'tmp_index_name')
     env.cmd('ft.aliasAdd', 'tmp_index_name', 'idx')
-
 
 def testNoCreate(env):
     env.cmd('ft.create', 'idx', 'ON', 'HASH', 'schema', 'f1', 'text')
