8b3489b3d7b8d134de874044feb4e8b7205643db
jonathan keinan <jonathan.keinan@redis.com>
2024-12-17 18:24:59 +0200

    * compilation + rebase fixes

--- src/obfuscation/format.c
+++ src/obfuscation/format.c
@@ -1,7 +1,8 @@
 #include "obfuscation/format.h"
 
 const char* FormatHiddenText(HiddenString *name, bool obfuscate) {
-  return obfuscate ? Obfuscate_Text() : HiddenString_GetUnsafe(name, NULL);
+  const char *value = HiddenString_GetUnsafe(name, NULL);
+  return obfuscate ? Obfuscate_Text(value) : value;
 }
 
 const char* FormatHiddenField(HiddenString *name, t_uniqueId fieldId, char buffer[MAX_OBFUSCATED_FIELD_NAME], bool obfuscate) {
