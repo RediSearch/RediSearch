3b5e5f14cf430ac423bf1acfda30358f4c2be452
DvirDukhan <dvir@redis.com>
2025-01-02 16:58:32 +0200

    Update_macos_names_for_s3 (#5432)
    * updated readies version
    
    * commented out macos defs on pack etc..
    
    * remove comments
    
    (cherry picked from commit 6f9a96ec7a541f38bc3c0882d0c8fd9611b9f748)

--- sbin/pack.sh
+++ sbin/pack.sh
@@ -75,14 +75,6 @@ OSNICK=$($READIES/bin/platform --osnick)
 [[ $OSNICK == rocky8 ]]  && OSNICK=rhel8
 [[ $OSNICK == rocky9 ]]  && OSNICK=rhel9
 
-if [[ $OS == macos ]]; then
-	# as we don't build on macOS for every platform, we converge to a least common denominator
-	if [[ $ARCH == x86_64 ]]; then
-		OSNICK=catalina  # to be aligned with the rest of the modules in redis stack
-	else
-		OSNICK=monterey
-	fi
-fi
 
 PLATFORM="$OS-$OSNICK-$ARCH"
 
