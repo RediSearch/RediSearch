049a7f7370cb61af5aae96da73bb3883ab9d86ac
lerman25 <58445352+lerman25@users.noreply.github.com>
2025-01-07 16:35:14 +0200

    MOD-7570: Valid safe vector index pointer access (#5422)
    * null ptr check + pytest
    
    * pytest + small fix
    
    * fix api calls, add debug test, remove field&index ft.info errors
    
    * format fixes, add pytest to debug dumpHNSW
    
    * fix kdv free
    
    * Comment change and PING after flushall
    
    (cherry picked from commit aeb1d54fb324051eb8c5b97204273f658da20de7)

--- src/debug_commands.c
+++ src/debug_commands.c
@@ -1152,6 +1152,10 @@ DEBUG_COMMAND(VecsimInfo) {
   // This call can't fail, since we already checked that the key exists
   // (or should exist, and this call will create it).
   VecSimIndex *vecsimIndex = openVectorIndex(sctx->spec, keyName, CREATE_INDEX);
+  if(!vecsimIndex) {
+    SearchCtx_Free(sctx);
+    return RedisModule_ReplyWithError(ctx, "Can't open vector index");
+  }
 
   VecSimInfoIterator *infoIter = VecSimIndex_InfoIterator(vecsimIndex);
   // Recursively reply with the info iterator
@@ -1218,6 +1222,12 @@ DEBUG_COMMAND(dumpHNSWData) {
   // This call can't fail, since we already checked that the key exists
   // (or should exist, and this call will create it).
   VecSimIndex *vecsimIndex = openVectorIndex(sctx->spec, keyName, CREATE_INDEX);
+  if(!vecsimIndex) {
+    RedisModule_ReplyWithError(ctx, "Can't open vector index");
+    goto cleanup;
+  }
+
+
   VecSimIndexBasicInfo info = VecSimIndex_BasicInfo(vecsimIndex);
   if (info.algo != VecSimAlgo_HNSWLIB) {
 	  RedisModule_ReplyWithError(ctx, "Vector index is not an HNSW index");
