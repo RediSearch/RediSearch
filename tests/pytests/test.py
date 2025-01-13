f1eb3aeff00a411849930808d0398e6fdfdaabbb
Itzikvaknin <82322982+Itzikvaknin@users.noreply.github.com>
2025-01-13 18:39:29 +0200

    MOD-5437: Report GIL time in FT.PROFILE (#5439)
    * Add GIL time data collection in ResultProcessor for FT.PROFILE reporting
    
    * Rename vars
    
    * Add test GIL ft.profile
    
    * Add main thread time to ft.report
    Small changes
    
    * Use wall clock instead of cpu time clock
    
    * Fix test serialization error
    
    * Add total GIL time field to ft.profile expected results
    
    * Fix coordinator tests
    
    * Rename vars
    
    * Fix use after free
    
    * Gather isProfile set to one func
    
    * Change style
    
    * Fix
    
    * Refactor and Add tests
    
    * Fix test
    
    * Add rs_timer_ms
    Support RP_Safe_loader multiple executions scenario
    Clean
    
    * Fix typo
    
    * Clean
    
    * Clean
    
    * Add new empty line
    
    * Clean

--- tests/pytests/test.py
+++ tests/pytests/test.py
@@ -3962,7 +3962,7 @@ def test_RED_86036(env):
     for i in range(1000):
         env.cmd('hset', f"doc{i}", 't', 'foo')
     res = env.cmd('FT.PROFILE', 'idx', 'search', 'query', '*', 'INKEYS', '2', 'doc0', 'doc999')
-    res = res[1][1][0][9][7][0] # get the list iterator profile
+    res = res[1][1][0][11][7][0] # get the list iterator profile
     env.assertEqual(res[1], 'ID-LIST')
     env.assertLess(res[5], 3)
 
