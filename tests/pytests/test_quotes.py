8b5f2a49e834d949b8f64c6d29a972b6a4b7f342
jonathan keinan <jonathan.keinan@redis.com>
2025-01-28 18:03:19 +0200

    * fix tests for coordinator flow

--- tests/pytests/test_quotes.py
+++ tests/pytests/test_quotes.py
@@ -7,6 +7,19 @@ from common import *
 def search(env, r, *args):
     return r.expect('ft.search', *args)
 
+def sort_list(lst):
+    lst.sort()
+    return lst
+
+def sort_document_names(document_list):
+    if len(document_list) == 0:
+        return {}
+
+    num_docs = document_list[0]
+    names = document_list[1:]
+    names.sort()
+    return [num_docs, *names]
+
 def setup_index(env):
     env.expect("FT.CREATE idx SCHEMA t1 TEXT NOSTEM t2 TAG").ok()
     conn = getConnectionByEnv(env)
@@ -17,24 +30,24 @@ def setup_index(env):
 def test_wildcard(env):
     setup_index(env)
     expected = [2, 'h1', 'h2']
-    all = [3, 'h3', 'h1', 'h2']
-    search(env, env, 'idx', '@t1:\'James*\'', 'NOCONTENT', 'DIALECT', 2).equal(expected)
-    search(env, env, 'idx', '@t1:"James*"', 'NOCONTENT', 'DIALECT', 2).equal(expected)
-    search(env, env, 'idx', '@t1:(James*)', 'NOCONTENT', 'DIALECT', 2).equal(all)
+    all = [3, 'h1', 'h2', 'h3']
+    search(env, env, 'idx', '@t1:\'James*\'', 'NOCONTENT', 'DIALECT', 2).apply(sort_document_names).equal(expected)
+    search(env, env, 'idx', '@t1:"James*"', 'NOCONTENT', 'DIALECT', 2).apply(sort_document_names).equal(expected)
+    search(env, env, 'idx', '@t1:(James*)', 'NOCONTENT', 'DIALECT', 2).apply(sort_document_names).equal(all)
 
 def test_no_wildcard(env):
     setup_index(env)
     expected = [2, 'h1', 'h2']
-    search(env, env, 'idx', '@t1:\'James\'', 'NOCONTENT', 'DIALECT', 2).equal(expected)
-    search(env, env, 'idx', '@t1:"James"', 'NOCONTENT', 'DIALECT', 2).equal(expected)
-    search(env, env, 'idx', '@t1:(James)', 'NOCONTENT', 'DIALECT', 2).equal(expected)
+    search(env, env, 'idx', '@t1:\'James\'', 'NOCONTENT', 'DIALECT', 2).apply(sort_document_names).equal(expected)
+    search(env, env, 'idx', '@t1:"James"', 'NOCONTENT', 'DIALECT', 2).apply(sort_document_names).equal(expected)
+    search(env, env, 'idx', '@t1:(James)', 'NOCONTENT', 'DIALECT', 2).apply(sort_document_names).equal(expected)
 
 def test_tags(env):
     setup_index(env)
     expected = [1, 'h1']
-    search(env, env, 'idx', '@t2:{NYC}', 'NOCONTENT', 'DIALECT', 2).equal(expected)
-    search(env, env, 'idx', '@t2:{"NYC"}', 'NOCONTENT', 'DIALECT', 2).equal(expected)
-    search(env, env, 'idx', '@t2:{\'NYC\'}', 'NOCONTENT', 'DIALECT', 2).equal(expected)
+    search(env, env, 'idx', '@t2:{NYC}', 'NOCONTENT', 'DIALECT', 2).apply(sort_document_names).equal(expected)
+    search(env, env, 'idx', '@t2:{"NYC"}', 'NOCONTENT', 'DIALECT', 2).apply(sort_document_names).equal(expected)
+    search(env, env, 'idx', '@t2:{\'NYC\'}', 'NOCONTENT', 'DIALECT', 2).apply(sort_document_names).equal(expected)
 
 def test_verbatim_escaping(env):
     setup_index(env)
@@ -45,8 +58,7 @@ def test_verbatim_escaping(env):
     # https://github.com/redis/redis/issues/6928
     # https://github.com/redis/redis/issues/8672
     squote = "@t1:('James\\!\\*')"
-    terms =  ['brown', 'exclaim', 'james', 'james!*', 'lore']
-    env.expect(debug_cmd(), 'DUMP_TERMS', 'idx').equal(terms)
+    env.expect(debug_cmd(), 'DUMP_TERMS', 'idx').contains('james!*')
     env.expect('FT.EXPLAIN', 'idx', dquote, 'DIALECT', 2).equal(expected_explain)
     search(env, env, 'idx', dquote, 'NOCONTENT', 'DIALECT', 2).equal(expected)
     env.expect('FT.EXPLAIN', 'idx', squote, 'DIALECT', 2).equal(expected_explain)
