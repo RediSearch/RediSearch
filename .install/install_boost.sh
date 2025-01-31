97c6773467aec488880f2a4ab9e8b805c6cbedb4
alon <alonreshef24@gmail.com>
2025-01-31 22:45:20 +0200

    fix installation

--- .install/install_boost.sh
+++ .install/install_boost.sh
@@ -2,11 +2,16 @@
 
 set -e
 VERSION=$1
-MODE=$2
 BOOST_NAME="boost_${VERSION//./_}"
 BOOST_DIR="boost" # here we search for the boost cached installation if exists. Do not change this value
 
-wget https://boostorg.jfrog.io/artifactory/main/release/${VERSION}/source/${BOOST_NAME}.tar.gz
+if [[ -d ${BOOST_DIR} ]]; then
+    echo "Boost cache directory present, skipping installation"
+    exit 0
+fi
+
+wget https://archives.boost.io/release/${VERSION}/source/${BOOST_NAME}.tar.gz
 
 tar -xzf ${BOOST_NAME}.tar.gz
 mv ${BOOST_NAME} ${BOOST_DIR}
+rm ${BOOST_NAME}.tar.gz
