3f2b654a6515245038e91de1f34099649827d47c
alon <alonreshef24@gmail.com>
2025-01-31 23:52:38 +0200

    update installs

--- .install/macos.sh
+++ .install/macos.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
+
+if ! which brew &> /dev/null; then
+    echo "Brew is not installed. Install from https://brew.sh"
+    exit 1
+fi
+
 export HOMEBREW_NO_AUTO_UPDATE=1
-BREW_PREFIX=$(brew --prefix)
-GNUBIN=$BREW_PREFIX/opt/make/libexec/gnubin
-LLVM=$BREW_PREFIX/opt/llvm@16/bin
-COREUTILS=$BREW_PREFIX/opt/coreutils/libexec/gnubin
+
+LLVM_VERSION="17"
+BOOST_VERSION="1.83.0"
 
 brew update
 brew install coreutils
 brew install make
-brew install llvm@16
-
-echo "export PATH=$COREUTILS:$LLVM:$GNUBIN:$PATH" >> ~/.bashrc
-echo "export PATH=$COREUTILS:$LLVM:$GNUBIN:$PATH" >> ~/.zshrc
-source ~/.bashrc
-source ~/.zshrc
-
 brew install openssl
+brew install llvm@$LLVM_VERSION
+
+BREW_PREFIX=$(brew --prefix)
+GNUBIN=$BREW_PREFIX/opt/make/libexec/gnubin
+LLVM="$BREW_PREFIX/opt/llvm@$LLVM_VERSION/bin"
+COREUTILS=$BREW_PREFIX/opt/coreutils/libexec/gnubin
+
+update_profile() {
+    local profile_path=$1
+    local newpath="export PATH=$COREUTILS:$LLVM:$GNUBIN:\$PATH"
+    grep -qxF "$newpath" "$profile_path" || echo "$newpath" >> "$profile_path"
+    source $profile_path
+}
+
+[[ -f ~/.bash_profile ]] && update_profile ~/.bash_profile
+[[ -f ~/.zshrc ]] && update_profile ~/.zshrc
+
 source install_cmake.sh
--- .install/rocky_linux_8.sh
+++ .install/rocky_linux_8.sh
@@ -5,18 +5,20 @@ set -e
 $MODE dnf update -y
 
 # Development Tools includes python11 and config-manager
-$MODE dnf groupinstall "Development Tools" -yqq
+$MODE dnf groupinstall "Development Tools" -yqq --nobest --skip-broken
 # install pip
-$MODE dnf install python3.11-pip -y
+$MODE dnf install python3.11-pip -y --nobest --skip-broken
 
 # powertools is needed to install epel
-$MODE dnf config-manager --set-enabled powertools
+$MODE dnf config-manager --set-enabled powertools --nobest --skip-broken
 
 # get epel to install gcc13
-$MODE dnf install epel-release -yqq
+$MODE dnf install epel-release -yqq --nobest --skip-broken
 
-$MODE dnf install -y gcc-toolset-13-gcc gcc-toolset-13-gcc-c++ gcc-toolset-13-libatomic-devel make wget git openssl openssl-devel \
-    bzip2-devel libffi-devel zlib-devel tar xz which rsync --nobest --skip-broken
+$MODE dnf install -y gcc-toolset-13-gcc gcc-toolset-13-gcc-c++ \
+    gcc-toolset-13-libatomic-devel make wget git openssl openssl-devel \
+    bzip2-devel libffi-devel zlib-devel tar xz which rsync python3.11-devel \
+    clang curl --nobest --skip-broken
 
 cp /opt/rh/gcc-toolset-13/enable /etc/profile.d/gcc-toolset-13.sh
 
