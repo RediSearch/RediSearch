09c46d89bd94cad739e3dc8ba06b19c306425601
jonathan keinan <jonathan.keinan@redis.com>
2025-02-09 18:33:23 +0200

    * test fix - expire the right doc

--- tests/pytests/test_expire.py
+++ tests/pytests/test_expire.py
@@ -511,7 +511,7 @@ def testSeekToExpirationChecks(env):
     for i in range(1, 1001):
         conn.execute_command('HSET', f'doc:{i}', 'x', 'hello', 'y', 'world')
         # important we expire now since that assigns a new doc id for doc:expire - 1002
-        conn.execute_command('HPEXPIRE', 'doc:expire', '1', 'FIELDS', '1', 'y') # doc:expire internal id is 5
+        conn.execute_command('HPEXPIRE', f'doc:{i}', '1', 'FIELDS', '1', 'y') # doc:expire internal id is 5
     conn.execute_command('HSET', 'doc:1001', 'x', 'hello', 'y', 'world')
     # inverted index state
     # 'hello': ['doc:0', 'doc:1', , ..., 'doc:1000', 'doc:1001']
@@ -523,7 +523,7 @@ def testSeekToExpirationChecks(env):
     # - world reader starts with doc:1
     # - intersect iterator reads doc:0 and tries to skip to it in world reader
     # - world reader should skip to doc:1001 since all the other docs will be expired
-    time.sleep(0.1)
+    time.sleep(0.5) # we want to sleep enough so we filter out the expired documents at the iterator phase
     # doc:1001 should not be returned, due to the nature of intersection iterator we expect SkipTo to be called at least once
     # since text fields have a seeker we expect IndexReader_ReadWithSeeker to be called
     # that should provide coverage for IndexReader_ReadWithSeeker.
