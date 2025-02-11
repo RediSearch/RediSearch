6469b6398a28787fb46fd357fa04b4eba477bc20
itzik.vaknin@redis.com <itzikvaknin6@gmail.com>
2025-02-11 17:12:24 +0200

    Remove unnecessary conn usage

--- tests/pytests/test_info.py
+++ tests/pytests/test_info.py
@@ -174,19 +174,19 @@ def test_vecsim_info_stats_marked_deleted(env):
   conn = env.getClusterConnectionIfNeeded()
   vec_size = 6
   data_type = 'FLOAT16'
-  conn.execute_command('FT.CREATE', 'idx', 'ON', 'HASH', 'SCHEMA', 'vector', 'VECTOR', 'HNSW', 6, 'DIM', 6, 'TYPE', 'float16', 'DISTANCE_METRIC', 'L2')
+  env.expect('FT.CREATE', 'idx', 'ON', 'HASH', 'SCHEMA', 'vector', 'VECTOR', 'HNSW', 6, 'DIM', 6, 'TYPE', 'float16', 'DISTANCE_METRIC', 'L2').ok()
   load_vectors_to_redis(env, 1000, 0, vec_size, data_type)
   env.expect(debug_cmd(), 'WORKERS', 'DRAIN').ok() # wait for HNSW graph construction to finish
   env.expect(debug_cmd(), 'WORKERS', 'PAUSE').ok() # pause to prevent repair jobs on the graph
   docs_to_delete = 100
   for i in range(1, 1 + docs_to_delete):
     conn.execute_command('DEL', f'{i}')
-  info = conn.execute_command('ft.info', 'idx')
+  info = index_info(env, 'idx')
   env.assertTrue("field statistics" in info)
   env.assertEqual(info["field statistics"][0]["marked_deleted"], docs_to_delete)
   env.expect(debug_cmd(), 'WORKERS', 'resume').ok()
   # Wait for all repair jobs to be finish, then run GC to remove the deleted vectors.
   env.expect(debug_cmd(), 'WORKERS', 'DRAIN').ok()
   env.expect(debug_cmd(), 'GC_FORCEINVOKE', 'idx').equal('DONE')
-  info = conn.execute_command('ft.info', 'idx')
+  info = index_info(env, 'idx')
   env.assertEqual(info["field statistics"][0]["marked_deleted"], 0)
