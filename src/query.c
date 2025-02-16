adaa96fa372d3ff3c863d01ac348cc741c1f1b3c
jonathan keinan <jonathan.keinan@redis.com>
2025-02-16 18:56:35 +0200

    * cleanup field name when moving data

--- src/query.c
+++ src/query.c
@@ -300,6 +300,10 @@ QueryNode *NewTagNode(const FieldSpec *field) {
 QueryNode *NewNumericNode(QueryParam *p, const FieldSpec *fs) {
   QueryNode *ret = NewQueryNode(QN_NUMERIC);
   // Move data and params pointers
+  if (!p->nf->field.resolved) {
+    // since we take ownership on the filter we need to clean up the field name
+    HiddenString_Free(p->nf->field.u.name, false);
+  }
   ret->nn.nf = p->nf;
   ret->params = p->params;
   ret->nn.nf->field.u.spec = fs;
