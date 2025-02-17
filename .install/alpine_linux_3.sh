59fd0a63e1e6337f9bac71a54cae7f3e9a904974
alon <alonreshef24@gmail.com>
2025-02-17 23:19:57 +0200

    remove deps not in json

--- .install/alpine_linux_3.sh
+++ .install/alpine_linux_3.sh
@@ -6,8 +6,8 @@ $MODE apk update
 
 $MODE apk add --no-cache build-base gcc g++ make linux-headers openblas-dev \
     xsimd curl wget git python3 python3-dev py3-pip openssl openssl-dev \
-    tar xz which rsync bsd-compat-headers clang clang17-libclang lld curl llvm clang-dev \
+    tar xz which rsync bsd-compat-headers curl \
     bash cargo gcc autoconf automake py3-cryptography \
-    musl-dev libffi-dev openssh py-virtualenv clang18-libclang gcompat
+    musl-dev libffi-dev openssh py-virtualenv clang18-libclang
 
 source install_cmake.sh $MODE
