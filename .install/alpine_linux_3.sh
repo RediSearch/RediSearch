830b864b12924ef77df5ec64616a23975e8b5aa2
alon <alonreshef24@gmail.com>
2025-02-17 22:59:04 +0200

    add gcompat

--- .install/alpine_linux_3.sh
+++ .install/alpine_linux_3.sh
@@ -8,6 +8,6 @@ $MODE apk add --no-cache build-base gcc g++ make linux-headers openblas-dev \
     xsimd curl wget git python3 python3-dev py3-pip openssl openssl-dev \
     tar xz which rsync bsd-compat-headers clang clang17-libclang lld curl llvm clang-dev \
     bash cargo gcc autoconf automake py3-cryptography \
-    musl-dev libffi-dev openssh py-virtualenv clang18-libclang
+    musl-dev libffi-dev openssh py-virtualenv clang18-libclang gcompat
 
 source install_cmake.sh $MODE
