e66c2a8c81c797edd2ef5f5ac5526800e06a514a
raz-mon <razmonsonego2@gmail.com>
2025-02-23 15:13:32 +0200

    Condition test on SAN

--- tests/cpptests/test_cpp_index.cpp
+++ tests/cpptests/test_cpp_index.cpp
@@ -294,7 +294,15 @@ TEST_F(IndexTest, testGetEncoderAndDecoders) {
 
     // invalid flags combination
     default:
-      continue;
+      // TODO: We currently test only with sanitizer since the sanitizer is
+      // running in debug mode always, while the regular tests are running in
+      // release mode.
+      #ifdef __SANITIZE_ADDRESS__
+        ASSERT_ANY_THROW(InvertedIndex_GetDecoder(IndexFlags(curFlags)));
+        ASSERT_ANY_THROW(InvertedIndex_GetEncoder(IndexFlags(curFlags)));
+      #else
+        continue;
+      #endif;
     }
   }
 }
@@ -302,7 +310,7 @@ TEST_F(IndexTest, testGetEncoderAndDecoders) {
 TEST_F(IndexTest, testReadIterator) {
   InvertedIndex *idx = createPopulateTermsInvIndex(10, 1);
 
-  IndexReader *r1 = NewTermIndexReader(idx);  //
+  IndexReader *r1 = NewTermIndexReader(idx);
 
   RSIndexResult *h = NULL;
 
