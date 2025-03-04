bebd162aedc398f4590b9d622391fb6e67a15b98
nafraf <nafraf@gmail.com>
2025-03-03 22:26:27 -0500

    Install python3-devel

--- .install/microsoft_azure_linux_3.0.sh
+++ .install/microsoft_azure_linux_3.0.sh
@@ -4,7 +4,7 @@ set -e
 export DEBIAN_FRONTEND=noninteractive
 
 $MODE tdnf install -q -y build-essential git wget ca-certificates tar unzip \
-                         rsync openssl-devel python3 python3-pip which clang \
-                         libxcrypt-devel
+                         rsync openssl-devel python3 python3-pip python3-devel \
+                         which clang libxcrypt-devel
 
 source install_cmake.sh $MODE
