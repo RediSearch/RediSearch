ec528c84a594b5d91e306ca0f5813a9ee7b2c669
DvirDukhan <dvir@redis.com>
2025-03-09 23:28:02 +0200

    PR Comments

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 cd .install
 ./install_script.sh
-./install_boost.sh 1.83.0
-cd ../
-./.install/test_deps/install_rust.sh
 ./.install//test_deps/install_llvm.sh 18
 ./.install/test_deps/common_installations.sh
-cd ~
+cd ../
 git clone https://github.com/Redis/Redis.git
 cd Redis
-make
+make BUILD_TLS=yes
 make install
 
 # Avoid default locale of "en_US.UTF-8" 
