4c114ae42660b7ee8ba1190ad767b35c9acf73c0
alonre24 <alon.reshef@redis.com>
2025-03-09 19:21:08 +0200

    [2.8] Simplify testing with JSON flow - purge readies [MOD-8967] (#5708)
    * CP + update redis json branch to run against to 2.6
    
    * revert accidental vecsim change
    
    * fix to set json branch to 2.6 where needed
    
    * revert accidental deps change
    
    * revert make help
    
    * fix json path in benchmark
    
    * Add libclang-dev to centos7
    
    * default branch in json for dispatch
    
    * add clang to centos7
    
    * add clang to centos7
    
    * try explicit
    
    * remove centos7 from OS that we ran tests and build

--- .install/amazon_linux_2.sh
+++ .install/amazon_linux_2.sh
@@ -12,7 +12,7 @@ $MODE yum install -y https://vault.centos.org/centos/7/extras/x86_64/Packages/ce
 $MODE sed -i 's/mirrorlist=/#mirrorlist=/g' /etc/yum.repos.d/CentOS-SCLo-scl-rh.repo                        # Disable mirrorlist
 $MODE sed -i 's/#baseurl=http:\/\/mirror/baseurl=http:\/\/vault/g' /etc/yum.repos.d/CentOS-SCLo-scl-rh.repo # Enable a working baseurl
 
-$MODE yum install -y wget git which devtoolset-11-gcc devtoolset-11-gcc-c++ devtoolset-11-make rsync python3 unzip curl
+$MODE yum install -y wget git which devtoolset-11-gcc devtoolset-11-gcc-c++ devtoolset-11-make rsync python3 unzip curl libclang-dev clang
 
 source /opt/rh/devtoolset-11/enable
 
