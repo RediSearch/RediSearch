bb2e08bdcbfa79e3fa4c455c5316383b88fd2746
nafraf <nafraf@gmail.com>
2025-03-12 07:33:44 -0500

    Remove cmake installation from Azure Linux 3.0 setup script

--- .install/microsoft_azure_linux_3.0.sh
+++ .install/microsoft_azure_linux_3.0.sh
@@ -6,5 +6,3 @@ export DEBIAN_FRONTEND=noninteractive
 $MODE tdnf install -q -y build-essential git wget ca-certificates tar unzip \
                          rsync openssl-devel python3 python3-pip python3-devel \
                          which clang libxcrypt-devel
-
-source install_cmake.sh $MODE
