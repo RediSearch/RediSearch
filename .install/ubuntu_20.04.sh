9163ad5c4c1cb189aa365655e123984bb13d246f
DvirDukhan <dvir@redis.com>
2025-03-12 09:30:16 +0200

    Simplify install script (#5726)
    * llvm
    
    * boost version
    
    * boost version in script
    
    * install script is one stop shop - avoid code duplication
    
    * removed boost install from CI scripts and dev readme
    
    * llvm version 18 in task-test
    
    * try to simplify the task-test flow
    
    * fixed test_deps path
    
    * ubuntu jammy install git before checkout
    
    * explicit test deps
    
    * install git on santizier
    
    * tying bash for pre steps script
    
    * remvoe shell default
    
    * debian based git install
    
    * merged os to array
    
    * git for mariner and amazonlinux2023
    
    * alpine + cleanup
    
    * git for amazon linux 2 and alpine
    
    * alpine x86 git
    
    * cerificates for mariner
    
    * remove install rust from dev.md since it is by default
    
    * alpine failes on finding venv, try bashrc
    
    * return to bash
    
    * prabobly fixed wrong shell command placment
    
    * use sh on alpine
    
    * typo
    
    * removed verify CC and CXX step
    
    * handle llvm version
    
    * new line
    
    * added pre steps script to sanitize flow on merge queue
    
    (cherry picked from commit da9adbf32ce8f6581e114d3fe53db805152912f3)

--- .install/ubuntu_20.04.sh
+++ .install/ubuntu_20.04.sh
@@ -10,5 +10,3 @@ $MODE apt install -yqq wget make clang-format gcc python3 python3-venv python3-p
     unzip rsync build-essential gcc-10 g++-10 curl libclang-dev
 
 $MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 60 --slave /usr/bin/g++ g++ /usr/bin/g++-10
-
-source install_cmake.sh $MODE
