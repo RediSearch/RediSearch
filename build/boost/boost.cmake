ad80bfb1f9c64ce2402ec9f9846f16a93892faa2
jonathan keinan <jonathan.keinan@redis.com>
2025-03-13 13:58:47 +0200

    - code review comments

--- build/boost/boost.cmake
+++ build/boost/boost.cmake
@@ -1,22 +1,20 @@
 message(STATUS "BOOST_DIR: ${BOOST_DIR}")
 
-if (DEFINED BOOST_DIR AND BOOST_DIR AND IS_DIRECTORY ${BOOST_DIR})
+if (IS_DIRECTORY ${BOOST_DIR})
     message(STATUS "BOOST_DIR points to a valid directory: ${BOOST_DIR}")
 else()
     message(STATUS "BOOST_DIR '${BOOST_DIR}' is not a valid boost directory path")
-    set(BOOST_INCLUDE_LIBRARIES geometry optional unordered smart_ptr)
     set(BOOST_ENABLE_CMAKE ON)
 
     message(STATUS "fetching boost")
 
     include(FetchContent)
-    # we want to see the progress of the git clone
+    # we want to see the progress of the download, so we set FETCHCONTENT_QUIET to false
     Set(FETCHCONTENT_QUIET FALSE)
     FetchContent_Declare(
             Boost
             URL https://archives.boost.io/release/1.84.0/source/boost_1_84_0.tar.gz
             USES_TERMINAL_DOWNLOAD TRUE
-            GIT_PROGRESS TRUE
             DOWNLOAD_NO_EXTRACT FALSE
     )
     FetchContent_MakeAvailable(Boost)
