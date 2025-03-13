3e761d4ef6c6aa857a4872a90c43fd5d4aac59a0
jonathan keinan <jonathan.keinan@redis.com>
2025-03-13 09:55:19 +0200

    - using the same link we use in install_boost.sh - no source files just headers - no installation

--- build/boost/boost.cmake
+++ build/boost/boost.cmake
@@ -2,10 +2,9 @@ message(STATUS "BOOST_DIR: ${BOOST_DIR}")
 
 if (DEFINED BOOST_DIR AND BOOST_DIR AND IS_DIRECTORY ${BOOST_DIR})
     message(STATUS "BOOST_DIR is not empty: ${BOOST_DIR}")
-    include_directories(${BOOST_DIR})
 else()
     message(STATUS "BOOST_DIR '${BOOST_DIR}' is not a valid boost directory path")
-    # set(BOOST_INCLUDE_LIBRARIES boost geometry optional unordered)
+    set(BOOST_INCLUDE_LIBRARIES geometry optional unordered smart_ptr)
     set(BOOST_ENABLE_CMAKE ON)
 
     message(STATUS "fetching boost")
@@ -15,15 +14,15 @@ else()
     Set(FETCHCONTENT_QUIET FALSE)
     FetchContent_Declare(
             Boost
-            GIT_REPOSITORY https://github.com/boostorg/boost.git
-            GIT_TAG boost-1.84.0
+            URL https://archives.boost.io/release/1.84.0/source/boost_1_84_0.tar.gz
+            USES_TERMINAL_DOWNLOAD TRUE
             GIT_PROGRESS TRUE
-            GIT_SHALLOW TRUE
+            DOWNLOAD_NO_EXTRACT FALSE
     )
     FetchContent_MakeAvailable(Boost)
 
     message(STATUS "boost fetched")
     message(STATUS "boost source dir: ${boost_SOURCE_DIR}")
     message(STATUS "boost binary dir: ${boost_BINARY_DIR}")
-    set(BOOST_DIR ${boost_SOURCE_DIR} PARENT_SCOPE)
+    set(BOOST_DIR ${boost_SOURCE_DIR})
 endif()
