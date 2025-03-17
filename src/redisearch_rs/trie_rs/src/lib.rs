1ec54ef7abddd27fe9329ae0b1a16ff8bca84fbd
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2025-03-17 16:12:49 +0100

    MOD-8969: Compute test coverage for Rust code (#5780)
    We introduce a new tool, `cargo-llvm-cov`, to compute test coverage
    for Rust code.
    The coverage file is located next to the coverage report for C code
    and uploaded alongside it to Codecov in CI. Codecov will
    merge the two reports for us on the server-side.

--- src/redisearch_rs/trie_rs/src/lib.rs
+++ src/redisearch_rs/trie_rs/src/lib.rs
@@ -1,1 +1,10 @@
-
+#[cfg(test)]
+mod tests {
+    #[test]
+    // `cargo llvm-cov` will fail to generate a coverage report if there
+    // are no tests.
+    // Adding a placeholder test to work around the issue.
+    // We can delete this placeholder once we merge Rust code
+    // with actual tests.
+    fn dummy() {}
+}
