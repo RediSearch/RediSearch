d58f2e96db92253ce7ff34e71eb8c745e12786af
GuyAv46 <guy.avimor@gmail.com>
2025-03-17 17:53:18 +0200

    fix flows

--- .install/install_llvm.sh
+++ .install/install_llvm.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Source the profile update utility
-source "$(dirname "$0")/macos_update_profile.sh"
-
 OS_TYPE=$(uname -s)
 VERSION=18
 MODE=$1
@@ -11,7 +8,10 @@ if [[ $OS_TYPE == Darwin ]]; then
     brew install llvm@$VERSION
     BREW_PREFIX=$(brew --prefix)
     LLVM="$BREW_PREFIX/opt/llvm@$VERSION/bin"
-    
+
+    # Source the profile update utility
+    source "$(dirname "$0")/macos_update_profile.sh"
+
     # Update profiles with LLVM path
     [[ -f ~/.bash_profile ]] && update_profile ~/.bash_profile "$LLVM"
     [[ -f ~/.zshrc ]] && update_profile ~/.zshrc "$LLVM"
@@ -20,4 +20,5 @@ else
     wget https://apt.llvm.org/llvm.sh
     chmod +x llvm.sh
     $MODE ./llvm.sh $VERSION
+    export LLVM_VERSION=$VERSION
 fi
