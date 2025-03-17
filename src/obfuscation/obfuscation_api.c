92f36170bdd29a6a41b9fefb3c29df9af1a931b6
kei-nan <jonathan.keinan@redis.com>
2025-03-17 16:58:16 +0200

    [2.10] MOD-8989 - Obfuscation API Changes (#5745)
    * MOD-8989 - Obfuscation API Changes (#5716)
    
    * * rename HiddenName to HiddenString
    * add HiddenStringUniocde
    * separate boost installation from geometry library
    * add hashing c wrapper library
    
    * * add hash test
    
    * * code review comments - Raz 9/3
    
    (cherry picked from commit 886b57bb847d7582dad3bf5804b46404722e0c7b)
    
    * * add missing space
    
    * * add missing library to coordinator compilation
    
    * * fix warnings and compilation issues in different environments

--- src/obfuscation/obfuscation_api.c
+++ src/obfuscation/obfuscation_api.c
@@ -1,50 +1,58 @@
 #include "obfuscation_api.h"
 #include "rmalloc.h"
+
 #include "query_node.h"
 
 #include <string.h>
 
-void Obfuscate_Index(t_uniqueId indexId, char* buffer) {
-  sprintf(buffer, "Index@%zu", indexId);
+void Obfuscate_Index(const Sha1 *hash, char* buffer) {
+  const char prefix[] = "Index@";
+  strcpy(buffer, prefix);
+  Sha1_FormatIntoBuffer(hash, buffer + strlen(prefix));
 }
 
 void Obfuscate_Field(t_uniqueId fieldId, char* buffer) {
-  sprintf(buffer, "Field@%zu", fieldId);
+  sprintf(buffer, "Field@%lu", fieldId);
 }
 
 void Obfuscate_FieldPath(t_uniqueId fieldId, char* buffer) {
-  sprintf(buffer, "FieldPath@%zu", fieldId);
+  sprintf(buffer, "FieldPath@%lu", fieldId);
 }
 
 void Obfuscate_Document(t_uniqueId docId, char* buffer) {
-  sprintf(buffer, "Document@%zu", docId);
+  sprintf(buffer, "Document@%lu", docId);
 }
 
-char *Obfuscate_Text(const char* text) {
+void Obfuscate_KeyWithTime(struct timespec spec, char* buffer) {
+  const size_t epoch = spec.tv_sec * 1000 + spec.tv_nsec / 1000000;
+  sprintf(buffer, "Key@%zu", epoch);
+}
+
+const char *Obfuscate_Text(const char* text) {
   return "Text";
 }
 
-char *Obfuscate_Number(double number) {
+const char *Obfuscate_Number(double number) {
   return "Number";
 }
 
-char *Obfuscate_Vector(const char* vector, size_t dim) {
+const char *Obfuscate_Vector(const char* vector, size_t dim) {
   return "Vector";
 }
 
-char *Obfuscate_Tag(const char* tag) {
+const char *Obfuscate_Tag(const char* tag) {
   return "Tag";
 }
 
-char *Obfuscate_Geo(uint16_t longitude, uint16_t latitude) {
+const char *Obfuscate_Geo(uint16_t longitude, uint16_t latitude) {
   return "Geo";
 }
 
-char *Obfuscate_GeoShape() {
+const char *Obfuscate_GeoShape() {
   return "GeoShape";
 }
 
-char *Obfuscate_QueryNode(struct RSQueryNode *node) {
+const char *Obfuscate_QueryNode(struct RSQueryNode *node) {
   switch (node->type) {
     case QN_PHRASE:
       return "Phrase";
