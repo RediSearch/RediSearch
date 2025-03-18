5c5bb62f433600f65b0b1350659299e12582214c
lerman25 <lerman25@gmail.com>
2025-03-18 12:34:03 +0200

    revert test config

--- tests/pytests/test_config.py
+++ tests/pytests/test_config.py
@@ -546,7 +546,7 @@ def testConfigAPIRunTimeNumericParams():
 
         # Check that the value is immutable
         env.expect('CONFIG', 'SET', configName, str(default)).error()\
-            .contains('CONFIG SET failed',message = configName)
+            .contains('CONFIG SET failed')
 
     # Test numeric parameters
     for configName, ftConfigName, default, min, max, immutable, clusterConfig in numericConfigs:
