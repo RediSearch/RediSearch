2c27c60ce1a99d7c6c4ab7d19d6d5ac4d7591f79
meiravgri <meirav.grimberg@redis.com>
2025-03-18 10:31:39 +0000

    return instead of exit to install script will proceed

--- .install/install_boost.sh
+++ .install/install_boost.sh
@@ -7,7 +7,7 @@ BOOST_DIR="boost" # here we search for the boost cached installation if exists. 
 
 if [[ -d ${BOOST_DIR} ]]; then
     echo "Boost cache directory present, skipping installation"
-    exit 0
+    return 0
 fi
 
 wget https://archives.boost.io/release/${VERSION}/source/${BOOST_NAME}.tar.gz -O ${BOOST_NAME}.tar.gz
