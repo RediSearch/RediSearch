02f2178adc57ffd5bfa88e83c7d07d3f0d5857cb
meiravgri <109056284+meiravgri@users.noreply.github.com>
2025-03-20 12:00:12 +0200

    [8.0] Prevent install.sh from exiting early when Boost is already installed (#5806)
    Prevent install.sh from exiting early when Boost is already installed (#5785)
    
    * return instead of exit to install script will proceed
    
    * revert and handle exit code
    
    (cherry picked from commit c440d23159b9eb56fcb5fe7a03bfc51cea7c9033)

--- .install/install_script.sh
+++ .install/install_script.sh
@@ -20,6 +20,15 @@ echo $OS
 
 source ${OS}.sh $MODE
 source install_cmake.sh $MODE
-source install_boost.sh 
+
+# run in a subshell to let this script continue if install_boost.sh calls exit 0
+(source ./install_boost.sh)
+BOOST_EXIT_CODE=$?
+
+# propagate the exit code
+if [[ $BOOST_EXIT_CODE -ne 0 ]]; then
+    echo "Boost installation failed with exit code $BOOST_EXIT_CODE, stopping the script."
+    exit $BOOST_EXIT_CODE
+fi
 
 git config --global --add safe.directory '*'
