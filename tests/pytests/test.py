cfae8500814861a0ef91357985d8c4e3bc3ebc51
kei-nan <jonathan.keinan@redis.com>
2025-03-20 10:18:38 +0200

    MOD-8993: Separate Index Error Messages Into Two Parts (#5719)
    * * rename HiddenName to HiddenString
    * add HiddenStringUniocde
    * separate boost installation from geometry library
    * add hashing c wrapper library
    
    * * add hash test
    
    * * separate legacy filter from normal filters to avoid holding char* inside field spec
    * legacy filters now hold hidden string instead of char*
    
    * * add te to ignore_wordlist.txt
    
    * * fix typo
    
    * * Separate the full error into two parts
    - without user data
    - with user data
    * Should allow outputting the error information to the user and us.
    
    * * add index error test
    
    * * remove test_debug_commands.py irrelevant tests
    
    * * fix compilation issues post merge
    * remove redundant ;
    
    * * code review comments - 10/3
    
    * * code review comments
    
    * * revert to %llu for uint64_t type
    
    * * code review comments
    
    * Apply suggestions from code review
    
    Co-authored-by: Omer Shadmi <76992134+oshadmi@users.noreply.github.com>
    
    * * rephrase error based on code review comment
    
    * * use QueryError_SetWithUserDataFmt for syntax errors on v2
    
    * * fix tests
    
    * * standardize the no index error so test will pass
    
    * * remove make bad args macro
    * add field in error message in test
    
    * * fix test
    
    ---------
    
    Co-authored-by: Omer Shadmi <76992134+oshadmi@users.noreply.github.com>
    (cherry picked from commit 6dcd1e302462444ef74b189c473adc47bbef32e9)

--- tests/pytests/test.py
+++ tests/pytests/test.py
@@ -4016,7 +4016,7 @@ def test_missing_schema(env):
     # make sure the index successfully index new docs
     conn.execute_command('HSET', 'doc1', 'foo', 'bar')
     env.expect('FT.SEARCH', 'idx1', '*').equal([1, 'doc1', ['foo', 'bar']] )
-    env.expect('FT.SEARCH', 'idx2', '*').error().equal('idx2: no such index')
+    env.expect('FT.SEARCH', 'idx2', '*').error().equal('No such index idx2')
 
 
 @skip(cluster=False) # this test is only relevant on cluster
