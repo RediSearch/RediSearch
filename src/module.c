e0f601d2b7f64c728f360e41c91d246bd0a38a9e
kei-nan <jonathan.keinan@redis.com>
2025-03-23 11:39:01 +0200

    [8.0] MOD-8993: Separate Index Error Messages Into Two Parts (#5808)
    MOD-8993: Separate Index Error Messages Into Two Parts (#5719)
    
    * * rename HiddenName to HiddenString
    * add HiddenStringUniocde
    * separate boost installation from geometry library
    * add hashing c wrapper library
    
    * * add hash test
    
    * * separate legacy filter from normal filters to avoid holding char* inside field spec
    * legacy filters now hold hidden string instead of char*
    
    * * add te to ignore_wordlist.txt
    
    * * fix typo
    
    * * Separate the full error into two parts
    - without user data
    - with user data
    * Should allow outputting the error information to the user and us.
    
    * * add index error test
    
    * * remove test_debug_commands.py irrelevant tests
    
    * * fix compilation issues post merge
    * remove redundant ;
    
    * * code review comments - 10/3
    
    * * code review comments
    
    * * revert to %llu for uint64_t type
    
    * * code review comments
    
    * Apply suggestions from code review
    
    Co-authored-by: Omer Shadmi <76992134+oshadmi@users.noreply.github.com>
    
    * * rephrase error based on code review comment
    
    * * use QueryError_SetWithUserDataFmt for syntax errors on v2
    
    * * fix tests
    
    * * standardize the no index error so test will pass
    
    * * remove make bad args macro
    * add field in error message in test
    
    * * fix test
    
    ---------
    
    Co-authored-by: Omer Shadmi <76992134+oshadmi@users.noreply.github.com>
    (cherry picked from commit 6dcd1e302462444ef74b189c473adc47bbef32e9)

--- src/module.c
+++ src/module.c
@@ -232,7 +232,7 @@ int SpellCheckCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
     ArgsCursor_InitRString(&ac, argv+dialectArgIndex, argc-dialectArgIndex);
     QueryError status = {0};
     if(parseDialect(&dialect, &ac, &status) != REDISMODULE_OK) {
-      RedisModule_ReplyWithError(ctx, QueryError_GetError(&status));
+      RedisModule_ReplyWithError(ctx, QueryError_GetUserError(&status));
       QueryError_ClearError(&status);
       return REDISMODULE_OK;
     }
@@ -251,7 +251,7 @@ int SpellCheckCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
   int rc = QAST_Parse(&qast, sctx, &opts, rawQuery, len, dialect, &status);
 
   if (rc != REDISMODULE_OK) {
-    RedisModule_ReplyWithError(ctx, QueryError_GetError(&status));
+    RedisModule_ReplyWithError(ctx, QueryError_GetUserError(&status));
     goto end;
   }
 
@@ -489,7 +489,7 @@ int CreateIndexCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) 
 
   IndexSpec *sp = IndexSpec_CreateNew(ctx, argv, argc, &status);
   if (sp == NULL) {
-    RedisModule_ReplyWithError(ctx, QueryError_GetError(&status));
+    RedisModule_ReplyWithError(ctx, QueryError_GetUserError(&status));
     QueryError_ClearError(&status);
     return REDISMODULE_OK;
   }
@@ -1760,7 +1760,7 @@ specialCaseCtx *prepareOptionalTopKCase(const char *query_string, RedisModuleStr
     QueryVectorNode queryVectorNode = queryNode->vn;
     size_t k = queryVectorNode.vq->knn.k;
     if (k > MAX_KNN_K) {
-      QueryError_SetErrorFmt(status, QUERY_ELIMIT, VECSIM_KNN_K_TOO_LARGE_ERR_MSG ", max supported K value is %zu", MAX_KNN_K);
+      QueryError_SetWithoutUserDataFmt(status, QUERY_ELIMIT, VECSIM_KNN_K_TOO_LARGE_ERR_MSG ", max supported K value is %zu", MAX_KNN_K);
       goto cleanup;
     }
     specialCaseCtx *ctx = SpecialCaseCtx_New();
@@ -3082,7 +3082,7 @@ int DistAggregateCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc
   IndexSpec *sp = StrongRef_Get(spec_ref);
   if (!sp) {
     // Reply with error
-    return RedisModule_ReplyWithErrorFormat(ctx, "%s: no such index", idx);
+    return RedisModule_ReplyWithErrorFormat(ctx, "No such index %s", idx);
   }
 
   bool isProfile = (RMUtil_ArgIndex("FT.PROFILE", argv, 1) != -1);
@@ -3399,7 +3399,7 @@ int DistSearchCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
   IndexSpec *sp = StrongRef_Get(spec_ref);
   if (!sp) {
     // Reply with error
-    return RedisModule_ReplyWithErrorFormat(ctx, "%s: no such index", idx);
+    return RedisModule_ReplyWithErrorFormat(ctx, "No such index %s", idx);
   }
 
   bool isProfile = (RMUtil_ArgIndex("FT.PROFILE", argv, 1) != -1);
