ec84f78e4479de3ac352df1f23fddd05bfe470e8
lerman25 <lerman25@gmail.com>
2025-03-26 16:39:02 +0200

    fix pytest

--- tests/pytests/test_config.py
+++ tests/pytests/test_config.py
@@ -478,7 +478,7 @@ numericConfigs = [
     ('search-vss-max-resize', 'VSS_MAX_RESIZE', 0, 0, UINT32_MAX, False, False),
     ('search-workers', 'WORKERS', 0, 0, 16, False, False),
     ('search-workers-priority-bias-threshold', 'WORKERS_PRIORITY_BIAS_THRESHOLD', 1, 0, LLONG_MAX, True, False),
-    ('search-_bg_index_mem_pct_thr', '_BG_INDEX_MEM_PCT_THR', 80, 0, 100, False, False),
+    ('search-_bg-index-mem-pct-thr', '_BG_INDEX_MEM_PCT_THR', 80, 0, 100, False, False),
     # Cluster parameters
     ('search-threads', 'SEARCH_THREADS', 20, 1, LLONG_MAX, True, True),
     ('search-topology-validation-timeout', 'TOPOLOGY_VALIDATION_TIMEOUT', 30_000, 0, LLONG_MAX, False, True),
