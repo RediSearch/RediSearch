2d80318f3e6966778493a9ca7499e9691b7c8948
jonathan keinan <jonathan.keinan@redis.com>
2025-03-30 15:44:10 +0300

    - fix leak

--- src/aggregate/reducer.c
+++ src/aggregate/reducer.c
@@ -81,7 +81,7 @@ int ReducerOpts_GetKey(const ReducerOptions *options, const RLookupKey **out) {
     // If we can't load keys, or the key we loaded is not in the schema, we fail.
     if (!options->loadKeys || !((*out)->flags & RLOOKUP_F_SCHEMASRC)) {
       QueryError_SetWithUserDataFmt(options->status, QUERY_ENOPROPKEY, "Property is not present in document or pipeline", ": `%s`", s);
-      return 0;
+      rc = 0;
     }
   }
   HiddenString_Free(name);
