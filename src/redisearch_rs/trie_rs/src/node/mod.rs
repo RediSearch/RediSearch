ade611391c4f133f53281fa3cc8025f13f5567f2
Tim Janus <tim@janus.rs>
2025-04-01 17:54:51 +0200

    smaller improvements

--- src/redisearch_rs/trie_rs/src/node/mod.rs
+++ src/redisearch_rs/trie_rs/src/node/mod.rs
@@ -224,7 +224,7 @@ impl<Data> Node<Data> {
         // case 1
         if key.is_empty() {
             debug_assert!(self.is_branching());
-            println!("case-1");
+            //println!("case-1");
             return (self, Err(usize::MAX), &[]); //  we insert an empty labeled node at the end
         }
 
@@ -240,34 +240,36 @@ impl<Data> Node<Data> {
                     std::cmp::Ordering::Equal => Ok(0),     // replace this leaf
                     std::cmp::Ordering::Greater => Err(0),  // insert new after this leaf
                 };
-                println!("case-2");
+                //println!("case-2");
                 (leaf.as_mut(), index_or_insert_at, suffix)
             }
             // case 3-x-y: self is branching:
             Either::Right(branching) => {
-                let search_result=  branching.children().binary_search_by(|e| e.label()[0].cmp(&key[0]));
-                println!("case-3");
+                // todo: use first binary_bytes
+                let search_result = branching.children_first_bytes().binary_search(&key[0]);
+                //let search_result=  branching.children().binary_search_by(|e| e.label()[0].cmp(&key[0]));
+                //println!("case-3");
                 match search_result {
                     Ok(found_at) => {
                         let candidate = &branching.children()[found_at];
                         let shared_candidate_suffix_len = get_shared_prefix_len(candidate.label(), suffix);
                         let candidate_suffix: &[c_char] = &suffix[shared_candidate_suffix_len..];
                         if candidate.is_leaf() {
-                            println!("case-3-1-1");
+                            //println!("case-3-1-1");
                             (branching.as_mut(), Ok(found_at), candidate_suffix)
                         } else {
                             if shared_candidate_suffix_len < candidate.label().len() {
-                                println!("case-3-1-2");
+                                //println!("case-3-1-2");
                                 branching.children_mut()[found_at].find_node_parent_mut(candidate_suffix)
                             } else {
                                 // we know shared_candidate_suffix_len = candidate_label_len
-                                println!("case-3-1-3");
+                                //println!("case-3-1-3");
                                 (branching.as_mut(), Ok(found_at), candidate_suffix)
                             }
                         }
                     },
                     Err(insert_at) => {
-                        println!("case-3-2");
+                        //println!("case-3-2");
                         (branching.as_mut(), Err(insert_at), suffix)
                     },
                 }
