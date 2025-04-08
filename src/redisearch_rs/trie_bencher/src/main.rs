bf6346be4eb85be02b3bf8aa917777582a59adb2
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2025-04-08 16:25:40 +0200

    [MOD-8974] Microbenchmarks for the Rust trie map implementation (#5819)
    MOD-8974: Benchmark the trie map implementation
    
    Compare memory usage and performance of three different trie map
    implementations:
    - The C implementation from `deps/triemap.c`
    - The Rust implementation from `trie_rs`
    - The Rust implementation from the `radix_trie` crate
    
    The latter is included as a baseline.

---
+++ src/redisearch_rs/trie_bencher/src/main.rs
@@ -0,0 +1,70 @@
+use std::{collections::BTreeSet, ptr::NonNull};
+use trie_bencher::{CTrieMap, RustTrieMap, str2c_char, str2c_input};
+
+fn main() {
+    compute_and_report_memory_usage();
+}
+
+/// Download a text corpus and build a trie from it, using
+/// both the Rust and the C implementations.
+///
+/// Report to stdout the memory usage of both tries, alongside
+/// the memory size of the original raw corpus.
+fn compute_and_report_memory_usage() {
+    let mut map = RustTrieMap::new();
+    let mut cmap = CTrieMap::new();
+    let contents = trie_bencher::download_or_read_corpus();
+    let mut raw_size = 0;
+    let mut n_words = 0;
+    let mut unique_words = BTreeSet::new();
+    for line in contents.lines() {
+        for word in line.split_whitespace() {
+            let converted = str2c_char(word);
+            raw_size += converted.len();
+            n_words += 1;
+            unique_words.insert(word.to_owned());
+
+            // Use a zero-sized type by passing a null pointer for `value`
+            let value = NonNull::dangling();
+
+            // Rust insertion
+            map.insert(&converted, value);
+
+            // C insertion
+            let (c_word, c_len) = str2c_input(&word);
+            cmap.insert(c_word, c_len);
+        }
+    }
+
+    // Sanity check
+    for unique_word in &unique_words {
+        let converted = str2c_char(unique_word);
+        assert!(
+            map.find(&converted).is_some(),
+            "{unique_word} not found in Rust map"
+        );
+        let (c_word, c_len) = str2c_input(unique_word);
+        assert!(
+            cmap.find(c_word, c_len) != unsafe { trie_bencher::ffi::TRIEMAP_NOTFOUND },
+            "{unique_word} not found in C map"
+        )
+    }
+    let n_stored_words = map.iter().count();
+    let n_unique_words = unique_words.len();
+    assert_eq!(n_stored_words, n_unique_words);
+    println!(
+        r#"Statistics:
+- Raw text size: {:.3} MBs
+- Number of words (with duplicates): {n_words}
+- Number of unique words: {n_unique_words}
+- Rust -> {:.3} MBs
+          {} nodes
+- C    -> {:.3} MBs
+          {} nodes"#,
+        raw_size as f64 / 1024. / 1024.,
+        map.mem_usage() as f64 / 1024. / 1024.,
+        map.num_nodes(),
+        cmap.mem_usage() as f64 / 1024. / 1024.,
+        cmap.n_nodes()
+    );
+}
