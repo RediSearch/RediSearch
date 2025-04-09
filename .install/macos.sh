9ef050b5ccd309ad6348aa7aad9cf44d621d76fa
meiravgri <meirav.grimberg@redis.com>
2025-04-09 15:48:56 +0300

    flow-build: install deps install macos: disable things (epect update and make and wget

--- .install/macos.sh
+++ .install/macos.sh
@@ -15,16 +15,16 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 # For now we went with pinning cmake to 3.31.6 which is the version that is exists in the current mac OS docker image we use
 brew pin cmake
 brew update
-brew install coreutils
+# brew install coreutils
 brew install make
-brew install openssl
+# brew install openssl
 brew install wget
-"$(dirname "$0")/install_llvm.sh"
+# "$(dirname "$0")/install_llvm.sh"
 
-BREW_PREFIX=$(brew --prefix)
-GNUBIN=$BREW_PREFIX/opt/make/libexec/gnubin
-COREUTILS=$BREW_PREFIX/opt/coreutils/libexec/gnubin
+# BREW_PREFIX=$(brew --prefix)
+# GNUBIN=$BREW_PREFIX/opt/make/libexec/gnubin
+# COREUTILS=$BREW_PREFIX/opt/coreutils/libexec/gnubin
 
-# Update both profile files with all tools
-[[ -f ~/.bash_profile ]] && update_profile ~/.bash_profile "$GNUBIN" "$COREUTILS"
-[[ -f ~/.zshrc ]] && update_profile ~/.zshrc "$GNUBIN" "$COREUTILS"
+# # Update both profile files with all tools
+# [[ -f ~/.bash_profile ]] && update_profile ~/.bash_profile "$GNUBIN" "$COREUTILS"
+# [[ -f ~/.zshrc ]] && update_profile ~/.zshrc "$GNUBIN" "$COREUTILS"
