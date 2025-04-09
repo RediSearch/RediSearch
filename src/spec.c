66ddc36d0afc6594d025b827dad9c770e0e4b77e
kei-nan <jonathan.keinan@redis.com>
2025-04-09 18:35:58 +0300

    MOD-9316: Fix Duplicate Index Loading From RDB (#5892)
    * - initial commit: need to consume the entirety of the rdb when loading an index
    
    * - fix typo
    
    * - code review comments
    
    * Update src/spec.c
    
    code review comment
    
    * - code review comment
    
    (cherry picked from commit 1da6fc3036600c9fed70fb2944c054368c9b3045)

--- src/spec.c
+++ src/spec.c
@@ -1569,8 +1569,10 @@ void IndexSpec_RemoveFromGlobals(StrongRef spec_ref) {
   // Remove spec from global index list
   dictDelete(specDict_g, spec->name);
 
-  // Remove spec from global aliases list
-  IndexSpec_ClearAliases(spec_ref);
+  if (!spec->isDuplicate) {
+    // Remove spec from global aliases list
+    IndexSpec_ClearAliases(spec_ref);
+  }
 
   SchemaPrefixes_RemoveSpec(spec_ref);
 
@@ -2491,10 +2493,11 @@ int IndexSpec_CreateFromRdb(RedisModuleCtx *ctx, RedisModuleIO *rdb, int encver,
   size_t len = strlen(rawName);
   char *name = rm_strndup(rawName, len);
   RedisModule_Free(rawName);
-  RefManager *oldSpec = dictFetchValue(specDict_g, name);
+
+  RefManager *oldSpec = dictFetchValue(specDict_g, specName);
   if (oldSpec) {
     // spec already exists lets just free this one
-    rm_free(name);
+    HiddenString_Free(specName, true);
     RedisModule_Log(RSDummyContext, "notice", "Loading an already existing index, will just ignore.");
     return REDISMODULE_OK;
   }
@@ -2502,6 +2505,14 @@ int IndexSpec_CreateFromRdb(RedisModuleCtx *ctx, RedisModuleIO *rdb, int encver,
   IndexSpec *sp = rm_calloc(1, sizeof(IndexSpec));
   StrongRef spec_ref = StrongRef_New(sp, (RefManager_Free)IndexSpec_Free);
   sp->own_ref = spec_ref;
+  // setting isDuplicate to true will make sure index will not be removed from aliases container.
+  const RefManager *oldSpec = dictFetchValue(specDict_g, specName);
+  sp->isDuplicate = oldSpec != NULL;
+  if (sp->isDuplicate) {
+    // spec already exists, however we need to finish consuming the rdb so redis won't issue an error(expecting an eof but seeing remaining data)
+    // right now this can cause nasty side effects, to avoid them we will set isDuplicate to true
+    RedisModule_Log(RSDummyContext, "notice", "Loading an already existing index, will just ignore.");
+  }
 
   // `indexError` must be initialized before attempting to free the spec
   sp->stats.indexError = IndexError_Init();
@@ -2590,9 +2601,18 @@ int IndexSpec_CreateFromRdb(RedisModuleCtx *ctx, RedisModuleIO *rdb, int encver,
   sp->indexer = NewIndexer(sp);
 
   sp->scan_in_progress = false;
-  dictAdd(specDict_g, (void*)sp->name, spec_ref.rm);
-  for (int i = 0; i < sp->numFields; i++) {
-    FieldsGlobalStats_UpdateStats(sp->fields + i, 1);
+  if (sp->isDuplicate) {
+    // spec already exists lets just free this one
+    // Remove the new spec from the global prefixes dictionary.
+    // This is the only global structure that we added the new spec to at this point
+    SchemaPrefixes_RemoveSpec(spec_ref);
+    addPendingIndexDrop();
+    StrongRef_Release(spec_ref);
+  } else {
+    dictAdd(specDict_g, (void*)sp->name, spec_ref.rm);
+    for (int i = 0; i < sp->numFields; i++) {
+      FieldsGlobalStats_UpdateStats(sp->fields + i, 1);
+    }
   }
   return REDISMODULE_OK;
 
