6f0b5144e5e13441098747de4967021e48e584ae
alon <alonreshef24@gmail.com>
2025-04-12 17:17:57 +0300

    try EXPORT properly with homebrew

--- .install/install_llvm.sh
+++ .install/install_llvm.sh
@@ -21,5 +21,6 @@ else
     chmod +x llvm.sh
     $MODE ./llvm.sh $VERSION
 fi
-export LDFLAGS="-L/usr/local/opt/llvm@$VERSION/lib"
-export CPPFLAGS="-I/usr/local/opt/llvm@$VERSION/include"
+
+export LDFLAGS="-L/opt/homebrew/opt/llvm$VERSION/lib -L/opt/homebrew/opt/llvm$VERSION/lib/c++ -lunwind"
+export CPPFLAGS="-I/opt/homebrew/opt/llvm$VERSION/include"
