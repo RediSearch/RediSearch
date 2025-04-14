7e822d53a3276ce5f4b5cda9744bf06ce18ec30e
meiravgri <meirav.grimberg@redis.com>
2025-04-14 18:23:16 +0300

    revert macos.sh

--- .install/macos.sh
+++ .install/macos.sh
@@ -11,16 +11,16 @@ fi
 export HOMEBREW_NO_AUTO_UPDATE=1
 
 brew update
-# brew install coreutils
+brew install coreutils
 brew install make
-# brew install openssl
+brew install openssl
 brew install wget
 "$(dirname "$0")/install_llvm.sh"
 
-# BREW_PREFIX=$(brew --prefix)
-# GNUBIN=$BREW_PREFIX/opt/make/libexec/gnubin
-# COREUTILS=$BREW_PREFIX/opt/coreutils/libexec/gnubin
+BREW_PREFIX=$(brew --prefix)
+GNUBIN=$BREW_PREFIX/opt/make/libexec/gnubin
+COREUTILS=$BREW_PREFIX/opt/coreutils/libexec/gnubin
 
-# # Update both profile files with all tools
-# [[ -f ~/.bash_profile ]] && update_profile ~/.bash_profile "$GNUBIN" "$COREUTILS"
-# [[ -f ~/.zshrc ]] && update_profile ~/.zshrc "$GNUBIN" "$COREUTILS"
+# Update both profile files with all tools
+[[ -f ~/.bash_profile ]] && update_profile ~/.bash_profile "$GNUBIN" "$COREUTILS"
+[[ -f ~/.zshrc ]] && update_profile ~/.zshrc "$GNUBIN" "$COREUTILS"
