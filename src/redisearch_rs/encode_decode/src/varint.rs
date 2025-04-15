f27769b6d0787e2adec848aa0c276f87ee594d4b
Zeeshan Ali Khan <zeenix@gmail.com>
2025-04-15 14:57:07 +0200

    Utility to convert a BufferReader to a io::Cursor

--- src/redisearch_rs/encode_decode/src/varint.rs
+++ src/redisearch_rs/encode_decode/src/varint.rs
@@ -1,7 +1,6 @@
 use crate::buffer::BufferReader;
 
-use core::slice;
-use std::io::{Cursor, Read};
+use std::io::Read;
 
 //
 // The C API goes here. This part will hopefully all be removed once all users of `varint` API are
@@ -11,13 +10,7 @@ use std::io::{Cursor, Read};
 #[unsafe(no_mangle)]
 extern "C" fn ReadVarint(b: *mut BufferReader) -> u32 {
     let buffer_reader = unsafe { b.as_mut() }.unwrap();
-    let buffer_slice = unsafe {
-        let buffer = &*buffer_reader.buf;
-        slice::from_raw_parts(buffer.data, buffer.offset)
-    };
-
-    let mut cursor = Cursor::new(buffer_slice);
-    cursor.set_position(buffer_reader.pos as u64);
+    let mut cursor = buffer_reader.to_cursor();
     let val = read(&mut cursor).unwrap();
     buffer_reader.pos = cursor.position() as usize;
 
