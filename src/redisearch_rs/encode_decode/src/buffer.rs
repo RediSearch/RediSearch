3e4ab34395591a7cb337cd5c85f84a820abe0b98
Zeeshan Ali Khan <zeenix@gmail.com>
2025-04-15 14:57:07 +0200

    Utility to convert a BufferReader to a io::Cursor

--- src/redisearch_rs/encode_decode/src/buffer.rs
+++ src/redisearch_rs/encode_decode/src/buffer.rs
@@ -12,3 +12,15 @@ pub(crate) struct BufferReader {
     pub buf: *const Buffer,
     pub pos: usize,
 }
+
+impl BufferReader {
+    pub fn to_cursor(&mut self) -> std::io::Cursor<&[u8]> {
+        let buffer_slice = unsafe {
+            let buffer = &*self.buf;
+            std::slice::from_raw_parts(buffer.data, buffer.offset)
+        };
+        let mut cursor = std::io::Cursor::new(buffer_slice);
+        cursor.set_position(self.pos as u64);
+        cursor
+    }
+}
