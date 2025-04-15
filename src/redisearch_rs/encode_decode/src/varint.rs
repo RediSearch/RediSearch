1424054f24621ea811e92faf2632862d0bcf7ce2
Zeeshan Ali Khan <zeenix@gmail.com>
2025-04-15 13:51:19 +0200

    Remove unnecessary mutability

--- src/redisearch_rs/encode_decode/src/varint.rs
+++ src/redisearch_rs/encode_decode/src/varint.rs
@@ -11,12 +11,12 @@ use std::io::{Cursor, Read};
 #[unsafe(no_mangle)]
 extern "C" fn ReadVarint(b: *mut BufferReader) -> u32 {
     let buffer_reader = unsafe { b.as_mut() }.unwrap();
-    let mut buffer_slice = unsafe {
+    let buffer_slice = unsafe {
         let buffer = &*buffer_reader.buf;
-        slice::from_raw_parts_mut(buffer.data, buffer.offset)
+        slice::from_raw_parts(buffer.data, buffer.offset)
     };
 
-    let mut cursor = Cursor::new(&mut buffer_slice);
+    let mut cursor = Cursor::new(buffer_slice);
     cursor.set_position(buffer_reader.pos as u64);
     let val = read(&mut cursor).unwrap();
     buffer_reader.pos = cursor.position() as usize;
