5c0fc9ced003d0c39568e07c5a309582bec495c0
Zeeshan Ali Khan <zeenix@gmail.com>
2025-04-16 12:07:31 +0200

    Set redis allocator as the global allocator

--- src/redisearch_rs/encode_decode/src/lib.rs
+++ src/redisearch_rs/encode_decode/src/lib.rs
@@ -1,6 +1,9 @@
 mod buffer;
 mod varint;
 
+#[global_allocator]
+static REDIS_MODULE_ALLOCATOR: redis_module::alloc::RedisAlloc = redis_module::alloc::RedisAlloc;
+
 /// Rust implementation of `t_fieldMask` from `redisearch.h`
 #[cfg(any(target_arch = "x86_64", target_arch = "aarch64"))]
 pub type FieldMask = u128;
