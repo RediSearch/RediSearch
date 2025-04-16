83d3e11294554e7b698a19d4afdac28ed8cbab39
Zeeshan Ali Khan <zeenix@gmail.com>
2025-04-16 12:36:53 +0200

    impl From<Cursor<Vec<u8>>> for BufferWriter
    The opposite of BufferWritter::into_cursor.

--- src/redisearch_rs/encode_decode/src/buffer.rs
+++ src/redisearch_rs/encode_decode/src/buffer.rs
@@ -1,3 +1,5 @@
+use std::{io::Cursor, mem::ManuallyDrop};
+
 /// Redefines the `Buffer` struct from `buffer.h`
 #[repr(C)]
 pub(crate) struct Buffer {
@@ -14,12 +16,12 @@ pub(crate) struct BufferReader {
 }
 
 impl BufferReader {
-    pub fn to_cursor(&mut self) -> std::io::Cursor<&[u8]> {
+    pub fn to_cursor(&mut self) -> Cursor<&[u8]> {
         let buffer_slice = unsafe {
             let buffer = &*self.buf;
             std::slice::from_raw_parts(buffer.data, buffer.cap)
         };
-        let mut cursor = std::io::Cursor::new(buffer_slice);
+        let mut cursor = Cursor::new(buffer_slice);
         cursor.set_position(self.pos as u64);
         cursor
     }
@@ -33,7 +35,7 @@ pub(crate) struct BufferWriter {
 }
 
 impl BufferWriter {
-    pub fn into_cursor(self) -> std::io::Cursor<Vec<u8>> {
+    pub fn into_cursor(self) -> Cursor<Vec<u8>> {
         let (buffer_vec, pos) = unsafe {
             let buffer = &mut *self.buf;
             let buffer_vec = Vec::from_raw_parts(buffer.data, buffer.offset, buffer.cap);
@@ -41,8 +43,25 @@ impl BufferWriter {
 
             (buffer_vec, pos)
         };
-        let mut cursor = std::io::Cursor::new(buffer_vec);
+        let mut cursor = Cursor::new(buffer_vec);
         cursor.set_position(pos as u64);
         cursor
+    }
+}
+
+impl From<Cursor<Vec<u8>>> for BufferWriter {
+    fn from(cursor: Cursor<Vec<u8>>) -> Self {
+        let pos = cursor.position() as usize;
+        let mut buffer_vec = ManuallyDrop::new(cursor.into_inner());
+        let buffer_ptr = buffer_vec.as_mut_ptr();
+
+        BufferWriter {
+            buf: Box::into_raw(Box::new(Buffer {
+                data: buffer_ptr,
+                cap: buffer_vec.capacity(),
+                offset: buffer_vec.len(),
+            })),
+            pos: unsafe { buffer_ptr.add(pos) },
+        }
     }
 }
