2bdfcd7acc526eba5a92341dced88f1d80f20447
Tim Janus <tim@janus.rs>
2025-04-22 14:29:05 +0200

    use feature for redis allocator

--- src/redisearch_rs/encode_decode/src/lib.rs
+++ src/redisearch_rs/encode_decode/src/lib.rs
@@ -1,6 +1,10 @@
 mod buffer;
 mod varint;
 
+/// Registers the Redis module allocator
+/// as the global allocator for the application.
+/// Disabled in tests.
+#[cfg(all(feature = "redis_allocator", not(test)))]
 #[global_allocator]
 static REDIS_MODULE_ALLOCATOR: redis_module::alloc::RedisAlloc = redis_module::alloc::RedisAlloc;
 
