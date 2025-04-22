2de14afb7a95f57cd8ca49bfc69d4539d69a7788
Tim Janus <tim@janus.rs>
2025-04-22 17:09:59 +0200

    MOD-9231 Enhance Allocator/Deallcator Shims for Rust Benchmarks (#5951)
    * Fixes two sources of memory leaks in the alloc/dealloc shim for benchmark FFI
    
    1. use size header for allocated memory to remove the right amount of data
    2. use a raii + new-type pattern to ensure that the non-null terminated input strings for the the C-side get deallocated
    
    * use trait extensions for view and conversion
    
    * refactor: CTrieMap use a view to keep ownership at rust side
    
    * document the safety of the redis_allocator module, add debug_assertions for implementation dependent behavior
    
    * add missing doc to unsafe block
    
    * address clippy warnings
    
    * format documents
    
    * improvements after review
    
    * Overwork compute and report memory usage binary
    
    fix: gutenberg corpus data generation to include more than 82 words and use correct lifetime of CString in main
    
    * refactoring, use c_map module name
    
    * address review comments
    
    * spelling
    
    * address review
    
    * review comments

--- src/redisearch_rs/trie_bencher/src/ffi.rs
+++ src/redisearch_rs/trie_bencher/src/ffi.rs
@@ -1,7 +1,29 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-#![allow(improper_ctypes)]
-#![allow(dead_code)]
-#![allow(unsafe_op_in_unsafe_fn)]
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+//! This module provides FFI bindings for the C trie functions used in the [crate::CTrieMap] implementation.
+
+// We hide the fairly large generated bindings in a separate module and reduce warnings.
+#[allow(non_upper_case_globals)]
+#[allow(non_camel_case_types)]
+#[allow(non_snake_case)]
+#[allow(improper_ctypes)]
+#[allow(dead_code)]
+#[allow(unsafe_op_in_unsafe_fn)]
+#[allow(clippy::ptr_offset_with_cast)]
+#[allow(clippy::upper_case_acronyms)]
+#[allow(clippy::useless_transmute)]
+#[allow(clippy::multiple_unsafe_ops_per_block)]
+#[allow(clippy::undocumented_unsafe_blocks)]
+mod bindings {
+    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+}
+
+// We re-export the required bindings only. This list will be extended as needed.
+// It allows us to keep the bindings module private while still exposing the necessary functions and types.
+pub(crate) use bindings::NewTrieMap;
+pub(crate) use bindings::TrieMap;
+pub(crate) use bindings::TrieMap_Add;
+pub(crate) use bindings::TrieMap_Delete;
+pub(crate) use bindings::TrieMap_ExactMemUsage;
+pub(crate) use bindings::TrieMap_Find;
+pub(crate) use bindings::TrieMap_Free;
+// used in outside binary crate (main.rs)
+pub use bindings::TRIEMAP_NOTFOUND;
