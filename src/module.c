04c7ee1a3d88f8bd33920abb017738fd5b20649d
GuyAv46 <guy.avimor@gmail.com>
2025-04-22 10:51:03 +0300

    comment out invalid read

--- src/module.c
+++ src/module.c
@@ -932,9 +932,9 @@ int CheckSupportedVestion() {
 int RediSearch_InitModuleInternal(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
   char *err;
 
-  char *invalid = rm_strdup("Invalid");
-  rm_free(invalid);
-  RedisModule_Log(ctx, "warning", "Invalid access: %7s, %p", invalid, invalid);
+  // char *invalid = rm_strdup("Invalid");
+  // rm_free(invalid);
+  // RedisModule_Log(ctx, "warning", "Invalid access: %7s, %p", invalid, invalid);
 
   legacySpecRules = dictCreate(&dictTypeHeapStrings, NULL);
 
