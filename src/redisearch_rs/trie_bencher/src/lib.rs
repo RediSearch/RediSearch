fe7ab0476e87e5d7ef283f614b30c0a68c7bed72
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2025-04-23 17:41:56 +0200

    [MOD-9230] "Faithful" trie reimplementation (#5882)
    * [MOD-9230] A Rust trie that's much closer to the structure of the original C implementation in deps/triemap
    
    The latter is included as a baseline.
    
    * Rename 'offset' to 'target' in 'shift_left'

--- src/redisearch_rs/trie_bencher/src/lib.rs
+++ src/redisearch_rs/trie_bencher/src/lib.rs
@@ -14,7 +14,7 @@ mod redis_allocator;
 
 // Convenient aliases for the trie types that are being benchmarked.
 pub use c_map::CTrieMap;
-pub type RustTrieMap = trie_rs::trie::TrieMap<NonNull<c_void>>;
+pub type RustTrieMap = trie_rs::TrieMap<NonNull<c_void>>;
 
 /// Convert a string to a slice of `c_char`, allocated on the heap, which is the expected input for [crate::RustTrieMap].
 pub fn str2boxed_c_char(input: &str) -> Box<[c_char]> {
