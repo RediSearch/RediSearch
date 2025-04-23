b81249079060548fa82b85ee00abe599c8eb5dec
alonre24 <alon.reshef@redis.com>
2025-04-23 19:51:33 +0300

    Revert "Refactor build system - remove readies from module build" (#6003)
    Revert "Refactor build system - remove readies from module build [MOD-5722] (#5737)"
    
    This reverts commit 39fcd2c0a38a915d73ad5aa6a56a19f7a88c18e0.

--- build/hiredis/hiredis.cmake
+++ build/hiredis/hiredis.cmake
@@ -1,15 +1,13 @@
 
-option(BUILD_SHARED_LIBS "Build shared libraries" OFF)
+set(BUILD_SHARED_LIBS OFF)
 set(ENABLE_SSL ON CACHE BOOL "Build hiredis with ssl")
 option(DISABLE_TESTS "If tests should be compiled or not" ON)
 
 add_subdirectory(${root}/deps/hiredis hiredis)
 
-if(APPLE)
+if (APPLE)
     include_directories(/usr/local/opt/openssl/include)
 	set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -L/usr/local/opt/openssl/lib")
-else()
-    set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,-Bsymbolic,-Bsymbolic-functions")
 endif()
 
 set(HIREDIS_LIBS hiredis hiredis_ssl)
