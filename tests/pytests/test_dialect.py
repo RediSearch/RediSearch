9b132fdcfee1c5d0da35bed926f50ed5427100c3
GuyAv46 <47632673+GuyAv46@users.noreply.github.com>
2025-04-27 13:26:20 +0300

    [2.10] Improve parser for intersections [MOD-9278] (#6007)
    * * improve intersection node parsing
    
    * refactor `one_not_null`
    
    * WIP test
    
    * complete test
    
    * reorder for a clearer diff
    
    * review fixes
    
    * fix spellcheck
    
    * fix tests for dialect 2 and above
    
    (cherry picked from commit 7c195eeb51daa5b11bfac7d0472387a6e4ddb041)
    
    * test fixes for 2.10

--- tests/pytests/test_dialect.py
+++ tests/pytests/test_dialect.py
@@ -99,7 +99,7 @@ def test_v1_vs_v2(env):
 
     res = env.cmd('FT.EXPLAINCLI', 'idx', "1.2e+3", 'DIALECT', 1)
     expected = [
-      'INTERSECT {',                                                                                                                                 
+      'INTERSECT {',
       '  UNION {',
       '    1.2',
       '    +1.2(expanded)',
@@ -144,14 +144,12 @@ def test_v1_vs_v2(env):
     res = env.cmd('FT.EXPLAINCLI', 'idx', "1.e+3", 'DIALECT', 2)
     expected = [
       'INTERSECT {',
-      '  1',
-      '  INTERSECT {',
-      '    UNION {',
-      '      e',
-      '      +e(expanded)',
-      '    }',
-      '    +3',
+      '  UNION {',
+      '    e',
+      '    +e(expanded)',
       '  }',
+      '  +3',
+      '  1',
       '}',
       ''
     ]
@@ -209,7 +207,7 @@ def test_dialect_aggregate(env):
     env.expect("FT.CREATE idx SCHEMA t1 TEXT t2 TEXT").ok()
     conn.execute_command("HSET", "h1", "t1", "James Brown", "t2", "Jimi Hendrix")
     conn.execute_command("HSET", "h2", "t1", "James", "t2", "Brown")
-    
+
     # In dialect 2, both documents are returned ("James" in t1 and "Brown" in any field)
     res = conn.execute_command('FT.AGGREGATE', 'idx', '@t1:James Brown', 'GROUPBY', '2', '@t1', '@t2', 'DIALECT', 1)
     env.assertEqual(res[0], 1)
