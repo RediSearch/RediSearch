24fbb335e2be94a4890f43e18b8dd325f89be379
Zeeshan Ali Khan <zeenix@gmail.com>
2025-04-29 16:15:37 +0200

    encode_decode shouldn't set a global allocator
    That's now the task of the wrapper `redisearch_rs` crate.

--- src/redisearch_rs/encode_decode/src/lib.rs
+++ src/redisearch_rs/encode_decode/src/lib.rs
@@ -2,13 +2,6 @@ mod buffer;
 pub mod varint;
 pub use buffer::*;
 
-/// Registers the Redis module allocator
-/// as the global allocator for the application.
-/// Disabled in tests.
-#[cfg(all(feature = "redis_allocator", not(test)))]
-#[global_allocator]
-static REDIS_MODULE_ALLOCATOR: redis_module::alloc::RedisAlloc = redis_module::alloc::RedisAlloc;
-
 /// Rust implementation of `t_fieldMask` from `redisearch.h`
 #[cfg(any(target_arch = "x86_64", target_arch = "aarch64"))]
 pub type FieldMask = u128;
