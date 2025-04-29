1dd57a18a439dd8b92918bf5b0e99df5e5e86a17
lerman25 <lerman25@gmail.com>
2025-04-29 15:08:18 +0300

    Change query error

--- src/query_error.h
+++ src/query_error.h
@@ -67,8 +67,7 @@ extern "C" {
   X(QUERY_EUNKNOWNINDEX, "Unknown index name")                                                   \
   X(QUERY_EDROPPEDBACKGROUND, "The index was dropped before the query could be executed")        \
   X(QUERY_EALIASCONFLICT, "Alias conflicts with an existing index name")                         \
-  X(QUERY_INDEXBGOOMFAIL, "Index background scan failed due to OOM. Queries cannot be executed on\
-     an incomplete index.")                                                                      \
+  X(QUERY_INDEXBGOOMFAIL, "Index background scan failed due to OOM")                             \
 
 #define QUERY_WMAXPREFIXEXPANSIONS "Max prefix expansions limit was reached"
 
