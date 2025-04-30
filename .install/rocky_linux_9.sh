5cbe5db444d7d7662466b4311b359deff56e77c3
Raz Monsonego <74051729+raz-mon@users.noreply.github.com>
2025-04-30 11:57:32 +0300

    MOD-9581: Fix rocky9 CI (#6061)
    * Fix rocky9 CI
    
    * Fix typo

--- .install/rocky_linux_9.sh
+++ .install/rocky_linux_9.sh
@@ -5,6 +5,6 @@ export DEBIAN_FRONTEND=noninteractive
 $MODE dnf update -y
 
 $MODE dnf install -y gcc-toolset-13-gcc gcc-toolset-13-gcc-c++ make wget git \
-    openssl openssl-devel python3 python3-devel which rsync unzip clang curl --nobest --skip-broken
+    openssl openssl-devel python3 python3-devel which rsync unzip clang curl --nobest --skip-broken --allowerasing
 
 cp /opt/rh/gcc-toolset-13/enable /etc/profile.d/gcc-toolset-13.sh
