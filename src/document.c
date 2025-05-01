0fd5c015ec1d2aa8d232d9261b37d2338787defb
DvirDukhan <dvir@redis.com>
2025-05-01 20:26:56 +0300

    update licnese options for branch master (#6071)
    * contributing.md
    
    * license.txt
    
    * agplv3
    
    * readme header
    
    * readme footer
    
    * source headers
    
    * ramp files
    
    * disable spell check for license change
    
    * disable lint for license change

--- src/document.c
+++ src/document.c
@@ -1,9 +1,11 @@
 /*
- * Copyright Redis Ltd. 2016 - present
- * Licensed under your choice of the Redis Source Available License 2.0 (RSALv2) or
- * the Server Side Public License v1 (SSPLv1).
- */
-
+ * Copyright (c) 2006-Present, Redis Ltd.
+ * All rights reserved.
+ *
+ * Licensed under your choice of the Redis Source Available License 2.0
+ * (RSALv2); or (b) the Server Side Public License v1 (SSPLv1); or (c) the
+ * GNU Affero General Public License v3 (AGPLv3).
+*/
 #include <string.h>
 #include <inttypes.h>
 
