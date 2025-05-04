a972d90632fb27859050996d9e54a53c4a3e239b
GuyAv46 <47632673+GuyAv46@users.noreply.github.com>
2025-05-04 18:00:08 +0300

    [8.0] Coverage Report Without `readies` - Phase 1 - [MOD-9056] (#6035)
    * Coverage Report Without `readies` - Phase 1 - [MOD-9056] (#5856)
    
    * remove report collection
    
    * Revert "remove report collection"
    
    This reverts commit c833cc5dfd81e6bbfac30ece6b33079a176aeabe.
    
    * attempt to split flow and unit
    
    * attempt
    
    * another attempt
    
    * another manual attempt
    
    * fix file path
    
    * another fix
    
    * attempt to capture fork coverage
    
    * fix typo
    
    * remove rust from flow report
    
    * another attempt
    
    * reorder flow
    
    * remove union upload
    
    * revert removing unit upload
    
    * attempt
    
    * add zero operation back
    
    * prettier return code
    
    * attempt to fix sanitizer
    
    * add different badges to the readme
    
    * git attempt to fix coverage build
    
    * another attempt
    
    * address CR, try removing COV variable
    
    * another attempt
    
    * Update flow-coverage.yml
    
    * Update .github/workflows/flow-coverage.yml
    
    Co-authored-by: Raz Monsonego <74051729+raz-mon@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Raz Monsonego <74051729+raz-mon@users.noreply.github.com>
    (cherry picked from commit 7a4c601d865753b5c53c17ef0654a0b210f28c80)
    
    * CI Improvement - Coverage fixes (#6044)
    
    * test some changes
    
    * more changes
    
    * try not removing coverage files
    
    * attempt
    
    * focus around the issue
    
    * cleanup
    
    * minor improvement
    
    * revert most of the makefile changes
    
    (cherry picked from commit e3bf220df77ad7ab9bca6107bb22f71baf89c32f)

--- tests/cpptests/redismock/redismock.cpp
+++ tests/cpptests/redismock/redismock.cpp
@@ -368,7 +368,7 @@ int RMCK_StringToLongLong(RedisModuleString *s, long long *l) {
 #define ENTRY_OK 1
 #define ENTRY_DONE 0
 #define ENTRY_ERROR -1
-// Retreives the hash value key and the following argument, and stores them in the provided pointers
+// Retrieves the hash value key and the following argument, and stores them in the provided pointers
 static int getNextEntry(va_list &ap, HashValue::Key &e, void **vpp) {
   void *kp = va_arg(ap, void *);
   if (!kp) {
@@ -986,8 +986,18 @@ static int RMCK_Fork(RedisModuleForkDoneHandler cb, void *user_data) {
 static void RMCK_SendChildHeartbeat(double progress) {
 }
 
+// like in Redis' `exitFromChild`, we exit from children using _exit() instead of
+// exit(), because the latter may interact with the same file objects used by
+// the parent process (may yield errors when testing with sanitizer).
+// However if we are testing the coverage normal exit() is
+// used in order to obtain the right coverage information.
 static int RMCK_ExitFromChild(int retcode) {
+#if defined(COV) || defined(COVERAGE)
+  exit(retcode);
+#else
   _exit(retcode);
+#endif
+  return REDISMODULE_OK; // never reached, but following the API "behavior"
 }
 
 static int RMCK_KillForkChild(int child_pid) {
