03ee7a3528e56ebcc64702223b5738fa4beb9782
lerman25 <58445352+lerman25@users.noreply.github.com>
2025-05-06 07:09:21 +0300

    [2.10] Avoid lazy expiration in background indexing for CRDT [MOD-9486] (#6056)
    Avoid lazy expiration in background indexing for CRDT [MOD-9486] (#6002)
    
    * fix leak +  open alawys on crdt
    
    * complete comment
    
    * format
    
    (cherry picked from commit 17193a5eed07f2bc43cb3fdfa4166f9466277209)

--- src/spec.c
+++ src/spec.c
@@ -2137,19 +2137,22 @@ static void Indexes_ScanProc(RedisModuleCtx *ctx, RedisModuleString *keyname, Re
   }
   // RMKey it is provided as best effort but in some cases it might be NULL
   bool keyOpened = false;
-  if (!key) {
+  if (!key || isCrdt) {
     key = RedisModule_OpenKey(ctx, keyname, REDISMODULE_READ | REDISMODULE_OPEN_KEY_NOEFFECTS);
     keyOpened = true;
   }
 
-  // check type of document is support and document is not empty
+  // Get the document type
   DocumentType type = getDocType(key);
-  if (type == DocumentType_Unsupported) {
-    return;
-  }
 
+  // Close the key if we opened it
   if (keyOpened) {
     RedisModule_CloseKey(key);
+  }
+
+  // Verify that the document type is supported and document is not empty
+  if (type == DocumentType_Unsupported) {
+    return;
   }
 
   if (scanner->global) {
