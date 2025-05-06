f7fb2ec65d884f1349934b1df2d783fb4b63d76d
Tim Janus <tim@janus.rs>
2025-05-06 13:20:20 +0200

    add license headers

--- src/redisearch_rs/qint/src/lib.rs
+++ src/redisearch_rs/qint/src/lib.rs
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2006-Present, Redis Ltd.
+ * All rights reserved.
+ *
+ * Licensed under your choice of the Redis Source Available License 2.0
+ * (RSALv2); or (b) the Server Side Public License v1 (SSPLv1); or (c) the
+ * GNU Affero General Public License v3 (AGPLv3).
+*/
+
 //! # qint encoding/decoding - From 2 up to 4 integers variable-length encoding scheme
 //!
 //! The qint encoding scheme is a variable-length encoding scheme for integers. It's header, i.e. leading byte,
