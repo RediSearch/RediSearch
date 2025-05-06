eeb4825d60880afdf32395b1413300f6a9b231fa
lerman25 <58445352+lerman25@users.noreply.github.com>
2025-05-06 17:14:03 +0300

    MOD-6151: Build without readies - simplify packing (#5908)
    * fix
    
    * change ramp version
    
    * fix macos
    
    * GHA3
    
    * fix macos
    
    * fix maxos
    
    * fix
    
    * fix
    
    * fix
    
    * new
    
    * purge readies
    
    * double
    
    * typo
    
    * remove debug
    
    * shapshot
    
    * gp
    
    * go conflict
    
    * darwin to macos
    
    * Account for get-platform change, remove redundant mkdir, remove not used/used once variables
    
    * remove unused DEP_NAMES
    
    * remove unused function
    
    * remove un condition
    
    * improve pack_ramp
    
    * remove unused DEPS
    
    * macos
    
    * remove xtx
    
    * replace eprint
    
    * remove runn
    
    * replace realapth
    
    * Revert "remove xtx"
    
    This reverts commit 6efb895107521874e7e5f03ed0d34f16109330b5.
    
    * try remove xtx
    
    * remove NOP
    
    * remove NUMVER
    
    * without tmp
    
    * delete
    
    * remove eval
    
    * remove release
    
    * Assume SNAPSHOT=1
    
    * remove SEMVER
    
    * fin
    
    * remove sbin/getver
    
    (cherry picked from commit 80aa242821bc9d0f734b19331b5d1646c263cc50)

--- sbin/pack.sh
+++ sbin/pack.sh
@@ -3,10 +3,22 @@
 PROGNAME="${BASH_SOURCE[0]}"
 HERE="$(cd "$(dirname "$PROGNAME")" &>/dev/null && pwd)"
 ROOT=$(cd $HERE/.. && pwd)
-export READIES=$ROOT/deps/readies
-. $READIES/shibumi/defs
-
 SBIN=$ROOT/sbin
+
+GET_PLATFORM="$SBIN/get-platform"
+
+realpath() {
+  local target="$1"
+  if [ -z "$target" ]; then
+    return 1
+  fi
+  (
+    cd "$(dirname "$target")" || exit 1
+    echo "$(pwd -P)/$(basename "$target")"
+  )
+}
+eprint() { echo "$@" >&2; }
+
 
 export PYTHONWARNINGS=ignore
 
@@ -22,16 +34,12 @@ if [[ $1 == --help || $1 == help || $HELP == 1 ]]; then
 
 		Argument variables:
 		RAMP=0|1            Build RAMP package
-		DEPS=0|1            Build dependencies files
-		SYM=0|1             Build debug symbols file
-		RELEASE=1           Generate "release" packages (artifacts/release/)
-		SNAPSHOT=1          Generate "shapshot" packages (artifacts/snapshot/)
 
 		MODULE_NAME=name    Module name (default: redisearch)
 		PACKAGE_NAME=name   Package stem name
 
 		BRANCH=name         Branch name for snapshot packages
-		WITH_GITSHA=1       Append Git SHA to shapshot package names
+		WITH_GITSHA=1       Append Git SHA to snapshot package names
 		VARIANT=name        Build variant
 		RAMP_VARIANT=name   RAMP variant (e.g. ramp-{name}.yml)
 
@@ -43,7 +51,6 @@ if [[ $1 == --help || $1 == help || $HELP == 1 ]]; then
 		JUST_PRINT=1        Only print package names, do not generate
 		VERBOSE=1           Print commands
 		HELP=1              Show help
-		NOP=1               Print commands, do not execute
 
 	END
 	exit 0
@@ -51,30 +58,13 @@ fi
 
 #----------------------------------------------------------------------------------------------
 
-OP=""
-[[ $NOP == 1 ]] && OP=echo
-
 # RLEC naming conventions
 
-ARCH=$($READIES/bin/platform --arch)
-[[ $ARCH == x64 ]] && ARCH=x86_64
-[[ $ARCH == arm64v8 ]] && ARCH=aarch64
+ARCH=$($GET_PLATFORM --arch)
 
-OS=$($READIES/bin/platform --os)
-[[ $OS == linux ]] && OS=Linux
+OS=$($GET_PLATFORM --os)
 
-OSNICK=$($READIES/bin/platform --osnick)
-[[ $OSNICK == trusty ]]  && OSNICK=ubuntu14.04
-[[ $OSNICK == xenial ]]  && OSNICK=ubuntu16.04
-[[ $OSNICK == bionic ]]  && OSNICK=ubuntu18.04
-[[ $OSNICK == focal ]]   && OSNICK=ubuntu20.04
-[[ $OSNICK == jammy ]]   && OSNICK=ubuntu22.04
-[[ $OSNICK == centos7 ]] && OSNICK=rhel7
-[[ $OSNICK == centos8 ]] && OSNICK=rhel8
-[[ $OSNICK == centos9 ]] && OSNICK=rhel9
-[[ $OSNICK == ol8 ]]     && OSNICK=rhel8
-[[ $OSNICK == rocky8 ]]  && OSNICK=rhel8
-[[ $OSNICK == rocky9 ]]  && OSNICK=rhel9
+OSNICK=$($GET_PLATFORM --version-artifact)
 
 
 PLATFORM="$OS-$OSNICK-$ARCH"
@@ -84,11 +74,7 @@ PLATFORM="$OS-$OSNICK-$ARCH"
 MODULE="$1"
 
 RAMP=${RAMP:-1}
-DEPS=${DEPS:-1}
-SYM=${SYM:-1}
 
-RELEASE=${RELEASE:-1}
-SNAPSHOT=${SNAPSHOT:-1}
 
 [[ -z $ARTDIR ]] && ARTDIR=bin/artifacts
 mkdir -p $ARTDIR $ARTDIR/snapshots
@@ -99,11 +85,35 @@ ARTDIR=$(cd $ARTDIR && pwd)
 MODULE_NAME=${MODULE_NAME:-redisearch}
 PACKAGE_NAME=${PACKAGE_NAME:-redisearch-oss}
 
-DEP_NAMES=""
-
 RAMP_CMD="python3 -m RAMP.ramp"
 
 #----------------------------------------------------------------------------------------------
+
+run_ramp_pack() {
+	local input="$1"
+	local output="$2"
+
+	$RAMP_CMD pack -m $RAMP_YAML \
+		$RAMP_ARGS \
+		-n "$MODULE_NAME" \
+		--verbose \
+		--debug \
+		--packname-file /tmp/ramp.fname \
+		-o "$output" \
+		"$input" \
+		>/tmp/ramp.err 2>&1 || true
+
+
+	if [[ ! -e "$output" ]]; then
+		eprint "Error generating RAMP file:"
+		>&2 cat /tmp/ramp.err
+		exit 1
+	else
+		local packname
+		packname="$(cat /tmp/ramp.fname)"
+		echo "# Created $(realpath "$packname")"
+	fi
+}
 
 pack_ramp() {
 	cd $ROOT
@@ -111,169 +121,41 @@ pack_ramp() {
 	local stem=${PACKAGE_NAME}.${PLATFORM}
 	local stem_debug=${PACKAGE_NAME}.debug.${PLATFORM}
 
-	if [[ $SNAPSHOT == 0 ]]; then
-		local verspec=${SEMVER}${VARIANT}
-		local packdir=.
-		local s3base=""
-	else
-		local verspec=${BRANCH}${VARIANT}
-		local packdir=snapshots
-		local s3base=snapshots/
-	fi
+	local verspec=${BRANCH}${VARIANT}
+	local packdir=snapshots
+	local s3base=snapshots/
 
 	local fq_package=$stem.${verspec}.zip
 	local fq_package_debug=$stem_debug.${verspec}.zip
 
-	[[ ! -d $ARTDIR/$packdir ]] && mkdir -p $ARTDIR/$packdir
+	mkdir -p $ARTDIR/$packdir
 
 	local packfile=$ARTDIR/$packdir/$fq_package
 	local packfile_debug=$ARTDIR/$packdir/$fq_package_debug
-
-	local xtx_vars=""
-	for dep in $DEP_NAMES; do
-		eval "export NAME_${dep}=${PACKAGE_NAME}_${dep}"
-		local dep_fname="${PACKAGE_NAME}.${dep}.${PLATFORM}.${verspec}.tgz"
-		eval "export PATH_${dep}=${s3base}${dep_fname}"
-		local dep_sha256="$ARTDIR/$packdir/${dep_fname}.sha256"
-		eval "export SHA256_${dep}=$(cat $dep_sha256)"
-
-		xtx_vars+=" -e NAME_$dep -e PATH_$dep -e SHA256_$dep"
-	done
 
 	if [[ -n $RAMP_YAML ]]; then
 		RAMP_YAML="$(realpath $RAMP_YAML)"
 	elif [[ -z $RAMP_VARIANT ]]; then
 		RAMP_YAML="$ROOT/pack/ramp.yml"
 	else
-		RAMP_YAML="$ROOT/pack/ramp${_RAMP_VARIANT}.yml"
+		RAMP_YAML="$ROOT/pack/ramp${RAMP_VARIANT:+-$RAMP_VARIANT}.yml"
 	fi
 
-	python3 $READIES/bin/xtx \
-		$xtx_vars \
-		-e NUMVER -e SEMVER \
-		$RAMP_YAML > /tmp/ramp.yml
 	if [[ $VERBOSE == 1 ]]; then
 		echo "# ramp.yml:"
-		cat /tmp/ramp.yml
 	fi
 
-	runn rm -f /tmp/ramp.fname $packfile
+	rm -f /tmp/ramp.fname $packfile
 
-	# ROOT is required so ramp will detect the right git commit
-	cd $ROOT
-	runn @ <<-EOF
-		$RAMP_CMD pack -m /tmp/ramp.yml \
-			$RAMP_ARGS \
-			-n $MODULE_NAME \
-			--verbose \
-			--debug \
-			--packname-file /tmp/ramp.fname \
-			-o $packfile \
-			$MODULE \
-			>/tmp/ramp.err 2>&1 || true
-		EOF
+	run_ramp_pack "$MODULE" "$packfile"
 
-	if [[ $NOP != 1 ]]; then
-		if [[ ! -e $packfile ]]; then
-			eprint "Error generating RAMP file:"
-			>&2 cat /tmp/ramp.err
-			exit 1
-		else
-			local packname=`cat /tmp/ramp.fname`
-			echo "# Created $(realpath $packname)"
-		fi
+	if [[ -f "$MODULE.debug" ]]; then
+		run_ramp_pack "$MODULE.debug" "$packfile_debug"
 	fi
 
-	if [[ -f $MODULE.debug ]]; then
-		runn @ <<-EOF
-			$RAMP_CMD pack -m /tmp/ramp.yml \
-				$RAMP_ARGS \
-				-n $MODULE_NAME \
-				--verbose \
-				--debug \
-				--packname-file /tmp/ramp.fname \
-				-o $packfile_debug \
-				$MODULE.debug \
-				>/tmp/ramp.err 2>&1 || true
-			EOF
-
-		if [[ $NOP != 1 ]]; then
-			if [[ ! -e $packfile_debug ]]; then
-				eprint "Error generating RAMP file:"
-				>&2 cat /tmp/ramp.err
-				exit 1
-			else
-				local packname=`cat /tmp/ramp.fname`
-				echo "# Created $(realpath $packname)"
-			fi
-		fi
-	fi
-
-	cd $ROOT
+	cd "$ROOT"
 }
 
-#----------------------------------------------------------------------------------------------
-
-pack_deps() {
-	local dep="$1"
-
-	cd $ROOT
-
-	local stem=${PACKAGE_NAME}.${dep}.${PLATFORM}
-	local verspec=${SEMVER}${VARIANT}
-	local fq_package=$stem.${verspec}.tgz
-
-	local depdir=$(cat $ARTDIR/$dep.dir)
-	local tar_path=$ARTDIR/$fq_package
-	local dep_prefix_dir=$(cat $ARTDIR/$dep.prefix)
-
-	rm -f $tar_path
-	if [[ $NOP != 1 ]]; then
-		{ cd $depdir ;\
-		  cat $ARTDIR/$dep.files | \
-		  xargs tar -c --sort=name --owner=root:0 --group=root:0 --mtime='UTC 1970-01-01' \
-			--transform "s,^,$dep_prefix_dir," 2> /tmp/pack.err | \
-		  gzip -n - > $tar_path ; E=$?; } || true
-		if [[ ! -e $tar_path || -z $(tar tzf $tar_path) ]]; then
-			eprint "Count not create $tar_path. Aborting."
-			rm -f $tar_path
-			exit 1
-		fi
-	else
-		runn @ <<-EOF
-			cd $depdir
-			cat $ARTDIR/$dep.files | \
-			xargs tar -c --sort=name --owner=root:0 --group=root:0 --mtime='UTC 1970-01-01' \
-				--transform "s,^,$dep_prefix_dir," 2> /tmp/pack.err | \
-			gzip -n - > $tar_path ; E=$?; } || true
-			EOF
-	fi
-	runn @ <<-EOF
-		sha256sum $tar_path | gawk '{print $1}' > $tar_path.sha256
-		EOF
-
-	mkdir -p $ARTDIR/snapshots
-	cd $ARTDIR/snapshots
-	if [[ -n $BRANCH ]]; then
-		local snap_package=$stem.${BRANCH}${VARIANT}.tgz
-		runn ln -sf ../$fq_package $snap_package
-		runn ln -sf ../$fq_package.sha256 $snap_package.sha256
-	fi
-
-	cd $ROOT
-}
-
-#----------------------------------------------------------------------------------------------
-
-NUMVER="$(NUMERIC=1 $SBIN/getver)"
-SEMVER="$($SBIN/getver)"
-
-if [[ -n $VARIANT ]]; then
-	_VARIANT="-${VARIANT}"
-fi
-if [[ ! -z $RAMP_VARIANT ]]; then
-	_RAMP_VARIANT="-${RAMP_VARIANT}"
-fi
 
 #----------------------------------------------------------------------------------------------
 
@@ -290,7 +172,8 @@ if [[ -z $BRANCH ]]; then
 	BRANCH=$(git rev-parse --abbrev-ref HEAD)
 	# this happens of detached HEAD
 	if [[ $BRANCH == HEAD ]]; then
-		BRANCH="$SEMVER"
+		GIT_COMMIT=$(git rev-parse --short HEAD)
+		BRANCH="$GIT_COMMIT"
 	fi
 fi
 BRANCH=${BRANCH//[^A-Za-z0-9._-]/_}
@@ -302,52 +185,16 @@ fi
 
 #----------------------------------------------------------------------------------------------
 
-RELEASE_ramp=${PACKAGE_NAME}.$OS-$OSNICK-$ARCH.$SEMVER${VARIANT}.zip
 SNAPSHOT_ramp=${PACKAGE_NAME}.$OS-$OSNICK-$ARCH.${BRANCH}${VARIANT}.zip
-
-RELEASE_deps=
-SNAPSHOT_deps=
-for dep in $DEP_NAMES; do
-	RELEASE_deps+=" ${PACKAGE_NAME}.${dep}.$OS-$OSNICK-$ARCH.$SEMVER${VARIANT}.tgz"
-	SNAPSHOT_deps+=" ${PACKAGE_NAME}.${dep}.$OS-$OSNICK-$ARCH.${BRANCH}${VARIANT}.tgz"
-done
 
 #----------------------------------------------------------------------------------------------
 
 if [[ $JUST_PRINT == 1 ]]; then
 	if [[ $RAMP == 1 ]]; then
-		[[ $RELEASE == 1 ]] && echo $RELEASE_ramp
-		[[ $SNAPSHOT == 1 ]] && echo $SNAPSHOT_ramp
-	fi
-	if [[ $DEPS == 1 ]]; then
-		[[ $RELEASE == 1 ]] && echo $RELEASE_deps
-		[[ $SNAPSHOT == 1 ]] && echo $SNAPSHOT_deps
+		echo $SNAPSHOT_ramp
 	fi
 	exit 0
 fi
-
-#----------------------------------------------------------------------------------------------
-
-mkdir -p $ARTDIR
-
-if [[ $DEPS == 1 && -n $DEP_NAMES ]]; then
-	# set up `debug` dep
-	dirname "$(realpath "$MODULE")" > "$ARTDIR/debug.dir"
-	echo "$(basename "$(realpath "$MODULE")").debug" > "$ARTDIR/debug.files"
-	echo "" > $ARTDIR/debug.prefix
-
-	echo "# Building dependencies ..."
-
-	for dep in $DEP_NAMES; do
-		if [[ $OS != macos ]]; then
-			echo "# $dep ..."
-			pack_deps $dep
-		fi
-	done
-	echo "# Done."
-fi
-
-#----------------------------------------------------------------------------------------------
 
 cd $ROOT
 
@@ -363,8 +210,7 @@ if [[ $RAMP == 1 ]]; then
 	[[ ! -f $MODULE ]] && { eprint "$MODULE does not exist. Aborting."; exit 1; }
 	MODULE=$(realpath $MODULE)
 
-	[[ $RELEASE == 1 ]] && SNAPSHOT=0 pack_ramp
-	[[ $SNAPSHOT == 1 ]] && pack_ramp
+	pack_ramp
 
 	echo "# Done."
 fi
