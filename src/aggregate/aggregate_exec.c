316040f3b2f1a3f7da546e4e8a3d8ece4e51ee28
lerman25 <lerman25@gmail.com>
2025-05-14 18:30:36 +0300

    Add warning to resp3

--- src/aggregate/aggregate_exec.c
+++ src/aggregate/aggregate_exec.c
@@ -632,8 +632,13 @@ done_3:
     } else if (rc == RS_RESULT_ERROR) {
       // Non-fatal error
       RedisModule_Reply_SimpleString(reply, QueryError_GetUserError(req->qiter.err));
-    } else if (req->qiter.err->reachedMaxPrefixExpansions) {
-      RedisModule_Reply_SimpleString(reply, QUERY_WMAXPREFIXEXPANSIONS);
+    } else {
+      if (req->qiter.err->reachedMaxPrefixExpansions) {
+        RedisModule_Reply_SimpleString(reply, QUERY_WMAXPREFIXEXPANSIONS);
+      }
+      if (req->sctx->spec && req->sctx->spec->scan_failed_OOM) {
+        RedisModule_Reply_SimpleString(reply, QUERY_WINDEXING_FAILURE);
+      }
     }
     RedisModule_Reply_ArrayEnd(reply); // >warnings
 
