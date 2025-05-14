8046ae91f3ddf6c3de681b2a6329e3c9bf88598f
lerman25 <lerman25@gmail.com>
2025-05-14 18:20:20 +0300

    Change config help message

--- src/config.c
+++ src/config.c
@@ -1264,8 +1264,7 @@ RSConfigOptions RSGlobalConfigOptions = {
          .setValue = set_EnableUnstableFeatures,
          .getValue = get_EnableUnstableFeatures},
         {.name = "_BG_INDEX_MEM_PCT_THR",
-         .helpText = "Set the percentage of memory usage threshold (out of maxmemory) at which background indexing will stop. Once this limit is reached,"
-                      " any queries on the affected index will result in an error. The default is 100 percent.",
+         .helpText = "Set the percentage of memory usage threshold (out of maxmemory) at which background indexing will stop. The default is 100 percent.",
          .setValue = setIndexingMemoryLimit,
          .getValue = getIndexingMemoryLimit},
         {.name = "BM25STD_TANH_FACTOR",
