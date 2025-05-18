06822af42ddd058c654df53d663f12ff89194f18
nafraf <nafraf@users.noreply.github.com>
2025-05-18 11:29:14 +0000

    rockylinux:8 install python3.12 packages (#6147)
    rockylinux:8 install python3.12-devel

--- .install/rocky_linux_8.sh
+++ .install/rocky_linux_8.sh
@@ -7,7 +7,7 @@ $MODE dnf update -y
 # Development Tools includes python11 and config-manager
 $MODE dnf groupinstall "Development Tools" -yqq
 # install pip
-$MODE dnf install python3.11-pip -y
+$MODE dnf install python3.12-pip -y
 
 # powertools is needed to install epel
 $MODE dnf config-manager --set-enabled powertools
@@ -17,7 +17,7 @@ $MODE dnf install epel-release -yqq
 
 $MODE dnf install -y gcc-toolset-13-gcc gcc-toolset-13-gcc-c++ \
     gcc-toolset-13-libatomic-devel make wget git openssl openssl-devel \
-    bzip2-devel libffi-devel zlib-devel tar xz which rsync python3.11-devel \
+    bzip2-devel libffi-devel zlib-devel tar xz which rsync python3.12-devel \
     clang curl --nobest --skip-broken
 
 cp /opt/rh/gcc-toolset-13/enable /etc/profile.d/gcc-toolset-13.sh
