148ddae269bcb05002ca970b4409201aba62b613
chesedo <pieter@chesedo.me>
2025-05-20 17:05:41 +0200

    Make a test runner

--- tests/shadow_tests/src/main.rs
+++ tests/shadow_tests/src/main.rs
@@ -2,12 +2,59 @@ use std::io::{Read, Write};
 use std::net::TcpStream;
 
 fn main() -> std::io::Result<()> {
-    let mut client = RedisClient::new("127.0.0.1:6379")?;
-    let response = client.query("PING")?;
+    let base_client = RedisClient::new("127.0.0.1:6379")?;
+    let changeset_client = RedisClient::new("127.0.0.1:6380")?;
 
-    println!("Response: {}", response);
+    let mut test_runner = TestRunner::new(base_client, changeset_client);
+    test_runner.add_command("PING");
+
+    let success = test_runner.run()?;
+
+    if success {
+        println!("All queries are the same!");
+    } else {
+        println!("Some queries failed.");
+    }
 
     Ok(())
+}
+
+struct TestRunner {
+    success: bool,
+    commands: Vec<String>,
+    base_client: RedisClient,
+    changeset_client: RedisClient,
+}
+
+impl TestRunner {
+    fn new(base_client: RedisClient, changeset_client: RedisClient) -> Self {
+        TestRunner {
+            success: true,
+            commands: Vec::new(),
+            base_client,
+            changeset_client,
+        }
+    }
+
+    fn add_command(&mut self, command: &str) {
+        self.commands.push(command.to_string());
+    }
+
+    fn run(mut self) -> std::io::Result<bool> {
+        for command in &self.commands {
+            let base_response = self.base_client.query(command)?;
+            let changeset_response = self.changeset_client.query(command)?;
+
+            if base_response != changeset_response {
+                eprintln!("Command: {} failed", command);
+                eprintln!("Base response: {}", base_response);
+                eprintln!("Changeset response: {}", changeset_response);
+                self.success = false;
+            }
+        }
+
+        Ok(self.success)
+    }
 }
 
 struct RedisClient {
