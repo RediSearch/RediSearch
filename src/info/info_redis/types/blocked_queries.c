9f79064a9d0a82bb70238dd6152e2f8c02978bf4
jonathan keinan <jonathan.keinan@redis.com>
2025-05-20 12:23:11 +0300

    initial commit

--- src/info/info_redis/types/blocked_queries.c
+++ src/info/info_redis/types/blocked_queries.c
@@ -9,6 +9,7 @@
 #include "info/info_redis/types/blocked_queries.h"
 #include "rmutil/rm_assert.h"
 #include "redismodule.h"
+#include "rmutil/rm_assert.h"
 
 BlockedQueries *BlockedQueries_Init() {
   BlockedQueries* blockedQueries = rm_calloc(1, sizeof(BlockedQueries));
@@ -17,14 +18,37 @@ BlockedQueries *BlockedQueries_Init() {
   return blockedQueries;
 }
 
+static size_t PrintActiveQueries(BlockedQueries *blockedQueries) {
+  size_t count = 0;
+  DLLIST_FOREACH(node, &blockedQueries->queries) {
+    BlockedQueryNode *at = DLLIST_ITEM(node, BlockedQueryNode, llnode);
+    IndexSpec *sp = StrongRef_Get(at->spec);
+    ++count; // increment regardless if sp is valid, the fact we have a valid node is problematic
+    if (!sp) {
+      continue;
+    }
+    RedisModule_Log(NULL, "warning", "Active query on index %s, started at %ld", IndexSpec_FormatName(sp, RSGlobalConfig.hideUserDataFromLog), at->start);
+  }
+  return count;
+}
+
+static size_t PrintActiveCursors(BlockedQueries *blockedQueries) {
+  size_t count = 0;
+  DLLIST_FOREACH(node, &blockedQueries->cursors) {
+    BlockedCursorNode *at = DLLIST_ITEM(node, BlockedCursorNode, llnode);
+    IndexSpec *sp = StrongRef_Get(at->spec);
+    ++count; // increment regardless if sp is valid, the fact we have a valid node is problematic
+    RedisModule_Log(NULL, "warning", "Active cursor %zu, on index %s, started at %ld", at->cursorId, sp ? IndexSpec_FormatName(sp, RSGlobalConfig.hideUserDataFromLog) : "n/a", at->start);
+  }
+  return count;
+}
+
 void BlockedQueries_Free(BlockedQueries *blockedQueries) {
-  // Assert that the lists are empty
-  RS_LOG_ASSERT((blockedQueries->queries.prev == blockedQueries->queries.next) &&
-                (blockedQueries->queries.next == &blockedQueries->queries),
-                "Active queries list is not empty");
-  RS_LOG_ASSERT((blockedQueries->cursors.prev == blockedQueries->cursors.next) &&
-              (blockedQueries->cursors.next == &blockedQueries->cursors),
-              "Active cursor list is not empty");
+  const size_t numQueries = PrintActiveQueries(blockedQueries);
+  const size_t numCursors = PrintActiveCursors(blockedQueries);
+  RS_LOG_ASSERT_FMT(numQueries == 0 && numCursors == 0, 
+    "There are %zu active queries and %zu active cursors. This is a bug. Please report it to https://github.com/redisearch/redisearch//issues", 
+    numQueries, numCursors);
   rm_free(blockedQueries);
 }
 
@@ -49,10 +73,15 @@ BlockedCursorNode* BlockedQueries_AddCursor(BlockedQueries* blockedQueries, Weak
 }
 
 void BlockedQueries_RemoveQuery(BlockedQueryNode* blockedQueryNode) {
-  StrongRef_Release(blockedQueryNode->spec);
+  if (blockedQueryNode->spec.rm) {
+    StrongRef_Release(blockedQueryNode->spec);
+  }
   dllist_delete(&blockedQueryNode->llnode);
 }
 
 void BlockedQueries_RemoveCursor(BlockedCursorNode* blockedCursorNode) {
+  if (blockedCursorNode->spec.rm) {
+    StrongRef_Release(blockedCursorNode->spec);
+  }
   dllist_delete(&blockedCursorNode->llnode);
 }
