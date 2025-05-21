2ffc52482ab503506c9aa30281c866ea11d9d0c5
chesedo <pieter@chesedo.me>
2025-05-21 12:20:04 +0200

    Success does not need to be a property

--- src/redisearch_rs/tools/shadow_tester/src/test_runner.rs
+++ src/redisearch_rs/tools/shadow_tester/src/test_runner.rs
@@ -1,7 +1,6 @@
 use crate::redis_client::RedisClient;
 
 pub struct TestRunner {
-    success: bool,
     commands: Vec<String>,
     base_client: RedisClient,
     changeset_client: RedisClient,
@@ -10,7 +9,6 @@ pub struct TestRunner {
 impl TestRunner {
     pub fn new(base_client: RedisClient, changeset_client: RedisClient) -> Self {
         TestRunner {
-            success: true,
             commands: Vec::new(),
             base_client,
             changeset_client,
@@ -22,6 +20,8 @@ impl TestRunner {
     }
 
     pub fn run(mut self) -> std::io::Result<bool> {
+        let mut success = true;
+
         for command in &self.commands {
             let base_response = self.base_client.query(command)?;
             let changeset_response = self.changeset_client.query(command)?;
@@ -30,10 +30,10 @@ impl TestRunner {
                 eprintln!("Command: {} failed", command);
                 eprintln!("Base response: {}", base_response);
                 eprintln!("Changeset response: {}", changeset_response);
-                self.success = false;
+                success = false;
             }
         }
 
-        Ok(self.success)
+        Ok(success)
     }
 }
