edbfa53a6a09ff11a5b9556f0a516342c7154d14
alon <alonreshef24@gmail.com>
2025-05-21 18:08:13 +0300

    build focal with gcc-11

--- .install/ubuntu_20.04.sh
+++ .install/ubuntu_20.04.sh
@@ -7,6 +7,6 @@ $MODE apt update -qq
 $MODE apt upgrade -yqq
 
 $MODE apt install -yqq wget make clang-format gcc python3 python3-venv python3-pip lcov git openssl libssl-dev \
-    unzip rsync build-essential gcc-10 g++-10 curl libclang-dev
+    unzip rsync build-essential gcc-11 g++-11 curl libclang-dev
 
-$MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 60 --slave /usr/bin/g++ g++ /usr/bin/g++-10
+$MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 60 --slave /usr/bin/g++ g++ /usr/bin/g++-11
