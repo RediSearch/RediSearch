0977063d1a924b4aa42fb035cc884af0b4fa5a20
alon <alonreshef24@gmail.com>
2025-05-21 18:18:06 +0300

    add software-properties-common and update

--- .install/ubuntu_20.04.sh
+++ .install/ubuntu_20.04.sh
@@ -5,8 +5,10 @@ MODE=$1 # whether to install using sudo or not
 
 $MODE apt update -qq
 $MODE apt upgrade -yqq
-
+$MODE apt install -yqq software-properties-common
 $MODE add-apt-repository ppa:ubuntu-toolchain-r/test -y
+$MODE apt update
+
 $MODE apt install -yqq wget make clang-format gcc python3 python3-venv python3-pip lcov git openssl libssl-dev \
     unzip rsync build-essential gcc-11 g++-11 curl libclang-dev
 
