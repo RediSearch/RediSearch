ca341e368a2640b1a1eaf56ed0773f62164114d2
chesedo <pieter@chesedo.me>
2025-05-21 14:15:24 +0200

    Remove short version of args

--- src/redisearch_rs/tools/shadow_tester/src/args.rs
+++ src/redisearch_rs/tools/shadow_tester/src/args.rs
@@ -7,15 +7,15 @@ use clap::Parser;
 pub struct Options {
     /// Path to the baseline .so file. The responses from this module will be considered the source
     /// of truth.
-    #[arg(short, long)]
+    #[arg(long)]
     pub baseline_so: PathBuf,
 
     /// Path to the changeset .so file. The responses from this module should match the baseline's.
-    #[arg(short, long)]
+    #[arg(long)]
     pub changeset_so: PathBuf,
 
     /// Path to a script which start up a RLTest server which will start a Redis server for each
     /// .so file.
-    #[arg(short, long)]
+    #[arg(long)]
     pub rltest_path: PathBuf,
 }
