d81202ba16ed11df2f1b2759c6ae6676b1aed472
meiravgri <109056284+meiravgri@users.noreply.github.com>
2025-05-25 23:14:58 +0300

    [MOD-9396] Fix indexError leak  (#6193)
    * free and invalidate NA_rstr (expect saniztizer to fail
    
    * fix 1
    should still leak on coordinator
    
    * fix final leak - override AggregatedFieldSpecInfo and lose ref
    
    * Change the number of issues
    
    * change to 1024
    
    * move fields IndexError_Clear to main thread
    
    * remove rm_free(spec->fields);
    
    * fix
    
    * add header
    
    ---------
    
    Co-authored-by: Dor Forer <dor.forer@redis.com>

--- src/info/index_error.h
+++ src/info/index_error.h
@@ -37,6 +37,11 @@ typedef struct IndexError {
 // Global constant to place an index error object in maps/dictionaries.
 extern char* const IndexError_ObjectName;
 
+/***************************************************************
+ *  This API is NOT THREAD SAFE as it utilizes RedisModuleString objects
+ * which are not thread safe. 
+***************************************************************/
+
 // Initializes an IndexError. The error_count is set to 0 and the last_error is set to NA.
 IndexError IndexError_Init();
 
@@ -75,6 +80,10 @@ void IndexError_Clear(IndexError error);
 // IO and cluster traits
 // Reply the index errors to the client.
 void IndexError_Reply(const IndexError *error, RedisModule_Reply *reply, bool withTimestamp, bool obfuscate, bool withOOMstatus);
+
+// Clears global variables used in the IndexError module.
+// This function should be called on shutdown.
+void IndexError_GlobalCleanup();
 
 #include "coord/rmr/reply.h"
 
