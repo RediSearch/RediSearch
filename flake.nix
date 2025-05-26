e6b5cf4b11bdc04b5ffb3a1d766a1b7e4cc1cc89
chesedo <pieter@chesedo.me>
2025-05-26 16:56:25 +0200

    Final touches

--- flake.nix
+++ flake.nix
@@ -37,19 +37,17 @@
 
               echo -e "\033[1;36m=== 🦀 Welcome to the RediSearch development environment ===\033[0m"
               echo -e "\033[1;33m• $cargo_version\033[0m"
-              echo ""
-
               echo -e "\n\033[1;33m• Checking for any outdated packages...\033[0m\n"
               cd src/redisearch_rs && cargo outdated --root-deps-only
 
               # For libclang dependency to work
               export LIBCLANG_PATH="${pkgs.llvmPackages.libclang.lib}/lib"
-              # For `sys/types.h` required by redismodules-rs
+              # For `sys/types.h` and `stddef.h` required by redismodules-rs
               export BINDGEN_EXTRA_CLANG_ARGS="-I${pkgs.glibc.dev}/include -I${pkgs.gcc-unwrapped}/lib/gcc/x86_64-unknown-linux-gnu/14.2.1/include"
             '';
 
             buildInputs = with pkgs; [
-              # For lsp
+              # For LSP
               ccls
 
               # Dev dependencies based on developer.md
@@ -73,7 +71,6 @@
               rust-bin.stable.latest.default
             ];
 
-            # Extra inputs can be added here; cargo and rustc are provided by default.
             packages = with pkgs; [
               rust-analyzer
               cargo-watch
@@ -93,7 +90,6 @@
               }))
             ];
 
-            # Extra inputs can be added here; cargo and rustc are provided by default.
             packages = with pkgs; [
               cargo-llvm-cov
             ];
