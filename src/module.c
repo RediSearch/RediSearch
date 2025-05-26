890d7daf4c891dda9686586de7065946b3ad76a3
meiravgri <meirav.grimberg@redis.com>
2025-05-26 14:41:04 +0000

    move cleanup before auto memory

--- src/module.c
+++ src/module.c
@@ -1188,9 +1188,10 @@ void RediSearch_CleanupModule(void) {
   SchemaPrefixes_Free(SchemaPrefixes_g);
   // GeometryApi_Free();
 
+  IndexError_GlobalCleanup();
+
   RedisModule_FreeThreadSafeContext(RSDummyContext);
   Dictionary_Free();
   RediSearch_LockDestory();
 
-  IndexError_GlobalCleanup();
 }
