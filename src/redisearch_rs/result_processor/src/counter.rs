571510db0e1e828e4bc46e45b97153d3d85ffff4
Jonas Kruckenberg <iterpre@protonmail.com>
2025-05-28 11:17:09 +0200

    add license headers

--- src/redisearch_rs/result_processor/src/counter.rs
+++ src/redisearch_rs/result_processor/src/counter.rs
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2006-Present, Redis Ltd.
+ * All rights reserved.
+ *
+ * Licensed under your choice of the Redis Source Available License 2.0
+ * (RSALv2); or (b) the Server Side Public License v1 (SSPLv1); or (c) the
+ * GNU Affero General Public License v3 (AGPLv3).
+*/
+
 use crate::{ResultProcessor, ResultProcessorType};
 
 pub struct Counter {
@@ -18,6 +27,12 @@ impl ResultProcessor for Counter {
         }
 
         Ok(Some(crate::ffi::SearchResult::default()))
+    }
+}
+
+impl Default for Counter {
+    fn default() -> Self {
+        Self::new()
     }
 }
 
