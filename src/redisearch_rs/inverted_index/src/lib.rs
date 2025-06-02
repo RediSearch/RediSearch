dcf5ddd54a8337bf85f2e6e4f117ae18b7f41acb
chesedo <pieter@chesedo.me>
2025-06-02 16:49:20 +0200

    Cleanup
    Remove unused imports.

--- src/redisearch_rs/inverted_index/src/lib.rs
+++ src/redisearch_rs/inverted_index/src/lib.rs
@@ -1,6 +1,6 @@
 use std::io::{Read, Seek, Write};
 
-pub use ffi::{t_docId, t_fieldMask, NumericFilter, RSIndexResult};
+pub use ffi::{t_docId, RSIndexResult};
 
 /// Encoder to write a record into an index
 pub trait Encoder {
