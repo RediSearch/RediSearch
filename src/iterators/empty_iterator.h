7851f3d95db3b0e6f6222d3dd7bec8b2a9de1268
Joan Fontanals Martinez <jfontanalsmartinez@gmail.com>
2025-06-03 08:53:57 +0200

    refactor EmptyIterator

--- src/iterators/empty_iterator.h
+++ src/iterators/empty_iterator.h
@@ -10,24 +10,18 @@
 #pragma once
 
 #include "iterator_api.h"
+#include "util/timeout.h"
+#include "query_ctx.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-typedef struct {
-  QueryIterator base;
-  t_docId *docIds;
-  t_offset size;
-  t_offset offset;
-} IdListIterator;
-
 /**
- * @param ids - the list of doc ids to iterate over
- * @param num - the number of doc ids in the list
- * @param weight - the weight of the node (assigned to the returned result)
+ * @param maxId - The maxID to return
+ * @param numDocs - the number of docs to return
  */
-QueryIterator *IT_V2(NewIdListIterator)(t_docId *ids, t_offset num, double weight);
+QueryIterator *IT_V2(NewEmptyIterator)(void);
 
 #ifdef __cplusplus
 }
