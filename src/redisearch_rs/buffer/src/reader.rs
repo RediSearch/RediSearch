b38ab72a233094541268899fa1d00525b94f63cc
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2025-06-04 17:29:27 +0200

    Update src/redisearch_rs/buffer/src/reader.rs
    Co-authored-by: Pieter <pieter@chesedo.me>

--- src/redisearch_rs/buffer/src/reader.rs
+++ src/redisearch_rs/buffer/src/reader.rs
@@ -16,7 +16,7 @@ use crate::Buffer;
 /// # Invariants
 ///
 /// 1. Position is smaller than or equal to the length of the buffer we're reading from.
-/// 2. `BufferReader` has the same memory layout of [`ffi::BufferReader`].
+/// 2. `BufferReader` has the same memory layout as [`ffi::BufferReader`].
 #[repr(C)]
 pub struct BufferReader<'a> {
     buffer: &'a Buffer,
