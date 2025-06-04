4e45d6348e7093ae49aba1cd42b6695e742710e9
chesedo <pieter@chesedo.me>
2025-06-04 14:40:42 +0200

    Add license headers

--- src/redisearch_rs/redisearch_rs/src/types/mod.rs
+++ src/redisearch_rs/redisearch_rs/src/types/mod.rs
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2006-Present, Redis Ltd.
+ * All rights reserved.
+ *
+ * Licensed under your choice of the Redis Source Available License 2.0
+ * (RSALv2); or (b) the Server Side Public License v1 (SSPLv1); or (c) the
+ * GNU Affero General Public License v3 (AGPLv3).
+*/
+
 //! This module contains pure Rust types that we want to expose to C code.
 
 pub use inverted_index::types::RSNumericRecord;
