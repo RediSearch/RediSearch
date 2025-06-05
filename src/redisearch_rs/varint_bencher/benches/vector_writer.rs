69793aa49117ab6b1c6a86e3f2790f73c50df7a8
Zeeshan Ali Khan <zeenix@gmail.com>
2025-06-05 16:06:30 +0200

    Rename a benchmark

--- src/redisearch_rs/varint_bencher/benches/vector_writer.rs
+++ src/redisearch_rs/varint_bencher/benches/vector_writer.rs
@@ -14,7 +14,7 @@ use std::time::Duration;
 use varint_bencher::VarintBencher;
 
 fn benchmark_vector_writer(c: &mut Criterion) {
-    let bencher = VarintBencher::new("Vector Writer".to_owned(), Duration::from_secs(10));
+    let bencher = VarintBencher::new("Varint Vector Writer".to_owned(), Duration::from_secs(10));
     bencher.vector_writer_group(c);
 }
 
