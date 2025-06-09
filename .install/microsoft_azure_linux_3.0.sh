aadea4a3a1e021c522be1f05c5bda0e4f4ac0352
GuyAv46 <47632673+GuyAv46@users.noreply.github.com>
2025-06-09 15:18:23 +0300

    Fix azure:linux setup (#6283)
    * attempt
    
    * test
    
    * Revert "test"
    
    This reverts commit b04669a008537f9637948d98d1a5d542e8376ef6.
    
    (cherry picked from commit e20a87d0b5d54f828e0a50f488ad1d076a03bb0c)

--- .install/microsoft_azure_linux_3.0.sh
+++ .install/microsoft_azure_linux_3.0.sh
@@ -5,4 +5,5 @@ export DEBIAN_FRONTEND=noninteractive
 
 $MODE tdnf install -q -y build-essential git wget ca-certificates tar unzip \
                          rsync openssl-devel python3 python3-pip python3-devel \
-                         which clang libxcrypt-devel
+                         which clang libxcrypt-devel \
+                         --exclude=aznfs # https://github.com/microsoft/azurelinux/issues/13971
