bc318b5713f112c5f5badfd44a5e4bcf6bfebcb3
Jonas Kruckenberg <iterpre@protonmail.com>
2025-06-10 10:12:33 +0200

    rebase fixes

--- src/redisearch_rs/c_entrypoint/result_processor_ffi/src/counter.rs
+++ src/redisearch_rs/c_entrypoint/result_processor_ffi/src/counter.rs
@@ -7,7 +7,7 @@
  * GNU Affero General Public License v3 (AGPLv3).
  */
 
-use result_processor::{counter::Counter, ffi};
+use result_processor::{ResultProcessorWrapper, counter::Counter};
 
 /// Crate a new heap-allocated `Counter` result processor
 ///
@@ -16,9 +16,9 @@ use result_processor::{counter::Counter, ffi};
 /// - The caller must never move the allocated result processor from its original allocation.
 /// - The caller must ensure to call the `Free` VTable function to properly destroy the type.
 #[unsafe(no_mangle)]
-pub unsafe extern "C" fn RPCounter_New() -> *mut ffi::Header {
-    let rp = Box::pin(ffi::ResultProcessor::new(Counter::new()));
+pub unsafe extern "C" fn RPCounter_New() -> *mut ffi::ResultProcessor {
+    let rp = Box::pin(ResultProcessorWrapper::new(Counter::new()));
 
     // Safety: The safety contract requires the caller to treat the returned pointer as pinned
-    unsafe { ffi::ResultProcessor::into_ptr(rp) }.cast()
+    unsafe { ResultProcessorWrapper::into_ptr(rp) }.cast()
 }
