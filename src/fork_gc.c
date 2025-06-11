8125e520e368d64979463f78e9e4fae6b929eb75
BenGoldberger <ben.goldberger@redis.com>
2025-06-11 16:22:03 +0300

    add timeout before read

--- src/fork_gc.c
+++ src/fork_gc.c
@@ -93,7 +93,23 @@ static void FGC_sendTerminator(ForkGC *fgc) {
 }
 
 static int __attribute__((warn_unused_result)) FGC_recvFixed(ForkGC *fgc, void *buf, size_t len) {
+  fd_set set;
+  struct timeval timeout;
+  int rv;
+
+  FD_ZERO(&set);
+  FD_SET(fgc->pipefd[GC_READERFD], &set);
+
+  timeout.tv_sec = 300;
+  timeout.tv_usec = 0;
   while (len) {
+      fd_set set;
+  
+
+    rv = select(fgc->pipefd[GC_READERFD] + 1, &set, NULL, NULL, &timeout);
+    if(rv <= 0)
+      RedisModule_Log(fgc->ctx, "verbose", "ForkGC - got error while reading from pipe (%s)", strerror(errno));
+      return REDISMODULE_ERR;   
     ssize_t nrecvd = read(fgc->pipefd[GC_READERFD], buf, len);
     if (nrecvd > 0) {
       buf += nrecvd;
