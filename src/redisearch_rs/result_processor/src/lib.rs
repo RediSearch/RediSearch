5a6cfb7eecae341bdc4ace410534acd41bd54628
Tim Janus <tim@janus.rs>
2025-06-11 10:19:56 +0200

    docs

--- src/redisearch_rs/result_processor/src/lib.rs
+++ src/redisearch_rs/result_processor/src/lib.rs
@@ -728,15 +728,19 @@ pub type DocId = u64;
 use ffi::RSSortingVector;
 use ffi::RSValue;
 
-/// Row data for a lookup key. This abstracts the question of "where" the
-/// data comes from.
+/// Row data for a lookup key. This abstracts the question of "where" the data comes from.
+///
+/// This stores the values received by an iteration over a RLookup.
+///
 /// cbindgen:field-names=[sv, dyn, ndyn]
 #[repr(C)]
 pub struct RLookupRow {
-    pub sv: *const RSSortingVector,
+    /// contains sortable values for the row, is depending on the filed sorting
+    pub sorting_vector: *const RSSortingVector,
 
-    // todo bindgen rename
-    pub dyn_: *mut *mut RSValue,
+    /// contains the dynamic values of the row
+    pub dyn_values: *mut *mut RSValue,
 
-    ndyn: usize,
+    /// the number of dynamic values in the row
+    pub num_values: usize,
 }
