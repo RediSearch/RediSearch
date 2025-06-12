ccf0f4a5fb27101774046fd8f853fc580f011ecf
chesedo <pieter@chesedo.me>
2025-06-12 12:32:18 +0200

    Also encode delta

--- src/redisearch_rs/inverted_index/src/tests.rs
+++ src/redisearch_rs/inverted_index/src/tests.rs
@@ -4,13 +4,30 @@ use crate::{Encoder, Numeric, RSIndexResult};
 
 #[test]
 fn numeric_tiny_int() {
-    let mut buf = Cursor::new(Vec::new());
-    let delta = crate::Delta(0);
-    let record = RSIndexResult::numeric(2.0);
+    let inputs = [
+        (
+            crate::Delta(0),
+            RSIndexResult::numeric(2.0),
+            1,
+            vec![0b010_00_000],
+        ),
+        (
+            crate::Delta(2),
+            RSIndexResult::numeric(7.0),
+            2,
+            vec![0b111_00_001, 0b0000_0010],
+        ),
+    ];
 
-    let bytes_written =
-        Numeric::encode(&mut buf, delta, &record).expect("to encode numeric record");
+    for input in inputs {
+        let mut buf = Cursor::new(Vec::new());
 
-    assert_eq!(bytes_written, 1, "tiny int should only fill the header");
-    assert_eq!(buf.into_inner(), &[0b010_00_000]);
+        let (delta, record, expected_bytes_written, expected_buffer) = input;
+
+        let bytes_written =
+            Numeric::encode(&mut buf, delta, &record).expect("to encode numeric record");
+
+        assert_eq!(bytes_written, expected_bytes_written);
+        assert_eq!(buf.into_inner(), expected_buffer);
+    }
 }
