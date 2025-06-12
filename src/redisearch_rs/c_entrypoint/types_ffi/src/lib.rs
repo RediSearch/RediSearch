a767d0956ec35eddd1902683a75c4d460292c47e
chesedo <pieter@chesedo.me>
2025-06-12 15:16:33 +0200

    Reduce exports

--- src/redisearch_rs/c_entrypoint/types_ffi/src/lib.rs
+++ src/redisearch_rs/c_entrypoint/types_ffi/src/lib.rs
@@ -9,6 +9,4 @@
 
 //! This module contains pure Rust types that we want to expose to C code.
 
-pub use inverted_index::{
-    RSAggregateResult, RSIndexResult, RSNumericRecord, RSResultType, RSTermRecord,
-};
+pub use inverted_index::RSIndexResult;
