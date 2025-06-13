63659c658b05b70a06ba7f6e7ea89832ec2a66da
chesedo <pieter@chesedo.me>
2025-06-13 11:33:09 +0200

    Implement numeric decoder

--- src/redisearch_rs/inverted_index/src/tests.rs
+++ src/redisearch_rs/inverted_index/src/tests.rs
@@ -1,19 +1,20 @@
 use std::io::Cursor;
 
-use crate::{Encoder, Numeric, RSIndexResult};
+use crate::{Decoder, DecoderResult, Delta, Encoder, Numeric, RSIndexResult};
+use pretty_assertions::assert_eq;
 
 #[test]
 fn numeric_tiny_int() {
     let inputs = [
         (
-            crate::Delta(0),
-            RSIndexResult::numeric(2.0),
+            0,
+            2.0,
             1,
             vec![0b010_00_000], // Header bits 7→0: |010|00|000| = value:2, type:TINY, delta_bytes:0
         ),
         (
-            crate::Delta(2),
-            RSIndexResult::numeric(7.0),
+            2,
+            7.0,
             2,
             vec![
                 0b111_00_001, // Header bits 7→0: |111|00|001| = value:7, type:TINY, delta_bytes:1
@@ -21,8 +22,8 @@ fn numeric_tiny_int() {
             ],
         ),
         (
-            crate::Delta(256),
-            RSIndexResult::numeric(0.0),
+            256,
+            0.0,
             3,
             vec![
                 0b000_00_010, // Header bits 7→0: |000|00|010| = value:0, type:TINY, delta_bytes:2
@@ -33,14 +34,26 @@ fn numeric_tiny_int() {
     ];
 
     for input in inputs {
+        let (delta, value, expected_bytes_written, expected_buffer) = input;
         let mut buf = Cursor::new(Vec::new());
-
-        let (delta, record, expected_bytes_written, expected_buffer) = input;
+        let record = RSIndexResult::numeric(1000, value);
 
         let bytes_written =
-            Numeric::encode(&mut buf, delta, &record).expect("to encode numeric record");
+            Numeric::encode(&mut buf, Delta(delta), &record).expect("to encode numeric record");
 
+        let buf = buf.into_inner();
         assert_eq!(bytes_written, expected_bytes_written);
-        assert_eq!(buf.into_inner(), expected_buffer);
+        assert_eq!(buf, expected_buffer);
+
+        let prev_doc_id = 1000 - (delta as u64);
+        let DecoderResult::Record(record_decoded) = Numeric
+            .decode(buf.as_slice(), prev_doc_id)
+            .expect("to decode numeric record")
+            .expect("to read a record from the buffer")
+        else {
+            panic!("Record was filtered out incorrectly")
+        };
+
+        assert_eq!(record_decoded, record);
     }
 }
