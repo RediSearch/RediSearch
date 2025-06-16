1f0310000fb41b51ff71cb652348f2355d57999b
Raz Monsonego <74051729+raz-mon@users.noreply.github.com>
2025-06-16 16:55:43 +0300

    Change unit-tests placement

--- tests/cpptests/test_cpp_resultprocessor.cpp
+++ tests/cpptests/test_cpp_resultprocessor.cpp
@@ -11,6 +11,8 @@
 #include "result_processor.h"
 #include "query.h"
 #include "gtest/gtest.h"
+#include <thread>
+#include <chrono>
 
 struct processor1Ctx : public ResultProcessor {
   processor1Ctx() {
@@ -87,4 +89,216 @@ TEST_F(ResultProcessorTest, testProcessorChain) {
   QITR_FreeChain(&qitr);
   ASSERT_EQ(2, numFreed);
   RLookup_Cleanup(&lk);
+}
+
+TEST_F(ResultProcessorTest, RPDepleter_Basic) {
+  // Mock upstream processor: yields 3 results, then EOF
+  const size_t n_docs = 3;
+  QueryIterator qitr = {0};
+  struct MockUpstream : public ResultProcessor {
+    int count = 0;
+    static int NextFn(ResultProcessor *rp, SearchResult *res) {
+      MockUpstream *self = (MockUpstream *)rp;
+      if (self->count >= n_docs) return RS_RESULT_EOF;
+      res->docId = ++self->count;
+      return RS_RESULT_OK;
+    }
+    MockUpstream() {
+      memset(this, 0, sizeof(*this));
+      this->Next = NextFn;
+    }
+  } mockUpstream;
+
+  // Create the depleter processor
+  ResultProcessor *depleter = RPDepleter_New();
+  QITR_PushRP(&qitr, &mockUpstream);
+  QITR_PushRP(&qitr, depleter);
+
+  SearchResult res = {0};
+  int rc;
+  int depletingCount = 0;
+  // The first call(s) should return RS_RESULT_DEPLETING until the thread is done
+  while ((rc = depleter->Next(depleter, &res)) == RS_RESULT_DEPLETING) {
+    depletingCount++;
+    // Sleep to let the background thread run
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+  }
+  ASSERT_GT(depletingCount, 0); // Should have at least one depleting state
+
+  // Now, results should be available
+  int resultCount = 0;
+  do {
+    if (rc == RS_RESULT_OK) {
+      ASSERT_EQ(res.docId, ++resultCount);
+      SearchResult_Clear(&res);
+    }
+  } while ((rc = depleter->Next(depleter, &res)) == RS_RESULT_OK);
+
+  // We expect to have received all results from the upstream processor.
+  ASSERT_EQ(resultCount, n_docs);
+  // The last return code should be RS_RESULT_EOF, as the upstream last returned.
+  ASSERT_EQ(rc, RS_RESULT_EOF);
+
+  SearchResult_Destroy(&res);
+  depleter->Free(depleter);
+}
+
+TEST_F(ResultProcessorTest, RPDepleter_Timeout) {
+  // Mock upstream processor: yields 3 results, then timeout.
+  const size_t n_docs = 3;
+  QueryIterator qitr = {0};
+  struct MockUpstream : public ResultProcessor {
+    int count = 0;
+    static int NextFn(ResultProcessor *rp, SearchResult *res) {
+      MockUpstream *self = (MockUpstream *)rp;
+      if (self->count >= n_docs) return RS_RESULT_TIMEDOUT;
+      res->docId = ++self->count;
+      return RS_RESULT_OK;
+    }
+    MockUpstream() {
+      memset(this, 0, sizeof(*this));
+      this->Next = NextFn;
+    }
+  } mockUpstream;
+
+  // Create the depleter processor
+  ResultProcessor *depleter = RPDepleter_New();
+  QITR_PushRP(&qitr, &mockUpstream);
+  QITR_PushRP(&qitr, depleter);
+
+  SearchResult res = {0};
+  int rc;
+  int depletingCount = 0;
+
+  // The first call(s) should return RS_RESULT_DEPLETING until the thread is done
+  while ((rc = depleter->Next(depleter, &res)) == RS_RESULT_DEPLETING) {
+    depletingCount++;
+    // Sleep to let the background thread run
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+  }
+  ASSERT_GT(depletingCount, 0);
+
+  // Now, results should be available
+  int resultCount = 0;
+  do {
+    if (rc == RS_RESULT_OK) {
+      ASSERT_EQ(res.docId, ++resultCount);
+      SearchResult_Clear(&res);
+    }
+  } while ((rc = depleter->Next(depleter, &res)) == RS_RESULT_OK);
+
+  ASSERT_EQ(resultCount, n_docs);
+  // The last return code should be RS_RESULT_TIMEDOUT, as the upstream last returned.
+  ASSERT_EQ(rc, RS_RESULT_TIMEDOUT);
+
+  SearchResult_Destroy(&res);
+  depleter->Free(depleter);
+}
+
+TEST_F(ResultProcessorTest, RPDepleter_LongRun) {
+  // Mock upstream processor mimics a long running operation, such that the
+  // Next function will be called several times before returning the results.
+
+  const size_t n_docs = 3;
+  QueryIterator qitr = {0};
+
+  struct MockUpstream : public ResultProcessor {
+    int count = 0;
+    static int NextFn(ResultProcessor *rp, SearchResult *res) {
+      MockUpstream *self = (MockUpstream *)rp;
+      if (self->count >= n_docs) return RS_RESULT_EOF;
+      // Sleep to simulate a long running operation
+      std::this_thread::sleep_for(std::chrono::milliseconds(10));
+      res->docId = ++self->count;
+      return RS_RESULT_OK;
+    }
+    MockUpstream() {
+      memset(this, 0, sizeof(*this));
+      this->Next = NextFn;
+    }
+  } mockUpstream;
+
+  // Create the depleter processor
+  ResultProcessor *depleter = RPDepleter_New();
+  QITR_PushRP(&qitr, &mockUpstream);
+  QITR_PushRP(&qitr, depleter);
+
+  SearchResult res = {0};
+  int rc;
+  int depletingCount = 0;
+
+  // The first call(s) should return RS_RESULT_DEPLETING until the thread is done
+  while ((rc = depleter->Next(depleter, &res)) == RS_RESULT_DEPLETING) {
+    depletingCount++;
+  }
+  // We now expect to have more than one call to the Next function while the
+  // depleter is running in the background.
+  ASSERT_GT(depletingCount, 1);
+
+  // Now, results should be available
+  int resultCount = 0;
+  do {
+    if (rc == RS_RESULT_OK) {
+      ASSERT_EQ(res.docId, ++resultCount);
+      SearchResult_Clear(&res);
+    }
+  } while ((rc = depleter->Next(depleter, &res)) == RS_RESULT_OK);
+
+  ASSERT_EQ(resultCount, n_docs);
+  // The last return code should be RS_RESULT_EOF, as the upstream last returned.
+  ASSERT_EQ(rc, RS_RESULT_EOF);
+
+  SearchResult_Destroy(&res);
+  depleter->Free(depleter);
+}
+
+TEST_F(ResultProcessorTest, RPDepleter_Error) {
+  // Mock upstream processor sends an error on the first call.
+
+  QueryIterator qitr = {0};
+
+  struct MockUpstream : public ResultProcessor {
+    int count = 0;
+    static int NextFn(ResultProcessor *rp, SearchResult *res) {
+      return RS_RESULT_ERROR;
+    }
+    MockUpstream() {
+      memset(this, 0, sizeof(*this));
+      this->Next = NextFn;
+    }
+  } mockUpstream;
+
+  // Create the depleter processor
+  ResultProcessor *depleter = RPDepleter_New();
+  QITR_PushRP(&qitr, &mockUpstream);
+  QITR_PushRP(&qitr, depleter);
+
+  SearchResult res = {0};
+  int rc;
+  int depletingCount = 0;
+
+  // The first call(s) should return RS_RESULT_DEPLETING until the thread is done
+  while ((rc = depleter->Next(depleter, &res)) == RS_RESULT_DEPLETING) {
+    depletingCount++;
+    // Sleep to let the background thread run
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+  }
+  // We now expect to have more than one call to the Next function while the
+  // depleter is running in the background.
+  ASSERT_GT(depletingCount, 0);
+
+  // Now, results should be available (no results will be reached here)
+  int resultCount = 0;
+  do {
+    if (rc == RS_RESULT_OK) {
+      ASSERT_EQ(res.docId, ++resultCount);
+      SearchResult_Clear(&res);
+    }
+  } while ((rc = depleter->Next(depleter, &res)) == RS_RESULT_OK);
+
+  // The last return code should be RS_RESULT_ERROR, as the upstream last returned.
+  ASSERT_EQ(rc, RS_RESULT_ERROR);
+
+  SearchResult_Destroy(&res);
+  depleter->Free(depleter);
 }
