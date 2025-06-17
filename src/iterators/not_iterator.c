d83e3f5e84b2741e88a059b19dce47fd90c21a60
Joan Fontanals Martinez <jfontanalsmartinez@gmail.com>
2025-06-17 16:27:53 +0200

    update call to Optimized WCIterator

--- src/iterators/not_iterator.c
+++ src/iterators/not_iterator.c
@@ -282,8 +282,7 @@ QueryIterator *IT_V2(NewNotIterator)(QueryIterator *it, t_docId maxDocId, double
   QueryIterator *ret = &ni->base;
   bool optimized = q && q->sctx->spec->rule && q->sctx->spec->rule->index_all;
   if (optimized) {
-    //TODO(Joan): Once all the WildCards are migrated, call the generic one
-    ni->wcii = IT_V2(NewWildcardIterator_NonOptimized)(q->docTable->maxDocId, q->docTable->size);
+    ni->wcii = IT_V2(NewWildcardIterator_Optimized)(q->docTable->maxDocId, q->docTable->size);
   }
   ni->child = it ? it : IT_V2(NewEmptyIterator)();
   ni->maxDocId = maxDocId;          // Valid for the optimized case as well, since this is the maxDocId of the embedded wildcard iterator
