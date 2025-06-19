2fc044060437261a481c888159ef5b5f2adec615
Tim Janus <tim@janus.rs>
2025-06-19 15:29:08 +0200

    exclude redis_mock from coverage, like trie_bencher

--- build.sh
+++ build.sh
@@ -519,6 +519,7 @@ run_rust_tests() {
       --workspace
       --exclude=trie_bencher
       --exclude=varint_bencher
+      --exclude=redis_mock
       --ignore-filename-regex="varint_bencher/*,trie_bencher/*"
       --output-path=$BINROOT/rust_cov.info
     "
