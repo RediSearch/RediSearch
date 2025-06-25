e00f6c4f614f04b398e02e52b44d5887ae4fa6db
chesedo <pieter@chesedo.me>
2025-06-25 16:16:10 +0200

    Remove `Copy` from `Decoder::seek`

--- src/redisearch_rs/inverted_index/src/lib.rs
+++ src/redisearch_rs/inverted_index/src/lib.rs
@@ -292,14 +292,14 @@ pub trait Decoder {
     /// Like `[Decoder::decode]`, but it skips all entries whose document ID is lower than `target`.
     ///
     /// Returns `None` if no record has a document ID greater than or equal to `target`.
-    fn seek<R: Read + Seek + Copy>(
+    fn seek<R: Read + Seek>(
         &self,
-        reader: R,
+        mut reader: R,
         base: t_docId,
         target: t_docId,
     ) -> std::io::Result<Option<RSIndexResult>> {
         loop {
-            match self.decode(reader, base)? {
+            match self.decode(&mut reader, base)? {
                 Some(DecoderResult::Record(record)) if record.doc_id >= target => {
                     return Ok(Some(record));
                 }
