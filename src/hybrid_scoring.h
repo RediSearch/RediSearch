147e78f36b3c0cd32adec6fe5b16fa19c4f7658c
Itzikvaknin <82322982+Itzikvaknin@users.noreply.github.com>
2025-06-29 12:29:46 +0300

    MOD-10031: RPHybridMerger (#6317)

---
+++ src/hybrid_scoring.h
@@ -0,0 +1,51 @@
+#ifndef __HYBRID_SCORING_H__
+#define __HYBRID_SCORING_H__
+
+#include "redisearch.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum {
+  HYBRID_SCORING_LINEAR,
+  HYBRID_SCORING_RRF
+} HybridScoringType;
+
+typedef struct {
+  double *linearWeights;
+  size_t numWeights;
+} HybridLinearContext;
+
+typedef struct {
+  size_t k;
+  size_t window;
+} HybridRRFContext;
+
+typedef struct {
+  HybridScoringType scoringType;
+  union {
+    HybridLinearContext linearCtx;
+    HybridRRFContext rrfCtx;
+  };
+} HybridScoringContext;
+
+/* Generic free function for HybridScoringContext */
+void HybridScoringContext_Free(HybridScoringContext *hybridCtx);
+
+typedef double (*HybridScoringFunction)(HybridScoringContext *scoringCtx, const double *scores, const bool *hasScores, const size_t numUpstreams);
+
+ /* Get scoring function based on scoring type */
+HybridScoringFunction GetScoringFunction(HybridScoringType scoringType);
+
+double HybridRRFScore(HybridScoringContext *scoringCtx, const double *ranks, const bool *has_rank, const size_t num_sources);
+
+double HybridLinearScore(HybridScoringContext *scoringCtx, const double *scores, const bool *has_score, const size_t num_sources);
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __HYBRID_SCORING_H__
