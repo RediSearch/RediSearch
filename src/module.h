f4f39bfbe4934ad0c26b343fdbd89f947bf6c4e7
GuyAv46 <guy.avimor@gmail.com>
2025-06-30 08:31:52 +0300

    continue fixing include mess

--- src/module.h
+++ src/module.h
@@ -19,6 +19,9 @@
 #include <sys/cdefs.h>
 #endif
 
+// Module-level dummy context for certain dummy RM_XXX operations
+extern RedisModuleCtx *RSDummyContext;
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -49,8 +52,6 @@ void RediSearch_CleanupModule(void);
 // Local spellcheck command
 int SpellCheckCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc);
 
-// Module-level dummy context for certain dummy RM_XXX operations
-extern RedisModuleCtx *RSDummyContext;
 // Indicates that RediSearch_Init was called
 extern int RS_Initialized;
 
