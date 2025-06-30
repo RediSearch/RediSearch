5f2fe4879d22a4227026eb301752b5cb9a61d0fa
oshadmi <omer.shadmi@redis.com>
2025-06-30 20:16:05 +0300

    Code review fixes

--- tests/cpptests/test_cpp_command_info.cpp
+++ tests/cpptests/test_cpp_command_info.cpp
@@ -13,42 +13,11 @@ protected:
             test_dir = test_dir.parent_path();
         }
         
-        commands_json_path = test_dir / "commands.json";
-        command_info_h_path = test_dir / "src" / "command_info" / "command_info.h";
         command_info_c_path = test_dir / "src" / "command_info" / "command_info.c";
     }
     
-    std::filesystem::path commands_json_path;
-    std::filesystem::path command_info_h_path;
     std::filesystem::path command_info_c_path;
 };
-
-TEST_F(CommandInfoTest, CommandsJsonExists) {
-    ASSERT_TRUE(std::filesystem::exists(commands_json_path)) 
-        << "commands.json should exist at: " << commands_json_path;
-}
-
-TEST_F(CommandInfoTest, GeneratedFilesExist) {
-    ASSERT_TRUE(std::filesystem::exists(command_info_h_path))
-        << "command_info.h should be generated at: " << command_info_h_path;
-    
-    ASSERT_TRUE(std::filesystem::exists(command_info_c_path))
-        << "command_info.c should be generated at: " << command_info_c_path;
-}
-
-TEST_F(CommandInfoTest, CommandsJsonIsValid) {
-    std::ifstream file(commands_json_path);
-    ASSERT_TRUE(file.is_open()) << "Could not open commands.json";
-
-    // Basic validation - check that it's not empty and contains expected patterns
-    std::string content((std::istreambuf_iterator<char>(file)),
-                        std::istreambuf_iterator<char>());
-
-    ASSERT_GT(content.size(), 100) << "commands.json should not be empty";
-    ASSERT_TRUE(content.find("FT.CREATE") != std::string::npos) << "Should contain FT.CREATE command";
-    ASSERT_TRUE(content.find("FT.SEARCH") != std::string::npos) << "Should contain FT.SEARCH command";
-    ASSERT_TRUE(content.find("FT.AGGREGATE") != std::string::npos) << "Should contain FT.AGGREGATE command";
-}
 
 TEST_F(CommandInfoTest, GeneratedFunctionsExist) {
     // Read the generated C file
