9c747bec8b26b7fa7a9633792af7dd13e0537406
chesedo <pieter@chesedo.me>
2025-07-01 15:48:30 +0200

    Basic `InvertedIndex` with simple writting

---
+++ src/redisearch_rs/inverted_index/src/tests.rs
@@ -0,0 +1,27 @@
+use crate::{Encoder, InvertedIndex, RSIndexResult};
+
+struct Dummy;
+
+impl Encoder for Dummy {
+    fn encode<W: std::io::Write + std::io::Seek>(
+        mut writer: W,
+        delta: crate::Delta,
+        record: &RSIndexResult,
+    ) -> std::io::Result<usize> {
+        let delta: usize = delta.into();
+
+        writer.write_all(&delta.to_be_bytes())?;
+
+        Ok(8)
+    }
+}
+
+#[test]
+fn add_record() {
+    let mut ii = InvertedIndex::new();
+    let record = RSIndexResult::numeric(10, 5.0);
+
+    ii.add_record(Dummy, &record).unwrap();
+
+    assert_eq!(ii.buffer, [0, 0, 0, 0, 0, 0, 0, 10])
+}
