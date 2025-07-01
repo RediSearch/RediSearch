622f4153a730745cdb245d515fec4bf54d3e93e4
jonathan keinan <jonathan.keinan@redis.com>
2025-07-01 15:13:19 +0300

    fix compilation

--- src/aggregate/aggregate_pipeline.h
+++ src/aggregate/aggregate_pipeline.h
@@ -6,7 +6,7 @@ extern "C" {
 #endif
 
 struct QOptimizer;
-int AggregationPipeline_Build(AggregationPipeline *pipeline, struct QOptimizer* optimizer, RSSearchOptions* searchOpts, QueryError *status, RSTimeoutPolicy timeoutPolicy);
+int AggregationPipeline_Build(AggregationPipeline *pipeline, struct QOptimizer* optimizer, RSSearchOptions* searchOpts, QueryError *status, RSTimeoutPolicy timeoutPolicy, size_t maxResultsLimit);
 /**
  * Constructs the pipeline objects needed to actually start processing
  * the requests. This does not yet start iterating over the objects
