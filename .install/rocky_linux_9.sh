61fbb8c088e0669846f17aad0e9315780afeb8af
GuyAv46 <guy.avimor@gmail.com>
2025-07-02 17:35:00 +0300

    another attempt

--- .install/rocky_linux_9.sh
+++ .install/rocky_linux_9.sh
@@ -2,9 +2,9 @@
 MODE=$1 # whether to install using sudo or not
 set -e
 export DEBIAN_FRONTEND=noninteractive
-$MODE dnf update -y
+$MODE dnf update -y --refresh
 
 $MODE dnf install -y gcc-toolset-13-gcc gcc-toolset-13-gcc-c++ make wget git \
-    openssl openssl-devel python3 python3-devel which rsync unzip clang curl --nobest --allowerasing
+    openssl openssl-devel python3 python3-devel which rsync unzip clang curl --allowerasing
 
 cp /opt/rh/gcc-toolset-13/enable /etc/profile.d/gcc-toolset-13.sh
