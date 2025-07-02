c4dc8b8e7353be5c686a9979237365cd7c146676
GuyAv46 <guy.avimor@gmail.com>
2025-07-02 16:20:44 +0300

    try upgrading gcc to 14

--- .install/rocky_linux_9.sh
+++ .install/rocky_linux_9.sh
@@ -4,7 +4,7 @@ set -e
 export DEBIAN_FRONTEND=noninteractive
 $MODE dnf update -y
 
-$MODE dnf install -y gcc-toolset-13-gcc gcc-toolset-13-gcc-c++ make wget git \
-    openssl openssl-devel python3 python3-devel which rsync unzip curl --nobest --skip-broken --allowerasing
+$MODE dnf install -y gcc-toolset-14-gcc gcc-toolset-14-gcc-c++ make wget git \
+    openssl openssl-devel python3 python3-devel which rsync unzip clang curl --nobest --skip-broken --allowerasing
 
-cp /opt/rh/gcc-toolset-13/enable /etc/profile.d/gcc-toolset-13.sh
+cp /opt/rh/gcc-toolset-14/enable /etc/profile.d/gcc-toolset-14.sh
