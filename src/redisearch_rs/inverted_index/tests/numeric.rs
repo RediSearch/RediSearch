5a03a6c334c6250cad2fe6bce2f8316bbd735ec2
chesedo <pieter@chesedo.me>
2025-07-02 14:48:47 +0200

    More tests

--- src/redisearch_rs/inverted_index/tests/numeric.rs
+++ src/redisearch_rs/inverted_index/tests/numeric.rs
@@ -415,6 +415,103 @@ fn encoding_non_numeric_record() {
     let _result = Numeric::encode(&mut buffer, Delta::new(0), &record);
 }
 
+#[test]
+fn encoding_to_fixed_buffer() {
+    let mut buffer = Cursor::new([0; 2]);
+    let record = RSIndexResult::numeric(1, 100.0);
+
+    let result = Numeric::encode(&mut buffer, Delta::new(1), &record);
+
+    assert!(result.is_err());
+    assert_eq!(
+        result.unwrap_err().kind(),
+        std::io::ErrorKind::UnexpectedEof
+    );
+}
+
+#[test]
+fn encoding_to_slow_writter() {
+    use std::io::{Seek, Write};
+
+    struct SlowWritter<W> {
+        inner: W,
+        call_count: usize,
+    }
+
+    impl<W> SlowWritter<W> {
+        const BYTES_PER_WRITE: usize = 2;
+
+        fn new(inner: W) -> Self {
+            Self {
+                inner,
+                call_count: 0,
+            }
+        }
+    }
+
+    impl<W: Write> Write for SlowWritter<W> {
+        fn write_vectored(&mut self, bufs: &[std::io::IoSlice<'_>]) -> std::io::Result<usize> {
+            self.call_count += 1;
+            let mut written = 0;
+
+            for buf in bufs {
+                if written >= Self::BYTES_PER_WRITE {
+                    break;
+                }
+
+                let remaining = Self::BYTES_PER_WRITE - written;
+                let to_write = buf.len().min(remaining);
+
+                self.inner.write_all(&buf[..to_write])?;
+                written += to_write;
+            }
+
+            Ok(written)
+        }
+
+        fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+            let to_write = buf.len().min(Self::BYTES_PER_WRITE);
+            self.inner.write(&buf[..to_write])
+        }
+
+        fn flush(&mut self) -> std::io::Result<()> {
+            self.inner.flush()
+        }
+    }
+
+    impl<W> Seek for SlowWritter<W> {
+        fn seek(&mut self, pos: std::io::SeekFrom) -> std::io::Result<u64> {
+            unimplemented!("nothing in this test should call this")
+        }
+    }
+
+    let mut buffer = SlowWritter::new(Vec::new());
+    let record = RSIndexResult::numeric(10, 3.124);
+
+    let result = Numeric::encode(&mut buffer, Delta::new(0), &record)
+        .expect("to encode the complete record");
+
+    assert_eq!(result, 9);
+    assert_eq!(
+        buffer.inner,
+        [
+            0b100_01_000, // FLOAT type, f64, !negative, !infinite, delta_bytes: 0
+            203,          // Value: 3.124 in IEEE 754 format
+            161,
+            69,
+            182,
+            243,
+            253,
+            8,
+            64,
+        ]
+    );
+    assert_eq!(
+        buffer.call_count, 5,
+        "9 bytes needed to be written in chunks of 2"
+    );
+}
+
 proptest! {
     #[test]
     fn numeric_encode_decode_integers(
