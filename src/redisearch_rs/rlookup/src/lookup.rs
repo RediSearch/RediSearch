e8dc6515b51c89f4cd9b4d5f7cebbff24aadbce1
Jonas Kruckenberg <iterpre@protonmail.com>
2025-07-03 15:57:59 +0200

    MOD-10391 KeyList linked-list implementation

--- src/redisearch_rs/rlookup/src/lookup.rs
+++ src/redisearch_rs/rlookup/src/lookup.rs
@@ -157,6 +157,26 @@ pub struct RLookupKey<'a> {
     _path: Option<Cow<'a, CStr>>,
 }
 
+#[derive(Debug)]
+#[repr(C)]
+struct KeyList<'a> {
+    head: Option<NonNull<RLookupKey<'a>>>,
+    tail: Option<NonNull<RLookupKey<'a>>>,
+    // Length of the data row. This is not necessarily the number
+    // of lookup keys
+    rowlen: u32,
+}
+
+pub struct Iter<'list, 'a> {
+    _rlookup: &'list KeyList<'a>,
+    curr: Option<NonNull<RLookupKey<'a>>>,
+}
+
+pub struct IterMut<'list, 'a> {
+    _rlookup: &'list mut KeyList<'a>,
+    curr: Option<NonNull<RLookupKey<'a>>>,
+}
+
 // ===== impl RLookupKey =====
 
 // SAFETY NOTICE
@@ -237,6 +257,137 @@ impl<'a> RLookupKey<'a> {
         let b = unsafe { Box::from_raw(ptr.as_ptr()) };
         // Safety: 3 -> Caller has to uphold the pin contract
         unsafe { Pin::new_unchecked(b) }
+    }
+}
+
+// ===== impl KeyList =====
+
+#[expect(unused, reason = "used by later stacked PRs")]
+impl<'a> KeyList<'a> {
+    pub const fn new() -> Self {
+        Self {
+            head: None,
+            tail: None,
+            rowlen: 0,
+        }
+    }
+
+    fn push(&mut self, mut key: RLookupKey<'a>) -> Pin<&mut RLookupKey<'a>> {
+        key.dstidx = u16::try_from(self.rowlen).unwrap();
+
+        // Safety: RLookup never hands out mutable references to the key (except `Pin<&mut T>` which is fine)
+        // and never copies, or memmoves the memory internally.
+        let mut ptr = unsafe { RLookupKey::into_ptr(Box::pin(key)) };
+
+        if let Some(mut tail) = self.tail.take() {
+            // if we have a tail we also must have a head
+            debug_assert!(self.head.is_some());
+
+            // Safety: We know we can borrow tail here, since we mutably borrow the RLookup
+            // which owns all keys allocated within it. This ensures the RLookup and all keys outlive
+            // this method call AND that we have exclusive access to mutate the key.
+            let tail = unsafe { tail.as_mut() };
+
+            tail.next = Some(ptr);
+            self.tail = Some(ptr);
+        } else {
+            // if we have no tail we also must have no head
+            debug_assert!(self.head.is_none());
+            self.head = Some(ptr);
+            self.tail = Some(ptr);
+        }
+
+        // Increase the RLookup table row length. (all rows have the same length).
+        self.rowlen += 1;
+
+        // Safety: we have allocated the memory above, this pointer is safe to dereference.
+        let key = unsafe { ptr.as_mut() };
+        // Safety: We treat the pointer as pinned internally and never hand out references that could be moved out of (in safe Rust)
+        // publicly.
+        unsafe { Pin::new_unchecked(key) }
+    }
+
+    pub fn iter(&self) -> Iter<'_, 'a> {
+        Iter {
+            _rlookup: self,
+            curr: self.head,
+        }
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<'_, 'a> {
+        IterMut {
+            curr: self.head,
+            _rlookup: self,
+        }
+    }
+
+    fn find(&self, name: &'a CStr) -> Option<&RLookupKey<'a>> {
+        // FIXME [MOD-10315] replace with more efficient search
+        self.iter().find(|key| key._name.as_ref() == name)
+    }
+
+    fn find_mut(&mut self, name: &'a CStr) -> Option<Pin<&mut RLookupKey<'a>>> {
+        // FIXME [MOD-10315] replace with more efficient search
+        self.iter_mut().find(|key| key._name.as_ref() == name)
+    }
+}
+
+impl Drop for KeyList<'_> {
+    fn drop(&mut self) {
+        let mut curr = self.head.take();
+
+        while let Some(curr_) = curr {
+            // Safety:
+            // 1 -> all keys here are created through `push_key`, which correctly calls into_ptr.
+            // 2 -> after this destructor runs, this RLookup is inaccessible making double frees impossible.
+            // 3 -> RLookupKey is about to be freed, we don't need to worry about pinning anymore.
+            let mut curr_ = unsafe { RLookupKey::from_ptr(curr_) };
+            curr = curr_.next.take();
+        }
+    }
+}
+
+// ===== impl Iter =====
+
+impl<'list, 'a> Iterator for Iter<'list, 'a> {
+    type Item = &'list RLookupKey<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let curr = self.curr.take()?;
+
+        // Safety: It is safe for us to borrow `curr`, because the iteraror mutably borrows the `KeyList`,
+        // ensuring it will not be dropped while the iterator exists AND we have exclusive access
+        // to the keys it owns (and can therefore hand out mutable references).
+        // The returned item will not outlive the iterator.
+        self.curr = unsafe { curr.as_ref().next };
+
+        // Safety: See above.
+        let curr = unsafe { curr.as_ref() };
+
+        Some(curr)
+    }
+}
+
+// ===== impl IterMut =====
+
+impl<'list, 'a> Iterator for IterMut<'list, 'a> {
+    type Item = Pin<&'list mut RLookupKey<'a>>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut curr = self.curr.take()?;
+
+        // Safety: It is safe for us to borrow `curr`, because the iteraror mutably borrows the `KeyList`,
+        // ensuring it will not be dropped while the iterator exists AND we have exclusive access
+        // to the keys it owns (and can therefore hand out mutable references).
+        // The returned item will not outlive the iterator.
+        self.curr = unsafe { curr.as_ref().next };
+
+        // Safety: See above.
+        let curr = unsafe { curr.as_mut() };
+
+        // Safety: RLookup treats the keys are pinned always, we just need consumers of this
+        // iterator to uphold the pinning invariant too
+        Some(unsafe { Pin::new_unchecked(curr) })
     }
 }
 
