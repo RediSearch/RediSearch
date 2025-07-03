55ee5cd4024f326bf6c995f825fb9750f11c8456
Jonas Kruckenberg <iterpre@protonmail.com>
2025-07-03 16:33:10 +0200

    MOD-10396 add RLookup::get_key_write

--- src/redisearch_rs/rlookup/src/lookup.rs
+++ src/redisearch_rs/rlookup/src/lookup.rs
@@ -77,7 +77,6 @@ pub enum RLookupKeyFlag {
 pub type RLookupKeyFlags = BitFlags<RLookupKeyFlag>;
 
 // Flags that are allowed to be passed to [`RLookup::get_key_read`], [`RLookup::get_key_write`], or [`RLookup::get_key_load`].
-#[expect(unused, reason = "used by later stacked PRs")]
 const GET_KEY_FLAGS: RLookupKeyFlags =
     make_bitflags!(RLookupKeyFlag::{Override | Hidden | ExplicitReturn | ForceLoad});
 
@@ -425,6 +424,47 @@ impl<'a> RLookup<'a> {
     pub fn init(&mut self, spcache: IndexSpecCache) {
         debug_assert!(self.spcache.is_none());
         self.spcache = Some(spcache);
+    }
+
+    // =====  Get key for writing =====
+
+    pub fn get_key_write(
+        &mut self,
+        name: &'a CStr,
+        mut flags: RLookupKeyFlags,
+    ) -> Option<&RLookupKey<'a>> {
+        // remove all flags that are not relevant to getting a key
+        flags &= GET_KEY_FLAGS;
+
+        // A. we found the key at the lookup table:
+        //    1. if we are in exclusive mode, return NULL
+        //    2. if we are in create mode, overwrite the key (remove schema related data, mark with new flags)
+        // B. we didn't find the key at the lookup table:
+        //    create a new key with the name and flags
+
+        // Safety: The non-lexical lifetime analysis of current Rust, incorrectly handles borrows in early
+        // return statements, expanding the borrow of `self` to the scope of the entire method. This is
+        // obviously not correct as `key` is either found and borrowed in which case we never reach the code below
+        // this early return OR it is *not* found and *not* borrowed which means the code below is fine to
+        // borrow self again. The current compiler is not smart enough to get this though, so we create a disjoint
+        // borrow below.
+        // TODO remove once <https://github.com/rust-lang/rust/issues/54663> is fixed.
+        let me = unsafe { &mut *(self as *mut Self) };
+
+        let key = if let Some(c) = me.keys.find_by_name_mut(name) {
+            if flags.contains(RLookupKeyFlag::Override) {
+                c.override_current(flags | RLookupKeyFlag::QuerySrc)
+                    .unwrap()
+            } else {
+                return None;
+            }
+        } else {
+            self.keys
+                .push(RLookupKey::new(name, flags | RLookupKeyFlag::QuerySrc))
+        };
+
+        // Safety: We treat the pointer as pinned internally and safe Rust cannot move out of the returned immutable reference.
+        Some(unsafe { Pin::into_inner_unchecked(key.into_ref()) })
     }
 }
 
@@ -776,7 +816,6 @@ impl<'list, 'a> CursorMut<'list, 'a> {
     ///
     /// The new key will inherit the `name`, `path`, and `dstidx` of the current key but receive a
     /// **new pointer identity**. The new key is returned.
-    #[cfg_attr(not(test), expect(unused, reason = "used by later stacked PRs"))]
     pub fn override_current(
         mut self,
         flags: RLookupKeyFlags,
