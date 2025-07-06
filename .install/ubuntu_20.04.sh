a007ea6575c550d9a3d9efdc3529ce7b1eea718b
GuyAv46 <guy.avimor@gmail.com>
2025-07-06 17:50:41 +0300

    fix

--- .install/ubuntu_20.04.sh
+++ .install/ubuntu_20.04.sh
@@ -10,9 +10,10 @@ $MODE apt install -yqq software-properties-common
 $MODE add-apt-repository ppa:deadsnakes/ppa -y
 $MODE apt update -qq
 
-$MODE apt install -yqq wget make clang-format gcc python3.10 python3.10-venv python3.10-pip lcov git openssl libssl-dev \
+$MODE apt install -yqq wget make clang-format gcc python3.10 python3.10-venv python3.10-dev lcov git openssl libssl-dev \
     unzip rsync build-essential gcc-10 g++-10 curl libclang-dev
 
 $MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 60 --slave /usr/bin/g++ g++ /usr/bin/g++-10
 
+curl -sS https://bootstrap.pypa.io/get-pip.py | python3.10
 python3 --version
