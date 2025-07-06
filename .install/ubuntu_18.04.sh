b4d3b3a3485f43fdbf28b4158589b65eb413b65a
GuyAv46 <guy.avimor@gmail.com>
2025-07-06 18:32:17 +0300

    revert bionic changes

--- .install/ubuntu_18.04.sh
+++ .install/ubuntu_18.04.sh
@@ -11,13 +11,12 @@ $MODE apt install -yqq software-properties-common unzip rsync
 # ppa for modern python and gcc10
 $MODE add-apt-repository ppa:ubuntu-toolchain-r/test -y
 $MODE add-apt-repository ppa:git-core/ppa -y
-$MODE add-apt-repository ppa:deadsnakes/ppa -y
-$MODE apt update -yqq
+$MODE apt update
 $MODE apt install -yqq build-essential git wget make gcc-10 g++-10 openssl libssl-dev curl libclang-dev clang
 $MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 60 --slave /usr/bin/g++ g++ /usr/bin/g++-10
 
-# Install Python 3.12
-$MODE apt -y install python3.12 python3.12-venv python3.12-dev python3-venv python3-dev
+# Install Python 3.8
+$MODE apt -y install python3.8 python3.8-venv python3.8-dev python3-venv python3-dev
 
-# Set python3 to point to python3.12
-$MODE update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.12 2
+# Set python3 to point to python3.8
+$MODE update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2
