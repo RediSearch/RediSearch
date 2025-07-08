18eb3a4642e073b9081cc773d551d84aaacf8764
BenGoldberger <ben.goldberger@redis.com>
2025-07-08 11:32:56 +0300

    remove set error

--- src/document_basic.c
+++ src/document_basic.c
@@ -177,7 +177,6 @@ int Document_LoadSchemaFieldJson(Document *doc, RedisSearchCtx *sctx, QueryError
 
   RedisJSON jsonRoot = japi->openKey(ctx, doc->docKey);
   if (!jsonRoot) {
-    QueryError_SetWithUserDataFmt(status, QUERY_EINVAL, "Key does not exist or is not a json", ": %s", RedisModule_StringPtrLen(doc->docKey, NULL));
     goto done;
   }
   Document_MakeStringsOwner(doc); // TODO: necessary??
