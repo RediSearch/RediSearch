d805c43b4c01638dd60cd1164b4fa5300b37f4e7
Tim Janus <tim@janus.rs>
2025-07-09 17:43:31 +0200

    RLookup Rust implementations and unit tests

--- src/redisearch_rs/rlookup/src/lib.rs
+++ src/redisearch_rs/rlookup/src/lib.rs
@@ -6,6 +6,9 @@
  * (RSALv2); or (b) the Server Side Public License v1 (SSPLv1); or (c) the
  * GNU Affero General Public License v3 (AGPLv3).
 */
+
 mod lookup;
+mod row;
 
 pub use lookup::{RLookupKey, RLookupKeyFlag, RLookupKeyFlags};
+pub use row::RLookupRow;
