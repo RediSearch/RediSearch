8ea18fcc5485910edc3390f381fa093e886e6d6b
Tim Janus <tim@janus.rs>
2025-07-10 09:50:29 +0200

    RLookup Rust implementations for cleanup/wipe

--- src/redisearch_rs/rlookup/src/row.rs
+++ src/redisearch_rs/rlookup/src/row.rs
@@ -12,9 +12,11 @@ use value::RSValueTrait;
 
 /// Row data for a lookup key. This abstracts the question of if the data comes from a SortinVector
 /// or from a dynamic value.
-#[expect(unused, reason = "used by later stacked PRs")]
 pub struct RLookupRow<T: RSValueTrait> {
+    ///
+    /// The cleanup of the RLookupRow can be triggered by Drop.
     /// Sorting vector attached to document
+    #[expect(unused, reason = "used by later stacked PRs")]
     sorting_vector: RSSortingVector<T>,
 
     /// Dynamic values obtained from prior processing
@@ -24,4 +26,24 @@ pub struct RLookupRow<T: RSValueTrait> {
     num: u32,
 }
 
-impl<T: RSValueTrait> RLookupRow<T> {}
+impl<T: RSValueTrait> Drop for RLookupRow<T> {
+    fn drop(&mut self) {
+        // Wipe the row, decrementing any RSValues
+        self.wipe();
+    }
+}
+
+impl<T: RSValueTrait> RLookupRow<T> {
+    /// Wipes the row, retaining its memory but decrementing any included values.
+    /// This does not free all the memory consumed by the row, but simply resets
+    /// the row data (preserving any caches) so that it may be refilled.    
+    pub fn wipe(&mut self) {
+        for value in &mut self.values.iter_mut().filter(|v| v.is_some()) {
+            if let Some(v) = value {
+                v.decrement();
+                *value = None;
+                self.num -= 1;
+            }
+        }
+    }
+}
