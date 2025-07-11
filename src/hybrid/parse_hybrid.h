34a6f5855cf4a03518792aa955726ec2b1e78bc5
nafraf <nafraf@gmail.com>
2025-07-11 16:48:51 -0500

    Update copyright notice and licensing information in parse_hybrid files

--- src/hybrid/parse_hybrid.h
+++ src/hybrid/parse_hybrid.h
@@ -1,6 +1,11 @@
-//
-// Created by Ofir Yanai on 03/07/2025.
-//
+/*
+ * Copyright (c) 2006-Present, Redis Ltd.
+ * All rights reserved.
+ *
+ * Licensed under your choice of the Redis Source Available License 2.0
+ * (RSALv2); or (b) the Server Side Public License v1 (SSPLv1); or (c) the
+ * GNU Affero General Public License v3 (AGPLv3).
+*/
 
 #ifndef PARSE_HYBRID_H
 #define PARSE_HYBRID_H
