012dcd523da1262494a5ec12c47c9bcbb0c45582
Guillaume Desmottes <guillaume@desmottes.be>
2025-07-14 15:54:40 +0200

    bench offsets only encoder/decoder

--- src/redisearch_rs/inverted_index_bencher/benches/encoding_decoding.rs
+++ src/redisearch_rs/inverted_index_bencher/benches/encoding_decoding.rs
@@ -66,6 +66,12 @@ fn benchmark_fields_offsets(c: &mut Criterion) {
     bencher.decoding(c);
 }
 
+fn benchmark_offsets_only(c: &mut Criterion) {
+    let bencher = benchers::offsets_only::Bencher::default();
+    bencher.encoding(c);
+    bencher.decoding(c);
+}
+
 criterion_group!(
     benches,
     benchmark_numeric,
@@ -74,6 +80,7 @@ criterion_group!(
     benchmark_fields_only,
     benchmark_full,
     benchmark_fields_offsets,
+    benchmark_offsets_only,
 );
 
 criterion_main!(benches);
