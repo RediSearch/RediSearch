acb4b7af1b46975066e721f9ec2037a9b5672ec9
chesedo <pieter@chesedo.me>
2025-07-16 17:31:32 +0200

    Have Rust drop a `RSIndexResult` correctly

--- src/redisearch_rs/headers/types_rs.h
+++ src/redisearch_rs/headers/types_rs.h
@@ -208,9 +208,6 @@ typedef struct RSNumericRecord {
   double value;
 } RSNumericRecord;
 
-/**
- * Represents a virtual result in an index record.
- */
 typedef struct RSVirtualResult {
 
 } RSVirtualResult;
