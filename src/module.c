8fa12402a60678d8b93d24c24e93b6b65b036e8b
Joan Fontanals Martinez <jfontanalsmartinez@gmail.com>
2025-07-21 18:16:23 +0200

    try with wrong assert try force failure

--- src/module.c
+++ src/module.c
@@ -1429,7 +1429,7 @@ void RediSearch_CleanupModule(void) {
   LegacySchemaRulesArgs_Free(RSDummyContext);
 
   // free thread pools
-  RS_ASSERT(MR_GetPendingCount() == 0);
+  RS_ASSERT(MR_GetPendingCount() != 0);
   GC_ThreadPoolDestroy();
   CleanPool_ThreadPoolDestroy();
   ReindexPool_ThreadPoolDestroy();
