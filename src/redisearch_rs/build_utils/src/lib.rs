dfce839e064a330b16d60db155999ab54ba008f2
Guillaume Desmottes <guillaume@desmottes.be>
2025-07-21 16:51:10 +0200

    c_entrypoint: regen if the binded Rust files changed

--- src/redisearch_rs/build_utils/src/lib.rs
+++ src/redisearch_rs/build_utils/src/lib.rs
@@ -23,21 +23,52 @@ pub fn git_root() -> Result<std::path::PathBuf, Box<dyn std::error::Error>> {
     Ok(path)
 }
 
-/// Walk the specified directory and emit granular `rerun-if-changed` statements,
-/// scoped to `*.c` and `*.h` files.
-/// It'd be nice if `cargo` supported globbing syntax natively, but that's not the
-/// case today.
-pub fn rerun_if_c_changes(dir: &Path) -> std::io::Result<()> {
+fn rerun_if_changes(dir: &Path, extensions: &[&str]) -> std::io::Result<()> {
     for entry in read_dir(dir)? {
         let Ok(entry) = entry else {
             continue;
         };
         let path = entry.path();
         if path.is_dir() {
-            return rerun_if_c_changes(&path);
-        } else if let Some(extension) = path.extension() {
-            if extension == "c" || extension == "h" {
-                println!("cargo:rerun-if-changed={}", path.display());
+            return rerun_if_changes(&path, extensions);
+        } else if let Some(extension) = path.extension().and_then(|ext| ext.to_str())
+            && extensions.contains(&extension)
+        {
+            println!("cargo:rerun-if-changed={}", path.display());
+        }
+    }
+    Ok(())
+}
+
+/// Walk the specified directory and emit granular `rerun-if-changed` statements,
+/// scoped to `*.c` and `*.h` files.
+/// It'd be nice if `cargo` supported globbing syntax natively, but that's not the
+/// case today.
+pub fn rerun_if_c_changes(dir: &Path) -> std::io::Result<()> {
+    rerun_if_changes(dir, &["c", "h"])
+}
+
+/// Walk the specified directory and emit granular `rerun-if-changed` statements,
+/// scoped to `*.rs` files.
+/// It'd be nice if `cargo` supported globbing syntax natively, but that's not the
+/// case today.
+fn rerun_if_rust_changes(dir: &Path) -> std::io::Result<()> {
+    rerun_if_changes(dir, &["rs"])
+}
+
+/// Emit granular `rerun-if-changed` statements for each files which are
+/// part of the crates listed in `config.parse.include`.
+/// This ensures cbindgen output is regenerated when the underlying Rust code
+/// is updated.
+pub fn rerun_cbinden(config: &cbindgen::Config) -> Result<(), Box<dyn std::error::Error>> {
+    if let Some(include) = &config.parse.include {
+        for included_crate in include.iter() {
+            let path = git_root()?
+                .join("src")
+                .join("redisearch_rs")
+                .join(included_crate);
+            if path.exists() {
+                let _ = rerun_if_rust_changes(&path);
             }
         }
     }
