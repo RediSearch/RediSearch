9af0085631b4b777885d5fb82f84d7119016a621
chesedo <pieter@chesedo.me>
2025-07-22 10:41:30 +0200

    License header and file comment

--- src/redisearch_rs/inverted_index/tests/c_mocks/mod.rs
+++ src/redisearch_rs/inverted_index/tests/c_mocks/mod.rs
@@ -1,3 +1,15 @@
+/*
+ * Copyright (c) 2006-Present, Redis Ltd.
+ * All rights reserved.
+ *
+ * Licensed under your choice of the Redis Source Available License 2.0
+ * (RSALv2); or (b) the Server Side Public License v1 (SSPLv1); or (c) the
+ * GNU Affero General Public License v3 (AGPLv3).
+*/
+
+//! This module contains mock implementations of C functions that are used in tests. Linking to a
+//! C static file with these implementations would have been a overkill.
+
 use ffi::RSQueryTerm;
 use inverted_index::{RSIndexResult, RSTermRecord};
 
