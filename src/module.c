de7bf49d4093310364db81f3435e9cae86c40c0b
Joan Fontanals Martinez <jfontanalsmartinez@gmail.com>
2025-07-22 09:46:28 +0200

    test dummy

--- src/module.c
+++ src/module.c
@@ -1429,6 +1429,7 @@ void RediSearch_CleanupModule(void) {
   LegacySchemaRulesArgs_Free(RSDummyContext);
 
   // free thread pools
+  RS_ASSERT(false);
   RS_ASSERT(MR_GetPendingCount() != 0);
   GC_ThreadPoolDestroy();
   CleanPool_ThreadPoolDestroy();
