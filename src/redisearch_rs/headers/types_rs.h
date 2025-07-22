9078b771293c6d76f9cb2aff306bac2cf6fde72a
chesedo <pieter@chesedo.me>
2025-07-22 10:43:24 +0200

    Restore comment
    Not sure how this got messed up.

--- src/redisearch_rs/headers/types_rs.h
+++ src/redisearch_rs/headers/types_rs.h
@@ -224,6 +224,9 @@ typedef struct RSNumericRecord {
   double value;
 } RSNumericRecord;
 
+/**
+ * Represents a virtual result in an index record.
+ */
 typedef struct RSVirtualResult {
 
 } RSVirtualResult;
--- src/redisearch_rs/inverted_index/src/lib.rs
+++ src/redisearch_rs/inverted_index/src/lib.rs
@@ -357,7 +357,8 @@ impl IntoIterator for RSAggregateResult {
         }
     }
 }
-// Represents a virtual result in an index record.
+
+/// Represents a virtual result in an index record.
 #[repr(C)]
 #[derive(Debug, PartialEq)]
 pub struct RSVirtualResult;
