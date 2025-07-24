9bc0ca4b564983f8b507f445d1d4b5ca2995aa13
Tim Janus <tim@janus.rs>
2025-07-24 10:47:05 +0200

    address review

--- src/redisearch_rs/rlookup/src/lookup.rs
+++ src/redisearch_rs/rlookup/src/lookup.rs
@@ -12,8 +12,6 @@ use crate::bindings::{
 };
 use enumflags2::{BitFlags, bitflags, make_bitflags};
 use pin_project::pin_project;
-#[cfg(any(debug_assertions, test))]
-use std::ptr;
 use std::{
     borrow::Cow,
     cell::UnsafeCell,
@@ -21,7 +19,7 @@ use std::{
     mem,
     ops::DerefMut,
     pin::Pin,
-    ptr::NonNull,
+    ptr::{self, NonNull},
     slice,
 };
 
@@ -886,6 +884,10 @@ impl<'a> RLookup<'a> {
 
     // ===== Get key for reading (create only if in schema and sortable) =====
 
+    /// Gets a key by its name from the lookup table, if not found it uses the schema as a fallback to search the key.
+    ///
+    /// If the flag `RLookupKeyFlag::AllowUnresolved` is set, it will create a new key if it does not exist in the lookup table
+    /// nor in the schema.
     pub fn get_key_read(
         &mut self,
         name: &'a CStr,
@@ -895,9 +897,10 @@ impl<'a> RLookup<'a> {
 
         // Safety: The non-lexical lifetime analysis of current Rust, incorrectly handles borrows in early
         // return statements, expanding the borrow of `self` to the scope of the entire method. This is
-        // obviously not correct as `key` is either found and borrowed in which case we never reach the code below
-        // this early return OR it is *not* found and *not* borrowed which means the code below is fine to
-        // borrow self again. The current compiler is not smart enough to get this though, so we create a disjoint
+        // obviously not correct as `key` is either found and borrowed in which case we never reach the code after
+        // the if let branch, which it's early return.
+        // If the key is *not* found then key is *not* borrowed which means the code below is fine to
+        // borrow it again. The current compiler is not smart enough to get this though, so we create a disjoint
         // borrow below.
         // TODO remove once <https://github.com/rust-lang/rust/issues/54663> is fixed.
         let me = unsafe { &*(self as *const Self) };
@@ -955,12 +958,8 @@ impl<'a> RLookup<'a> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::{
-        cmp,
-        ffi::c_int,
-        mem::{MaybeUninit, offset_of},
-        sync::atomic::{AtomicUsize, Ordering},
-    };
+
+    use std::mem::MaybeUninit;
 
     #[cfg(not(miri))]
     use {proptest::prelude::*, std::ffi::CString};
@@ -1785,107 +1784,6 @@ mod tests {
             prop_assert_eq!(key._name.as_ref(), wrong_name.as_c_str());
             prop_assert_eq!(key.path, wrong_name.as_ptr());
             prop_assert!(key._path.is_none());
-        }
-    }
-
-    // ===== mock implementations for testing purposes =====
-
-    #[derive(Default, Copy, Clone)]
-    #[repr(C)]
-    struct UserString {
-        user: *const c_char,
-        length: usize,
-    }
-
-    /// Mock implementation of `HiddenString_GetUnsafe` from obfuscation/hidden.h for testing purposes
-    #[unsafe(no_mangle)]
-    extern "C" fn HiddenString_GetUnsafe(
-        value: *const ffi::HiddenString,
-        length: *mut usize,
-    ) -> *const c_char {
-        let text = unsafe { value.cast::<UserString>().as_ref().unwrap() };
-        if text.length != 0 {
-            unsafe {
-                *length = text.length;
-            }
-        }
-
-        text.user
-    }
-
-    /// Mock implementation of `NewHiddenString` from obfuscation/hidden.h for testing purposes
-    #[unsafe(no_mangle)]
-    extern "C" fn NewHiddenString(
-        user: *const c_char,
-        length: usize,
-        take_ownership: bool,
-    ) -> *mut ffi::HiddenString {
-        assert!(
-            !take_ownership,
-            "tests are not allowed to move ownership to C"
-        );
-        let value = Box::new(UserString { user, length });
-        Box::into_raw(value).cast()
-    }
-
-    /// Mock implementation of `HiddenString_Free` from obfuscation/hidden.h for testing purposes
-    #[unsafe(no_mangle)]
-    extern "C" fn HiddenString_Free(value: *const ffi::HiddenString, took_ownership: bool) {
-        assert!(
-            !took_ownership,
-            "tests are not allowed to move ownership to C"
-        );
-
-        drop(unsafe { Box::from_raw(value.cast_mut().cast::<UserString>()) });
-    }
-
-    /// Returns the following:
-    /// - 0, if the strings are the same
-    /// - a negative value if left is less than right
-    /// - a positive value if left is greater than right
-    #[unsafe(no_mangle)]
-    extern "C" fn HiddenString_CompareC(
-        left: Option<NonNull<ffi::HiddenString>>,
-        right: *const c_char,
-        right_length: usize,
-    ) -> c_int {
-        let Some(left) = left else {
-            return -1 * c_int::try_from(right_length).unwrap();
-        };
-
-        let left = unsafe { left.cast::<UserString>().as_ref() };
-
-        unsafe {
-            let left = (!left.user.is_null()).then(|| CStr::from_ptr(left.user));
-            let right = (!right.is_null()).then(|| CStr::from_ptr(right));
-            println!("HiddenString_CompareC = left {left:?} right {right:?}",);
-        }
-
-        let result =
-            unsafe { libc::strncmp(left.user, right, cmp::min(left.length, right_length)) };
-
-        println!("HiddenString_CompareC = result = {result}");
-
-        if result != 0 || left.length == right_length {
-            result
-        } else {
-            c_int::try_from(left.length).unwrap() - c_int::try_from(right_length).unwrap()
-        }
-    }
-
-    /// Mock implementation of `IndexSpecCache_Decref` from spec.h for testing purposes
-    #[unsafe(no_mangle)]
-    extern "C" fn IndexSpecCache_Decref(s: Option<NonNull<ffi::IndexSpecCache>>) {
-        let s = s.unwrap();
-        let refcount = unsafe {
-            s.byte_add(offset_of!(ffi::IndexSpecCache, refcount))
-                .cast::<usize>()
-        };
-
-        let refcount = unsafe { AtomicUsize::from_ptr(refcount.as_ptr()) };
-
-        if refcount.fetch_sub(1, Ordering::Relaxed) == 1 {
-            drop(unsafe { Box::from_raw(s.as_ptr()) });
         }
     }
 }
