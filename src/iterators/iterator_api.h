dad5c7bc784eed10a0dba7fbe733e819654fd84d
GuyAv46 <guy.avimor@gmail.com>
2025-07-25 14:43:48 +0300

    refine ValidateStatus definition

--- src/iterators/iterator_api.h
+++ src/iterators/iterator_api.h
@@ -24,8 +24,10 @@ typedef enum IteratorStatus {
 } IteratorStatus;
 
 typedef enum VALIDATE_OK {
-  VALIDATE_OK,      // The iterator is still valid and lastDocID did not change (may be EOF)
-  VALIDATE_MOVED,   // The iterator is still valid but lastDocID changed, and `current` is a new valid result
+  VALIDATE_OK,      // The iterator is still valid and at the same position - if wasn't at EOF,
+                    // the `current` result is still valid
+  VALIDATE_MOVED,   // The iterator is still valid but lastDocID changed, and `current` is a new valid result or
+                    // at EOF. If not at EOF, the `current` result should be used before the next read, or it will be overwritten.
   VALIDATE_ABORTED, // The iterator is no longer valid, and should not be used or rewound. Should be freed.
 } ValidateStatus;
 
