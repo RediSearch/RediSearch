8062413076482645f9ba05dcf9c2e6a63aa8ee64
ofiryanai <ofiryanai1@gmail.com>
2025-07-27 14:26:24 +0300

    MOD-10041 Parse FT.HYBRID command (#6428)
    * Initial FT.HYBRID parsing
    
    * Fix parseSearchSubquery compilation error
    
    * Fix search parsing and endless loop in error handling
    
    * Improve cpp tests argv creation and fix
    
    * Parse tail AGGPlan & rebase
    
    * All tests pass
    
    * Add database selection functions to redismock
    
    * Fix parseCombine() memory leak
    
    * Add HybridPipelineParams* to HybridRequest  struct
    
    * Fix AggPlan handling in parser
    
    * Update copyright notice and licensing information in parse_hybrid files
    
    * Change AREQ pipeline to pointer
    
    * Apply context to vector requests and refactoring assertions in tests
    
    * Copy reqConfig to searchRequest and vectorRequest.
    
    * Cleanup
    
    * MOD-10303: Hybrid Request (#6404)
    
    * wrap common areq members usage with functions
    
    * use AREQ_RequestFlags in a few other places
    
    * common initialization
    
    * use functions to access and manipulate common areq members
    
    * use local variables to reduce duplicate function calls
    
    * add AggregationPipeline_Free
    
    * move more code to AggregationPipeline_Free
    
    * fix compilation + max search result
    
    * address code review comments, better pipeline and params separation for pipeline construction
    
    * add cmake changes
    
    * bring back old clean logic
    
    * move AREQ_BuildPipeline to original position in header
    
    * fix compilation post merge
    
    * fix some tests seg faults
    
    * remove hybrid request, not supposed to be in this branch
    
    * add missing newlines
    
    * bring back missing freeing of areq members
    
    * minor api changes
    
    * copy the context before releasing the loader
    
    * add comments
    
    * add hybrid request
    
    * add loader to areq + tests
    
    * add cmake changes
    
    * align hybrid request api
    
    * fix tests - use the AGGPLN inside the hybrid request for consistent linke list pointer comparison
    
    * remove RLookup_CloneKey API
    
    * remove pln member from params
    
    * fix memory leak
    
    * Moved loadDtor from aggregate_request to aggregate_plan and remove duplicate implementation of it
    
    * Remove redundant VERBATIM flag setting in CreateTestAREQ function
    
    * Add TODO comments for sorter
    
    ---------
    
    Co-authored-by: itzik.vaknin@redis.com <itzikvaknin6@gmail.com>
    
    * CR: Add docs for a few functions
    
    * CR: extract HYBRID_REQUEST_NUM_SUBQUERIES constant
    
    * Fix pipeline reference in parseQueryArgs and AREQ_BuildPipeline functions
    
    * Refactor to don't use AREQ to parse tail
    
    * Refactor pipeline handling to use direct struct access instead of pointers
    
    * Remove unused test util
    
    * Revert "Remove unused test util"
    
    This reverts commit 7130515e5e2500bc26216ff3cc5af2d88745c725.
    
    * Revert "Revert "Remove unused test util""
    
    This reverts commit d7e54d2cffdd5038c49c4b694867aa5ea0d2d8b1.
    
    * Remove multiline request test
    
    This test only tested ArgvList initialization with multiline command
    
    * Revert AREQ_AddRequestFlags() and AREQ_RemoveRequestFlags() changes
    
    * Fix maxResultsLimit assignment logic
    
    * Update HybridRequest_Free and tests to reflect scoring context is freed by hybrid merger
    
    * Add test for valid input with parameters
    
    * Implement context cleanup scheduling for background thread processing
    
    ---------
    
    Co-authored-by: nafraf <nafraf@gmail.com>
    Co-authored-by: kei-nan <jonathan.keinan@redis.com>
    Co-authored-by: itzik.vaknin@redis.com <itzikvaknin6@gmail.com>

---
+++ src/hybrid/parse_hybrid.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2006-Present, Redis Ltd.
+ * All rights reserved.
+ *
+ * Licensed under your choice of the Redis Source Available License 2.0
+ * (RSALv2); or (b) the Server Side Public License v1 (SSPLv1); or (c) the
+ * GNU Affero General Public License v3 (AGPLv3).
+*/
+
+#ifndef PARSE_HYBRID_H
+#define PARSE_HYBRID_H
+
+#include <stdint.h>
+
+#include "redismodule.h"
+
+#include "aggregate/aggregate.h"
+#include "pipeline/pipeline.h"
+#include "search_ctx.h"
+#include "hybrid_request.h"
+#include "hybrid_scoring.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+int hybridCommandHandler(RedisModuleCtx *ctx, RedisModuleString **argv, int argc);
+
+// Function for parsing hybrid command arguments - exposed for testing
+HybridRequest* parseHybridCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc,
+                                  RedisSearchCtx *sctx, const char* indexname,
+                                  QueryError *status);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //PARSE_HYBRID_H
