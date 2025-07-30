46160e93f3d5199517b7a7382e10c94e58c3295d
BenGoldberger <ben.goldberger@redis.com>
2025-07-30 10:54:06 +0300

    different folder

--- .install/test_deps/install_rust.sh
+++ .install/test_deps/install_rust.sh
@@ -2,16 +2,15 @@
 processor=$(uname -m)
 OS_TYPE=$(uname -s)
 
-export RUSTUP_HOME=/usr/local/rust
-export CARGO_HOME=/usr/local/rust
-apt install sudo -y
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sudo sh -s -- -y --no-modify-path
-export PATH=$PATH:/usr/local/rust/bin
+export RUSTUP_HOME=/usr/
+export CARGO_HOME=/usr/
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path
+# export PATH=$PATH:/usr/local/rust/bin
 
 # Verify Cargo is in path
 echo $PATH
 
-sudo rustup update
-sudo rustup update nightly
+rustup update
+rustup update nightly
 # for RedisJSON build with address sanitizer
-sudo rustup component add rust-src --toolchain nightly
+rustup component add rust-src --toolchain nightly
