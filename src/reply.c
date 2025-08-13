56bd4be58addadaaa08bb94197463e271fba5a99
GuyAv46 <guy.avimor@gmail.com>
2025-08-13 09:52:39 +0300

    make inline
    (cherry picked from commit e3082eb6537e8de1fb53151f81103596f1e70695)

--- src/reply.c
+++ src/reply.c
@@ -18,7 +18,7 @@ typedef struct RedisModule_Reply_StackEntry StackEntry;
 
 //---------------------------------------------------------------------------------------------
 
-bool RedisModule_HasMap(RedisModule_Reply *reply) {
+inline bool RedisModule_HasMap(RedisModule_Reply *reply) {
   return reply->resp3;
 }
 
