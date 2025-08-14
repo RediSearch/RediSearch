45a9337c95ae77a458f1ef085bfe94617af88c93
Raz Monsonego <74051729+raz-mon@users.noreply.github.com>
2025-08-14 10:28:39 +0300

    Fix hash

--- src/util/hash/hash.cpp
+++ src/util/hash/hash.cpp
@@ -13,14 +13,23 @@
 void Sha1_Compute(const char *value, size_t len, Sha1* output) {
   boost::uuids::detail::sha1 sha1;
   sha1.process_bytes(value, len);
-  sha1.get_digest(output->hash);
+  boost::uuids::detail::sha1::digest_type digest;
+  sha1.get_digest(digest);
+
+  // Convert from digest_type (unsigned int[5]) to unsigned char[20]
+  for (int i = 0; i < 5; i++) {
+    output->hash[i*4] = (digest[i] >> 24) & 0xFF;
+    output->hash[i*4+1] = (digest[i] >> 16) & 0xFF;
+    output->hash[i*4+2] = (digest[i] >> 8) & 0xFF;
+    output->hash[i*4+3] = digest[i] & 0xFF;
+  }
 }
 
 void Sha1_FormatIntoBuffer(const Sha1 *sha1, char *buffer) {
   for (int i = 0; i < 5; i++) {
     uint32_t word = (sha1->hash[i*4] << 24) | (sha1->hash[i*4+1] << 16) |
                     (sha1->hash[i*4+2] << 8) | sha1->hash[i*4+3];
-    sprintf(buffer + i * 8, "%08x", word);
+    snprintf(buffer + i * 8, 9, "%08x", word);
   }
   buffer[40] = '\0';
 }
