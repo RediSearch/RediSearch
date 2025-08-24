2c8dfa9dd2aa42d8ffa95eba9a1f25c9f16914d6
alon <alonreshef24@gmail.com>
2025-08-24 14:37:47 +0300

    fix setup_rejson.sh to fetch the nightly version from the right place

--- .install/test_deps/common_installations.sh
+++ .install/test_deps/common_installations.sh
@@ -4,7 +4,7 @@ OS_TYPE=$(uname -s)
 MODE=$1 # whether to install using sudo or not
 
 activate_venv() {
-	echo "copy ativation script to shell config"
+	echo "copy activation script to shell config"
 	if [[ $OS_TYPE == Darwin ]]; then
 		echo "source venv/bin/activate" >> ~/.bashrc
 		echo "source venv/bin/activate" >> ~/.zshrc
@@ -13,20 +13,6 @@ activate_venv() {
 		echo "source $PWD/venv/bin/activate" >> ~/.bashrc
 	fi
 }
-
-# Pin a specific working version of nightly to prevent breaking the CI because
-# regressions in a nightly build.
-# Make sure to synchronize updates across all modules: Redis and RedisJSON.
-NIGHTLY_VERSION="nightly-2025-07-30"
-
-# --allow-downgrade:
-#   Allow `rustup` to install an older `nightly` if the latest one
-#   is missing one of the components we need.
-# rust-src:
-#   Required to build RedisJSON with address sanitizer
-rustup toolchain install $NIGHTLY_VERSION \
-    --allow-downgrade \
-    --component rust-src
 
 python3 -m venv venv
 activate_venv
