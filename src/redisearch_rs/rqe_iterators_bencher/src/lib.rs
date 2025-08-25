69d06d206ccf99957a694d3d1b975c4f91e826cc
GuyAv46 <guy.avimor@gmail.com>
2025-08-25 17:04:49 +0300

    build fixes

--- src/redisearch_rs/rqe_iterators_bencher/src/lib.rs
+++ src/redisearch_rs/rqe_iterators_bencher/src/lib.rs
@@ -13,25 +13,3 @@ pub mod benchers;
 pub mod ffi;
 
 redis_mock::bind_redis_alloc_symbols_to_mock_impl!();
-
-#[unsafe(no_mangle)]
-pub extern "C" fn ResultMetrics_Free(result: *mut inverted_index::RSIndexResult) {
-    if result.is_null() {
-        panic!("did not expect `RSIndexResult` to be null");
-    }
-
-    let metrics = unsafe { (*result).metrics };
-    if metrics.is_null() {
-        return;
-    }
-
-    panic!(
-        "did not expect any benchmark to set metrics, but got: {:?}",
-        unsafe { *metrics }
-    );
-}
-
-#[unsafe(no_mangle)]
-pub extern "C" fn Term_Free(_t: *mut ::ffi::RSQueryTerm) {
-    // RSQueryTerm used by the benchers are created on the stack so we don't need to free them.
-}
