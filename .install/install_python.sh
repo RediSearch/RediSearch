6ecc790b077a8d8219631def4dc30fadd34aec24
LukeMathWalker <20745048+LukeMathWalker@users.noreply.github.com>
2025-08-26 14:40:33 +0200

    Add uv to PATH

--- .install/install_python.sh
+++ .install/install_python.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
+set -ex
+
 processor=$(uname -m)
 OS_TYPE=$(uname -s)
 
 curl --proto '=https' --tlsv1.2 -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
+# Add the newly installed `uv` to the PATH
+export PATH="$HOME/.local/bin:$PATH"
 
 # Verify uv is in path
 uv -vV
