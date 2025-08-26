27cc522ad64dd7bb5cafd1b95b66deec4c3de197
LukeMathWalker <20745048+LukeMathWalker@users.noreply.github.com>
2025-08-26 15:49:36 +0200

    Avoid updating since we just installed

--- .install/install_python.sh
+++ .install/install_python.sh
@@ -6,12 +6,9 @@ OS_TYPE=$(uname -s)
 
 curl --proto '=https' --tlsv1.2 -LsSf https://astral.sh/uv/install.sh | sh
 # Add the newly installed `uv` to the PATH
-source $HOME/.local/bin/env
+export PATH="$HOME/.local/bin:$PATH"
 
 # Verify uv is in path
 uv -vV
 # Print where `cargo` is located for debugging purposes
 echo "uv binary location: $(which uv)"
-
-# Update to the latest version of `uv`, in case it was already installed
-uv self update
