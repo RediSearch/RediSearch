e9a08fe189b19862e9eaccb7e09b7a669c6e9308
chesedo <pieter@chesedo.me>
2025-08-26 14:47:00 +0200

    Switch to Rust inverted index

--- src/debug_commands.c
+++ src/debug_commands.c
@@ -182,7 +182,7 @@ DEBUG_COMMAND(InvertedIndexSummary) {
     RedisModule_ReplySetArrayLength(ctx, blockBulkLen);
   }
 
-  InvertedIndex_BlocksSummaryFree(blocksSummary);
+  InvertedIndex_BlocksSummaryFree(blocksSummary, blockCount);
 
   invIdxBulkLen += 2;
 
