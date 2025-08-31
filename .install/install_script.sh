d0b50e35f476c2b7a0240b5eaf64a082284c9331
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2025-08-31 16:39:27 +0200

    backport using uv in CI to 8.0

--- .install/install_script.sh
+++ .install/install_script.sh
@@ -31,4 +31,9 @@ if [[ $BOOST_EXIT_CODE -ne 0 ]]; then
     exit $BOOST_EXIT_CODE
 fi
 
+# Install Rust and Python here since they're needed on all platforms and
+# the installer doesn't rely on any platform-specific tools (e.g. the package manager)
+source install_rust.sh
+source install_python.sh
+
 git config --global --add safe.directory '*'
--- .install/test_deps/common_installations.sh
+++ .install/test_deps/common_installations.sh
@@ -4,26 +4,55 @@ OS_TYPE=$(uname -s)
 MODE=$1 # whether to install using sudo or not
 
 activate_venv() {
-	echo "copy ativation script to shell config"
-	if [[ $OS_TYPE == Darwin ]]; then
-		echo "source venv/bin/activate" >> ~/.bashrc
-		echo "source venv/bin/activate" >> ~/.zshrc
-	else
-		echo "source $PWD/venv/bin/activate" >> ~/.bash_profile
-		echo "source $PWD/venv/bin/activate" >> ~/.bashrc
-	fi
+  echo "copy ativation script to shell config"
+  if [[ $OS_TYPE == Darwin ]]; then
+    echo "source .venv/bin/activate" >> ~/.bashrc
+    echo "source .venv/bin/activate" >> ~/.zshrc
+  else
+    echo "source $PWD/.venv/bin/activate" >> ~/.bash_profile
+    echo "source $PWD/.venv/bin/activate" >> ~/.bashrc
+    # Adding the virtual environment activation script to the shell profile
+    # causes $PATH issues on platforms like Debian and Alpine,
+    # shadowing the pre-existing source command to make some of our tools available.
+    # We work around it by appending the required lines to the shell profile
+    # _after_ the venv activation script
+
+    # cargo
+    echo '. "$HOME/.cargo/env"' >> ~/.bash_profile
+    # uv
+    echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bash_profile
+  fi
 }
 
-python3 -m venv venv
+# retrieve nightly version from build.sh
+NIGHTLY_VERSION=$(grep "NIGHTLY_VERSION=" build.sh | cut -d'=' -f2 | tr -d '"')
+# --allow-downgrade:
+#   Allow `rustup` to install an older `nightly` if the latest one
+#   is missing one of the components we need.
+# llvm-tools-preview:
+#   Required by `cargo-llvm-cov` for test coverage
+# miri:
+#   Required to run `cargo miri test` for UB detection
+# rust-src:
+#   Required to build RedisJSON with address sanitizer
+rustup toolchain install $NIGHTLY_VERSION \
+    --allow-downgrade \
+    --component llvm-tools-preview \
+    --component miri \
+    --component rust-src
+
+# Tool required to compute test coverage for Rust code
+cargo install cargo-llvm-cov --locked
+# Make sure `miri` is fully operational before running tests with it.
+# See https://github.com/rust-lang/miri/blob/master/README.md#running-miri-on-ci
+# for more details.
+cargo +$NIGHTLY_VERSION miri setup
+
+# Create a virtual environment for Python tests, with `pip` pre-installed (--seed)
+uv venv --seed
 activate_venv
-source venv/bin/activate
-
-pip install --upgrade pip
-pip install -q --upgrade setuptools
-echo "pip version: $(pip --version)"
-echo "pip path: $(which pip)"
-
-pip install -q -r tests/pytests/requirements.txt
+source .venv/bin/activate
+uv sync --locked --all-packages
 
 # List installed packages
-pip list
+uv run pip list
