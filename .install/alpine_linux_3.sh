59e8ffb5512bd05561fb254d84403f8d2f0240bc
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2025-08-31 16:39:27 +0200

    Use uv to manage the Python environment for e2e tests. (#6715)
    * Use uv to manage the Python environment for e2e tests.
    
    * Allow forcing the testing workflow
    
    * Use canonical location for venv
    
    * Don't install Python using the system package manager
    
    * Add uv to PATH
    
    * Use the same PATH workaround for uv as we used for cargo
    
    * Improve comment
    
    * Ensure that the lockfile is up-to-date
    
    * Avoid updating since we just installed
    
    * Fix path issues and Alpine issue
    
    * Downgrade to 3.12
    
    * Update lockfile
    
    * Handle psutil issue
    
    * Update .install/install_python.sh
    
    Co-authored-by: Omer Shadmi <76992134+oshadmi@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Omer Shadmi <76992134+oshadmi@users.noreply.github.com>
    (cherry picked from commit 797659b77210d67e71f6f09c2ea9af9a50f2fbda)

--- .install/alpine_linux_3.sh
+++ .install/alpine_linux_3.sh
@@ -5,6 +5,18 @@ set -e
 $MODE apk update
 
 $MODE apk add --no-cache build-base gcc g++ make linux-headers openblas-dev \
-    xsimd curl wget git python3 python3-dev py3-pip openssl openssl-dev \
+    xsimd curl wget git openssl openssl-dev \
     tar xz which rsync bsd-compat-headers clang clang17-libclang curl \
     clang-static ncurses-dev llvm-dev
+
+# We must install Python via the package manager until
+# `uv` starts providing aarch64-musl builds.
+# See https://github.com/astral-sh/python-build-standalone/pull/569
+if [ "$(uname -m)" = "aarch64" ]; then
+    $MODE apk add --no-cache python3 python3-dev py3-pip
+else
+    # On x86_64, we need Python headers to build psutil@5.x.y from
+    # source, since it only started providing wheels for musl
+    # in version 6.w.z.
+    $MODE apk add --no-cache python3-dev
+fi
