25af28ed7cc683872f96d87dc09bdba75d28d850
Raz Monsonego <74051729+raz-mon@users.noreply.github.com>
2025-09-02 16:47:58 +0300

    Fix rpcountFree casting

--- src/result_processor.c
+++ src/result_processor.c
@@ -1164,7 +1164,7 @@ static int rpcountNext(ResultProcessor *base, SearchResult *res) {
 
 /* Free impl. for scorer - frees up the scorer privdata if needed */
 static void rpcountFree(ResultProcessor *rp) {
-  RPScorer *self = (RPScorer *)rp;
+  RPCounter *self = (RPCounter *)rp;
   rm_free(self);
 }
 
