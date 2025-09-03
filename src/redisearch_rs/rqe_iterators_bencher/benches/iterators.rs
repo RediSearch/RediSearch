d781c72cca35570ad96e8d48b350c86542850b52
Guillaume Desmottes <guillaume@desmottes.be>
2025-09-03 09:32:02 +0200

    bench II numeric full iterator

--- src/redisearch_rs/rqe_iterators_bencher/benches/iterators.rs
+++ src/redisearch_rs/rqe_iterators_bencher/benches/iterators.rs
@@ -27,11 +27,17 @@ fn benchmark_wildcard(c: &mut Criterion) {
     bencher.bench(c);
 }
 
+fn benchmark_inverted_index_numeric_full(c: &mut Criterion) {
+    let bencher = benchers::inverted_index::NumericFullBencher::default();
+    bencher.bench(c);
+}
+
 criterion_group!(
     benches,
     benchmark_empty,
     benchmark_id_list,
     benchmark_wildcard,
+    benchmark_inverted_index_numeric_full,
 );
 
 criterion_main!(benches);
