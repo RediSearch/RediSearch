1df550e132b8ada01591e6e9e8f22d3d20eb9e0c
itzik.vaknin@redis.com <itzikvaknin6@gmail.com>
2025-09-14 13:42:29 +0300

    Add flow test for FT.HYBRID with explicit scorer

--- tests/pytests/test_rrf.py
+++ tests/pytests/test_rrf.py
@@ -31,8 +31,6 @@ def run_test_scenario(env, no_tag_search_query, with_tag_search_query):
     hybrid_res_results_index = recursive_index(hybrid_res_no_tag, 'results')
     hybrid_res_results_index[-1] += 1
 
-
-
     results_no_tag = get_results_from_hybrid_response(hybrid_res_no_tag)
     results_with_tag = get_results_from_hybrid_response(hybrid_res_with_tag)
     shared_keys = results_no_tag.keys() & results_with_tag.keys()
