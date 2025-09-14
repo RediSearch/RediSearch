7785cf5d3973c7b4489a59cf1cbbcccf4cec52c5
GuyAv46 <guy.avimor@gmail.com>
2025-09-14 15:56:25 +0300

    attempt to fix build (install libc++-dev)

--- .install/debian_gnu_linux_12.sh
+++ .install/debian_gnu_linux_12.sh
@@ -6,4 +6,4 @@ MODE=$1 # whether to install using sudo or not
 
 $MODE apt update -qq
 $MODE apt install -yqq git wget build-essential lcov openssl libssl-dev \
-        rsync unzip curl libclang-dev
+        rsync unzip curl libclang-dev libc++-dev
