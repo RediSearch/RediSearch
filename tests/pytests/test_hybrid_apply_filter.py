66be35fc4a58b37a3cb0dad2c8c516e1ae90bfa3
itzik.vaknin@redis.com <itzikvaknin6@gmail.com>
2025-09-16 09:37:05 +0300

    Minor fix

--- tests/pytests/test_hybrid_apply_filter.py
+++ tests/pytests/test_hybrid_apply_filter.py
@@ -47,7 +47,7 @@ def setup_basic_index(env):
     """Setup basic index with test data"""
     dim = 2
     conn = env.getClusterConnectionIfNeeded()
-    env.expect('FT.CREATE idx SCHEMA description TEXT embedding VECTOR FLAT 6 TYPE FLOAT32 DIM 2 DISTANCE_METRIC L2').ok
+    env.expect('FT.CREATE idx SCHEMA description TEXT embedding VECTOR FLAT 6 TYPE FLOAT32 DIM 2 DISTANCE_METRIC L2').ok()
 
     # Load test data
     for doc_id, doc_data in test_data.items():
