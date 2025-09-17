fccf39c860d40cfae2757e5aaf0db73ec33a8c5a
itzik.vaknin@redis.com <itzikvaknin6@gmail.com>
2025-09-17 11:16:09 +0300

    Remove unused param in test_hybrid_search

--- tests/pytests/test_hybrid_search.py
+++ tests/pytests/test_hybrid_search.py
@@ -52,8 +52,8 @@ def test_hybrid_search_explicit_scorer():
     setup_basic_index(env)
     for scorer in ['TFIDF', 'TFIDF.DOCNORM', 'BM25', 'BM25STD', 'BM25STD.NORM', 'DISMAX', 'DOCSCORE', 'HAMMING']:
         env.assertEqual(b"\x9a\x99\x99\x3f\xcd\xcc\x4c\x3e" ,np.array([1.2, 0.2]).astype(np.float32).tobytes())
-        hybrid_response = env.cmd('FT.HYBRID', 'idx', 'SEARCH', 'shoes', 'SCORER', scorer, 'VSIM' ,'@embedding', b"\x9a\x99\x99\x3f\xcd\xcc\x4c\x3e",\
-                'COMBINE', 'LINEAR', '4', 'ALPHA', '1.0', 'BETA', '0.0', 'PARAMS', "2", "BLOB", b"\x9a\x99\x99\x3f\xcd\xcc\x4c\x3e", )
+        hybrid_response = env.cmd('FT.HYBRID', 'idx', 'SEARCH', 'shoes', 'SCORER', scorer, 'VSIM' ,'@embedding', \
+            b"\x9a\x99\x99\x3f\xcd\xcc\x4c\x3e",'COMBINE', 'LINEAR', '4', 'ALPHA', '1.0', 'BETA', '0.0')
         results = get_results_from_hybrid_response(hybrid_response)
         results = {a: float(results[a][SCORE_FIELD]) for a in results}
         agg_response = env.cmd('FT.AGGREGATE', 'idx', 'shoes', 'ADDSCORES', 'SCORER', scorer, 'LOAD', 2, '__key', '__score')
