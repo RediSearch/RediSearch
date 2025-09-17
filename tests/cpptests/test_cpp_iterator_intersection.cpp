e6e41901fd5f7407c2dc4d0f48c38733d95f3143
chesedo <pieter@chesedo.me>
2025-09-17 08:29:16 +0200

    Update include path

--- tests/cpptests/test_cpp_iterator_intersection.cpp
+++ tests/cpptests/test_cpp_iterator_intersection.cpp
@@ -12,7 +12,7 @@
 
 #include "src/iterators/intersection_iterator.h"
 #include "src/iterators/inverted_index_iterator.h"
-#include "src/inverted_index/inverted_index.h"
+#include "inverted_index.h"
 #include "src/iterators/empty_iterator.h"
 #include "src/iterators/wildcard_iterator.h"
 #include "src/forward_index.h"
