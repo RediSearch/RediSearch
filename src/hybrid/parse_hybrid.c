799fddb2b12ab30e245bac7c6789a4f31249357c
jonathan keinan <jonathan.keinan@redis.com>
2025-09-17 13:27:37 +0300

    add withscores test

--- src/hybrid/parse_hybrid.c
+++ src/hybrid/parse_hybrid.c
@@ -546,6 +546,7 @@ int parseHybridCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc,
   RS_ASSERT(*mergeReqflags == 0);
   *parsedCmdCtx->reqConfig = RSGlobalConfig.requestConfigParams;
   RSSearchOptions mergeSearchopts = {0};
+  RSSearchOptions_Init(&mergeSearchopts);
   size_t maxHybridResults = RSGlobalConfig.maxSearchResults;
 
   AREQ *vectorRequest = parsedCmdCtx->vector;
@@ -612,7 +613,6 @@ int parseHybridCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc,
   const bool hadArgumentBesidesCombine = (hybridParseCtx.specifiedArgs & ~SPECIFIED_ARG_COMBINE) != 0;
   if (hadArgumentBesidesCombine) {
     *mergeReqflags |= QEXEC_F_IS_HYBRID_TAIL;
-    RSSearchOptions_Init(&mergeSearchopts);
 
     if (mergeSearchopts.params) {
       searchRequest->searchopts.params = Param_DictClone(mergeSearchopts.params);
@@ -627,6 +627,10 @@ int parseHybridCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc,
       // Copy cursor configuration using the helper function
       copyCursorConfig(&searchRequest->cursorConfig, parsedCmdCtx->cursorConfig);
       copyCursorConfig(&vectorRequest->cursorConfig, parsedCmdCtx->cursorConfig);
+    }
+    if (*mergeReqflags & QEXEC_F_SEND_SCORES) {
+      searchRequest->reqflags |= QEXEC_F_SEND_SCORES;
+      vectorRequest->reqflags |= QEXEC_F_SEND_SCORES;
     }
 
     // Copy request configuration using the helper function
