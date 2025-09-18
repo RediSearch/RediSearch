2d22a4c8d2a531126719f768d7b073e514a67b2c
ofiryanai <ofir.yanai@redis.com>
2025-09-18 13:04:26 +0300

    PR: include inverted_index only for iterators bencher

--- src/redisearch_rs/rqe_iterators_bencher/build.rs
+++ src/redisearch_rs/rqe_iterators_bencher/build.rs
@@ -34,7 +34,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     .iter()
     .map(|h| root.join("src").join("iterators").join(h))
     .collect::<Vec<_>>();
-    generate_c_bindings(headers, ".*/iterators/.*.h")?;
+    generate_c_bindings(headers, ".*/iterators/.*.h", true)?;
 
     Ok(())
 }
