9cf466f178f94639ae8dec4382ac1b6df2bb9fa8
Joan Fontanals Martinez <jfontanalsmartinez@gmail.com>
2025-09-25 15:38:36 +0200

    test: change test to reflect desired behavior

--- tests/pytests/test_hybrid_dialect.py
+++ tests/pytests/test_hybrid_dialect.py
@@ -31,6 +31,7 @@ def exec_and_validate_query(env, hybrid_cmd):
     """Execute query and validate results"""
     response = env.cmd(*hybrid_cmd)
     results = get_results_from_hybrid_response(response)
+    print(f"Results: {results}")
     env.assertTrue(set(results.keys()) == {"doc:1", "doc:2"})
 
 
@@ -59,10 +60,7 @@ def test_hybrid_dialects():
             'VSIM', '@vector', query_vector,
             'PARAMS', '2', 'WORD', 'apples'
         ]
-        if dialect == 1:
-            env.expect(*hybrid_cmd).error().contains('Syntax error')
-        else:
-            exec_and_validate_query(env, hybrid_cmd)
+        exec_and_validate_query(env, hybrid_cmd)
 
         # Simple query with parameters in VSIM
         hybrid_cmd = [
@@ -73,39 +71,30 @@ def test_hybrid_dialects():
         ]
         exec_and_validate_query(env, hybrid_cmd)
 
-        # Tag autoescaping in SEARCH - invalid syntax in DIALECT 1
+        # Tag autoescaping in SEARCH - invalid syntax in DIALECT 1, but default overrides to DIALECT 2
         hybrid_cmd = [
             'FT.HYBRID', 'idx',
             'SEARCH', '@tag:{"57-300"}',
             'VSIM', '@vector', query_vector,
         ]
-        if dialect == 1:
-            env.expect(*hybrid_cmd).error().contains('Syntax error')
-        else:
-            exec_and_validate_query(env, hybrid_cmd)
+        exec_and_validate_query(env, hybrid_cmd)
 
-        # Tag autoescaping in VSIM FILTER - invalid syntax in DIALECT 1
+        # Tag autoescaping in VSIM FILTER - invalid syntax in DIALECT 1, but default overrides to DIALECT 2
         hybrid_cmd = [
             'FT.HYBRID', 'idx',
             'SEARCH', '@text:(apples)',
             'VSIM', '@vector', query_vector, 'FILTER', '@tag:{"57-300"}',
         ]
-        if dialect == 1:
-            env.expect(*hybrid_cmd).error().contains('Syntax error')
-        else:
-            exec_and_validate_query(env, hybrid_cmd)
+        exec_and_validate_query(env, hybrid_cmd)
 
-        # Parameters in VSIM FILTER - invalid syntax in DIALECT 1
+        # Parameters in VSIM FILTER - invalid syntax in DIALECT 1, but default overrides to DIALECT 2
         hybrid_cmd = [
             'FT.HYBRID', 'idx',
             'SEARCH', '@text:(apples)',
             'VSIM', '@vector', query_vector, 'FILTER', '@text:($WORD)',
             'PARAMS', '2', 'WORD', 'apples',
         ]
-        if dialect == 1:
-            env.expect(*hybrid_cmd).error().contains('Syntax error')
-        else:
-            exec_and_validate_query(env, hybrid_cmd)
+        exec_and_validate_query(env, hybrid_cmd)
 
         # Post filter
         hybrid_cmd = [
@@ -116,4 +105,3 @@ def test_hybrid_dialects():
             'FILTER', '@__key == "doc:1" || @__key == "doc:2"',
         ]
         exec_and_validate_query(env, hybrid_cmd)
-
