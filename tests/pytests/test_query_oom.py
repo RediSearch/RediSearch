da5b637483a8894782a3194ad871234079d00bb0
lerman25 <lerman25@gmail.com>
2025-09-28 15:07:38 +0300

    Prepare testing
    (cherry picked from commit 4f8cd0baf96bb47b2641870e73ec4ab7ed2208ea)

--- tests/pytests/test_query_oom.py
+++ tests/pytests/test_query_oom.py
@@ -1,4 +1,5 @@
 from common import *
+import threading
 
 OOM_QUERY_ERROR = "Not enough memory available to execute the query"
 
@@ -18,6 +19,9 @@ def allShards_change_maxmemory_low(env):
         res = env.getConnection(shardId).execute_command('config', 'set', 'maxmemory', 1)
         env.assertEqual(res, 'OK')
 
+# Helper to call a function and push its return value into a list
+def _call_and_store(fn, args, out_list):
+    out_list.append(fn(*args))
 
 def _common_test_scenario(env):
     # Create an index
@@ -30,7 +34,7 @@ def _common_test_scenario(env):
 def _common_cluster_test_scenario(env):
     conn = getConnectionByEnv(env)
     # Create an index
-    env.expect('FT.CREATE', 'idx', 'SCHEMA', 'name', 'TEXT').ok()
+    env.expect('FT.CREATE', 'idx', 'SCHEMA', 'name', 'TEXT', 'SORTABLE').ok()
 
     n_docs_per_shard = 100
     n_docs = n_docs_per_shard * env.shardsCount
@@ -41,6 +45,17 @@ def _common_cluster_test_scenario(env):
     env.expect('CONFIG', 'SET', 'maxmemory', '1').ok()
 
     return n_docs
+
+def runDebugQueryCommand_expect_oom(env, query_cmd, debug_params):
+    # Use the helper function to build the argument list
+    args = parseDebugQueryCommandArgs(query_cmd, debug_params)
+    return env.expect(debug_cmd(), *args).error().contains(OOM_QUERY_ERROR)
+
+def runDebugQueryCommandPauseBeforeRPAfterN_expect_oom(env, query_cmd, rp_type, pause_after_n, extra_args=None):
+    debug_params = ['PAUSE_BEFORE_RP_N', rp_type, pause_after_n]
+    if extra_args:
+        debug_params.extend(extra_args)
+    return runDebugQueryCommand_expect_oom(env, query_cmd, debug_params)
 
 # Test ignore policy
 @skip(cluster=True)
@@ -89,6 +104,68 @@ def test_query_oom_standalone(env):
 # Test OOM error returned from shards (only for fail)
 @skip(cluster=False)
 def test_query_oom_cluster_shards_error():
+    env  = Env(shardsCount=3)
+
+    # Set OOM policy to fail on all shards
+    allShards_change_oom_policy(env, 'fail')
+
+    _ = _common_cluster_test_scenario(env)
+
+    # Change maxmemory on all shards to 1
+    allShards_change_maxmemory_low(env)
+    # Change back coord maxmemory to 0
+    set_unlimited_maxmemory_for_oom(env)
+
+    # Verify query fails
+    env.expect('FT.SEARCH', 'idx', '*', 'SORTBY', 'name', 'ASC').error().contains(OOM_QUERY_ERROR)
+    # Verify aggregation query fails with sorting so it has to wait for all shards
+    env.expect('FT.AGGREGATE', 'idx', '*', 'LOAD', 1, '@name', 'SORTBY', 2, '@name', 'ASC').error().contains(OOM_QUERY_ERROR)
+
+# Test OOM error returned from shards (only for fail), enforcing first reply from non-error shard
+# @skip(cluster=False)
+def test_query_oom_cluster_shards_error_first_reply():
+    env  = Env(shardsCount=3, moduleArgs='WORKERS 1')
+
+    # Set OOM policy to fail on all shards
+    allShards_change_oom_policy(env, 'return')
+
+    _ = _common_cluster_test_scenario(env)
+
+    # Change maxmemory on all shards to 1
+    allShards_change_maxmemory_low(env)
+    # Change back coord maxmemory to 0
+    set_unlimited_maxmemory_for_oom(env)
+
+    ##### Use debug command to enforce first reply from non-error shard #####
+
+    # We need to call the queries in MT so the paused query won't block the test
+    query_result = []
+    # Build threads
+    query_args = ['FT.AGGREGATE', 'idx', '*', 'LIMIT', 0, 120]
+    t_query = threading.Thread(
+        target=_call_and_store,
+        args=(runDebugQueryCommandPauseBeforeRPAfterN_expect_oom,
+            (env, query_args, 'Index', 0, ['INTERNAL_ONLY']),
+            query_result),
+        daemon=True
+    )
+    # Start the query and the pause-check in parallel
+    t_query.start()
+    # Wait for the coordinator to be paused
+    while False in allShards_getIsRPPaused(env):
+        time.sleep(0.1)
+
+    # Resume the coordinator
+    setPauseRPResume(env)
+    t_query.join()
+    allShards_setPauseRPResume(env, 2)
+    # Now, the result from the coordinator is the first to arrive, which is not an error
+    # Resume the rest of the shards
+    print(query_result)
+
+# Test OOM error returned from shards (only for fail) with Sorting
+@skip(cluster=False)
+def test_query_oom_cluster_shards_sorting_error():
     env  = Env(shardsCount=3)
 
     # Set OOM policy to fail on all shards
