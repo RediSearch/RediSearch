46a84212757c1faa3150fc6628f68f4fdd3bbf4d
Joan Fontanals <jfontanalsmartinez@gmail.com>
2025-10-03 14:46:13 +0200

    [MOD-11233] - fix: empty string token should be counted in byteOffsets for highlighting (#6938)
    * initial commit
    
    * add newline at end of file
    
    * add debug prints
    
    * test: fix testing index being awaited
    
    * add test from the MOD-11233
    
    * test: simplify tests to be easier to handle/debug
    
    * test: update test
    
    * test: improve test assertion
    
    * add test with some debug prints
    
    * fix: fix tokenization empty
    
    * remove useless field from tokenizer
    
    * fix: simpler fix
    
    * fix: improved behavior for forward index
    
    * fix spell check
    
    * change test name and var name
    
    * change test text
    
    * refactor: byteoffset handling independent
    
    * test: add more tests with highlighting and empty combination
    
    * apply changes as per code review
    
    ---------
    
    Co-authored-by: jonathan keinan <jonathan.keinan@redis.com>
    (cherry picked from commit 521847a7e3e041be75b160df1859e57edadcad84)

--- src/forward_index.h
+++ src/forward_index.h
@@ -68,7 +68,7 @@ typedef struct {
   uint32_t curBucketIdx;
 } ForwardIndexIterator;
 
-int forwardIndexTokenFunc(void *ctx, const Token *tokInfo);
+int forwardIndexTokenFunc(ForwardIndexTokenizerCtx *tokCtx, const Token *tokInfo);
 void ForwardIndexFree(ForwardIndex *idx);
 
 void ForwardIndex_Reset(ForwardIndex *idx, Document *doc, uint32_t idxFlags);
