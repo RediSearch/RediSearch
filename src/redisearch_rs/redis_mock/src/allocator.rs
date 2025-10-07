3e03f812478835371c30230321ea9a52c25e50a6
Glen De Cauwsemaecker <contact@glendc.com>
2025-10-07 13:13:21 +0200

    fix license header

--- src/redisearch_rs/redis_mock/src/allocator.rs
+++ src/redisearch_rs/redis_mock/src/allocator.rs
@@ -5,7 +5,7 @@
  * Licensed under your choice of the Redis Source Available License 2.0
  * (RSALv2); or (b) the Server Side Public License v1 (SSPLv1); or (c) the
  * GNU Affero General Public License v3 (AGPLv3).
- */
+*/
 
 use std::alloc::{Layout, alloc, alloc_zeroed, dealloc, realloc};
 use std::ffi::c_void;
