d3d623a97b1690efb1cd84b439cdf19c36bf7cf8
Joan Fontanals Martinez <jfontanalsmartinez@gmail.com>
2025-10-07 09:41:43 +0200

    fix: fix problem with amazon linux 2

--- .install/amazon_linux_2.sh
+++ .install/amazon_linux_2.sh
@@ -7,24 +7,48 @@ export DEBIAN_FRONTEND=noninteractive
 $MODE yum update -y
 $MODE yum install -y which
 
-# Install the RPM package that provides the Software Collections (SCL) required for devtoolset-11
-$MODE yum install -y https://vault.centos.org/centos/7/extras/x86_64/Packages/centos-release-scl-rh-2-3.el7.centos.noarch.rpm
+if [[ $ARCH = 'x86_64' ]]
+then
+    # Install the RPM package that provides the Software Collections (SCL) required for devtoolset-11
+    $MODE yum install -y https://vault.centos.org/centos/7/extras/x86_64/Packages/centos-release-scl-rh-2-3.el7.centos.noarch.rpm
 
-# http://mirror.centos.org/centos/7/ is deprecated, so we changed the above link to `https://vault.centos.org`,
-# and we have to change the baseurl in the repo file to the working mirror (from mirror.centos.org to vault.centos.org)
-$MODE sed -i 's/mirrorlist=/#mirrorlist=/g' /etc/yum.repos.d/CentOS-SCLo-scl-rh.repo                        # Disable mirrorlist
-$MODE sed -i 's/#baseurl=http:\/\/mirror/baseurl=http:\/\/vault/g' /etc/yum.repos.d/CentOS-SCLo-scl-rh.repo # Enable a working baseurl
+    # http://mirror.centos.org/centos/7/ is deprecated, so we changed the above link to `https://vault.centos.org`,
+    # and we have to change the baseurl in the repo file to the working mirror (from mirror.centos.org to vault.centos.org)
+    $MODE sed -i 's/mirrorlist=/#mirrorlist=/g' /etc/yum.repos.d/CentOS-SCLo-scl-rh.repo                        # Disable mirrorlist
+    $MODE sed -i 's/#baseurl=http:\/\/mirror/baseurl=http:\/\/vault/g' /etc/yum.repos.d/CentOS-SCLo-scl-rh.repo # Enable a working baseurl
 
-$MODE yum install -y wget git devtoolset-11-gcc devtoolset-11-gcc-c++ devtoolset-11-make rsync unzip clang-devel clang llvm-devel spdlog-devel fmt-devel
+    $MODE yum install -y wget git devtoolset-11-make rsync unzip libclang-dev clang
 
-source /opt/rh/devtoolset-11/enable
+    source /opt/rh/devtoolset-11/enable
 
-cp /opt/rh/devtoolset-11/enable /etc/profile.d/scl-devtoolset-11.sh
+    cp /opt/rh/devtoolset-11/enable /etc/profile.d/scl-devtoolset-11.sh
 
+    # install newer binutils
+    wget https://ftp.gnu.org/gnu/binutils/binutils-2.42.tar.gz
+    tar -xzf binutils-2.42.tar.gz
+    cd binutils-2.42
+    ./configure --prefix=/usr/local
+    make -j$(nproc)
+    make install
+    echo "/usr/local/bin" >> "$GITHUB_PATH"
+    as --version   # Should now be 2.42
+    cd ..
+else
+    # Install the RPM package that provides the Software Collections (SCL) required for devtoolset-10
+    $MODE yum install -y https://vault.centos.org/altarch/7/extras/aarch64/Packages/centos-release-scl-rh-2-3.el7.centos.noarch.rpm
 
-$MODE yum install -y openssl11 openssl11-devel
-$MODE ln -s "$(which openssl11)" /usr/bin/openssl
+    # http://mirror.centos.org/centos/7/ is deprecated, so we changed the above link to `https://vault.centos.org`,
+    # and we have to change the baseurl in the repo file to the working mirror (from mirror.centos.org to vault.centos.org)
+    # Disable mirrorlist
+    $MODE sed -i 's/mirrorlist=/#mirrorlist=/g' /etc/yum.repos.d/CentOS-SCLo-scl-rh.repo
+    # Enable a working baseurl
+    $MODE sed -i 's/#baseurl=http:\/\/mirror.centos.org\/centos/baseurl=http:\/\/vault.centos.org\/altarch/g' /etc/yum.repos.d/CentOS-SCLo-scl-rh.repo
 
-# Show final Python selection (should be 3.8)
-command -v python3
-python3 --version
+    $MODE yum install -y wget git \
+        devtoolset-10-make rsync unzip clang curl libclang-dev
+
+    source /opt/rh/devtoolset-10/enable
+
+    $MODE cp /opt/rh/devtoolset-10/enable /etc/profile.d/scl-devtoolset-10.sh
+
+fi
