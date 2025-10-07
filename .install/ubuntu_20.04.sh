3ae89d91c80351faa822f91e0d3c209008994827
Joan Fontanals <jfontanalsmartinez@gmail.com>
2025-10-07 17:36:20 +0200

    [2.6] Use uv to manage the Python environment for e2e tests. (#6715) (#6990)
    * Use uv to manage the Python environment for e2e tests. (#6715)
    
    * Use uv to manage the Python environment for e2e tests.
    
    * Allow forcing the testing workflow
    
    * Use canonical location for venv
    
    * Don't install Python using the system package manager
    
    * Add uv to PATH
    
    * Use the same PATH workaround for uv as we used for cargo
    
    * Improve comment
    
    * Ensure that the lockfile is up-to-date
    
    * Avoid updating since we just installed
    
    * Fix path issues and Alpine issue
    
    * Downgrade to 3.12
    
    * Update lockfile
    
    * Handle psutil issue
    
    * Update .install/install_python.sh
    
    Co-authored-by: Omer Shadmi <76992134+oshadmi@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Omer Shadmi <76992134+oshadmi@users.noreply.github.com>
    (cherry picked from commit 797659b77210d67e71f6f09c2ea9af9a50f2fbda)
    
    * remove python direct installation without uv from amazon_linux2
    
    ---------
    
    Co-authored-by: Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
    Co-authored-by: Omer Shadmi <76992134+oshadmi@users.noreply.github.com>

--- .install/ubuntu_20.04.sh
+++ .install/ubuntu_20.04.sh
@@ -9,13 +9,7 @@ $MODE apt install -yqq software-properties-common
 $MODE add-apt-repository ppa:deadsnakes/ppa -y
 $MODE apt update -yqq
 
-$MODE apt install -yqq wget make clang-format gcc python3 python3-venv python3-pip lcov git openssl libssl-dev \
+$MODE apt install -yqq wget make clang-format gcc lcov git openssl libssl-dev \
     unzip rsync build-essential gcc-10 g++-10 curl libclang-dev
 
 $MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 60 --slave /usr/bin/g++ g++ /usr/bin/g++-10
-
-# Install Python 3.12
-$MODE apt -y install python3.12 python3.12-venv python3.12-dev
-
-# Set python3 to point to python3.12
-$MODE update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.12 2
