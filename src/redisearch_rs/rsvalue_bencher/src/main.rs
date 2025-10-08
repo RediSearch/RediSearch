9f35bcd4093e4f483fa27e10d00c2e2bd8bd802a
Enrico Zandomeni Borba <enricozb@gmail.com>
2025-10-08 16:33:41 +0200

    move query error to be built as a static lib

--- src/redisearch_rs/rsvalue_bencher/src/main.rs
+++ src/redisearch_rs/rsvalue_bencher/src/main.rs
@@ -2,10 +2,13 @@ use rsvalue_bencher as _;
 
 unsafe extern "C" {
     fn RSValue_NewNumber(num: f64) -> *mut std::ffi::c_void;
+    fn RSValue_Number_Get(num: *mut std::ffi::c_void) -> f64;
 }
 
 fn main() {
-    let ptr = unsafe { RSValue_NewNumber(0.0) };
+    let ptr = unsafe { RSValue_NewNumber(123456.123456) };
+    let number = unsafe { RSValue_Number_Get(ptr) };
 
     println!("ptr: {ptr:?}");
+    println!("number: {number:?}");
 }
