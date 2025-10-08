2179c753d6594239f5e39dcc50cb72ddbbc55adf
Enrico Zandomeni Borba <enricozb@gmail.com>
2025-10-08 16:00:08 +0200

    compile fast_floast_strtod as a static lib

--- src/redisearch_rs/rsvalue_bencher/build.rs
+++ src/redisearch_rs/rsvalue_bencher/build.rs
@@ -12,16 +12,14 @@ use build_utils::{generate_c_bindings, git_root, link_static_libraries};
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Always link the static libraries, independent of bindgen
     link_static_libraries(&[
+        ("deps/fast_float", "fast_float_strtod_static"),
         ("src/util/mempool", "mempool"),
         ("src/value", "value"),
     ]);
 
     // Generate C bindings - fail build if this doesn't work
     let root = git_root().expect("Could not find git root");
-    let ii_header = root
-        .join("src")
-        .join("value")
-        .join("value.h");
+    let ii_header = root.join("src").join("value").join("value.h");
     generate_c_bindings(vec![ii_header], ".*/value.h", false)?;
 
     Ok(())
