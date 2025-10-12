ae06f866e18be43986652a2c5fe3f6da63a54595
GuyAv46 <guy.avimor@gmail.com>
2025-10-12 20:46:27 +0300

    improve test

--- tests/pytests/test_index.py
+++ tests/pytests/test_index.py
@@ -145,7 +145,12 @@ def test_lazy_index_creation_info_modules(env):
 
 @skip(cluster=True)
 def test_restore_schema(env: Env):
+
+    # Test that the command is not exposed to normal users
+    env.expect('_FT._RESTOREIFNX', 'SCHEMA').error().contains('unknown command')
+    # Mark the client as internal for the rest of the test
     env.cmd('DEBUG', 'MARK-INTERNAL-CLIENT')
+
     # create index with all fields types
     env.expect(
         "FT.CREATE", "idx", "SCHEMA",
