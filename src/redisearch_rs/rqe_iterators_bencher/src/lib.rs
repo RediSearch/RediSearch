955d2e0c16e8f14609888a4547ee362b91239b46
BenGoldberger <ben.goldberger@redis.com>
2025-10-13 10:10:54 +0300

    add metric iter benches

--- src/redisearch_rs/rqe_iterators_bencher/src/lib.rs
+++ src/redisearch_rs/rqe_iterators_bencher/src/lib.rs
@@ -7,6 +7,7 @@
  * GNU Affero General Public License v3 (AGPLv3).
 */
 
+use ::ffi::{RSValue, RSYieldableMetric, array_clear_func, array_ensure_append_n_func, array_free};
 use std::ffi::c_void;
 
 pub mod benchers;
@@ -23,10 +24,39 @@ pub extern "C" fn ResultMetrics_Free(metrics: *mut ::ffi::RSYieldableMetric) {
         return;
     }
 
-    panic!(
-        "did not expect any benchmark to set metrics, but got: {:?}",
-        unsafe { *metrics }
-    );
+    unsafe {
+        array_free(metrics as *mut c_void);
+    }
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn RSValue_NewNumber(val: f64) -> *mut RSValue {
+    // Allocate the f64 value and cast the pointer
+    let mock_value = Box::new(val);
+    Box::into_raw(mock_value) as *mut RSValue
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn RSYieldableMetric_Concat(
+    metrics: *mut *mut RSYieldableMetric,
+    new_metric: *const RSYieldableMetric,
+) {
+    if new_metric.is_null() {
+        return;
+    }
+    unsafe {
+        let elem_sz = std::mem::size_of::<RSYieldableMetric>() as u16;
+        // array_ensure_append_n_func returns a new array pointer, but we don't need to use it in this mock
+        *metrics = array_ensure_append_n_func(
+            *metrics as *mut c_void,
+            new_metric as *mut c_void,
+            1,
+            elem_sz,
+        ) as *mut RSYieldableMetric;
+
+        // array_clear_func returns a new array pointer, but we don't need to use it in this mock
+        let _ = array_clear_func(new_metric as *mut c_void, elem_sz);
+    }
 }
 
 #[unsafe(no_mangle)]
