a0d9ac068e3cae4b5b80f6e19a785cdf82dfb2c4
LukeMathWalker <20745048+LukeMathWalker@users.noreply.github.com>
2025-10-14 11:48:53 +0200

    ci: Abstract away system setup in a reusable Docker image

--- .install/common_base_linux_mariner_2.0.sh
+++ .install/common_base_linux_mariner_2.0.sh
@@ -2,6 +2,5 @@
 MODE=$1 # whether to install using sudo or not
 set -e
 export DEBIAN_FRONTEND=noninteractive
-
 $MODE tdnf install -q -y build-essential git wget ca-certificates tar unzip rsync \
-                         openssl-devel openssl which clang clang-devel
+                         openssl-devel openssl which clang clang-devel gzip
