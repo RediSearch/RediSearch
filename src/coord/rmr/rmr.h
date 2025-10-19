f5a29907d695bdf021ac966b00a5ce2bbfb24c05
GuyAv46 <47632673+GuyAv46@users.noreply.github.com>
2025-10-19 11:38:35 +0300

    Topology Info Cleanup (#6987)
    * fix all test to correctly use ft.add ft.get and ft.del
    
    * revert test deletion
    
    * remove some now dead code
    
    * remove all sharding and slot calculation code
    
    * fix all test to correctly use ft.add ft.get and ft.del
    
    * remove slots info
    
    * stop collecting replicas info on `SEARCH.CLUSTERSET` (it was never sent to us)
    
    * fix unit-tests
    
    * fix flow tests
    
    * fix spellings
    
    * leak fix
    
    * remove multi-node per shard (keep a single master)
    
    * fix tests
    
    * trim info even more
    
    * align tests
    
    * remove left assertion
    
    * fix unit test
    
    * after rebase fixes
    
    * address review comments

--- src/coord/rmr/rmr.h
+++ src/coord/rmr/rmr.h
@@ -37,10 +37,6 @@ typedef int (*MRReduceFunc)(struct MRCtx *ctx, int count, MRReply **replies);
  * reply to the reducer callback */
 int MR_Fanout(struct MRCtx *ctx, MRReduceFunc reducer, MRCommand cmd, bool block);
 
-int MR_MapSingle(struct MRCtx *ctx, MRReduceFunc reducer, MRCommand cmd);
-
-void MR_SetCoordinationStrategy(struct MRCtx *ctx, bool mastersOnly);
-
 /* Initialize the MapReduce engine with a given number of I/O threads and connections per each node in the Cluster */
 void MR_Init(size_t num_io_threads, size_t conn_pool_size, long long timeoutMS);
 
