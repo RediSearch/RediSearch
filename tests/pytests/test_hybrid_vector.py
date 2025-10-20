95425e56de20d3cdce031a137bf02bf560b37334
nafraf <nafraf@gmail.com>
2025-10-20 16:39:35 -0500

    Undo test changes

--- tests/pytests/test_hybrid_vector.py
+++ tests/pytests/test_hybrid_vector.py
@@ -90,14 +90,12 @@ def test_hybrid_vector_direct_blob_range():
     env.assertEqual(count, len(results.keys()))
     env.assertTrue(set(results.keys()) == {"doc:2", "doc:4"})
 
-# TODO: remove skip once FT.HYBRID for cluster is implemented
-# @skip(cluster=True)
 def test_hybrid_vector_direct_blob_range_with_filter():
     env = Env()
     setup_basic_index(env)
     env.assertEqual(b"\x9a\x99\x99\x3f\xcd\xcc\x4c\x3e" ,np.array([1.2, 0.2]).astype(np.float32).tobytes())
     response = env.cmd('FT.HYBRID', 'idx', 'SEARCH', 'green', 'VSIM' ,'@embedding', b"\x9a\x99\x99\x3f\xcd\xcc\x4c\x3e",\
-                        'RANGE', '4', 'RADIUS', '1', 'YIELD_SCORE_AS', 'v_score', 'FILTER', '@description:blue')
+                        'RANGE', '2', 'RADIUS', '1', 'FILTER', '@description:blue')
     results, count = get_results_from_hybrid_response(response)
     env.assertTrue(set(results.keys()) == {"doc:4"})
     env.assertEqual(count, len(results.keys()))
