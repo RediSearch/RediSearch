c9d0fcf75750a0b66e0d720cb19bb9c14706face
kei-nan <jonathan.keinan@redis.com>
2025-10-21 15:17:35 +0300

    MOD-11754: Enforce rlookup keys are prefixed correctly in FT.HYBRID (#7059)
    * initial commit
    
    * move reducer verification to where rlookup key is created
    
    * small fix - add group by and reduce tests
    
    * fix flow tests
    
    * fix tests
    
    * add missing @ when sending LOAD clauses and implicit LOAD
    
    * remove skip from tests
    
    * code review comments - also added a function to extract the key name for an rlookup key
    
    * code review fixes
    
    * fix tests + code review comments
    
    * missing file change
    
    * fix tests
    
    * code review comments
    
    * code review fixes
    
    * code review comments
    
    * fix tests
    
    * code review comment
    
    * fix test

--- src/aggregate/aggregate_plan.h
+++ src/aggregate/aggregate_plan.h
@@ -111,6 +111,7 @@ typedef struct {
   ArgsCursor args;
   const RLookupKey **keys;
   size_t nkeys;
+  bool strictPrefix; // Whether we should fail if a field is not prefixed with an @ or $ sign
 } PLN_LoadStep;
 
 /** VECTOR_NORMALIZER normalizes vector distance scores to [0,1] range */
@@ -135,14 +136,17 @@ typedef struct {
     ArgsCursor args;
   } * reducers;
   int idx;
+  // Whether we should fail if a key is not prefixed with an @ sign
+  bool strictPrefix;
 } PLN_GroupStep;
 
  /**
   * Allocates and initializes a new group step.
   * @param properties_ref StrongRef referencing the properties array (must be cloned by caller)
+  * @param strictPrefix Whether we should fail if a key is not prefixed with an @ sign
   * @return Pointer to the newly created group step
   */
-PLN_GroupStep *PLNGroupStep_New(StrongRef properties_ref);
+PLN_GroupStep *PLNGroupStep_New(StrongRef properties_ref, bool strictPrefix);
 
 /**
  * Gets the properties array from a group step (via StrongRef)
