c0ea47c19696dc1749caf679d5fc4d51017161b1
meiravgri <meirav.grimberg@redis.com>
2025-10-23 09:28:50 +0000

    return correct field name

--- tests/pytests/test_vecsim_svs.py
+++ tests/pytests/test_vecsim_svs.py
@@ -56,19 +56,20 @@ def test_small_window_size():
     svs_transfer_th = 1024 * 10
     keep_count = 10
     num_vectors = svs_transfer_th
+    field_name = 'v_SVS_VAMANA'
     for data_type in VECSIM_SVS_DATA_TYPES:
         query_vec = create_random_np_array_typed(dim, data_type)
         for compression in [[], ["COMPRESSION", "LVQ8"]]:
             params = ['TYPE', data_type, 'DIM', dim, 'DISTANCE_METRIC', 'L2', "CONSTRUCTION_WINDOW_SIZE", 10, *compression]
-            conn.execute_command('FT.CREATE', 'idx', 'SCHEMA', 'v_SVS_VAMANA', 'VECTOR', 'SVS-VAMANA', len(params), *params)
+            conn.execute_command('FT.CREATE', 'idx', 'SCHEMA', field_name, 'VECTOR', 'SVS-VAMANA', len(params), *params)
 
             # Add enough vector to trigger transfer to svs
             for i in range(num_vectors):
                 vector = create_random_np_array_typed(dim, data_type)
-                conn.execute_command('HSET', f'doc_{i}', 'v_SVS_VAMANA', vector.tobytes())
+                conn.execute_command('HSET', f'doc_{i}', field_name, vector.tobytes())
 
             try:
-                conn.execute_command('FT.SEARCH', 'idx', f'*=>[KNN {keep_count} @v_SVS_VAMANA $vec_param]', 'PARAMS', 2, 'vec_param', query_vec.tobytes(), 'RETURN', 1, '__v_score')
+                conn.execute_command('FT.SEARCH', 'idx', f'*=>[KNN {keep_count} @{field_name} $vec_param]', 'PARAMS', 2, 'vec_param', query_vec.tobytes(), 'RETURN', 1, f'__{field_name}_score')
             except Exception as e:
                 env.assertTrue(False, message=f"compression: {compression} data_type: {data_type}. Search failed with exception: {e}")
             # delete most
@@ -78,7 +79,7 @@ def test_small_window_size():
             # run topk for remaining
             # Before fixing MOD-10771, search crashed
             try:
-                conn.execute_command('FT.SEARCH', 'idx', f'*=>[KNN {keep_count} @v_SVS_VAMANA $vec_param]', 'PARAMS', 2, 'vec_param', query_vec.tobytes(), 'RETURN', 1, '__v_score')
+                conn.execute_command('FT.SEARCH', 'idx', f'*=>[KNN {keep_count} @{field_name} $vec_param]', 'PARAMS', 2, 'vec_param', query_vec.tobytes(), 'RETURN', 1, f'__{field_name}_score')
             except Exception as e:
                 env.assertTrue(False, message=f"compression: {compression} data_type: {data_type}. Search failed with exception: {e}")
             conn.execute_command('FLUSHALL')
