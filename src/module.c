644e599fdca4ff5198032425a273cf8e0769836c
Joan Fontanals Martinez <jfontanalsmartinez@gmail.com>
2025-11-04 13:07:35 +0100

    fix: handle proper init

--- src/module.c
+++ src/module.c
@@ -1579,6 +1579,7 @@ void RediSearch_CleanupModule(void) {
   IndexAlias_DestroyGlobal(&AliasTable_g);
   freeGlobalAddStrings();
   SchemaPrefixes_Free(SchemaPrefixes_g);
+  SharedExclusiveLock_Destroy();
   // GeometryApi_Free();
 
   Dictionary_Free();
