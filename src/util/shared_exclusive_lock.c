bbdc4efdf1ef1dbc0aa4f0ea1bed847a915a2a93
Joan Fontanals Martinez <jfontanalsmartinez@gmail.com>
2025-11-05 18:12:09 +0100

    some refactoring of Shared Exclusive Lock

--- src/util/shared_exclusive_lock.c
+++ src/util/shared_exclusive_lock.c
@@ -15,8 +15,8 @@
 
 pthread_mutex_t GILAlternativeLock = PTHREAD_MUTEX_INITIALIZER; // Lock used as an alternative to the GIL, this is used when the GIL is owned by the main thread.
 pthread_mutex_t InternalLock = PTHREAD_MUTEX_INITIALIZER; // Lock to handle communication mechanism internally
-pthread_cond_t GILCondition = PTHREAD_COND_INITIALIZER;
-pthread_cond_t AuxLockCondition = PTHREAD_COND_INITIALIZER;
+pthread_cond_t TryLockCondition = PTHREAD_COND_INITIALIZER; // Condition to signal threads waiting to try acquire the GIL or the alternative lock that they may try again.
+pthread_cond_t GILSafeCondition = PTHREAD_COND_INITIALIZER; // Condition to signal the main thread that it can safely release the GIL. (In this case return from UnsetOwned)
 atomic_bool GILOwned;
 bool GILAlternativeLockHeld = false;
 
@@ -26,20 +26,21 @@ void SharedExclusiveLock_Init() {
   GILAlternativeLockHeld = false;
   pthread_mutex_init(&GILAlternativeLock, NULL);
   pthread_mutex_init(&InternalLock, NULL);
-  pthread_cond_init(&GILCondition, NULL);
-  pthread_cond_init(&AuxLockCondition, NULL);
+  pthread_cond_init(&TryLockCondition, NULL);
+  pthread_cond_init(&GILSafeCondition, NULL);
 }
 
 void SharedExclusiveLock_Destroy() {
   pthread_mutex_destroy(&GILAlternativeLock);
-  pthread_cond_destroy(&GILCondition);
+  pthread_cond_destroy(&TryLockCondition);
   pthread_mutex_destroy(&InternalLock);
-  pthread_cond_destroy(&AuxLockCondition);
+  pthread_cond_destroy(&GILSafeCondition);
 }
 
 void SharedExclusiveLock_SetOwned() {
   atomic_store_explicit(&GILOwned, true, memory_order_release);
-  pthread_cond_broadcast(&GILCondition);
+  // Signal any waiting threads that they may try to acquire the GIL or the alternative lock.
+  pthread_cond_broadcast(&TryLockCondition);
 }
 
 void SharedExclusiveLock_UnsetOwned() {
@@ -47,7 +48,7 @@ void SharedExclusiveLock_UnsetOwned() {
   // Here we make sure that any thread that may assume the GIL is protected by the main thread releases the lock before returning.
   pthread_mutex_lock(&InternalLock);
   while (GILAlternativeLockHeld) {
-    pthread_cond_wait(&AuxLockCondition, &InternalLock);
+    pthread_cond_wait(&GILSafeCondition, &InternalLock);
   }
   pthread_mutex_unlock(&InternalLock);
 }
@@ -57,11 +58,18 @@ SharedExclusiveLockType SharedExclusiveLock_Acquire(RedisModuleCtx *ctx) {
   while (true) {
     int rc;
     if (atomic_load_explicit(&GILOwned, memory_order_acquire)) {
-      // The GIL is owned in a safe manner by the module in the main thread, so we hold the internal lock and return.
-      pthread_mutex_unlock(&InternalLock);
-      pthread_mutex_lock(&GILAlternativeLock);
-      GILAlternativeLockHeld = true;
-      return Internal_Locked; // internal handle is the non-GIL lock
+      // Keep InternalLock held while acquiring alternative lock
+      rc = pthread_mutex_lock(&GILAlternativeLock);
+      // Double-check GILOwned while holding both locks (double check can be okey because common path for now is GIL is not owned)
+      if (atomic_load_explicit(&GILOwned, memory_order_acquire)) {
+        GILAlternativeLockHeld = true;
+        pthread_mutex_unlock(&InternalLock);
+        return Internal_Locked;
+      } else {
+        // GIL was released, release alternative lock and retry
+        pthread_mutex_unlock(&GILAlternativeLock);
+        continue;
+      }
     } else {
       rc = RedisModule_ThreadSafeContextTryLock(ctx);
     }
@@ -78,18 +86,20 @@ SharedExclusiveLockType SharedExclusiveLock_Acquire(RedisModuleCtx *ctx) {
       timeout.tv_sec += timeout.tv_nsec / 1000000000;
       timeout.tv_nsec %= 1000000000;
     }
-    pthread_cond_timedwait(&GILCondition, &InternalLock, &timeout);
+    pthread_cond_timedwait(&TryLockCondition, &InternalLock, &timeout);
   }
 }
 
 void SharedExclusiveLock_Release(RedisModuleCtx *ctx, SharedExclusiveLockType type) {
-  if (type == Internal_Locked) {// Attempt to wake up some waiting thread
+  if (type == Internal_Locked) {
+    pthread_mutex_unlock(&GILAlternativeLock);
     pthread_mutex_lock(&InternalLock);
     GILAlternativeLockHeld = false;
-    pthread_cond_signal(&AuxLockCondition);
+    // If main thread is waiting to release the GIL, signal it that it can proceed.
+    pthread_cond_signal(&GILSafeCondition);
     pthread_mutex_unlock(&InternalLock);
-    pthread_mutex_unlock(&GILAlternativeLock);
-    pthread_cond_broadcast(&GILCondition);
+    // Signal any waiting threads that they may try to acquire the GIL or the alternative lock.
+    pthread_cond_broadcast(&TryLockCondition);
   } else {
     RedisModule_ThreadSafeContextUnlock(ctx);
   }
