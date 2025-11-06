28b9f99b83564419df1627d45438efe5ae45c694
meiravgri <109056284+meiravgri@users.noreply.github.com>
2025-11-06 15:16:29 +0200

    Update GCC toolset version in installation script

--- .install/rocky_linux_9.sh
+++ .install/rocky_linux_9.sh
@@ -7,4 +7,4 @@ $MODE dnf update -y
 $MODE dnf install -y gcc-toolset-14-gcc gcc-toolset-14-gcc-c++ make wget git \
     openssl openssl-devel which rsync unzip clang curl clang-devel --nobest --skip-broken --allowerasing
 
-cp /opt/rh/gcc-toolset-13/enable /etc/profile.d/gcc-toolset-13.sh
+cp /opt/rh/gcc-toolset-14/enable /etc/profile.d/gcc-toolset-14.sh
