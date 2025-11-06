d402506bb89e7cc30a43a17539ef3ef06324c12d
Joan Fontanals Martinez <jfontanalsmartinez@gmail.com>
2025-11-06 11:22:42 +0100

    fix: fix compile

--- src/util/shared_exclusive_lock.c
+++ src/util/shared_exclusive_lock.c
@@ -73,7 +73,7 @@ SharedExclusiveLockType SharedExclusiveLock_Acquire(RedisModuleCtx *ctx) {
       rc = RedisModule_ThreadSafeContextTryLock(ctx);
     }
     if (rc == REDISMODULE_OK) {
-      RS_LOG_ASSERT(!GILAlternativeLockHeld, "If we acquired the GIL, the alternative lock should not be held by another thread.");
+      RS_LOG_ASSERT(gilState == GILState_Unknown, "gilState should be GILState_Unknown");
       pthread_mutex_unlock(&InternalLock);
       return GIL_Locked;
     }
