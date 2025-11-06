5259437561d79b0125ce50f3c1e78fd201b6a484
Joan Fontanals Martinez <jfontanalsmartinez@gmail.com>
2025-11-06 13:55:46 +0100

    fix: aim to fix rockylinux install

--- .install/rocky_linux_9.sh
+++ .install/rocky_linux_9.sh
@@ -4,7 +4,7 @@ set -e
 export DEBIAN_FRONTEND=noninteractive
 $MODE dnf update -y
 
-$MODE dnf install -y gcc-toolset-13-gcc gcc-toolset-13-gcc-c++ make wget git \
+$MODE dnf install -y gcc gcc-c++ make wget git \
     openssl openssl-devel which rsync unzip clang curl clang-devel --nobest --skip-broken --allowerasing
 
 cp /opt/rh/gcc-toolset-13/enable /etc/profile.d/gcc-toolset-13.sh
