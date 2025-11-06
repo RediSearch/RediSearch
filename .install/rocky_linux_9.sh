5aff4f2d4304d9ea946e096d3b7281b392b09581
meiravgri <109056284+meiravgri@users.noreply.github.com>
2025-11-06 15:15:39 +0200

    Upgrade GCC toolset to version 14
    Updated GCC toolset installation from version 13 to 14 and added profile script for GCC 13.

--- .install/rocky_linux_9.sh
+++ .install/rocky_linux_9.sh
@@ -4,8 +4,7 @@ set -e
 export DEBIAN_FRONTEND=noninteractive
 $MODE dnf update -y
 
-$MODE dnf install -y gcc-toolset-13-gcc gcc-toolset-13-gcc-c++ make wget git \
+$MODE dnf install -y gcc-toolset-14-gcc gcc-toolset-14-gcc-c++ make wget git \
     openssl openssl-devel which rsync unzip clang curl clang-devel --nobest --skip-broken --allowerasing
 
-echo `gcc --version`
-echo `g++ --version`
+cp /opt/rh/gcc-toolset-13/enable /etc/profile.d/gcc-toolset-13.sh
