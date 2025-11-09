21ddc0654256c31dd032b38c662675e02880f6cb
meiravgri <meirav.grimberg@redis.com>
2025-11-09 13:13:32 +0200

    enable rocky 9

--- .install/rocky_linux_8.sh
+++ .install/rocky_linux_8.sh
@@ -13,7 +13,7 @@ $MODE dnf config-manager --set-enabled powertools
 # get epel to install gcc13
 $MODE dnf install epel-release -yqq
 
-$MODE dnf install -y gcc gcc-c++ \
+$MODE dnf install -y gcc-toolset-13-gcc gcc-toolset-13-gcc-c++ \
     gcc-toolset-13-libatomic-devel make wget git openssl openssl-devel \
     bzip2-devel libffi-devel zlib-devel tar xz which rsync \
     clang curl clang-devel --nobest --skip-broken
