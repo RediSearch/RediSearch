e47170c188066b8b0f3633619c8ec9239be11b2b
meiravgri <meirav.grimberg@redis.com>
2025-11-09 11:50:54 +0200

    remove flags

--- .install/rocky_linux_9.sh
+++ .install/rocky_linux_9.sh
@@ -4,7 +4,7 @@ set -e
 export DEBIAN_FRONTEND=noninteractive
 $MODE dnf update -y
 
-$MODE dnf install -y gcc-toolset-14-gcc gcc-toolset-14-gcc-c++ make wget git \
-    openssl openssl-devel which rsync unzip clang curl clang-devel --nobest --skip-broken --allowerasing
+$MODE dnf install -y gcc-toolset-13-gcc gcc-toolset-13-gcc-c++ make wget git \
+    openssl openssl-devel which rsync unzip clang curl clang-devel
 
-cp /opt/rh/gcc-toolset-14/enable /etc/profile.d/gcc-toolset-14.sh
+cp /opt/rh/gcc-toolset-13/enable /etc/profile.d/gcc-toolset-13.sh
