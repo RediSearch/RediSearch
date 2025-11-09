eaea967cd81939e8d74cf3fbabf00c35a37f0c9e
meiravgri <meirav.grimberg@redis.com>
2025-11-09 13:26:54 +0200

    fix script
    skip vuild redis

--- .install/rocky_linux_9.sh
+++ .install/rocky_linux_9.sh
@@ -7,8 +7,7 @@ $MODE dnf update -y
 $MODE dnf install -y gcc-toolset-13-gcc gcc-toolset-13-gcc-c++ make wget git \
     openssl openssl-devel which rsync unzip clang curl clang-devel --nobest --skip-broken --allowerasing
 
-# Remove any gcc and g++ versions other than 13
-$MODE dnf remove -y gcc-toolset-* --exclude=gcc-toolset-13-* 2>/dev/null || true
-$MODE dnf remove -y gcc g++ 2>/dev/null || true
+# Remove any gcc-toolset versions other than 13 (keep clang and clang-devel for bindgen)
+$MODE dnf remove -y gcc-toolset-* --exclude=gcc-toolset-13-\* 2>/dev/null || true
 
 cp /opt/rh/gcc-toolset-13/enable /etc/profile.d/gcc-toolset-13.sh
