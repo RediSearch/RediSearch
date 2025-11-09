ccd474e7b057afd163df7e4c7cf9f6aa5fe13dab
meiravgri <meirav.grimberg@redis.com>
2025-11-09 11:54:17 +0200

    remove only allowerasing

--- .install/rocky_linux_9.sh
+++ .install/rocky_linux_9.sh
@@ -5,6 +5,6 @@ export DEBIAN_FRONTEND=noninteractive
 $MODE dnf update -y
 
 $MODE dnf install -y gcc-toolset-13-gcc gcc-toolset-13-gcc-c++ make wget git \
-    openssl openssl-devel which rsync unzip clang curl clang-devel
+    openssl openssl-devel which rsync unzip clang curl clang-devel --nobest --skip-broken
 
 cp /opt/rh/gcc-toolset-13/enable /etc/profile.d/gcc-toolset-13.sh
