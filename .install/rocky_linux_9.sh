5c0dee62406dd5dc9177e15828ddaa15f69e97b6
meiravgri <meirav.grimberg@redis.com>
2025-11-10 15:14:09 +0200

    try with flags

--- .install/rocky_linux_9.sh
+++ .install/rocky_linux_9.sh
@@ -4,9 +4,9 @@ set -e
 export DEBIAN_FRONTEND=noninteractive
 $MODE dnf update -y
 
-$MODE dnf install -y gcc-toolset-13-gcc gcc-toolset-13-gcc-c++ make wget git \
+$MODE dnf install -y gcc-toolset-13-gcc gcc-toolset-13-gcc-c++ make wget git --nobest --skip-broken --allowerasing
 
 
 cp /opt/rh/gcc-toolset-13/enable /etc/profile.d/gcc-toolset-13.sh
-
+# install other stuff after installing gcc-toolset-13 to avoid dependencies conflicts
 $MODE dnf install -y openssl openssl-devel which rsync unzip curl clang  clang-devel --nobest --skip-broken --allowerasing
