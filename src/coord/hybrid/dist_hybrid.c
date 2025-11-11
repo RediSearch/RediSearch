85bcaf8c4c05db9debda3d5464dece93442300d5
nafraf <nafraf@gmail.com>
2025-11-11 06:44:40 -0500

    Fix expected offset handling for YIELD_SCORE_AS in HybridRequest_appendVsim

--- src/coord/hybrid/dist_hybrid.c
+++ src/coord/hybrid/dist_hybrid.c
@@ -116,12 +116,13 @@ static void HybridRequest_appendVsim(RedisModuleString **argv, int argc, MRComma
 
   int actualFilterOffset = RMUtil_ArgIndex("FILTER", argv + vsimOffset, argc - vsimOffset);
   actualFilterOffset = actualFilterOffset != -1 ? actualFilterOffset + vsimOffset : -1;
+  int expectedYieldScoreOffset = expectedFilterOffset;
 
   if (actualFilterOffset == expectedFilterOffset && actualFilterOffset < argc - 1) {
     // This is a VSIM FILTER - append it to the command
     MRCommand_AppendRstr(xcmd, argv[actualFilterOffset]);     // FILTER keyword
     MRCommand_AppendRstr(xcmd, argv[actualFilterOffset + 1]); // filter expression
-    expectedFilterOffset += 2; // Update expected offset after processing FILTER
+    expectedYieldScoreOffset += 2; // Update expected offset after processing FILTER
   }
 
   // Add YIELD_SCORE_AS if present
@@ -129,7 +130,7 @@ static void HybridRequest_appendVsim(RedisModuleString **argv, int argc, MRComma
   int yieldScoreOffset = RMUtil_ArgIndex("YIELD_SCORE_AS", argv + vsimOffset, argc - vsimOffset);
   yieldScoreOffset = yieldScoreOffset != -1 ? yieldScoreOffset + vsimOffset : -1;
 
-  if (yieldScoreOffset == expectedFilterOffset && yieldScoreOffset < argc - 1) {
+  if (yieldScoreOffset == expectedYieldScoreOffset && yieldScoreOffset < argc - 1) {
     // This is a VSIM YIELD_SCORE_AS - append it to the command
     MRCommand_AppendRstr(xcmd, argv[yieldScoreOffset]);     // YIELD_SCORE_AS keyword
     MRCommand_AppendRstr(xcmd, argv[yieldScoreOffset + 1]); // score alias
