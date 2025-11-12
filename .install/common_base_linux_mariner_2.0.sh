c50299fed75acc84fcadbfd8348871a51f564d2d
meiravgri <meirav.grimberg@redis.com>
2025-11-12 17:17:11 +0000

    revert

--- .install/common_base_linux_mariner_2.0.sh
+++ .install/common_base_linux_mariner_2.0.sh
@@ -4,4 +4,4 @@ set -e
 export DEBIAN_FRONTEND=noninteractive
 
 $MODE tdnf install -q -y build-essential git wget ca-certificates tar unzip rsync \
-                         openssl-devel openssl which clang clang-devel gzip
+                         openssl-devel openssl which clang clang-devel
