dfa1e86e1251569ddb886ea02ec5320710df3611
Joan Fontanals Martinez <jfontanalsmartinez@gmail.com>
2025-11-14 13:06:14 +0100

    create meta with multiple docIds

--- src/doc_id_meta.c
+++ src/doc_id_meta.c
@@ -8,13 +8,46 @@
 */
 
 #include "doc_id_meta.h"
+#include "util/arr/arr.h"
+
+#define DOCID_META_INVALID 0
+
+static RedisModuleKeyMetaClassId docIdKeyMetaClassId;
+
+// TODO (Joan): Should we use a hashmap instead of an array, where the key is the index name?
+// Also review the growing policy (Should we care more for speed than space?)
+// Can we rely on the index order to be constant, even considering RDB save/load?
+struct DocIdMeta {
+  arrayof(uint64_t) docId;
+  size_t size;
+};
 
 static int docIdMetaCopy(RedisModuleKeyOptCtx *ctx, uint64_t *meta) {
+  REDISMODULE_NOT_USED(ctx);
+  struct DocIdMeta *docIdMeta = (struct DocIdMeta *)meta;
+  if (docIdMeta) {
+    struct DocIdMeta *newDocIdMeta = rm_malloc(sizeof(struct DocIdMeta));
+    newDocIdMeta->size = docIdMeta->size;
+    newDocIdMeta->docId = array_new(uint64_t, docIdMeta->size);
+    memcpy(newDocIdMeta->docId, docIdMeta->docId, sizeof(uint64_t) * docIdMeta->size);
+    *meta = (uint64_t)newDocIdMeta;
+  }
   return 1;
 }
 
-static RedisModuleKeyMetaClassId docIdKeyMetaClassId;
-static RedisModuleKeyMetaClassConfig docIdKeyMetaClassIdConfig = {
+/* Free callback - called when metadata needs to be freed */
+static void docIdMetaFree(const char *keyname, uint64_t meta) {
+  REDISMODULE_NOT_USED(keyname);
+  if (meta == 0) return;
+  struct DocIdMeta *docIdMeta = (struct DocIdMeta *)meta;
+  array_free(docIdMeta->docId);
+  rm_free(docIdMeta);
+}
+
+#define INITIAL_DOCID_META_SIZE 10
+
+void DocIdMeta_Init(RedisModuleCtx *ctx) {
+  RedisModuleKeyMetaClassConfig docIdKeyMetaClassIdConfig = {
     .version = 1,
     .reset_value = 0,
     .flags = 0,
@@ -22,7 +55,7 @@ static RedisModuleKeyMetaClassConfig docIdKeyMetaClassIdConfig = {
     .rename = NULL, // If NULL, meta is kept during rename
     .move = NULL, // If NULL, meta is kept during move
     .unlink = NULL, // If NULL, meta is ignored during unlink
-    .free = NULL, // No dynamic memory
+    .free = (RedisModuleKeyMetaFreeFunc)docIdMetaFree, // Will need to free the DocIdMeta struct
     // TODO(Joan): Ask clarification for these callbacks
     .defrag = NULL,
     .mem_usage = NULL,
@@ -33,17 +66,53 @@ static RedisModuleKeyMetaClassConfig docIdKeyMetaClassIdConfig = {
     .rdb_save = NULL,
     .aof_rewrite = NULL,
 };
-
-void DocIdMeta_Init(RedisModuleCtx *ctx) {
   docIdKeyMetaClassId = RedisModule_CreateKeyMetaClass(ctx, "docId", 1, &docIdKeyMetaClassIdConfig);
 }
 
-int DocIdMeta_Set(RedisModuleKey *key, uint64_t docId) {
-  return RedisModule_SetKeyMeta(key, docIdKeyMetaClassId, &docId);
+int DocIdMeta_Set(RedisModuleKey *key, size_t idx, uint64_t docId) {
+  RS_ASSERT(docId != DOCID_META_INVALID);
+  uint64_t meta = 0;
+  if (RedisModule_GetKeyMeta(docIdKeyMetaClassId, key, &meta) == REDISMODULE_OK) {
+    if (meta != 0) {
+      // key has meta
+      // Interpret the meta as DocIdMeta. Here we should append to the list or to the hashmap
+
+      struct DocIdMeta *docIdMeta = (struct DocIdMeta *)meta;
+      if (idx >= docIdMeta->size) {
+        // reallocate (review policy of resizing)
+        size_t newSize = docIdMeta->size * 2;
+        docIdMeta->docId = rm_realloc(docIdMeta->docId, sizeof(uint64_t) * newSize);
+        memset(docIdMeta->docId + docIdMeta->size, DOCID_META_INVALID, sizeof(uint64_t) * docIdMeta->size);
+        docIdMeta->size = newSize;
+      }
+      docIdMeta->docId[idx] = docId;
+      return REDISMODULE_OK;
+    }
+  }
+  size_t initialSize = idx >= INITIAL_DOCID_META_SIZE ? idx + 1 : INITIAL_DOCID_META_SIZE;
+  struct DocIdMeta *docIdMeta = rm_malloc(sizeof(struct DocIdMeta));
+  docIdMeta->size = initialSize;
+  docIdMeta->docId = array_new(uint64_t, initialSize);
+  memset(docIdMeta->docId, DOCID_META_INVALID, sizeof(uint64_t) * initialSize);
+  docIdMeta->docId[idx] = docId;
+  return RedisModule_SetKeyMeta(docIdKeyMetaClassId, key, (uint64_t)docIdMeta);
 }
 
-uint64_t DocIdMeta_Get(RedisModuleKey *key) {
-  uint64_t docId;
-  RedisModule_GetKeyMeta(key, docIdKeyMetaClassId, &docId);
-  return docId;
+int DocIdMeta_Get(RedisModuleKey *key, size_t idx, uint64_t *docId) {
+  uint64_t meta = 0;
+  if (RedisModule_GetKeyMeta(docIdKeyMetaClassId, key, &meta) != REDISMODULE_OK) {
+    return REDISMODULE_ERR;
+  }
+  if (meta == 0) {
+    return REDISMODULE_ERR;
+  }
+  struct DocIdMeta *docIdMeta = (struct DocIdMeta *)meta;
+  if (idx >= docIdMeta->size) {
+    return REDISMODULE_ERR;
+  }
+  if (docIdMeta->docId[idx] == DOCID_META_INVALID) {
+    return REDISMODULE_ERR;
+  }
+  *docId = docIdMeta->docId[idx];
+  return REDISMODULE_OK;
 }
--- src/doc_id_meta.h
+++ src/doc_id_meta.h
@@ -10,8 +10,25 @@
 
 #include "redismodule.h"
 
+// Initialize the DocIdMeta module
 void DocIdMeta_Init(RedisModuleCtx *ctx);
 
-int DocIdMeta_Set(RedisModuleKey *key, uint64_t docId);
+/*
+ * TODO: Review if idx is a good API
+ * Set the docId for the given key and index. If the key already has a docId for the given index, overwrite it.
+ * If the key does not have a docId for the given index, add it.
+ * @param key The key to set the docId for
+ * @param idx The index (as position in the index global array) to set the docId for
+ * @param docId The docId to set
+ * @return REDISMODULE_OK if the docId was set, REDISMODULE_ERR otherwise
+*/
+int DocIdMeta_Set(RedisModuleKey *key, size_t idx, uint64_t docId);
 
-uint64_t DocIdMeta_Get(RedisModuleKey *key);
+/*
+* Get the docId for the given key and index.
+* @param key The key to get the docId for
+* @param idx The index (as position in the index global array) to get the docId for
+* @param docId The docId to get
+* @return REDISMODULE_OK if the docId was found, REDISMODULE_ERR otherwise
+*/
+int DocIdMeta_Get(RedisModuleKey *key, size_t idx, uint64_t *docId);
