a178beaafc713297aafa3ecee89d5ad8490493e1
meiravgri <meirav.grimberg@redis.com>
2025-11-16 12:39:41 +0000

    fix

--- .install/install_python.sh
+++ .install/install_python.sh
@@ -4,11 +4,10 @@ set -ex
 processor=$(uname -m)
 OS_TYPE=$(uname -s)
 
-if [[ $GITHUB_ACTIONS == "true" ]]; then
-	export UV_INSTALL_DIR=/root/.local/bin
-else
-	export UV_INSTALL_DIR=$HOME/.local/bin
-fi
+# Always install to the current user's HOME directory
+# In containers: HOME=/root (running as root)
+# On GitHub runners: HOME=/home/runner (running as runner user)
+export UV_INSTALL_DIR=$HOME/.local/bin
 
 curl --proto '=https' --tlsv1.2 -LsSf https://astral.sh/uv/install.sh | env UV_INSTALL_DIR="$UV_INSTALL_DIR" sh
 # Add the newly installed `uv` to the PATH
