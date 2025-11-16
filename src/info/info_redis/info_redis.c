8cff9f0d519720b8b09278082ad1a6c644d70e2f
lerman25 <lerman25@gmail.com>
2025-11-16 10:45:30 +0200

    remove coord - not used yet

--- src/info/info_redis/info_redis.c
+++ src/info/info_redis/info_redis.c
@@ -253,13 +253,6 @@ void AddToInfo_ErrorsAndWarnings(RedisModuleInfoCtx *ctx, TotalIndexesInfo *tota
 
   RedisModule_InfoAddFieldULongLong(ctx, "total_query_errors_syntax", stats.errors.syntax);
   RedisModule_InfoAddFieldULongLong(ctx, "total_query_errors_arguments", stats.errors.arguments);
-  // Coordinator errors and warnings
-  RedisModule_InfoAddSection(ctx, "coordinator_warnings_and_errors");
-  RedisModule_InfoAddFieldULongLong(ctx, "coord_total_query_errors_timeout", stats.coord_errors.timeout);
-  RedisModule_InfoAddFieldULongLong(ctx, "coord_total_query_errors_oom", stats.coord_errors.oom);
-  RedisModule_InfoAddFieldULongLong(ctx, "coord_total_query_warnings_timeout", stats.coord_warnings.timeout);
-  RedisModule_InfoAddFieldULongLong(ctx, "coord_total_query_warnings_oom", stats.coord_warnings.oom);
-  RedisModule_InfoAddFieldULongLong(ctx, "coord_total_query_warnings_reached_max_prefix_expansions", stats.coord_warnings.max_prefix_expansions);
 }
 
 void AddToInfo_Dialects(RedisModuleInfoCtx *ctx) {
