7f330c37214026f869397c3061f6e0b9c35ada61
lerman25 <lerman25@gmail.com>
2025-11-18 11:20:18 +0200

    Track timeout in sendchunk resp3
    (cherry picked from commit 6853bb97bdc4cce389607f4c111543f684052d2d)

--- src/aggregate/aggregate_exec.c
+++ src/aggregate/aggregate_exec.c
@@ -525,14 +525,20 @@ static void sendChunk_Resp3(AREQ *req, RedisModule_Reply *reply, size_t limit,
     ResultProcessor *rp = qctx->endProc;
     SearchResult **results = NULL;
     bool cursor_done = false;
+    // For statistics purposes - distinguish between coordinator and shard
+    bool isCoord = GetNumShards_UnSafe() > 1 && !IsInternal(req);
 
     startPipeline(req, rp, &results, &r, &rc);
 
     if (ShouldReplyWithError(QueryError_GetCode(rp->parent->err), req->reqConfig.timeoutPolicy, IsProfile(req))) {
+      // Track errors in global statistics
+      QueryErrorsGlobalStats_UpdateError(QueryError_GetCode(rp->parent->err), 1, isCoord);
       RedisModule_Reply_Error(reply, QueryError_GetUserError(qctx->err));
       cursor_done = true;
       goto done_3_err;
     } else if (ShouldReplyWithTimeoutError(rc, req->reqConfig.timeoutPolicy, IsProfile(req))) {
+      // Track errors in global statistics
+      QueryErrorsGlobalStats_UpdateError(QUERY_ERROR_CODE_TIMED_OUT, 1, isCoord);
       ReplyWithTimeoutError(reply);
       cursor_done = true;
       goto done_3_err;
@@ -606,6 +612,8 @@ done_3:
       RedisModule_Reply_SimpleString(reply, QUERY_WOOM_CLUSTER);
     }
     if (rc == RS_RESULT_TIMEDOUT) {
+      // Track warnings in global statistics
+      QueryWarningsGlobalStats_UpdateWarning(QUERY_WARNING_CODE_TIMED_OUT, 1, isCoord);
       RedisModule_Reply_SimpleString(reply, QueryError_Strerror(QUERY_ERROR_CODE_TIMED_OUT));
     } else if (rc == RS_RESULT_ERROR) {
       // Non-fatal error
