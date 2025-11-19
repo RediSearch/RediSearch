75e7a6f064e3a308114f8d1acfd3217c1b7c8454
Tim Janus <tim@janus.rs>
2025-11-19 17:18:06 +0100

    refactor extract function `SchemaRuleWrapper::field_as_cstr`

--- src/redisearch_rs/rlookup/src/schema_rule.rs
+++ src/redisearch_rs/rlookup/src/schema_rule.rs
@@ -57,39 +57,40 @@ impl SchemaRuleWrapper {
         unsafe { self.0.as_ref() }
     }
 
-    /// Get the language field CStr, if present.
-    pub const fn lang_field(&self) -> Option<&CStr> {
-        let rule = self.as_ref();
-        if rule.lang_field.is_null() {
+    /// Convert a raw C string pointer to an Option<&CStr>, returning None if the pointer is null.
+    ///
+    /// # Safety
+    ///
+    /// 1. The caller must ensure that if the is associated with `self`, as the returned reference's lifetime is tied to `self`.
+    /// 2. The type invariants described on the type documentation is uphold if an external caller complies to the safety requirements of `from_raw`.
+    const unsafe fn field_as_cstr(&self, ffi_field: *mut ::std::os::raw::c_char) -> Option<&CStr> {
+        if ffi_field.is_null() {
             None
         } else {
-            // Safety: Usage of this type requires that a non-null lang_field points to a valid CStr.
-            Some(unsafe { CStr::from_ptr(rule.lang_field) })
+            // Safety: The caller must ensure (1), and (2), by using `from_raw` to create `self` and upholding `from_raw`'s safety requirements.
+            Some(unsafe { CStr::from_ptr(ffi_field) })
         }
+    }
+
+    /// Get the language field CStr, if present.
+    pub const fn lang_field(&self) -> Option<&CStr> {
+        // Safety: (1) due to creation with `SchemaRule::from_raw` it field pointers are valid or null, (2) lifetime tied to `self.0.lang_field`.
+        unsafe { self.field_as_cstr(self.as_ref().lang_field) }
     }
 
     /// Get the score field CStr, if present.
     pub const fn score_field(&self) -> Option<&CStr> {
-        let rule = self.as_ref();
-        if rule.score_field.is_null() {
-            None
-        } else {
-            // Safety: Usage of this type requires that a non-null score_field points to a valid CStr.
-            Some(unsafe { CStr::from_ptr(rule.score_field) })
-        }
+        // Safety: (1) due to creation with `SchemaRule::from_raw` it field pointers are valid or null, (2) lifetime tied to `self.0.score_field`.
+        unsafe { self.field_as_cstr(self.as_ref().score_field) }
     }
 
     /// Get the payload field CStr, if present.
     pub const fn payload_field(&self) -> Option<&CStr> {
-        let rule = self.as_ref();
-        if rule.payload_field.is_null() {
-            None
-        } else {
-            // Safety: Usage of this type requires that non-null payload_field points to a valid CStr.
-            Some(unsafe { CStr::from_ptr(rule.payload_field) })
-        }
+        // Safety: (1) due to creation with `SchemaRule::from_raw` it field pointers are valid or null, (2) lifetime tied to `self.0.payload_field`.
+        unsafe { self.field_as_cstr(self.as_ref().payload_field) }
     }
 
+    /// Tests if the given [`crate::lookup::RLookupKey`] is a special key (lang, score, or payload field) in respect to this schema rule.
     pub fn is_special_key(&self, key: &RLookupKey) -> bool {
         // check if the key is one of the special fields
         let key_is_lang_field = self.lang_field().is_some_and(|lf| lf == key.name_as_cstr());
