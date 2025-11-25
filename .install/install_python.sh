13e98e742862705dc51654cc5e3cabb4123cc523
meiravgri <109056284+meiravgri@users.noreply.github.com>
2025-11-25 07:31:21 +0200

    Revert "[8.2] MOD-12444 [MOD-12628] run on cached containers" (#7504)
    Revert "[8.2] MOD-12444 [MOD-12628] run on cached containers (#7440)"
    
    This reverts commit 7745696a0a129887a9059c755f5af43683cc85d3.

--- .install/install_python.sh
+++ .install/install_python.sh
@@ -4,14 +4,9 @@ set -ex
 processor=$(uname -m)
 OS_TYPE=$(uname -s)
 
-# Always install to the current user's HOME directory
-# In containers: HOME=/root (running as root)
-# On GitHub runners: HOME=/home/runner (running as runner user)
-export UV_INSTALL_DIR=$HOME/.local/bin
-
-curl --proto '=https' --tlsv1.2 -LsSf https://astral.sh/uv/install.sh | env UV_INSTALL_DIR="$UV_INSTALL_DIR" sh
+curl --proto '=https' --tlsv1.2 -LsSf https://astral.sh/uv/install.sh | sh
 # Add the newly installed `uv` to the PATH
-export PATH="$UV_INSTALL_DIR:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
 
 # Verify uv is in path
 uv -vV
