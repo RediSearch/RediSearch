43aa324682efb9d61ea5d9952bd0786b0771fcda
LukeMathWalker <20745048+LukeMathWalker@users.noreply.github.com>
2025-11-25 17:42:51 +0100

    Add missing symbol stubs

--- src/redisearch_rs/rqe_iterators/tests/integration/ffi/c_symbols.rs
+++ src/redisearch_rs/rqe_iterators/tests/integration/ffi/c_symbols.rs
@@ -12,6 +12,8 @@
 use std::ffi::c_void;
 
 redis_mock::bind_redis_alloc_symbols_to_mock_impl!();
+#[allow(unused)]
+pub use types_ffi;
 
 // symbols required by the C code we need to redefine
 #[unsafe(no_mangle)]
@@ -35,56 +37,44 @@ macro_rules! stub_c_fn {
     };
 }
 
-// Those C symbols are required for the c benchmarking code to build and run.
-// They have been added by adding them until it runs fine.
+// On macOS, we can tell the loader to ignore undefined symbols via `-undefined=dynamic_lookup`.
+// As long as they are not invoked at runtime, everything will work out.
+// On Linux, there is no way equivalent option. We can tell the _linker_ to ignore undefined
+// symbols at link time, but we will still get a runtime error if there *strong* references
+// to undefined symbols.
+// The simplest workaround is to provide stubs for those symbols: dummy entries that make the loader
+// happy but are actually going to fail catastrophically if invoked at runtime. You wouldn't want
+// to do it for production builds, but for tests it's fine.
+//
+// What symbols should we stub?
+// One approach is to "fix when the loader complains". You try to run tests, you get a "symbol lookup error",
+// you add a stub for that symbol. Tedious, it can require tens of iterations depending on what's being linked.
+// Luckily enough, we can be more scientific!
+// Start by building the test binary you care about. In this case:
+//
+// ```bash
+// cargo test --no-run --test integration
+// ```
+//
+// `cargo` will output the path to the newly compiled test binary in its output. For example,
+// `../../bin/redisearch_rs/debug/deps/integration-fbaef160d4b407a6`.
+//
+// You then extract the undefined symbols with a strong reference from the compiler artifact via:
+//
+// ```bash
+// readelf -Ws --dyn-syms ../../bin/redisearch_rs/debug/deps/integration-fbaef160d4b407a6 | \
+// awk '$5 == "GLOBAL" && $7 == "UND" && $4 == "NOTYPE" { print $8 }' | sort -u
+// ```
+//
+// You add those symbols to the list below... and you're done!
+// Still tedious, but faster.
 stub_c_fn! {
-    DocTable_Exists,
-    IndexSpec_GetFormattedKey,
-    RS_dictFetchValue,
-    RedisModule_ClusterCanAccessKeysInSlot,
-    RedisModule_ClusterFreeSlotRanges,
-    RedisModule_ClusterGetLocalSlotRanges,
-    RedisModule_ClusterKeySlotC,
-    RedisModule_ClusterPropagateForSlotMigration,
-    RedisModule_ConfigGet,
-    RedisModule_ConfigGetBool,
-    RedisModule_ConfigGetEnum,
-    RedisModule_ConfigGetNumeric,
-    RedisModule_ConfigGetType,
-    RedisModule_ConfigSet,
-    RedisModule_ConfigSetBool,
-    RedisModule_ConfigSetEnum,
-    RedisModule_ConfigSetNumeric,
-    RedisModule_ConfigIteratorCreate,
-    RedisModule_ConfigIteratorNext,
-    RedisModule_ConfigIteratorRelease,
-    RedisModule_CreateStringFromLongDouble,
-    RedisModule_DefragRedisModuleDict,
-    RedisModule_GetSwapKeyMetadata,
-    RedisModule_RegisterDefragFunc2,
-    RedisModule_IsKeyInRam,
-    RedisModule_LoadDefaultConfigs,
-    RedisModule_LoadLongDouble,
-    RedisModule_ReplyWithLongDouble,
-    RedisModule_SaveLongDouble,
-    RedisModule_SetDataTypeExtensions,
-    RedisModule_SetSwapKeyMetadata,
-    RedisModule_ShardingGetKeySlot,
-    RedisModule_ShardingGetSlotRange,
-    RedisModule_StringToLongDouble,
-    RedisModule_SwapPrefetchKey,
-    RedisModule_UnsubscribeFromKeyspaceEvents,
-    Redis_OpenInvertedIndex,
-    TagIndex_OpenIndex,
-    TimeToLiveTable_VerifyDocAndField,
-    TimeToLiveTable_VerifyDocAndFieldMask,
-    TimeToLiveTable_VerifyDocAndWideFieldMask,
     fast_float_strtod,
-    isWithinRadius,
-    nu_bytenlen,
-    nu_strtransformnlen,
-    nu_tolower,
-    nu_utf8_write,
-    nu_writenstr,
-    openNumericKeysDict,
+    Obfuscate_Number,
+    Obfuscate_Text,
+    QueryError_SetWithUserDataFmt,
+    RSIndexResult_IterateOffsets,
+    sdscat,
+    sdscatfmt,
+    sdscatlen,
 }
