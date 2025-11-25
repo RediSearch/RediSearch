cf8f94ac9280f7a2207d6a95e6b377f9d12f053e
meiravgri <109056284+meiravgri@users.noreply.github.com>
2025-11-25 06:53:32 +0200

    Revert "[8.2] MOD-12444 [MOD-12628] run on cached containers (#7440)"
    This reverts commit 7745696a0a129887a9059c755f5af43683cc85d3.

--- .install/alpine_linux_3.sh
+++ .install/alpine_linux_3.sh
@@ -7,15 +7,13 @@ $MODE apk update
 $MODE apk add --no-cache build-base gcc g++ make linux-headers openblas-dev \
     xsimd curl wget git openssl openssl-dev \
     tar xz which rsync bsd-compat-headers clang clang17-libclang curl \
-    clang-static ncurses-dev llvm-dev bash
+    clang-static ncurses-dev llvm-dev
 
 # We must install Python via the package manager until
 # `uv` starts providing aarch64-musl builds.
 # See https://github.com/astral-sh/python-build-standalone/pull/569
 if [ "$(uname -m)" = "aarch64" ]; then
     $MODE apk add --no-cache python3 python3-dev py3-pip
-    # Needed before checkout
-    $MODE apk add --no-cache gcompat libstdc++ libgcc
 else
     # On x86_64, we need Python headers to build psutil@5.x.y from
     # source, since it only started providing wheels for musl
