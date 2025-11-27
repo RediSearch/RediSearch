5394519811af6a0cc10f61ea2f1bd790df110c49
meiravgri <meirav.grimberg@redis.com>
2025-11-27 13:20:16 +0000

    add io threads update to rqAsyncCb

--- src/coord/rmr/rq.c
+++ src/coord/rmr/rq.c
@@ -20,6 +20,7 @@
 #include "rmr.h"
 #include "coord/config.h"
 #include "rmutil/rm_assert.h"
+#include "info/global_stats.h"
 
 struct queueItem {
   void *privdata;
@@ -240,7 +241,9 @@ static void rqAsyncCb(uv_async_t *async) {
   MRWorkQueue *q = async->data;
   struct queueItem *req;
   while (NULL != (req = rqPop(q))) {
+    GlobalStats_UpdateActiveIoThreads(1);
     req->cb(req->privdata);
+    GlobalStats_UpdateActiveIoThreads(-1);
     rm_free(req);
   }
 }
