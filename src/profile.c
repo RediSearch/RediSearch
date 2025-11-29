18f68547253be40b2e089af6fc9c340a9dce6a46
meiravgri <109056284+meiravgri@users.noreply.github.com>
2025-11-29 23:45:32 +0200

    [MOD-12627] Add Debug Support for `FT.PROFILE` Command (#7510)
    * imp debug profile for SA:
    
    introduce in mocule.h: RSProfileCommandImp
    RSProfileCommand calls RSProfileCommandImp(isDebug = false) for regular execution
    ProfileCommandCommand_DebugWrapper mcalls it with isDebug=true and skips _FT.DEBUG
    
    introduce entrypoint for _FT.DEBUG FT.PROFILE in debug_commands:
    ProfileCommandCommand_DebugWrapper
    
    RSProfileCommandImp calls DEBUG_execCommandCommon is its debug
    
    _recursiveProfilePrint skips printing debug RP
    
    * pass is debug instead of extracting:
    
    module.h:
    replace declaration: DistAggregateCommand
    DistSearchCommand
    with Imp version that receives isDebug
    
    expose ProfileCommandHandlerImp
    
    align debug_commands
    
    introducr _FT.DEBUG _FT.PROFILE
    
    * add test for cluster
    
    * return res
    
    * augi fixes
    
    * fix spell check
    
    * fix for real
    
    * fix test
    
    * skip tests according to env
    
    * revrt test_profile changes
    
    * reove changes from internal_only
    
    (cherry picked from commit 00ca3cfa686f171ddc0af800e4daca275ed39219)

--- src/profile.c
+++ src/profile.c
@@ -76,6 +76,11 @@ static double _recursiveProfilePrint(RedisModule_Reply *reply, ResultProcessor *
   }
   double upstreamTime = _recursiveProfilePrint(reply, rp->upstream, printProfileClock);
 
+  if (rp->type > RP_MAX) {
+    RS_LOG_ASSERT_FMT(rp->type < RP_MAX_DEBUG, "RPType error, type: %d", rp->type);
+    return upstreamTime;
+  }
+
   // Array is filled backward in pair of [common, profile] result processors
   if (rp->type != RP_PROFILE) {
     RedisModule_Reply_Map(reply); // start of recursive map
@@ -106,15 +111,13 @@ static double _recursiveProfilePrint(RedisModule_Reply *reply, ResultProcessor *
         printProfileGILTime(rp->GILTime);
         break;
 
-      case RP_PROFILE:
-      case RP_MAX:
+      default:
         RS_ABORT("RPType error");
         break;
     }
 
     return upstreamTime;
   }
-
   double totalRPTime = rs_wall_clock_convert_ns_to_ms_d(RPProfile_GetClock(rp));
   if (printProfileClock) {
     printProfileTime(totalRPTime - upstreamTime);
