78a39ef03237f284b5455c42f96a9495ce537b52
lerman25 <lerman25@gmail.com>
2025-12-01 17:37:57 +0200

    test aligment again v2

--- tests/pytests/test_multithread.py
+++ tests/pytests/test_multithread.py
@@ -687,9 +687,9 @@ def testNameLoader(env: Env):
         if not env.isCluster():
             return profile_res[1][6][-1][1]
         if isinstance(profile_res[0], list):
-            return profile_res[2][6][-1][1] # 2.10 aggregate
+            return profile_res[2][7][-1][1] # 2.10 aggregate
         else:
-                return profile_res[-1][6][-1][1] # 2.10 search
+            return profile_res[-1][7][-1][1] # 2.10 search
 
     env.expect('FT.CREATE', 'idx', 'SCHEMA', 'sortable', 'TEXT', 'SORTABLE', 'UNF', 'not-sortable', 'TEXT').ok()
     with env.getClusterConnectionIfNeeded() as con:
