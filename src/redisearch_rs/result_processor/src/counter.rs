460e8e70104d40ee53ffeed7631cda755281cb44
Magnus Markling <magnus@markling.com>
2025-12-03 12:46:32 +0100

    minimize unsafe blocks

--- src/redisearch_rs/result_processor/src/counter.rs
+++ src/redisearch_rs/result_processor/src/counter.rs
@@ -47,14 +47,15 @@ impl ResultProcessor for Counter {
         // apparently (in a way enricozb cannot figure out) prevents the very last RPProfile from
         // appropriately counting, so this patches that by manually incrementing the counter.
         if upstream.ty() == ffi::ResultProcessorType_RP_PROFILE {
+            let end_proc_ptr = cx
+                .parent()
+                .expect("This processor has no parent.")
+                .endProc
+                .get();
+
             // Safety: We trust that the result processor parent structure (QueryProcessingCtx) was
             // constructed correctly, and thus has a valid pointer to the end processor.
-            let end_proc = unsafe {
-                *cx.parent()
-                    .expect("This processor has no parent.")
-                    .endProc
-                    .get()
-            };
+            let end_proc = unsafe { *end_proc_ptr };
 
             // Safety: If the previous (upstream) result processor is a profiling result processor,
             // then we are in profiling mode, and every other result processor is an RPProfile.
