39337794b63f0e446d061655abcee70e3e2fd12c
Dax Huiberts <daxhuiberts@gmail.com>
2025-12-04 11:52:36 +0100

    Remove stack based RSValue functions

--- src/value/value.h
+++ src/value/value.h
@@ -133,32 +133,6 @@ typedef struct RSValue {
 // Constructors
 ///////////////////////////////////////////////////////////////
 
-#ifndef __cplusplus
-/**
- * Creates a stack-allocated undefined RSValue.
- * The returned value is not allocated on the heap and should not be freed.
- * @return A stack-allocated RSValue of type RSValueType_Undef
- */
-RSValue RSValue_Undefined();
-
-/**
- * Creates a stack-allocated RSValue containing a number.
- * The returned value is not allocated on the heap and should not be freed.
- * @param n The numeric value to wrap
- * @return A stack-allocated RSValue of type RSValueType_Number
- */
-RSValue RSValue_Number(double n);
-
-/**
- * Creates a stack-allocated RSValue containing a malloc'd string.
- * The returned value itself is not heap-allocated, but takes ownership of the string.
- * @param str The malloc'd string to wrap (ownership is transferred)
- * @param len The length of the string
- * @return A stack-allocated RSValue of type RSValue_String with RSString_Malloc subtype
- */
-RSValue RSValue_String(char *str, uint32_t len);
-#endif
-
 /**
  * Creates a heap-allocated Undefined RSValue.
  * @return A pointer to a heap-allocated RSValue
