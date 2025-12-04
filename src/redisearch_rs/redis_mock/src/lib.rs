e0756492a214b1d3ee918074c80ab18dba8fa3e0
Guillaume Desmottes <guillaume@desmottes.be>
2025-12-04 14:44:52 +0100

    redis_mock: implement RedisModule_Strdup
    Will need it to test II iterator revalidation as the ffi C code
    called is using rm_strdup() which call RedisModule_Strdup.

--- src/redisearch_rs/redis_mock/src/lib.rs
+++ src/redisearch_rs/redis_mock/src/lib.rs
@@ -111,6 +111,7 @@ pub fn init_redis_module_mock() {
     unsafe { redis_module::raw::RedisModule_CreateString = Some(RedisModule_CreateString) };
     unsafe { redis_module::raw::RedisModule_StringPtrLen = Some(RedisModule_StringPtrLen) };
     unsafe { redis_module::raw::RedisModule_FreeString = Some(RedisModule_FreeString) };
+    unsafe { redis_module::raw::RedisModule_Strdup = Some(RedisModule_Strdup) };
 
     // register key methods
     unsafe { redis_module::raw::RedisModule_OpenKey = Some(RedisModule_OpenKey) };
