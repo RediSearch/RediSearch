5f08df7d6605d12873c2a2b295668947456c4cf9
meiravgri <109056284+meiravgri@users.noreply.github.com>
2025-12-09 17:29:45 +0200

    Skipping flaky svs tests on sanitizer (#7698)
    skip svs tests that are flaky on sanitizer:
    
    MOD-12901 test_hybrid_query_with_text_vamana

--- tests/pytests/test_vecsim.py
+++ tests/pytests/test_vecsim.py
@@ -871,6 +871,8 @@ def test_memory_info():
 # This test doesn't cover medium and large index scenarios to avoid extensive CI running time.
 # The heuristic is implemented in VectorSimilarity library in SVSIndex::preferAdHocSearch.
 # The test scenarios below demonstrate each heuristic path with detailed explanations.
+@skip(asan=True)
+# Skipping on sanitizer due to MOD-12901
 def test_hybrid_query_with_text_vamana():
     # Set high GC threshold so to eliminate sanitizer warnings from of false leaks from forks (MOD-6229)
     env = Env(moduleArgs='DEFAULT_DIALECT 2 FORK_GC_CLEAN_THRESHOLD 10000 WORKERS 8')
