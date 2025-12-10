5aed349af97ac60b3223300cce72ed562486b0b1
Guillaume Desmottes <guillaume@desmottes.be>
2025-12-10 13:24:16 +0100

    turn src/util/workers to static lib

---
+++ src/util/workers/workers.h
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2006-Present, Redis Ltd.
+ * All rights reserved.
+ *
+ * Licensed under your choice of the Redis Source Available License 2.0
+ * (RSALv2); or (b) the Server Side Public License v1 (SSPLv1); or (c) the
+ * GNU Affero General Public License v3 (AGPLv3).
+*/
+
+#pragma once
+
+#include "redismodule.h"
+#include "thpool/thpool.h"
+#include <stdbool.h>
+#include <stddef.h>
+#include <assert.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// create workers thread pool
+// returns REDISMODULE_OK if thread pool created, REDISMODULE_ERR otherwise
+int workersThreadPool_CreatePool(size_t worker_count);
+
+// Set the number of workers according to the configuration and server state
+// Should only be called from the main thread
+void workersThreadPool_SetNumWorkers(void);
+
+// return number of currently working threads
+size_t workersThreadPool_WorkingThreadCount(void);
+
+// Return the number of low priority jobs waiting to be executed.
+size_t workersThreadPool_LowPriorityPendingJobsCount(void);
+
+// Return the number of high priority jobs waiting to be executed.
+size_t workersThreadPool_HighPriorityPendingJobsCount(void);
+
+// Return the number of admin priority jobs waiting to be executed.
+size_t workersThreadPool_AdminPriorityPendingJobsCount(void);
+
+// return n_threads value.
+size_t workersThreadPool_NumThreads(void);
+
+// adds a task
+int workersThreadPool_AddWork(redisearch_thpool_proc, void *arg_p);
+
+// Wait until the workers job queue contains no more than <threshold> jobs.
+void workersThreadPool_Drain(RedisModuleCtx *ctx, size_t threshold);
+
+// Terminate threads, allows threads to exit gracefully (without deallocating).
+void workersThreadPool_Terminate(void);
+
+// Destroys thread pool, can be called on uninitialized threadpool.
+void workersThreadPool_Destroy(void);
+
+/// Configure the thread pool for operation start according to module configuration.
+/// @warning Should only be called from the main thread
+void workersThreadPool_OnEventStart(void);
+
+/** Configure the thread pool for operation end according to module configuration.
+ * @param wait - if true, the function will wait for all pending jobs to finish.
+ * @return REDISMODULE_ERR if `wait` is true but another event is already in progress, REDISMODULE_OK otherwise.
+ * @warning Should only be called from the main thread
+ */
+int workersThreadPool_OnEventEnd(bool wait);
+
+/********************************************* for debugging **********************************/
+
+int workerThreadPool_isPaused();
+
+int workersThreadPool_pause();
+
+int workersThreadPool_resume();
+
+thpool_stats workersThreadPool_getStats();
+
+void workersThreadPool_wait();
+
+#ifdef __cplusplus
+}
+#endif
--- src/util/workers/workers_pool.h
+++ src/util/workers/workers_pool.h
@@ -6,5 +6,5 @@
  * (RSALv2); or (b) the Server Side Public License v1 (SSPLv1); or (c) the
  * GNU Affero General Public License v3 (AGPLv3).
 */
-#pragma once
-#include "workers/workers.h"
+
+extern struct redisearch_thpool_t* _workers_thpool;
