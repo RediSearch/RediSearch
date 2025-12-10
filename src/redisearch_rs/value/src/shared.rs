29e1b66e3964e0a544ed239e72a86088a6b32d8d
Dax Huiberts <daxhuiberts@gmail.com>
2025-12-10 11:53:25 +0100

    Work with actual RsValue pointers in FFI

--- src/redisearch_rs/value/src/shared.rs
+++ src/redisearch_rs/value/src/shared.rs
@@ -21,10 +21,21 @@ use crate::{RsValue, Value};
 /// - A non-null pointer represents one clone of said `Arc`, and as such, as
 ///   long as the [`SharedRsValue`] lives and holds a non-null pointer, the Arc
 ///   is still valid.
-#[repr(C)]
 pub struct SharedRsValue {
     /// Pointer representing the `Arc<RsValue>`.
     ptr: *const RsValue,
+}
+
+impl SharedRsValue {
+    pub fn into_raw(self) -> *const RsValue {
+        let ptr = self.ptr;
+        std::mem::forget(self);
+        ptr
+    }
+
+    pub unsafe fn from_raw(ptr: *const RsValue) -> Self {
+        Self { ptr }
+    }
 }
 
 impl Default for SharedRsValue {
