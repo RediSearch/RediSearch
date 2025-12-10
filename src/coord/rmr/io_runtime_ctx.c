af08a31b683e478aac09e0b4a98563f2d76fec04
meiravgri <109056284+meiravgri@users.noreply.github.com>
2025-12-10 13:30:39 +0200

    [MOD-12069] [MOD-12790] introduce active_topology_update_threads (#7693)
    * introduce active_topology_update_threads
    
    * WIP!!!!!! test by augi (NEED TO FIX!!!)
    
    * test
    
    * fix names
    
    * fix
    
    * add basic pytest
    
    * free topology
    
    * rename uv search threads
    
    * rename active_topology_update_threads->uv_threads_running_topology_update
    
    (cherry picked from commit a6a99043460242e0d2e05fb6efd7ce7b37f83943)

--- src/coord/rmr/io_runtime_ctx.c
+++ src/coord/rmr/io_runtime_ctx.c
@@ -50,9 +50,9 @@ static void rqAsyncCb(uv_async_t *async) {
   }
   queueItem *req;
   while (NULL != (req = RQ_Pop(io_runtime_ctx->queue, &io_runtime_ctx->uv_runtime.async))) {
-    GlobalStats_UpdateActiveIoThreads(1);
+    GlobalStats_UpdateUvRunningQueries(1);
     req->cb(req->privdata);
-    GlobalStats_UpdateActiveIoThreads(-1);
+    GlobalStats_UpdateUvRunningQueries(-1);
     rm_free(req);
   }
 }
@@ -104,7 +104,9 @@ static void topologyAsyncCB(uv_async_t *async) {
     // will be the topology check. If the topology hasn't changed, the topology check will quickly
     // mark the event loop thread as ready again.
     io_runtime_ctx->uv_runtime.loop_th_ready = false;
+    GlobalStats_UpdateUvRunningTopoUpdate(1);
     task->cb(task->privdata);
+    GlobalStats_UpdateUvRunningTopoUpdate(-1);
     rm_free(task);
     // Finish this round of topology checks to give the topology connections a chance to connect.
     // Schedule connectivity check immediately with a 1ms repeat interval
