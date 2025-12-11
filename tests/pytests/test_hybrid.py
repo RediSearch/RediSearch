199a12d82be435b486f2bb92ace20bf3dee6485d
itzik.vaknin@redis.com <itzikvaknin6@gmail.com>
2025-12-11 14:14:09 +0200

    Refactor test_hybrid.py to ensure info collection and logs print occurs after test execution, even on failure

--- tests/pytests/test_hybrid.py
+++ tests/pytests/test_hybrid.py
@@ -121,13 +121,15 @@ class testHybridSearch:
             index_name = 'prefixed_idx'
         info = {'info before': {'ft.info': self.env.cmd('ft.info', index_name), 'info search': self.env.cmd('info', 'search')}}
 
-        logs = run_test_scenario(self.env, index_name, scenario, self.vector_blob, collect_logs=True)
-        info['info after'] = {'ft.info': self.env.cmd('ft.info', index_name), 'info search': self.env.cmd('info', 'search')}
-        if self.env.getNumberOfFailedAssertion() > 0:
-            from pprint import pprint
-            pprint(logs)
-            print("info:")
-            pprint(info)
+        try:
+            logs = run_test_scenario(self.env, index_name, scenario, self.vector_blob, collect_logs=True)
+        finally:
+            info['info after'] = {'ft.info': self.env.cmd('ft.info', index_name), 'info search': self.env.cmd('info', 'search')}
+            if self.env.getNumberOfFailedAssertion() > 0:
+                from pprint import pprint
+                pprint(logs)
+                print("info:")
+                pprint(info)
 
     def test_knn_custom_k(self):
         """Test hybrid search using KNN with custom k scenario"""
