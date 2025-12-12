591cd5b300f77a517aa7a566f753a7f38abdc079
Dax Huiberts <daxhuiberts@gmail.com>
2025-12-12 09:28:30 +0100

    Change get_ref in RSValueTrait/RSValueFFI to deep_ref through existing c FFI call

--- src/redisearch_rs/value/src/rs_value_ffi.rs
+++ src/redisearch_rs/value/src/rs_value_ffi.rs
@@ -36,7 +36,7 @@ where
     fn is_null(&self) -> bool;
 
     /// Gets a reference to the RSValue instance, if it is a reference type or None.
-    fn get_ref(&self) -> Option<&Self>;
+    fn deep_deref(&self) -> &Self;
 
     /// Gets the string slice of the RSValue instance, if it is a string type or None otherwise.
     fn as_str(&self) -> Option<&str>;
@@ -132,18 +132,9 @@ impl RSValueTrait for RSValueFFI {
         self.0.as_ptr() == unsafe { ffi::RSValue_NullStatic() }
     }
 
-    fn get_ref(&self) -> Option<&Self> {
-        // Safety: We assume a valid ptr is given by the C side
-        let p = unsafe { self.0.as_ref() };
-        if p._t() == ffi::RSValueType_RSValueType_Reference {
-            // Safety: We tested that the type is a reference, so we access it over the union safely.
-            let ref_ptr = unsafe { p.__bindgen_anon_1._ref };
-
-            // Safety: We assume that a valid pointer is given by the C side
-            Some(unsafe { &*(ref_ptr as *const RSValueFFI) })
-        } else {
-            None
-        }
+    fn deep_deref(&self) -> &Self {
+        let deref_ptr = unsafe { ffi::RSValue_Dereference(self.0.as_ptr()) };
+        unsafe { &*(deref_ptr as *const RSValueFFI) }
     }
 
     fn as_str(&self) -> Option<&str> {
--- src/redisearch_rs/value/src/test_utils.rs
+++ src/redisearch_rs/value/src/test_utils.rs
@@ -49,12 +49,12 @@ impl RSValueTrait for RSValueMock {
         matches!(self.0.as_ref(), RSValueMockInner::Null)
     }
 
-    fn get_ref(&self) -> Option<&Self> {
-        if let RSValueMockInner::Reference(boxed) = self.0.as_ref() {
-            Some(boxed)
-        } else {
-            None
+    fn deep_deref(&self) -> &Self {
+        let mut result = self;
+        while let RSValueMockInner::Reference(reference) = result.0.as_ref() {
+            result = reference;
         }
+        return result;
     }
 
     fn as_str(&self) -> Option<&str> {
