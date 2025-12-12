c0f310b7994e0afe88f29b3701c0c43e162f8e1b
Dax Huiberts <daxhuiberts@gmail.com>
2025-12-12 09:28:30 +0100

    Change get_ref in RSValueTrait/RSValueFFI to deep_ref through existing c FFI call

--- src/redisearch_rs/sorting_vector/tests/sorting_vector.rs
+++ src/redisearch_rs/sorting_vector/tests/sorting_vector.rs
@@ -39,7 +39,7 @@ fn test_insert() -> Result<(), IndexOutOfBounds> {
     assert_eq!(vector[0].get_type(), ffi::RSValueType_RSValueType_Number);
     assert_eq!(vector[1].as_str(), Some("abcdefg"));
     assert_eq!(vector[1].get_type(), ffi::RSValueType_RSValueType_String);
-    assert_eq!(vector[2].get_ref().unwrap().as_num(), Some(3.0));
+    assert_eq!(vector[2].deep_deref().as_num(), Some(3.0));
     assert_eq!(vector[3].as_str(), Some("hello world")); // we normalize --> lowercase
     assert!(vector[4].is_null());
 
