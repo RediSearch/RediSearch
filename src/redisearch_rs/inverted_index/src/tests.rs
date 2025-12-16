0f6c619d15b0451159339d3dca78c8d6f38c5e66
Joan Fontanals Martinez <jfontanalsmartinez@gmail.com>
2025-12-16 09:15:14 +0100

    fix fmt

--- src/redisearch_rs/inverted_index/src/tests.rs
+++ src/redisearch_rs/inverted_index/src/tests.rs
@@ -2009,7 +2009,7 @@ fn ii_apply_gc_entries_tracking_index() {
     );
 }
 // the memory hack below raises error in miri
-#[cfg(not(miri))] 
+#[cfg(not(miri))]
 #[test]
 fn test_refresh_buffer_pointers_after_reallocation() {
     let mut ii = InvertedIndex::<Dummy>::new(IndexFlags_Index_DocIdsOnly);
