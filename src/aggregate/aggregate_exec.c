7d552794406cddae65dae5b48072767135e4856a
itzik.vaknin@redis.com <itzikvaknin6@gmail.com>
2025-12-24 17:55:39 +0200

    Cursor comment

--- src/aggregate/aggregate_exec.c
+++ src/aggregate/aggregate_exec.c
@@ -1457,6 +1457,8 @@ int RSCursorProfileCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int ar
     // Notify the client that the query was aborted.
     RedisModule_ReplyWithError(ctx, "The index was dropped while the cursor was idle");
   } else {
+    QueryError status = QueryError_Default();
+    AREQ_QueryProcessingCtx(req)->err = &status;
     sendChunk_ReplyOnly_EmptyResults(ctx, req);
     IndexSpecRef_Release(execution_ref);
   }
