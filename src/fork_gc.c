692e14fa369df2c41bf1d577cbac92b0c6fee217
GuyAv46 <47632673+GuyAv46@users.noreply.github.com>
2025-12-28 12:04:33 +0200

    Refactor keys dict - [MOD-13151] (#7843)
    * mark legacy API as such
    
    * refactor spec's keysDict to hold char buffer
    
    * cleanups
    
    * make keysDict a dict for inverted indexes only (simplify value struct)
    
    * use bool
    
    * some comment fixes
    
    * address review comment
    
    * dead code cleanup

--- src/fork_gc.c
+++ src/fork_gc.c
@@ -589,13 +589,11 @@ static FGCError FGC_parentHandleTerms(ForkGC *gc) {
   if (InvertedIndex_NumDocs(idx) == 0) {
 
     // inverted index was cleaned entirely lets free it
-    RedisModuleString *termKey = fmtRedisTermKey(sctx, term, len);
-    size_t formatedTremLen;
-    const char *formatedTrem = RedisModule_StringPtrLen(termKey, &formatedTremLen);
     if (sctx->spec->keysDict) {
+      CharBuf termKey = {.buf = term, .len = len};
       // get memory before deleting the inverted index
       size_t inv_idx_size = InvertedIndex_MemUsage(idx);
-      if (dictDelete(sctx->spec->keysDict, termKey) == DICT_OK) {
+      if (dictDelete(sctx->spec->keysDict, &termKey) == DICT_OK) {
         info.bytes_freed += inv_idx_size;
       }
     }
@@ -607,7 +605,6 @@ static FGCError FGC_parentHandleTerms(ForkGC *gc) {
     }
     sctx->spec->stats.numTerms--;
     sctx->spec->stats.termsSize -= len;
-    RedisModule_FreeString(sctx->redisCtx, termKey);
     if (sctx->spec->suffix) {
       deleteSuffixTrie(sctx->spec->suffix, term, len);
     }
@@ -631,7 +628,6 @@ static FGCError FGC_parentHandleNumeric(ForkGC *gc) {
   size_t fieldNameLen;
   char *fieldName = NULL;
   const FieldSpec *fs = NULL;
-  RedisModuleString *keyName = NULL;
   uint64_t rtUniqueId;
   NumericRangeTree *rt = NULL;
   FGCError status = recvNumericTagHeader(gc, &fieldName, &fieldNameLen, &rtUniqueId);
@@ -735,7 +731,6 @@ static FGCError FGC_parentHandleTags(ForkGC *gc) {
   while (status == FGC_COLLECTED) {
     InvertedIndexGcDelta *delta = NULL;
     II_GCScanStats info = {0};
-    RedisModuleString *keyName = NULL;
     TagIndex *tagIdx = NULL;
     char *tagVal = NULL;
     size_t tagValLen;
