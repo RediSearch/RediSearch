b0bcb5a6aa9e5ff0bdcee175c5cb2be19789c02f
Raz Monsonego <74051729+raz-mon@users.noreply.github.com>
2025-12-30 16:07:12 +0200

    Change RDB API to start with lowercase

--- src/search_disk_api.h
+++ src/search_disk_api.h
@@ -30,8 +30,8 @@ typedef struct BasicDiskAPI {
   void (*close)(RedisSearchDisk *disk);
   RedisSearchDiskIndexSpec *(*openIndexSpec)(RedisSearchDisk *disk, const char *indexName, size_t indexNameLen, DocumentType type);
   void (*closeIndexSpec)(RedisSearchDiskIndexSpec *index);
-  void (*IndexSpecRdbSave)(RedisModuleIO *rdb, RedisSearchDiskIndexSpec *index);
-  u_int32_t (*IndexSpecRdbLoad)(RedisModuleIO *rdb, RedisSearchDiskIndexSpec *index);
+  void (*indexSpecRdbSave)(RedisModuleIO *rdb, RedisSearchDiskIndexSpec *index);
+  u_int32_t (*indexSpecRdbLoad)(RedisModuleIO *rdb, RedisSearchDiskIndexSpec *index);
 } BasicDiskAPI;
 
 typedef struct IndexDiskAPI {
