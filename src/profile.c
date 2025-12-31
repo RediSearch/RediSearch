04d9641e38d9f72f5d0b0c36e5b304e63b3fec33
Joan Fontanals Martinez <jfontanalsmartinez@gmail.com>
2025-12-31 17:34:31 +0100

    fix crash in profile

--- src/profile.c
+++ src/profile.c
@@ -55,9 +55,13 @@ void printInvIdxIt(RedisModule_Reply *reply, QueryIterator *root, ProfileCounter
       RedisModule_Reply_SimpleStringf(reply, "%g,%g - %g,%g", se[0], se[1], nw[0], nw[1]);
     }
   } else {
-    printProfileType("TEXT");
-    RSQueryTerm *term = IndexResult_QueryTermRef(root->current);
-    REPLY_KVSTR_SAFE("Term", term->str);
+    if (root && root->current) {
+      RSQueryTerm *term = IndexResult_QueryTermRef(root->current);
+      if (term != NULL) {
+        printProfileType("TEXT");
+        REPLY_KVSTR_SAFE("Term", term->str);
+      }
+    }
   }
 
   // print counter and clock
@@ -141,6 +145,8 @@ void Profile_Print(RedisModule_Reply *reply, void *ctx) {
   bool reachedMaxPrefixExpansions = ProfileWarnings_Has(&profileCtx->warnings, PROFILE_WARNING_TYPE_MAX_PREFIX_EXPANSIONS);
   bool bgScanOOM = ProfileWarnings_Has(&profileCtx->warnings, PROFILE_WARNING_TYPE_BG_SCAN_OOM);
   bool queryOOM = ProfileWarnings_Has(&profileCtx->warnings, PROFILE_WARNING_TYPE_QUERY_OOM);
+  bool asmTrimmingDelayTimeout = ProfileWarnings_Has(&profileCtx->warnings, PROFILE_WARNING_TYPE_ASM_INACCURATE_RESULTS);
+
   req->profileTotalTime += rs_wall_clock_elapsed_ns(&req->initClock);
   QueryProcessingCtx *qctx = AREQ_QueryProcessingCtx(req);
 
@@ -181,7 +187,7 @@ void Profile_Print(RedisModule_Reply *reply, void *ctx) {
   }
 
   // Print whether a warning was raised throughout command execution
-  bool warningRaised = bgScanOOM || queryOOM || timedout || reachedMaxPrefixExpansions;
+  bool warningRaised = bgScanOOM || queryOOM || timedout || reachedMaxPrefixExpansions || asmTrimmingDelayTimeout;
   RedisModule_ReplyKV_Array(reply, "Warning");
   if (!warningRaised) {
     RedisModule_Reply_SimpleString(reply, "None");
@@ -198,6 +204,9 @@ void Profile_Print(RedisModule_Reply *reply, void *ctx) {
     }
     if (reachedMaxPrefixExpansions) {
       RedisModule_Reply_SimpleString(reply, QUERY_WMAXPREFIXEXPANSIONS);
+    }
+    if (asmTrimmingDelayTimeout) {
+      RedisModule_Reply_SimpleString(reply, QUERY_ASM_INACCURATE_RESULTS);
     }
   }
   RedisModule_Reply_ArrayEnd(reply); // >warnings
