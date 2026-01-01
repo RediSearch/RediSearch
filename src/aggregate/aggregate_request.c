b06592c54c9ded2edfd4f21600955f7fce76682d
meiravgri <meirav.grimberg@redis.com>
2026-01-01 08:21:53 +0000

    push last

--- src/aggregate/aggregate_request.c
+++ src/aggregate/aggregate_request.c
@@ -1014,7 +1014,6 @@ AREQ *AREQ_New(void) {
   req->optimizer = QOptimizer_New();
   req->profile = Profile_PrintDefault;
   req->prefixesOffset = 0;
-  req->has_timedout = false;
   return req;
 }
 
