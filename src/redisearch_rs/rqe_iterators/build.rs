8104453c0330f3c983e400387f0aed4405cd43f1
LukeMathWalker <20745048+LukeMathWalker@users.noreply.github.com>
2026-01-08 14:16:44 +0100

    Use the newly created static library instead of the smaller ones

--- src/redisearch_rs/rqe_iterators/build.rs
+++ src/redisearch_rs/rqe_iterators/build.rs
@@ -11,15 +11,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Link the static libraries that contain our array functions
     #[cfg(feature = "unittest")]
     {
-        build_utils::link_static_libraries(&[
-            ("src/index_result", "index_result"),
-            ("src/iterators", "iterators"),
-            ("src/ttl_table", "ttl_table"),
-            ("src/util/arr", "arr"),
-            ("src/util/dict", "dict"),
-            ("src/util/mempool", "mempool"),
-            ("src/value", "value"),
-        ]);
+        build_utils::link_static_libraries(&[("src", "redisearch_c")]);
     }
 
     Ok(())
