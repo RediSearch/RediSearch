6eda5eb55b0b4af601d1bce5e6505477d0163cfa
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2026-01-13 16:56:36 +0100

    feat: Log the panic payload when the Rust code crashes (#7969)
    * feat: Log the panic payload when the Rust code crashes
    
    The crash report already includes a backtrace, thanks to RedisJSON, but
    it doesn't include a panic message.
    This PR adds a RediSearch-specific panic hook to capture the panic
    payload, as well as the precise location where the panic happened.
    
    It renames `tracing_module_ffi` to `module_init_ffi` as a way to group
    together initialization routines that must be run when the module is
    initialized.
    It also amends the color-detection function to ensure we don't use
    ANSI color codes if stderr is not a TTY.
    
    * Add Rust backtrace to the INFO section of RediSearch when printing for a crash
    
    * Test Rust crash report in flow tests
    
    * Fix header file
    
    * Remove stray import.

--- src/redisearch_rs/c_entrypoint/redisearch_rs/src/lib.rs
+++ src/redisearch_rs/c_entrypoint/redisearch_rs/src/lib.rs
@@ -17,9 +17,9 @@
 #[global_allocator]
 static REDIS_MODULE_ALLOCATOR: redis_module::alloc::RedisAlloc = redis_module::alloc::RedisAlloc;
 
+pub use module_init_ffi as tracing_redismodule;
 pub use result_processor_ffi as result_processor;
 pub use slots_tracker_ffi as slots_tracker;
-pub use tracing_redismodule_ffi as tracing_redismodule;
 pub use triemap_ffi as triemap;
 pub use types_ffi as types;
 pub use varint_ffi as varint;
