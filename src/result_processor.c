d002f12561317adf807201e29d971281040bc5ba
LukeMathWalker <20745048+LukeMathWalker@users.noreply.github.com>
2026-01-13 15:48:34 +0100

    Remove stray import.

--- src/result_processor.c
+++ src/result_processor.c
@@ -6,7 +6,6 @@
  * (RSALv2); or (b) the Server Side Public License v1 (SSPLv1); or (c) the
  * GNU Affero General Public License v3 (AGPLv3).
 */
-#include "4.0/redismodule.h"
 #include "aggregate/aggregate.h"
 #include "result_processor.h"
 #include "query.h"
