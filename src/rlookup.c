de1733fa51964e8c759b2adb0bef7cfbbe4eead3
nafraf <nafraf@gmail.com>
2026-01-14 15:20:01 -0500

    Using single line `else if`

--- src/rlookup.c
+++ src/rlookup.c
@@ -987,14 +987,12 @@ void RLookupRow_WriteFieldsFrom(const RLookupRow *srcRow, const RLookup *srcLook
     RLookupKey *dest_key = RLookup_FindKey(destLookup, src_key->name, src_key->name_len);
     if (!createMissingKeys) {
       RS_ASSERT(dest_key != NULL);  // Assumption: all source keys exist in destination
-    } else {
-      if (!dest_key) {
+    } else if (!dest_key) {
         // Key doesn't exist in destination - create it on demand.
         // This can happen with LOAD * where keys are created dynamically.
         // Inherit non-transient flags from source.
         uint32_t flags = src_key->flags & ~RLOOKUP_TRANSIENT_FLAGS;
         dest_key = RLookup_GetKey_WriteEx(destLookup, src_key->name, src_key->name_len, flags);
-      }
     }
     // Write fields to destination (increments refcount, shares ownership)
     RLookup_WriteKey(dest_key, destRow, value);
