b243d7c6d5ac98ed3ef1db0fde716d92b1ea027e
Dax Huiberts <daxhuiberts@gmail.com>
2026-01-16 11:51:25 +0100

    add licence headers

--- src/redisearch_rs/value/src/util.rs
+++ src/redisearch_rs/value/src/util.rs
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2006-Present, Redis Ltd.
+ * All rights reserved.
+ *
+ * Licensed under your choice of the Redis Source Available License 2.0
+ * (RSALv2); or (b) the Server Side Public License v1 (SSPLv1); or (c) the
+ * GNU Affero General Public License v3 (AGPLv3).
+*/
+
 use libc::snprintf;
 
 pub fn num_to_string_cstyle(float: f64, buf: &mut [u8]) -> i32 {
