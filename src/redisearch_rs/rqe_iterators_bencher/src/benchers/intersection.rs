1e73b2b719dad925c83e2af656ddc61355d68e4c
Magnus Markling <magnus@markling.com>
2026-01-17 17:08:30 +0100

    fix new occurances

--- src/redisearch_rs/rqe_iterators_bencher/src/benchers/intersection.rs
+++ src/redisearch_rs/rqe_iterators_bencher/src/benchers/intersection.rs
@@ -12,7 +12,7 @@
 //! Compares C and Rust implementations of the intersection iterator
 //! using SortedIdList as child iterators.
 
-use std::time::Duration;
+use std::{hint::black_box, time::Duration};
 
 use criterion::{
     BenchmarkGroup, Criterion,
@@ -135,7 +135,7 @@ impl Bencher {
                 || ffi::QueryIterator::new_intersection(&make_ids(), WEIGHT),
                 |it| {
                     while it.read() == IteratorStatus_ITERATOR_OK {
-                        criterion::black_box(it.current());
+                        black_box(it.current());
                     }
                     it.free();
                 },
@@ -149,7 +149,7 @@ impl Bencher {
                 || Intersection::new(ids_to_rust_children(make_ids())),
                 |it| {
                     while let Ok(Some(current)) = it.read() {
-                        criterion::black_box(current);
+                        black_box(current);
                     }
                 },
                 criterion::BatchSize::SmallInput,
@@ -168,7 +168,7 @@ impl Bencher {
                 || ffi::QueryIterator::new_intersection(&make_ids(), WEIGHT),
                 |it| {
                     while it.skip_to(it.last_doc_id() + STEP) == IteratorStatus_ITERATOR_OK {
-                        criterion::black_box(it.current());
+                        black_box(it.current());
                     }
                     it.free();
                 },
@@ -182,7 +182,7 @@ impl Bencher {
                 || Intersection::new(ids_to_rust_children(make_ids())),
                 |it| {
                     while let Ok(Some(current)) = it.skip_to(it.last_doc_id() + STEP) {
-                        criterion::black_box(current);
+                        black_box(current);
                     }
                 },
                 criterion::BatchSize::SmallInput,
