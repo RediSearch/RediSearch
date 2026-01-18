3f94035090a7c87d0946da38c82594f16780f572
jonathan keinan <jonathan.keinan@redis.com>
2026-01-18 10:01:04 +0200

    add how long the thread was running on an index
    (cherry picked from commit 3bb20364a65e873020c65a8ad51d3d528c4af9c1)

--- src/info/info_redis/info_redis.c
+++ src/info/info_redis/info_redis.c
@@ -370,6 +370,9 @@ void AddToInfo_CurrentThread(RedisModuleInfoCtx *ctx) {
       // This includes the index name, so no need to output it separately
       IndexInfoCrashSafe(spec, ctx, RSGlobalConfig.hideUserDataFromLog);
     }
+    // give us a sense of how long this thread was active on this index before we crashed
+    rs_wall_clock_ns_t duration = rs_wall_clock_elapsed_ns(&specInfo->runningTime);
+    RedisModule_InfoAddFieldULongLong(ctx, "run_time_ns", duration);
   }
 }
 
