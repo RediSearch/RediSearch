d2833e3b7a7c7096a17659f54ab521a829066f54
lerman25 <58445352+lerman25@users.noreply.github.com>
2026-01-19 18:57:04 +0200

    [8.2] [MOD-13432] Fix FULLTEXT field metric count (#8037) (#8083)
    [MOD-13432] Fix FULLTEXT field metric count ([[#8037](https://github.com/RediSearch/RediSearch/issues/8037)](https://github.com/RediSearch/RediSearch/issues/8037))
    
    * Fix metric full text metric counting
    
    * initialize fieldspec
    
    * Add missing include
    
    * remove redundant lines
    
    * test null fields
    
    * Don't count null text fields
    
    * reset counter
    
    * typo fix
    
    * remove redudant wrapper
    
    * Count terms indexed
    
    * remove extra line
    
    * test text indexing
    
    * fix test
    
    (cherry picked from commit 4e09df11613f0369318038f8012b0b3190ab7285)

--- src/info/global_stats.h
+++ src/info/global_stats.h
@@ -44,7 +44,7 @@ typedef struct {
   size_t numVectorFieldsHNSW;
   size_t numVectorFieldsSvsVamana;
   size_t numVectorFieldsSvsVamanaCompressed;
-  // Total number of documents indexed by each field type
+  // Total number of indexing operations by each field type, doc can be counted multiple times if it has multiple fields of the same type.
   size_t textTotalDocsIndexed;
   size_t tagTotalDocsIndexed;
   size_t numericTotalDocsIndexed;
@@ -169,7 +169,7 @@ void GlobalStats_UpdateUvRunningTopoUpdate(int toAdd);
 MultiThreadingStats GlobalStats_GetMultiThreadingStats();
 
 // Increase the number of documents indexed by the given field type by `toAdd`.
-void FieldsGlobalStats_UpdateFieldDocsIndexed(const FieldSpec *fs, int toAdd);
+void FieldsGlobalStats_UpdateFieldDocsIndexed(FieldType field_types, int toAdd);
 
 #ifdef __cplusplus
 }
