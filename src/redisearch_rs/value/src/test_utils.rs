b043f4c4325faf1e9c5586e1b641eed27f03bd4e
LukeMathWalker <20745048+LukeMathWalker@users.noreply.github.com>
2026-01-20 13:34:51 +0100

    Fix crashes

--- src/redisearch_rs/value/src/test_utils.rs
+++ src/redisearch_rs/value/src/test_utils.rs
@@ -8,7 +8,7 @@
 */
 
 use crate::RSValueTrait;
-use std::sync::Arc;
+use std::{borrow::Cow, mem::ManuallyDrop, sync::Arc};
 
 /// Mock implementation of `RSValue` for testing purposes.
 #[derive(Clone, Debug, PartialEq)]
@@ -50,12 +50,12 @@ impl RSValueTrait for RSValueMock {
         matches!(self.0.as_ref(), RSValueMockInner::Null)
     }
 
-    fn deep_deref(&self) -> &Self {
+    fn deep_deref(&self) -> ManuallyDrop<Cow<'_, Self>> {
         let mut result = self;
         while let RSValueMockInner::Reference(reference) = result.0.as_ref() {
             result = reference;
         }
-        return result;
+        return ManuallyDrop::new(Cow::Borrowed(result));
     }
 
     fn as_str_bytes(&self) -> Option<&[u8]> {
