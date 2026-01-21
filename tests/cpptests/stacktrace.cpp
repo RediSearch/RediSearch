6cbe3d011d8b0690efc65c178600daeedd41c654
meiravgri <meirav.grimberg@redis.com>
2026-01-21 15:11:31 +0000

    fix build

--- tests/cpptests/stacktrace.cpp
+++ tests/cpptests/stacktrace.cpp
@@ -6,9 +6,6 @@
  * The raw backtrace output can be decoded by the decode_stacktrace.sh script
  * which parses the addresses and calls addr2line.
  */
-
-#include "common.h"
-
 // execinfo.h (backtrace) is a glibc extension, not available on musl (Alpine)
 #if defined(__APPLE__) || (defined(__unix__) && defined(__GLIBC__))
 
