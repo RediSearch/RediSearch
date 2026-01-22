fd1f715a7af72e34035ce8bc404b2bf4723424d1
Magnus Markling <magnus@markling.com>
2026-01-22 10:30:41 +0100

    wip

--- src/redisearch_rs/c_entrypoint/result_processor_ffi/tests/counter.rs
+++ src/redisearch_rs/c_entrypoint/result_processor_ffi/tests/counter.rs
@@ -11,6 +11,11 @@
 
 use result_processor_ffi::counter::*;
 
+// Link both Rust-provided and C-provided symbols
+extern crate redisearch_rs;
+// Mock or stub the ones that aren't provided by the line above
+redis_mock::mock_or_stub_missing_redis_c_symbols!();
+
 #[test]
 fn rp_counter_new_returns_valid_pointer() {
     let counter = unsafe { RPCounter_New() };
--- src/redisearch_rs/c_entrypoint/rlookup_ffi/tests/row.rs
+++ src/redisearch_rs/c_entrypoint/rlookup_ffi/tests/row.rs
@@ -23,6 +23,11 @@ use rlookup_ffi::row::{
 use value::RSValueFFI;
 use value::RSValueTrait;
 
+// Link both Rust-provided and C-provided symbols
+extern crate redisearch_rs;
+// Mock or stub the ones that aren't provided by the line above
+redis_mock::mock_or_stub_missing_redis_c_symbols!();
+
 #[test]
 fn rlookuprow_move() {
     let mut lookup = RLookup::new();
