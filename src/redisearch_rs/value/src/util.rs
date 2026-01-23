d0fb1deeea72e601d7ca6e1fc2e8cfe1df29faef
Dax Huiberts <daxhuiberts@gmail.com>
2026-01-23 10:47:51 +0100

    Fix architecture specific issue

--- src/redisearch_rs/value/src/util.rs
+++ src/redisearch_rs/value/src/util.rs
@@ -1,11 +1,12 @@
 use libc::snprintf;
+use std::ffi::c_char;
 
 pub fn num_to_string_cstyle(float: f64, buf: &mut [u8]) -> i32 {
     let integer = float as i64;
     if integer as f64 == float {
         unsafe {
             snprintf(
-                buf.as_mut_ptr() as *mut i8,
+                buf.as_mut_ptr() as *mut c_char,
                 buf.len(),
                 c"%lld".as_ptr(),
                 integer,
@@ -14,7 +15,7 @@ pub fn num_to_string_cstyle(float: f64, buf: &mut [u8]) -> i32 {
     } else {
         unsafe {
             snprintf(
-                buf.as_mut_ptr() as *mut i8,
+                buf.as_mut_ptr() as *mut c_char,
                 buf.len(),
                 c"%.12g".as_ptr(),
                 float,
