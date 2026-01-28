2e9d527198eb05788d1b70c92a194940e3ac31eb
Dax Huiberts <daxhuiberts@gmail.com>
2026-01-28 11:55:50 +0100

    Add a expect_shared_value util helper to use in the setters

--- src/redisearch_rs/c_entrypoint/value_ffi/src/setters.rs
+++ src/redisearch_rs/c_entrypoint/value_ffi/src/setters.rs
@@ -1,11 +1,10 @@
+use crate::util::expect_shared_value;
 use std::ffi::c_double;
-use std::mem::ManuallyDrop;
-use value::{RsValue, shared::SharedRsValue};
+use value::RsValue;
 
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn RSValue_SetNumber(value: *mut RsValue, n: c_double) {
-    let shared_value = unsafe { SharedRsValue::from_raw(value) };
-    let mut shared_value = ManuallyDrop::new(shared_value);
+    let mut shared_value = unsafe { expect_shared_value(value) };
 
     let new_value = RsValue::Number(n);
     unsafe { shared_value.set_value(new_value) };
@@ -13,8 +12,7 @@ pub unsafe extern "C" fn RSValue_SetNumber(value: *mut RsValue, n: c_double) {
 
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn RSValue_SetNull(value: *mut RsValue) {
-    let shared_value = unsafe { SharedRsValue::from_raw(value) };
-    let mut shared_value = ManuallyDrop::new(shared_value);
+    let mut shared_value = unsafe { expect_shared_value(value) };
 
     unsafe { shared_value.set_value(RsValue::Null) };
 }
