1f3466ddc347c40d4fd45e87290327d8ea10515f
Dax Huiberts <daxhuiberts@gmail.com>
2026-02-05 17:47:13 +0100

    SharedRsValue related RSValue_ FFI functions

--- src/redisearch_rs/c_entrypoint/value_ffi/src/shared.rs
+++ src/redisearch_rs/c_entrypoint/value_ffi/src/shared.rs
@@ -7,6 +7,8 @@
  * GNU Affero General Public License v3 (AGPLv3).
 */
 
+use std::{mem::ManuallyDrop, ops::Deref};
+
 use value::{RsValue, SharedRsValue};
 
 /// Decrement the reference count of the provided [`RsValue`] object. If this was
@@ -20,4 +22,74 @@ use value::{RsValue, SharedRsValue};
 pub unsafe extern "C" fn RSValue_DecrRef(value: *const RsValue) {
     // Safety: ensured by caller (1.)
     let _ = unsafe { SharedRsValue::from_raw(value) };
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn RSValue_Dereference(value: *const RsValue) -> *mut RsValue {
+    let shared_value = unsafe { SharedRsValue::from_raw(value) };
+    let shared_value = ManuallyDrop::new(shared_value);
+    let dereferenced_value = shared_value.fully_dereferenced();
+    dereferenced_value.as_ptr() as *mut _
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn RSValue_DereferenceRefAndTrio(value: *const RsValue) -> *mut RsValue {
+    let shared_value = unsafe { SharedRsValue::from_raw(value) };
+    let shared_value = ManuallyDrop::new(shared_value);
+    let dereferenced_value = shared_value.value().fully_dereferenced_ref_and_trio();
+    let value = unsafe { crate::util::expect_shared_value(dereferenced_value) };
+    value.as_ptr() as *mut _
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn RSValue_Clear(value: *const RsValue) {
+    let shared_value = unsafe { SharedRsValue::from_raw(value) };
+    let mut shared_value = ManuallyDrop::new(shared_value);
+    shared_value.set_value(RsValue::Undefined);
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn RSValue_IncrRef(value: *const RsValue) -> *mut RsValue {
+    let shared_value = unsafe { SharedRsValue::from_raw(value) };
+    let shared_value = ManuallyDrop::new(shared_value);
+    <SharedRsValue as Clone>::clone(&shared_value).into_raw() as *mut _
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn RSValue_MakeReference(dst: *const RsValue, src: *const RsValue) {
+    let shared_dst = unsafe { SharedRsValue::from_raw(dst) };
+    let mut shared_dst = ManuallyDrop::new(shared_dst);
+    let shared_src = unsafe { SharedRsValue::from_raw(src) };
+    let shared_src = ManuallyDrop::new(shared_src);
+
+    let new_value = RsValue::Ref(shared_src.deref().clone());
+    shared_dst.set_value(new_value);
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn RSValue_MakeOwnReference(dst: *const RsValue, src: *const RsValue) {
+    let shared_dst = unsafe { SharedRsValue::from_raw(dst) };
+    let mut shared_dst = ManuallyDrop::new(shared_dst);
+    let shared_src = unsafe { SharedRsValue::from_raw(src) };
+
+    let new_value = RsValue::Ref(shared_src);
+    shared_dst.set_value(new_value);
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn RSValue_Replace(dstpp: *mut *mut RsValue, src: *const RsValue) {
+    let _shared_dst = unsafe { SharedRsValue::from_raw(*dstpp) };
+    let shared_src = unsafe { SharedRsValue::from_raw(src) };
+    let shared_src = ManuallyDrop::new(shared_src);
+    let shared_src_clone = shared_src.deref().clone();
+    unsafe {
+        *dstpp = shared_src_clone.into_raw() as *mut _;
+    }
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn RSValue_Refcount(value: *const RsValue) -> u16 {
+    let shared_value = unsafe { SharedRsValue::from_raw(value) };
+    let shared_value = ManuallyDrop::new(shared_value);
+    SharedRsValue::refcount(&shared_value) as u16
 }
