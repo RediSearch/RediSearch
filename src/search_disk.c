a9aaa3e57e9580b9483854ca1e3015b4a3a32098
itzik.vaknin@redis.com <itzikvaknin6@gmail.com>
2026-02-08 10:59:22 +0200

    Rename GC functions in BasicDiskAPI to clarify their purpose as thread management functions

--- src/search_disk.c
+++ src/search_disk.c
@@ -43,14 +43,14 @@ bool SearchDisk_Initialize(RedisModuleCtx *ctx) {
 
   disk_db = disk->basic.open(ctx);
   if (disk_db) {
-    disk->basic.startGC();
+    disk->basic.startGCThread();
   }
   return disk_db != NULL;
 }
 
 void SearchDisk_Close() {
   if (disk && disk_db) {
-    disk->basic.stopGC();
+    disk->basic.stopGCThread();
     disk->basic.close(disk_db);
     disk_db = NULL;
   }
