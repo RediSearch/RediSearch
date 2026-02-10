8b5969f99c5e8553c00129b6c64235c7ff8644dd
Magnus Markling <magnus@markling.com>
2026-02-10 05:28:08 +0100

    rename run_cbinden to run_cbindgen

--- src/redisearch_rs/c_entrypoint/result_processor_ffi/build.rs
+++ src/redisearch_rs/c_entrypoint/result_processor_ffi/build.rs
@@ -7,8 +7,8 @@
  * GNU Affero General Public License v3 (AGPLv3).
 */
 
-use build_utils::run_cbinden;
+use build_utils::run_cbindgen;
 
 fn main() {
-    run_cbinden("../../headers/result_processor_rs.h").unwrap();
+    run_cbindgen("../../headers/result_processor_rs.h").unwrap();
 }
