be4e4befefcbfe6e82e184070edd3720baca9cf6
Magnus Markling <magnus@markling.com>
2026-02-11 14:44:03 +0100

    disable things

--- src/redisearch_rs/c_entrypoint/rlookup_ffi/tests/row.rs
+++ src/redisearch_rs/c_entrypoint/rlookup_ffi/tests/row.rs
@@ -9,10 +9,10 @@
 
 #![allow(clippy::missing_safety_doc, clippy::undocumented_unsafe_blocks)]
 
-// Link both Rust-provided and C-provided symbols
-extern crate redisearch_rs;
-// Mock or stub the ones that aren't provided by the line above
-redis_mock::mock_or_stub_missing_redis_c_symbols!();
+// // Link both Rust-provided and C-provided symbols
+// extern crate redisearch_rs;
+// // Mock or stub the ones that aren't provided by the line above
+// redis_mock::mock_or_stub_missing_redis_c_symbols!();
 
 use std::ffi::CString;
 use std::ptr::NonNull;
