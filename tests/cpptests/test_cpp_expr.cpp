bbf95bc3b6e72935e74c846a08162806f24aa164
oshadmi <omer.shadmi@redis.com>
2026-02-15 19:22:40 +0200

    Add missing prefix SEARCH_EXPR in expected cpp tests

--- tests/cpptests/test_cpp_expr.cpp
+++ tests/cpptests/test_cpp_expr.cpp
@@ -265,15 +265,15 @@ TEST_F(ExprTest, testFunction) {
 
   ctx.assign("banana(1, 2, 3)");
   EXPECT_TRUE(!ctx) << "Parsed invalid function";
-  EXPECT_STREQ(ctx.error(), "Unknown function name 'banana'");
+  EXPECT_STREQ(ctx.error(), "SEARCH_EXPR Unknown function name 'banana'");
 
   ctx.assign("!banana(1, 2, 3)");
   EXPECT_TRUE(!ctx) << "Parsed invalid function";
-  EXPECT_STREQ(ctx.error(), "Unknown function name 'banana'");
+  EXPECT_STREQ(ctx.error(), "SEARCH_EXPR Unknown function name 'banana'");
 
   ctx.assign("!!banana(1, 2, 3)");
   EXPECT_TRUE(!ctx) << "Parsed invalid function";
-  EXPECT_STREQ(ctx.error(), "Unknown function name 'banana'");
+  EXPECT_STREQ(ctx.error(), "SEARCH_EXPR Unknown function name 'banana'");
 }
 
 struct EvalResult {
