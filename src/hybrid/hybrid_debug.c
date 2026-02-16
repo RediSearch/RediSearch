c684e0e54601178e0411ee1dc5ae32c95239738d
nafraf <nafraf@users.noreply.github.com>
2026-02-16 08:47:17 -0500

    [8.4] MOD-13561: Support HYBRID in FT.PROFILE (#8060) (#8261)
    (cherry picked from commit 84e873a0d0d543d6e359ea88470a348bee9f53ae)

--- src/hybrid/hybrid_debug.c
+++ src/hybrid/hybrid_debug.c
@@ -209,7 +209,7 @@ static HybridRequest_Debug* HybridRequest_Debug_New(RedisModuleCtx *ctx, RedisMo
   cmd.tailPlan = &hreq->tailPipeline->ap;
   cmd.reqConfig = &hreq->reqConfig;
 
-  int rc = parseHybridCommand(ctx, &ac, sctx, &cmd, status, false);
+  int rc = parseHybridCommand(ctx, &ac, sctx, &cmd, status, false, EXEC_NO_FLAGS);
   if (rc != REDISMODULE_OK) {
     if (hybridParams.scoringCtx) {
       HybridScoringContext_Free(hybridParams.scoringCtx);
