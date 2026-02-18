fbefa20c2f7eb8e5cce65d6a059555c487a39716
nafraf <nafraf@gmail.com>
2026-02-18 16:11:20 -0500

    Refactor Rust installation process: move install script to test_deps and update common_installations.sh

--- .install/test_deps/common_installations.sh
+++ .install/test_deps/common_installations.sh
@@ -3,5 +3,9 @@ set -e
 OS_TYPE=$(uname -s)
 MODE=$1 # whether to install using sudo or not
 
+# Check if rustup is installed, if not, install it
+if ! command -v rustup &> /dev/null; then
+    ./.install/install_rust.sh
+fi
 ./.install/test_deps/install_rust_deps.sh
 ./.install/test_deps/install_python_deps.sh
