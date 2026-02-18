32d2e8a24831fb649c7547c6fa6f1cdb78f17029
Dax Huiberts <daxhuiberts@gmail.com>
2026-02-18 10:45:06 +0100

    Fix formatting

--- src/redisearch_rs/value/src/redis_string.rs
+++ src/redisearch_rs/value/src/redis_string.rs
@@ -8,7 +8,7 @@
 */
 
 use ffi::context::redisearch_module_context;
-use ffi::{RedisModuleString, RedisModule_FreeString, RedisModule_StringPtrLen};
+use ffi::{RedisModule_FreeString, RedisModule_StringPtrLen, RedisModuleString};
 use std::ffi::c_char;
 use std::fmt;
 use std::mem::MaybeUninit;
