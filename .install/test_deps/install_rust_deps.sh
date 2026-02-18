ffb85357f4bb4cb1ebfadff85d1fabd3e8d6f764
nafraf <nafraf@gmail.com>
2026-02-18 12:25:17 -0500

    Add installation scripts for Python and Rust dependencies, and update RedisJSON setup to use pinned nightly version

---
+++ .install/test_deps/install_rust_deps.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -e
+OS_TYPE=$(uname -s)
+MODE=$1 # whether to install using sudo or not
+
+# retrieve nightly version
+NIGHTLY_VERSION=$(cat .rust-nightly)
+# --allow-downgrade:
+#   Allow `rustup` to install an older `nightly` if the latest one
+#   is missing one of the components we need.
+# llvm-tools-preview:
+#   Required by `cargo-llvm-cov` for test coverage
+# miri:
+#   Required to run `cargo miri test` for UB detection
+# rust-src:
+#   Required to build RedisJSON with address sanitizer
+rustup toolchain install $NIGHTLY_VERSION \
+    --allow-downgrade \
+    --component llvm-tools-preview \
+    --component miri \
+    --component rust-src
+
+# Tool required to compute test coverage for Rust code
+cargo install cargo-llvm-cov --locked
+# Our preferred test runner, instead of the default `cargo test`
+cargo install cargo-nextest --locked
+# Tool to aggressively unify the feature sets of our dependencies,
+# thus improving the cacheability of our builds
+# See https://docs.rs/cargo-hakari/latest/cargo_hakari/about/
+cargo install cargo-hakari --locked
+# Make sure `miri` is fully operational before running tests with it.
+# See https://github.com/rust-lang/miri/blob/master/README.md#running-miri-on-ci
+# for more details.
+cargo +$NIGHTLY_VERSION miri setup
