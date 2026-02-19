837ec3b6b1e53ce44eb7cdee919d0a93b14ac8c7
ofiryanai <ofir.yanai@redis.com>
2026-02-19 13:08:53 +0200

    Revert "fix: Revert gcc-13 and fix VectorSimilarity SVE benchmark bug"
    This reverts commit f08127cee381a6d6410fd4a7d4bec39c40c90f70.

--- .install/ubuntu_20.04.sh
+++ .install/ubuntu_20.04.sh
@@ -10,7 +10,8 @@ $MODE add-apt-repository ppa:ubuntu-toolchain-r/test -y
 $MODE add-apt-repository ppa:deadsnakes/ppa -y
 $MODE apt update -yqq
 
+# Install gcc-13 for better ARM64 SVE2 support (required for VectorSimilarity SVS on aarch64)
 $MODE apt install -yqq wget make clang-format gcc lcov git openssl libssl-dev \
-    unzip rsync build-essential gcc-11 g++-11 curl libclang-dev gdb
+    unzip rsync build-essential gcc-13 g++-13 curl libclang-dev gdb
 
-$MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 60 --slave /usr/bin/g++ g++ /usr/bin/g++-11
+$MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-13 60 --slave /usr/bin/g++ g++ /usr/bin/g++-13
