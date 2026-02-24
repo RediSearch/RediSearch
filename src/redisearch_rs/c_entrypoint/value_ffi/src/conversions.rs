d45d719660b9ff789182ea5835432e70a2d1afe0
Dax Huiberts <daxhuiberts@gmail.com>
2026-02-24 14:29:21 +0100

    Implement RSValue_ToString

--- src/redisearch_rs/c_entrypoint/value_ffi/src/conversions.rs
+++ src/redisearch_rs/c_entrypoint/value_ffi/src/conversions.rs
@@ -2,7 +2,7 @@ use crate::util::{expect_shared_value, expect_value};
 use libc::size_t;
 use std::ffi::{CString, c_char, c_double, c_int};
 use value::util::{num_to_str, str_to_float};
-use value::{RsString, RsValue};
+use value::{RsString, RsValue, SharedRsValue};
 
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn RSValue_ToNumber(value: *const RsValue, d: *mut c_double) -> c_int {
@@ -72,16 +72,30 @@ pub unsafe extern "C" fn RSValue_ToString(dst: *mut RsValue, value: *const RsVal
     let value = unsafe { expect_value(value) };
     let value = value.fully_dereferenced_ref_and_trio();
 
-    match value {
+    let new_value = match value {
+        RsValue::String(_) => {
+            let shared_value = unsafe { expect_shared_value(value) };
+            RsValue::Ref(SharedRsValue::clone(&shared_value))
+        }
+        RsValue::RedisString(string) => {
+            let (ptr, len) = string.as_ptr_len();
+            let len = len.try_into().expect("len > u32::MAX");
+            let string = unsafe { RsString::borrowed_string(ptr, len) };
+            RsValue::String(string)
+        }
         RsValue::Number(number) => {
             let mut buf = [0u8; 32];
             let len = num_to_str(*number, &mut buf);
             let cstring = CString::new(&buf[..(len as usize)]).unwrap();
-            let new_val = RsValue::String(RsString::cstring(cstring));
-            dst.set_value(new_val);
+            RsValue::String(RsString::cstring(cstring))
         }
-        _ => unimplemented!("RSValue_ToString for type 'unknown'"),
-    }
+        _ => {
+            let string = unsafe { RsString::borrowed_string(b"\0".as_ptr().cast(), 0) };
+            RsValue::String(string)
+        }
+    };
+
+    dst.set_value(new_value);
 }
 
 #[unsafe(no_mangle)]
