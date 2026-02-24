89b1ad364fc34ba63e8b0274f937728c3a31c6e4
Joan Fontanals Martinez <jfontanalsmartinez@gmail.com>
2026-02-24 12:15:00 +0100

    align with master implementation

--- src/fork_gc.c
+++ src/fork_gc.c
@@ -33,6 +33,7 @@
 #include "info/info_redis/threads/current_thread.h"
 #include "obfuscation/obfuscation_api.h"
 #include "obfuscation/hidden.h"
+#include "util/redis_mem_info.h"
 
 #define GC_WRITERFD 1
 #define GC_READERFD 0
@@ -1241,24 +1242,24 @@ FGCError FGC_parentHandleFromChild(ForkGC *gc) {
 
 // GIL must be held before calling this function
 static inline bool isOutOfMemory(RedisModuleCtx *ctx) {
-  #define MIN_NOT_0(a,b) (((a)&&(b))?MIN((a),(b)):MAX((a),(b)))
   // Check if we are a slave/replica
   bool isSlave = RedisModule_GetContextFlags(ctx) & REDISMODULE_CTX_FLAGS_SLAVE;
-  RedisModuleServerInfoData *info = RedisModule_GetServerInfo(ctx, "memory");
+  float used_memory_ratio = 0;
+  if (!isSlave) {
+    // On master, use the original unified logic
+    used_memory_ratio = RedisMemory_GetUsedMemoryRatioUnified(ctx);
+  } else {
+    // On slaves, only consider max_process_mem
+    RedisModuleServerInfoData *info = RedisModule_GetServerInfo(ctx, "memory");
+    size_t used_memory = RedisModule_ServerInfoGetFieldUnsigned(info, "used_memory", NULL);
+    size_t max_process_mem = RedisModule_ServerInfoGetFieldUnsigned(info, "max_process_mem", NULL);
+    RedisModule_FreeServerInfo(ctx, info);
 
-  size_t maxmemory = isSlave ? 0 : RedisModule_ServerInfoGetFieldUnsigned(info, "maxmemory", NULL);
-  size_t max_process_mem = RedisModule_ServerInfoGetFieldUnsigned(info, "max_process_mem", NULL); // Enterprise limit
-  maxmemory = MIN_NOT_0(maxmemory, max_process_mem);
+    used_memory_ratio = max_process_mem ? (float)used_memory / (float)max_process_mem : 0;
+  }
+  RedisModule_Log(ctx, "debug", "ForkGC - used memory ratio: %f", used_memory_ratio);
 
-  size_t total_system_memory = RedisModule_ServerInfoGetFieldUnsigned(info, "total_system_memory", NULL);
-  maxmemory = MIN_NOT_0(maxmemory, total_system_memory);
-
-  size_t used_memory = RedisModule_ServerInfoGetFieldUnsigned(info, "used_memory", NULL);
-
-  RedisModule_FreeServerInfo(ctx, info);
-
-  RedisModule_Log(ctx, "debug", "ForkGC - checking memory: maxmemory=%zu, used_memory=%zu", maxmemory, used_memory);
-  return used_memory > maxmemory;
+  return used_memory_ratio > 1;
 }
 
 static int periodicCb(void *privdata) {
