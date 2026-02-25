756871481ed32c09eb63d73d17c708b106e5c49f
Guillaume Desmottes <guillaume@desmottes.be>
2026-02-25 09:28:00 +0100

    split inverted_index.rs into submodule files
    Cleaner structure and will reduce the risk of conflicts when working on
    multiple iterators in parallel.

---
+++ src/redisearch_rs/rqe_iterators/src/inverted_index/mod.rs
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2006-Present, Redis Ltd.
+ * All rights reserved.
+ *
+ * Licensed under your choice of the Redis Source Available License 2.0
+ * (RSALv2); or (b) the Server Side Public License v1 (SSPLv1); or (c) the
+ * GNU Affero General Public License v3 (AGPLv3).
+*/
+
+//! Supporting types for [`Numeric`], [`Term`], and [`Wildcard`].
+
+mod core;
+mod numeric;
+mod term;
+mod wildcard;
+
+pub use numeric::Numeric;
+pub use term::Term;
+pub use wildcard::Wildcard;
