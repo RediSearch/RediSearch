fa7baaf3665eedc5ecd7c3edf4c597aa34816ea9
Dax Huiberts <daxhuiberts@gmail.com>
2026-02-26 16:07:27 +0100

    Add tests

---
+++ src/redisearch_rs/value/tests/integration/main.rs
@@ -0,0 +1,7 @@
+// Link both Rust-provided and C-provided symbols
+extern crate redisearch_rs;
+// Mock or stub the ones that aren't provided by the line above
+redis_mock::mock_or_stub_missing_redis_c_symbols!();
+
+mod debug;
+mod rs_string;
