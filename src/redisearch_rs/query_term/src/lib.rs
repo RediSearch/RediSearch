2903c27efaf8e04b2fb7288315640b1c727d4df3
chesedo <pieter@chesedo.me>
2026-03-05 09:25:22 +0200

    Add simple interview instructions

--- src/redisearch_rs/query_term/src/lib.rs
+++ src/redisearch_rs/query_term/src/lib.rs
@@ -6,3 +6,6 @@
  * (RSALv2); or (b) the Server Side Public License v1 (SSPLv1); or (c) the
  * GNU Affero General Public License v3 (AGPLv3).
 */
+
+// Port `RSQueryTerm` from `redisearch.h:215` here. Only focus on making the C code compile using
+// `build.sh` and ignore any other Rust changes for now.
