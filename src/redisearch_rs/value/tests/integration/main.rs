b3f44e7122b473b95cea6c423077981fed7f7943
Dax Huiberts <daxhuiberts@gmail.com>
2026-03-06 09:58:53 +0100

    Add license headers

--- src/redisearch_rs/value/tests/integration/main.rs
+++ src/redisearch_rs/value/tests/integration/main.rs
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2006-Present, Redis Ltd.
+ * All rights reserved.
+ *
+ * Licensed under your choice of the Redis Source Available License 2.0
+ * (RSALv2); or (b) the Server Side Public License v1 (SSPLv1); or (c) the
+ * GNU Affero General Public License v3 (AGPLv3).
+*/
+
 // Link both Rust-provided and C-provided symbols
 extern crate redisearch_rs;
 // Mock or stub the ones that aren't provided by the line above
