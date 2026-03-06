a488df1df0f15b897b603c6879d9c5bae47e9af6
Dax Huiberts <daxhuiberts@gmail.com>
2026-03-06 11:41:30 +0100

    Move compare function to value trait

--- src/redisearch_rs/c_entrypoint/value_ffi/src/comparisons.rs
+++ src/redisearch_rs/c_entrypoint/value_ffi/src/comparisons.rs
@@ -1,8 +1,8 @@
 use crate::util::expect_value;
 use query_error::{QueryError, QueryErrorCode};
 use std::mem::ManuallyDrop;
-use std::ops::Deref;
 use std::{cmp::Ordering, ffi::c_int};
+use value::comparison::{CompareError, compare};
 use value::{RsValue, SharedRsValue};
 
 #[unsafe(no_mangle)]
@@ -55,6 +55,7 @@ pub unsafe extern "C" fn RSValue_Equal(
 
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn RSValue_BoolTest(value: *const RsValue) -> c_int {
+    let value = unsafe { expect_value(value) };
     let shared_value = unsafe { SharedRsValue::from_raw(value) };
     let shared_value = ManuallyDrop::new(shared_value);
     let value = shared_value.value().fully_dereferenced_ref();
@@ -68,86 +69,4 @@ pub unsafe extern "C" fn RSValue_BoolTest(value: *const RsValue) -> c_int {
     };
 
     result as c_int
-}
-
-#[derive(Debug)]
-enum CompareError {
-    NaNNumber,
-    NoNumberToStringFallback,
-    MapComparison,
-    IncompatibleTypes,
-}
-
-/// Compare two values.
-/// If `num_to_str_cmp_fallback` is true, falls back to string comparison when number conversion fails.
-/// If `num_to_str_cmp_fallback` is false, returns CompareError::NoNumberToStringFallback when number conversion fails.
-fn compare(
-    v1: &RsValue,
-    v2: &RsValue,
-    num_to_str_cmp_fallback: bool,
-) -> Result<Ordering, CompareError> {
-    match (v1, v2) {
-        (RsValue::Null, RsValue::Null) => Ok(Ordering::Equal),
-        (RsValue::Null, _) => Ok(Ordering::Less),
-        (_, RsValue::Null) => Ok(Ordering::Greater),
-        (RsValue::Number(n1), RsValue::Number(n2)) => {
-            n1.partial_cmp(n2).ok_or(CompareError::NaNNumber)
-        }
-        (RsValue::String(s1), RsValue::String(s2)) => Ok(s1.as_bytes().cmp(s2.as_bytes())),
-        (RsValue::Trio(t1), RsValue::Trio(t2)) => compare(
-            t1.left().value(),
-            t2.left().value(),
-            num_to_str_cmp_fallback,
-        ),
-        (RsValue::Array(a1), RsValue::Array(a2)) => {
-            for (i1, i2) in a1.iter().zip(a2.deref()) {
-                let cmp = compare(i1.value(), i2.value(), num_to_str_cmp_fallback)?;
-                if cmp != Ordering::Equal {
-                    return Ok(cmp);
-                }
-            }
-            Ok(a1.len().cmp(&a2.len()))
-        }
-        (RsValue::Map(_), RsValue::Map(_)) => Err(CompareError::MapComparison),
-        (RsValue::Number(n1), RsValue::String(s2)) => {
-            compare_number_to_string(*n1, s2.as_bytes(), num_to_str_cmp_fallback)
-        }
-        (RsValue::Number(n1), RsValue::RedisString(s2)) => {
-            compare_number_to_string(*n1, s2.as_bytes(), num_to_str_cmp_fallback)
-        }
-        (RsValue::String(s1), RsValue::Number(n2)) => {
-            compare_number_to_string(*n2, s1.as_bytes(), num_to_str_cmp_fallback)
-                .map(Ordering::reverse)
-        }
-        (RsValue::RedisString(s1), RsValue::Number(n2)) => {
-            compare_number_to_string(*n2, s1.as_bytes(), num_to_str_cmp_fallback)
-                .map(Ordering::reverse)
-        }
-        (RsValue::RedisString(rs1), RsValue::RedisString(rs2)) => {
-            Ok(rs1.as_bytes().cmp(rs2.as_bytes()))
-        }
-        (RsValue::String(s1), RsValue::RedisString(rs2)) => Ok(s1.as_bytes().cmp(rs2.as_bytes())),
-        (RsValue::RedisString(rs1), RsValue::String(s2)) => Ok(rs1.as_bytes().cmp(s2.as_bytes())),
-        _ => Err(CompareError::IncompatibleTypes),
-    }
-}
-
-fn compare_number_to_string(
-    number: f64,
-    slice: &[u8],
-    num_to_str_cmp_fallback: bool,
-) -> Result<Ordering, CompareError> {
-    // first try to convert the slice to a number for comparison
-    if let Some(other_number) = value::util::str_to_float(slice) {
-        number
-            .partial_cmp(&other_number)
-            .ok_or(CompareError::NaNNumber)
-    // else only if num_to_str_cmp_fallback is enabled, convert the number to a slice for comparison
-    } else if num_to_str_cmp_fallback {
-        let mut buf = [0; 32];
-        let n = value::util::num_to_str(number, &mut buf).unwrap();
-        Ok(buf[0..n].cmp(slice))
-    } else {
-        Err(CompareError::NoNumberToStringFallback)
-    }
 }
