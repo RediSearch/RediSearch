138894a441e7d99fa9fd209c8b4dc70f92970100
Dax Huiberts <daxhuiberts@gmail.com>
2026-03-06 11:49:51 +0100

    Add license headers

--- src/redisearch_rs/value/src/comparison.rs
+++ src/redisearch_rs/value/src/comparison.rs
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2006-Present, Redis Ltd.
+ * All rights reserved.
+ *
+ * Licensed under your choice of the Redis Source Available License 2.0
+ * (RSALv2); or (b) the Server Side Public License v1 (SSPLv1); or (c) the
+ * GNU Affero General Public License v3 (AGPLv3).
+*/
+
 use crate::RsValue;
 use crate::util::{num_to_str, str_to_float};
 use std::cmp::Ordering;
