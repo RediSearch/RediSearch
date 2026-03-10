32fdec582117bae75e027960f04d47aec3a3ebb7
Thierry Berger <contact@thierryberger.com>
2026-03-10 13:41:23 +0100

    intersection reducer in Rust ffi

--- src/profile/profile.c
+++ src/profile/profile.c
@@ -9,7 +9,6 @@
 #include "profile.h"
 #include "iterators/iterator_api.h"
 #include "iterators/optional_iterator.h"
-#include "iterators/intersection_iterator.h"
 #include "iterators/union_iterator.h"
 #include "iterators/hybrid_reader.h"
 #include "iterators/optimizer_reader.h"
