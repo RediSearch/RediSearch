d090373f2615eee0f1c1d54b19cfa1ce24eac306
kei-nan <jonathan.keinan@redis.com>
2026-03-17 12:35:29 +0200

    Cache LLVM Installation (#8700)
    * cache llvm installation
    
    * don't modify workdir
    
    * fix comment
    
    * address sonarqube annotations
    
    * fix run statements
    
    * make install_rust_deps path independent

--- .install/install_llvm.sh
+++ .install/install_llvm.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -e
+export DEBIAN_FRONTEND=noninteractive
 
 # Source the profile update utility
 source "$(dirname "$0")/macos_update_profile.sh"
@@ -7,6 +9,12 @@ OS_TYPE=$(uname -s)
 # Source $LLVM_VERSION
 source "$(dirname "$0")/LLVM_VERSION.sh"
 MODE=$1
+APT_GET_LOCK_TIMEOUT_SECONDS="${APT_GET_LOCK_TIMEOUT_SECONDS:-600}"
+
+apt_get_cmd() {
+    $MODE apt-get -o DPkg::Lock::Timeout="$APT_GET_LOCK_TIMEOUT_SECONDS" "$@"
+    return $?
+}
 
 if [[ $OS_TYPE == Darwin ]]; then
     # Keep using older LLVM on Mac as some deps do not build with the newer one.
@@ -20,7 +28,8 @@ if [[ $OS_TYPE == Darwin ]]; then
     [[ -f ~/.bash_profile ]] && update_profile ~/.bash_profile "$LLVM"
     [[ -f ~/.zshrc ]] && update_profile ~/.zshrc "$LLVM"
 else
-    $MODE apt install -y lsb-release wget software-properties-common gnupg
+    apt_get_cmd update -qq
+    apt_get_cmd install -yqq lsb-release wget software-properties-common gnupg
     wget https://apt.llvm.org/llvm.sh
     chmod +x llvm.sh
     $MODE ./llvm.sh $LLVM_VERSION
