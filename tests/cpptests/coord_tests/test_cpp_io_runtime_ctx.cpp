ae3dfeb47fdf23ae6b9b2ce77daae13d02d97f89
kei-nan <jonathan.keinan@redis.com>
2026-03-24 16:06:57 +0200

    Skip Schedule Topology Test (#8816)
    initial commit

--- tests/cpptests/coord_tests/test_cpp_io_runtime_ctx.cpp
+++ tests/cpptests/coord_tests/test_cpp_io_runtime_ctx.cpp
@@ -172,6 +172,8 @@ TEST_F(IORuntimeCtxCommonTest, Schedule) {
 }
 
 TEST_F(IORuntimeCtxCommonTest, ScheduleTopology) {
+  GTEST_SKIP() << "Temporarily skipping ScheduleTopology.";
+
   // Reset the signal before starting
   lastAppliedCapShards.store(0, std::memory_order_relaxed);
 
