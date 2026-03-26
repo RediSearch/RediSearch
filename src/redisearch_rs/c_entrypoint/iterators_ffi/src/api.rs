4b375e8c1eac59909d15862eeb293a5e265518a9
chesedo <pieter@chesedo.me>
2026-03-26 11:24:31 +0200

    Fix linting issues

--- src/redisearch_rs/c_entrypoint/iterators_ffi/src/api.rs
+++ src/redisearch_rs/c_entrypoint/iterators_ffi/src/api.rs
@@ -14,13 +14,16 @@ use rqe_iterators::SearchEnterpriseIterators;
 /// Initialize the global `SEARCH_ENTERPRISE_ITERATORS` with the provided implementation.
 ///
 /// # Safety
-/// The caller must ensure it has the same version of the `SearchEnterpriseIterators` trait as this library.
+/// The following safety conditions must be upheld by the caller:
+/// 1. `iters` must be a valid pointer to a heap-allocated instance of a type that implements the `SearchEnterpriseIterators` trait.
+/// 2. The caller must ensure it has the same version of the `SearchEnterpriseIterators` trait as this library.
 #[unsafe(no_mangle)]
 #[expect(
     improper_ctypes_definitions,
     reason = "This function is intended to be called from other Rust code having the same version of the `SearchEnterpriseIterators` trait, and is not intended to be called from C code directly."
 )]
-pub extern "C" fn redisearch_init_iterators(iters: *mut dyn SearchEnterpriseIterators) {
+pub unsafe extern "C" fn redisearch_init_iterators(iters: *mut dyn SearchEnterpriseIterators) {
+    // Safety: See safety comment 1 and 2
     let boxed = unsafe { Box::from_raw(iters) };
     let _ = SEARCH_ENTERPRISE_ITERATORS.set(boxed);
 }
@@ -36,7 +39,7 @@ pub extern "C" fn redisearch_init_iterators(iters: *mut dyn SearchEnterpriseIter
     improper_ctypes_definitions,
     reason = "This function is intended to be called from other Rust code having the same version of the `SearchEnterpriseIterators` trait, and is not intended to be called from C code directly."
 )]
-pub extern "C" fn redisearch_get_iterators() -> *const dyn SearchEnterpriseIterators {
+pub unsafe extern "C" fn redisearch_get_iterators() -> *const dyn SearchEnterpriseIterators {
     SEARCH_ENTERPRISE_ITERATORS.get().map(|b| b.as_ref()).unwrap_or_else(|| {
         panic!("SEARCH_ENTERPRISE_ITERATORS is not initialized. Please ensure that `redisearch_init_iterators` is called before invoking this function.")
     })
