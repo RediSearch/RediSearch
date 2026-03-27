fcc71ab9ac7e3596c82d3b13038a664619d1c085
LukeMathWalker <20745048+LukeMathWalker@users.noreply.github.com>
2026-03-27 10:23:06 +0100

    perf: Avoid runtime checks for properties in the safety contract

--- src/redisearch_rs/c_entrypoint/rlookup_ffi/src/row.rs
+++ src/redisearch_rs/c_entrypoint/rlookup_ffi/src/row.rs
@@ -329,15 +329,16 @@ pub unsafe extern "C-unwind" fn RLookupRow_WriteFieldsFrom<'a>(
 pub unsafe extern "C" fn RLookupRow_Get(
     key: *const RLookupKey,
     row: *const OpaqueRLookupRow,
-) -> Option<NonNull<RsValue>> {
+) -> *mut RsValue {
     // Safety: ensured by caller (1.)
-    let key = unsafe { key.as_ref().unwrap() };
+    let key = unsafe { &*key };
 
     // Safety: ensured by caller (2.)
-    let row = unsafe { RLookupRow::from_opaque_ptr(row).unwrap() };
+    let row = unsafe { RLookupRow::from_opaque_ptr_unchecked(row) };
 
     row.get(key)
-        .map(|x| NonNull::new(x.as_ptr().cast_mut()).unwrap())
+        .map(|x| x.as_ptr().cast_mut())
+        .unwrap_or(std::ptr::null_mut())
 }
 
 /// Returns the sorting vector for the row, or null if none exists.
