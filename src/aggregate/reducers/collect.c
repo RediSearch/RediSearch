53500d84d99c89a4e7c75558ddc72969a072cf3c
itzik.vaknin@redis.com <itzikvaknin6@gmail.com>
2026-03-31 19:17:51 +0300

    Add COLLECT reducer parser and tests

---
+++ src/aggregate/reducers/collect.c
@@ -0,0 +1,273 @@
+/*
+ * Copyright (c) 2006-Present, Redis Ltd.
+ * All rights reserved.
+ *
+ * Licensed under your choice of the Redis Source Available License 2.0
+ * (RSALv2); or (b) the Server Side Public License v1 (SSPLv1); or (c) the
+ * GNU Affero General Public License v3 (AGPLv3).
+*/
+#include <aggregate/reducer.h>
+#include "util/arg_parser.h"
+#include "spec.h"
+#include "config.h"
+#include <string.h>
+#include <limits.h>
+
+#define COLLECT_MAX_SORT_KEYS SORTASCMAP_MAXFIELDS
+#define SORT_DIR_ASC "ASC"
+#define SORT_DIR_DESC "DESC"
+
+typedef struct {
+  Reducer base;
+
+  int num_fields;
+  const RLookupKey **field_keys;
+  int wildcard_idx;  // index of '*' in field_keys, or -1 if absent
+
+  int num_sort_keys;
+  const RLookupKey **sort_keys;
+  uint64_t sortAscMap;
+
+  bool has_limit;
+  long long limit_offset;
+  long long limit_count;
+} CollectReducer;
+
+typedef struct {
+  CollectReducer *cr;
+  const ReducerOptions *options;
+} CollectParseCtx;
+
+// --- Stub vtable functions (to be implemented in later tasks) ---
+
+static void *collectNewInstance(Reducer *rbase) {
+  return NULL;
+}
+
+static int collectAdd(Reducer *r, void *ctx, const RLookupRow *srcrow) {
+  return 1;
+}
+
+static RSValue *collectFinalize(Reducer *parent, void *ctx) {
+  return RSValue_NullStatic();
+}
+
+static void collectFreeInstance(Reducer *parent, void *p) {
+}
+
+static void collectFree(Reducer *r) {
+  CollectReducer *cr = (CollectReducer *)r;
+  rm_free(cr->field_keys);
+  rm_free(cr->sort_keys);
+  BlkAlloc_FreeAll(&r->alloc, NULL, 0, 0);
+  rm_free(cr);
+}
+
+// --- ArgParser callbacks ---
+
+static void handleCollectFields(ArgParser *parser, const void *value, void *user_data) {
+  CollectParseCtx *pctx = (CollectParseCtx *)user_data;
+  CollectReducer *cr = pctx->cr;
+  const ReducerOptions *opts = pctx->options;
+  ArgsCursor *ac = (ArgsCursor *)value;
+
+  int count = AC_NumRemaining(ac);
+  if (count < 1) {
+    QueryError_SetError(opts->status, QUERY_ERROR_CODE_PARSE_ARGS,
+      "FIELDS requires at least 1 field");
+    return;
+  }
+  if (count > SPEC_MAX_FIELDS) {
+    QueryError_SetWithoutUserDataFmt(opts->status, QUERY_ERROR_CODE_LIMIT,
+      "FIELDS count exceeds maximum of %d", SPEC_MAX_FIELDS);
+    return;
+  }
+
+  ReducerOptions sub_opts = *opts;
+  sub_opts.args = ac;
+  sub_opts.name = "FIELDS";
+
+  cr->num_fields = count;
+  cr->field_keys = rm_calloc(count, sizeof(const RLookupKey *));
+
+  for (int i = 0; i < count; i++) {
+    if (strcmp(AC_StringArg(ac, ac->offset), "*") == 0) {
+      if (cr->wildcard_idx >= 0) {
+        QueryError_SetError(opts->status, QUERY_ERROR_CODE_PARSE_ARGS,
+          "Wildcard `*` can only appear once in FIELDS");
+        return;
+      }
+      cr->wildcard_idx = i;
+      cr->field_keys[i] = NULL;
+      ac->offset++;
+    } else if (!ReducerOpts_GetKey(&sub_opts, &cr->field_keys[i])) {
+      return;
+    }
+  }
+}
+
+static void handleCollectSortBy(ArgParser *parser, const void *value, void *user_data) {
+  CollectParseCtx *pctx = (CollectParseCtx *)user_data;
+  CollectReducer *cr = pctx->cr;
+  const ReducerOptions *opts = pctx->options;
+  ArgsCursor *ac = (ArgsCursor *)value;
+
+  cr->sort_keys = rm_calloc(COLLECT_MAX_SORT_KEYS, sizeof(const RLookupKey *));
+  cr->sortAscMap = SORTASCMAP_INIT;
+  cr->num_sort_keys = 0;
+
+  ReducerOptions key_opts = *opts;
+  key_opts.args = ac;
+  key_opts.name = "SORTBY";
+
+  while (!AC_IsAtEnd(ac)) {
+    const char *s = AC_StringArg(ac, ac->offset);
+    if (!s) {
+      QueryError_SetError(opts->status, QUERY_ERROR_CODE_PARSE_ARGS,
+        "SORTBY: unexpected null argument");
+      return;
+    }
+
+    if (s[0] == '@' || s[0] == '$') {
+      if (cr->num_sort_keys >= COLLECT_MAX_SORT_KEYS) {
+        QueryError_SetWithoutUserDataFmt(opts->status, QUERY_ERROR_CODE_LIMIT,
+          "SORTBY exceeds maximum of %d fields", COLLECT_MAX_SORT_KEYS);
+        return;
+      }
+      if (!ReducerOpts_GetKey(&key_opts, &cr->sort_keys[cr->num_sort_keys])) {
+        return;
+      }
+      cr->num_sort_keys++;
+    } else if (strcasecmp(s, SORT_DIR_ASC) == 0) {
+      ac->offset++;
+      if (cr->num_sort_keys > 0) {
+        SORTASCMAP_SETASC(cr->sortAscMap, cr->num_sort_keys - 1);
+      }
+    } else if (strcasecmp(s, SORT_DIR_DESC) == 0) {
+      ac->offset++;
+      if (cr->num_sort_keys > 0) {
+        SORTASCMAP_SETDESC(cr->sortAscMap, cr->num_sort_keys - 1);
+      }
+    } else {
+      QueryError_SetWithUserDataFmt(opts->status, QUERY_ERROR_CODE_PARSE_ARGS,
+        "Bad arguments for SORTBY",
+        ": Expected `@field`, `" SORT_DIR_ASC "`, or `" SORT_DIR_DESC "`, got `%s`", s);
+      return;
+    }
+  }
+
+  if (cr->num_sort_keys == 0) {
+    QueryError_SetError(opts->status, QUERY_ERROR_CODE_PARSE_ARGS,
+      "SORTBY requires at least one sort field");
+    return;
+  }
+}
+
+static void handleCollectLimit(ArgParser *parser, const void *value, void *user_data) {
+  CollectParseCtx *pctx = (CollectParseCtx *)user_data;
+  CollectReducer *cr = pctx->cr;
+  QueryError *status = pctx->options->status;
+  ArgsCursor *ac = (ArgsCursor *)value;
+
+  long long offset = 0, count = 0;
+  if (AC_GetLongLong(ac, &offset, 0) != AC_OK) {
+    QueryError_SetError(status, QUERY_ERROR_CODE_PARSE_ARGS,
+      "LIMIT offset must be an integer");
+    return;
+  }
+  if (AC_GetLongLong(ac, &count, 0) != AC_OK) {
+    QueryError_SetError(status, QUERY_ERROR_CODE_PARSE_ARGS,
+      "LIMIT count must be an integer");
+    return;
+  }
+
+  if (offset < 0) {
+    QueryError_SetError(status, QUERY_ERROR_CODE_PARSE_ARGS,
+      "LIMIT offset must be non-negative");
+    return;
+  }
+  if (count < 0) {
+    QueryError_SetError(status, QUERY_ERROR_CODE_PARSE_ARGS,
+      "LIMIT count must be non-negative");
+    return;
+  }
+  if (count > (long long)MAX_AGGREGATE_REQUEST_RESULTS) {
+    QueryError_SetWithoutUserDataFmt(status, QUERY_ERROR_CODE_LIMIT,
+      "LIMIT count exceeds maximum of %llu", MAX_AGGREGATE_REQUEST_RESULTS);
+    return;
+  }
+  if (offset > LLONG_MAX - count) {
+    QueryError_SetError(status, QUERY_ERROR_CODE_PARSE_ARGS,
+      "LIMIT offset + count overflow");
+    return;
+  }
+
+  cr->has_limit = true;
+  cr->limit_offset = offset;
+  cr->limit_count = count;
+}
+
+// --- Factory function ---
+
+Reducer *RDCRCollect_New(const ReducerOptions *options) {
+  CollectReducer *cr = rm_calloc(1, sizeof(*cr));
+  cr->wildcard_idx = -1;
+  cr->sortAscMap = SORTASCMAP_INIT;
+  CollectParseCtx pctx = {.cr = cr, .options = options};
+
+  ArgsCursor *ac = options->args;
+  ArgParser *parser = ArgParser_New(ac, NULL);
+  if (!parser) {
+    QueryError_SetError(options->status, QUERY_ERROR_CODE_PARSE_ARGS,
+      "Failed to create argument parser for COLLECT");
+    rm_free(cr);
+    return NULL;
+  }
+
+  ArgsCursor subArgs = {0};
+
+  ArgParser_AddSubArgsV(parser, "FIELDS", "Projected fields",
+    &subArgs, 1, -1,
+    ARG_OPT_REQUIRED,
+    ARG_OPT_CALLBACK, handleCollectFields, &pctx,
+    ARG_OPT_END);
+
+  ArgParser_AddSubArgsV(parser, "SORTBY", "In-group sort keys",
+    &subArgs, 1, COLLECT_MAX_SORT_KEYS * 2,
+    ARG_OPT_OPTIONAL,
+    ARG_OPT_CALLBACK, handleCollectSortBy, &pctx,
+    ARG_OPT_END);
+
+  ArgParser_AddSubArgsV(parser, "LIMIT", "Per-group limit",
+    &subArgs, 2, 2,
+    ARG_OPT_OPTIONAL,
+    ARG_OPT_CALLBACK, handleCollectLimit, &pctx,
+    ARG_OPT_END);
+
+  ArgParseResult result = ArgParser_Parse(parser);
+
+  if (QueryError_HasError(options->status)) {
+    ArgParser_Free(parser);
+    collectFree(&cr->base);
+    return NULL;
+  }
+
+  if (!result.success) {
+    QueryError_SetWithUserDataFmt(options->status, QUERY_ERROR_CODE_PARSE_ARGS,
+      "Bad arguments for COLLECT", ": %s", ArgParser_GetErrorString(parser));
+    ArgParser_Free(parser);
+    collectFree(&cr->base);
+    return NULL;
+  }
+
+  ArgParser_Free(parser);
+
+  Reducer *rbase = &cr->base;
+  rbase->NewInstance = collectNewInstance;
+  rbase->Add = collectAdd;
+  rbase->Finalize = collectFinalize;
+  rbase->FreeInstance = collectFreeInstance;
+  rbase->Free = collectFree;
+
+  return rbase;
+}
---
+++ tests/cpptests/test_cpp_collect.cpp
@@ -0,0 +1,382 @@
+/*
+ * Copyright (c) 2006-Present, Redis Ltd.
+ * All rights reserved.
+ *
+ * Licensed under your choice of the Redis Source Available License 2.0
+ * (RSALv2); or (b) the Server Side Public License v1 (SSPLv1); or (c) the
+ * GNU Affero General Public License v3 (AGPLv3).
+*/
+
+#include "gtest/gtest.h"
+#include "aggregate/reducer.h"
+#include "spec.h"
+#include "config.h"
+#include "result_processor.h"
+#include "redismock/redismock.h"
+
+#include <vector>
+#include <string>
+#include <initializer_list>
+#include <limits.h>
+
+class CollectParserTest : public ::testing::Test {
+protected:
+  RLookup lk;
+
+  void SetUp() override {
+    lk = RLookup_New();
+  }
+
+  void TearDown() override {
+    RLookup_Cleanup(&lk);
+  }
+
+  void registerKeys(std::initializer_list<const char *> names) {
+    for (const char *name : names) {
+      RLookup_GetKey_Write(&lk, name, RLOOKUP_F_NOFLAGS);
+    }
+  }
+
+  Reducer *parseCollect(std::vector<const char *> &args, QueryError *status) {
+    ArgsCursor ac;
+    ArgsCursor_InitCString(&ac, args.data(), args.size());
+    ReducerOptions opts = REDUCEROPTS_INIT("COLLECT", &ac, &lk, NULL, status, true);
+    return RDCRCollect_New(&opts);
+  }
+};
+
+// ====== Happy path tests ======
+
+TEST_F(CollectParserTest, FieldsOnly) {
+  registerKeys({"price", "name"});
+  QueryError status = QueryError_Default();
+  std::vector<const char *> args = {"FIELDS", "2", "@price", "@name"};
+  Reducer *r = parseCollect(args, &status);
+  ASSERT_NE(r, nullptr) << QueryError_GetUserError(&status);
+  r->Free(r);
+  QueryError_ClearError(&status);
+}
+
+TEST_F(CollectParserTest, FieldsWildcard) {
+  QueryError status = QueryError_Default();
+  std::vector<const char *> args = {"FIELDS", "1", "*"};
+  Reducer *r = parseCollect(args, &status);
+  ASSERT_NE(r, nullptr) << QueryError_GetUserError(&status);
+  r->Free(r);
+  QueryError_ClearError(&status);
+}
+
+TEST_F(CollectParserTest, WildcardAmongFields) {
+  registerKeys({"price", "name"});
+  QueryError status = QueryError_Default();
+  std::vector<const char *> args = {"FIELDS", "3", "@price", "*", "@name"};
+  Reducer *r = parseCollect(args, &status);
+  ASSERT_NE(r, nullptr) << QueryError_GetUserError(&status);
+  r->Free(r);
+  QueryError_ClearError(&status);
+}
+
+TEST_F(CollectParserTest, FieldsAndSortBy) {
+  registerKeys({"price", "name"});
+  QueryError status = QueryError_Default();
+  std::vector<const char *> args = {
+    "FIELDS", "2", "@price", "@name",
+    "SORTBY", "3", "@price", "DESC", "@name"
+  };
+  Reducer *r = parseCollect(args, &status);
+  ASSERT_NE(r, nullptr) << QueryError_GetUserError(&status);
+  r->Free(r);
+  QueryError_ClearError(&status);
+}
+
+TEST_F(CollectParserTest, FieldsSortByAndLimit) {
+  registerKeys({"price"});
+  QueryError status = QueryError_Default();
+  std::vector<const char *> args = {
+    "FIELDS", "1", "@price",
+    "SORTBY", "2", "@price", "ASC",
+    "LIMIT", "0", "10"
+  };
+  Reducer *r = parseCollect(args, &status);
+  ASSERT_NE(r, nullptr) << QueryError_GetUserError(&status);
+  r->Free(r);
+  QueryError_ClearError(&status);
+}
+
+TEST_F(CollectParserTest, FieldsAndLimitWithoutSortBy) {
+  registerKeys({"price"});
+  QueryError status = QueryError_Default();
+  std::vector<const char *> args = {
+    "FIELDS", "1", "@price",
+    "LIMIT", "5", "100"
+  };
+  Reducer *r = parseCollect(args, &status);
+  ASSERT_NE(r, nullptr) << QueryError_GetUserError(&status);
+  r->Free(r);
+  QueryError_ClearError(&status);
+}
+
+TEST_F(CollectParserTest, MultipleSortKeysWithDirections) {
+  registerKeys({"a", "b", "c"});
+  QueryError status = QueryError_Default();
+  std::vector<const char *> args = {
+    "FIELDS", "3", "@a", "@b", "@c",
+    "SORTBY", "5", "@a", "ASC", "@b", "DESC", "@c"
+  };
+  Reducer *r = parseCollect(args, &status);
+  ASSERT_NE(r, nullptr) << QueryError_GetUserError(&status);
+  r->Free(r);
+  QueryError_ClearError(&status);
+}
+
+TEST_F(CollectParserTest, SortByDefaultsToAscending) {
+  registerKeys({"price"});
+  QueryError status = QueryError_Default();
+  std::vector<const char *> args = {
+    "FIELDS", "1", "@price",
+    "SORTBY", "1", "@price"
+  };
+  Reducer *r = parseCollect(args, &status);
+  ASSERT_NE(r, nullptr) << QueryError_GetUserError(&status);
+  r->Free(r);
+  QueryError_ClearError(&status);
+}
+
+TEST_F(CollectParserTest, SortByMaxFields) {
+  registerKeys({"x", "a", "b", "c", "d", "e", "f", "g", "h"});
+  QueryError status = QueryError_Default();
+  std::vector<const char *> args = {
+    "FIELDS", "1", "@x",
+    "SORTBY", "8", "@a", "@b", "@c", "@d", "@e", "@f", "@g", "@h"
+  };
+  Reducer *r = parseCollect(args, &status);
+  ASSERT_NE(r, nullptr) << QueryError_GetUserError(&status);
+  r->Free(r);
+  QueryError_ClearError(&status);
+}
+
+TEST_F(CollectParserTest, LimitZeroOffset) {
+  registerKeys({"x"});
+  QueryError status = QueryError_Default();
+  std::vector<const char *> args = {
+    "FIELDS", "1", "@x",
+    "LIMIT", "0", "50"
+  };
+  Reducer *r = parseCollect(args, &status);
+  ASSERT_NE(r, nullptr) << QueryError_GetUserError(&status);
+  r->Free(r);
+  QueryError_ClearError(&status);
+}
+
+TEST_F(CollectParserTest, JsonPathField) {
+  registerKeys({"$..price"});
+  QueryError status = QueryError_Default();
+  std::vector<const char *> args = {"FIELDS", "1", "$..price"};
+  Reducer *r = parseCollect(args, &status);
+  ASSERT_NE(r, nullptr) << QueryError_GetUserError(&status);
+  r->Free(r);
+  QueryError_ClearError(&status);
+}
+
+TEST_F(CollectParserTest, SortByJsonPath) {
+  registerKeys({"$..price"});
+  QueryError status = QueryError_Default();
+  std::vector<const char *> args = {
+    "FIELDS", "1", "$..price",
+    "SORTBY", "1", "$..price"
+  };
+  Reducer *r = parseCollect(args, &status);
+  ASSERT_NE(r, nullptr) << QueryError_GetUserError(&status);
+  r->Free(r);
+  QueryError_ClearError(&status);
+}
+
+// ====== Validation / error tests ======
+
+TEST_F(CollectParserTest, EmptyArgs) {
+  QueryError status = QueryError_Default();
+  std::vector<const char *> args = {};
+  Reducer *r = parseCollect(args, &status);
+  ASSERT_EQ(r, nullptr);
+  EXPECT_STREQ(QueryError_GetUserError(&status), "Bad arguments for COLLECT");
+  QueryError_ClearError(&status);
+}
+
+TEST_F(CollectParserTest, MissingFieldsRequired) {
+  registerKeys({"price"});
+  QueryError status = QueryError_Default();
+  std::vector<const char *> args = {"SORTBY", "1", "@price"};
+  Reducer *r = parseCollect(args, &status);
+  ASSERT_EQ(r, nullptr);
+  EXPECT_STREQ(QueryError_GetUserError(&status), "Bad arguments for COLLECT");
+  QueryError_ClearError(&status);
+}
+
+TEST_F(CollectParserTest, FieldWithoutAtPrefix) {
+  QueryError status = QueryError_Default();
+  std::vector<const char *> args = {"FIELDS", "1", "price"};
+  Reducer *r = parseCollect(args, &status);
+  ASSERT_EQ(r, nullptr);
+  EXPECT_STREQ(QueryError_GetUserError(&status),
+    "Missing prefix: name requires '@' prefix, JSON path require '$' prefix");
+  QueryError_ClearError(&status);
+}
+
+TEST_F(CollectParserTest, FieldEmptyAfterAt) {
+  QueryError status = QueryError_Default();
+  std::vector<const char *> args = {"FIELDS", "1", "@"};
+  Reducer *r = parseCollect(args, &status);
+  ASSERT_EQ(r, nullptr);
+  EXPECT_STREQ(QueryError_GetUserError(&status), "Property not loaded nor in pipeline");
+  QueryError_ClearError(&status);
+}
+
+TEST_F(CollectParserTest, FieldNotInPipeline) {
+  QueryError status = QueryError_Default();
+  std::vector<const char *> args = {"FIELDS", "1", "@nonexistent"};
+  Reducer *r = parseCollect(args, &status);
+  ASSERT_EQ(r, nullptr);
+  EXPECT_STREQ(QueryError_GetUserError(&status), "Property not loaded nor in pipeline");
+  QueryError_ClearError(&status);
+}
+
+TEST_F(CollectParserTest, DuplicateWildcard) {
+  registerKeys({"price"});
+  QueryError status = QueryError_Default();
+  std::vector<const char *> args = {"FIELDS", "3", "*", "@price", "*"};
+  Reducer *r = parseCollect(args, &status);
+  ASSERT_EQ(r, nullptr);
+  EXPECT_STREQ(QueryError_GetUserError(&status),
+    "Wildcard `*` can only appear once in FIELDS");
+  QueryError_ClearError(&status);
+}
+
+TEST_F(CollectParserTest, UnknownSubcommand) {
+  registerKeys({"x"});
+  QueryError status = QueryError_Default();
+  std::vector<const char *> args = {
+    "FIELDS", "1", "@x",
+    "FOO", "1", "2"
+  };
+  Reducer *r = parseCollect(args, &status);
+  ASSERT_EQ(r, nullptr);
+  EXPECT_STREQ(QueryError_GetUserError(&status), "Bad arguments for COLLECT");
+  QueryError_ClearError(&status);
+}
+
+TEST_F(CollectParserTest, SortByFieldWithoutAtPrefix) {
+  registerKeys({"x"});
+  QueryError status = QueryError_Default();
+  std::vector<const char *> args = {
+    "FIELDS", "1", "@x",
+    "SORTBY", "1", "bad_field"
+  };
+  Reducer *r = parseCollect(args, &status);
+  ASSERT_EQ(r, nullptr);
+  EXPECT_STREQ(QueryError_GetUserError(&status), "Bad arguments for SORTBY");
+  QueryError_ClearError(&status);
+}
+
+TEST_F(CollectParserTest, SortByTooManyFields) {
+  registerKeys({"x", "a", "b", "c", "d", "e", "f", "g", "h", "i"});
+  QueryError status = QueryError_Default();
+  std::vector<const char *> args = {
+    "FIELDS", "1", "@x",
+    "SORTBY", "9", "@a", "@b", "@c", "@d", "@e", "@f", "@g", "@h", "@i"
+  };
+  Reducer *r = parseCollect(args, &status);
+  ASSERT_EQ(r, nullptr);
+  EXPECT_STREQ(QueryError_GetUserError(&status),
+    "SORTBY exceeds maximum of 8 fields");
+  QueryError_ClearError(&status);
+}
+
+TEST_F(CollectParserTest, SortByExceedsMaxTokens) {
+  registerKeys({"x", "a", "b", "c", "d", "e", "f", "g", "h", "i"});
+  QueryError status = QueryError_Default();
+  // 2 * SORTASCMAP_MAXFIELDS + 1 = 17 tokens — exceeds parser max_args
+  std::vector<const char *> args = {
+    "FIELDS", "1", "@x",
+    "SORTBY", "17",
+    "@a", "ASC", "@b", "ASC", "@c", "ASC", "@d", "ASC",
+    "@e", "ASC", "@f", "ASC", "@g", "ASC", "@h", "ASC",
+    "@i"
+  };
+  Reducer *r = parseCollect(args, &status);
+  ASSERT_EQ(r, nullptr);
+  EXPECT_STREQ(QueryError_GetUserError(&status), "Bad arguments for COLLECT");
+  QueryError_ClearError(&status);
+}
+
+TEST_F(CollectParserTest, FieldsExceedsMax) {
+  QueryError status = QueryError_Default();
+  // Build FIELDS with SPEC_MAX_FIELDS + 1 = 1025 entries
+  std::vector<const char *> args;
+  args.push_back("FIELDS");
+  args.push_back("1025");
+  std::vector<std::string> field_strs(1025);
+  for (int i = 0; i < 1025; i++) {
+    field_strs[i] = "@f" + std::to_string(i);
+    args.push_back(field_strs[i].c_str());
+  }
+  Reducer *r = parseCollect(args, &status);
+  ASSERT_EQ(r, nullptr);
+  EXPECT_STREQ(QueryError_GetUserError(&status),
+    "FIELDS count exceeds maximum of 1024");
+  QueryError_ClearError(&status);
+}
+
+TEST_F(CollectParserTest, SortByInvalidTokenBetweenFields) {
+  registerKeys({"x", "a", "b"});
+  QueryError status = QueryError_Default();
+  std::vector<const char *> args = {
+    "FIELDS", "1", "@x",
+    "SORTBY", "3", "@a", "INVALID", "@b"
+  };
+  Reducer *r = parseCollect(args, &status);
+  ASSERT_EQ(r, nullptr);
+  EXPECT_STREQ(QueryError_GetUserError(&status), "Bad arguments for SORTBY");
+  QueryError_ClearError(&status);
+}
+
+TEST_F(CollectParserTest, LimitNegativeOffset) {
+  registerKeys({"x"});
+  QueryError status = QueryError_Default();
+  std::vector<const char *> args = {
+    "FIELDS", "1", "@x",
+    "LIMIT", "-1", "10"
+  };
+  Reducer *r = parseCollect(args, &status);
+  ASSERT_EQ(r, nullptr);
+  EXPECT_STREQ(QueryError_GetUserError(&status),
+    "LIMIT offset must be non-negative");
+  QueryError_ClearError(&status);
+}
+
+TEST_F(CollectParserTest, LimitNegativeCount) {
+  registerKeys({"x"});
+  QueryError status = QueryError_Default();
+  std::vector<const char *> args = {
+    "FIELDS", "1", "@x",
+    "LIMIT", "0", "-5"
+  };
+  Reducer *r = parseCollect(args, &status);
+  ASSERT_EQ(r, nullptr);
+  EXPECT_STREQ(QueryError_GetUserError(&status),
+    "LIMIT count must be non-negative");
+  QueryError_ClearError(&status);
+}
+
+TEST_F(CollectParserTest, LimitNonNumericOffset) {
+  registerKeys({"x"});
+  QueryError status = QueryError_Default();
+  std::vector<const char *> args = {
+    "FIELDS", "1", "@x",
+    "LIMIT", "abc", "10"
+  };
+  Reducer *r = parseCollect(args, &status);
+  ASSERT_EQ(r, nullptr);
+  EXPECT_STREQ(QueryError_GetUserError(&status),
+    "LIMIT offset must be an integer");
+  QueryError_ClearError(&status);
+}
