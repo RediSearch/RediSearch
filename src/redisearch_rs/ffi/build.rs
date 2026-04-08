37eabb997cdecb10c676065fc543f9dfa2503714
Thierry Berger <contact@thierryberger.com>
2026-04-08 23:39:28 +0200

    vector parity - m2 - rust
    use fast path for unfiltered + Update child_num_estimated in batch mode.
    
    topk: Add a begin/end adhoc function to allow room for expensive state
    
    make vector path use this new mechanism
    
    pr feedbacks ; simplifications

--- src/redisearch_rs/ffi/build.rs
+++ src/redisearch_rs/ffi/build.rs
@@ -80,7 +80,12 @@ const HEADERS: &[HeaderAllowlist] = &[
     HeaderAllowlist {
         path: "deps/VectorSimilarity/src/VecSim/vec_sim_common.h",
         fns: &[],
-        types: &["ThrottleCB", "VecSimIndexBasicInfo", "VecSimParamsDisk"],
+        types: &[
+            "ThrottleCB",
+            "VecSimIndexBasicInfo",
+            "VecSimParamsDisk",
+            "VecSimQueryParams",
+        ],
         vars: &[],
     },
     HeaderAllowlist {
