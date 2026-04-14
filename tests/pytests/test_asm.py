124965572128d324a8ecfef8e61da4f82873e3b5
Joan Fontanals <jfontanalsmartinez@gmail.com>
2026-04-14 15:46:11 +0200

    [8.6-rse][MOD-14828] test: add sleep to avoid epoch collision (#9033) (#9056)
    * fix
    
    * remove unwanted test

--- tests/pytests/test_asm.py
+++ tests/pytests/test_asm.py
@@ -712,10 +712,10 @@ def add_shard_and_migrate_test(env: Env, query_type: str = 'FT.SEARCH'):
 
     # Add a new shard
     env.addShardToClusterIfExists()
+    time.sleep(5)
     new_shard = env.getConnection(shardId=initial_shards_count+1)
     # ...and migrate slots from shard 1 to the new shard
     wait_for_migration_complete(env, new_shard, shard1, query_during_migration={'query': query, 'shards': shards, 'expected': expected, 'query_type': query_type})
-
     # Expect new shard to have the index schema
     env.assertEqual(new_shard.execute_command('FT._LIST'), ['idx'])
 
