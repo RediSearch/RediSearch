12c9fa707f97e47cc032b2cd75a38166d3431d26
Magnus Markling <magnus@markling.com>
2026-04-14 07:11:06 +0200

    CI: Remove unnecessary DEBIAN_FRONTEND (#9007)
    remove unused DEBIAN_FRONTEND

--- .install/microsoft_azure_linux_3.0.sh
+++ .install/microsoft_azure_linux_3.0.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 MODE=$1 # whether to install using sudo or not
 set -eo pipefail
-export DEBIAN_FRONTEND=noninteractive
 
 $MODE tdnf install -yq build-essential ca-certificates gdb git libxcrypt-devel openssl-devel rsync tar unzip wget which xz
 
