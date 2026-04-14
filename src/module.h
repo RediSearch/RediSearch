b2b1b90bcf116a874175fd5cf8e2f2017d4ee7a4
Magnus Markling <magnus@markling.com>
2026-04-14 07:10:57 +0200

    [MOD-14426] Fix <sys/cdefs.h> warning (#8996)
    * use STRINGIFY to fix warning
    
    * remove duplicate in module.c

--- src/module.h
+++ src/module.h
@@ -20,9 +20,9 @@
 #include "thpool/thpool.h"
 #include "profile/options.h"
 
-// Hack to support Alpine Linux 3 where __STRING is not defined
-#if !defined(__GLIBC__) && !defined(__STRING)
-#include <sys/cdefs.h>
+#ifndef STRINGIFY
+#define __STRINGIFY(x) #x
+#define STRINGIFY(x) __STRINGIFY(x)
 #endif
 
 // Module-level dummy context for certain dummy RM_XXX operations
@@ -77,10 +77,10 @@ do {                                            \
 #define CLUSTERDOWN_ERR "ERRCLUSTER Uninitialized cluster state, could not perform command"
 #define NODEBUG_ERR "Debug commands are disabled, please follow the redis configuration guide to enable them"
 
-#define RM_TRY(expr)                                                  \
-  if (expr == REDISMODULE_ERR) {                                      \
-    RedisModule_Log(ctx, "warning", "Could not run " __STRING(expr)); \
-    return REDISMODULE_ERR;                                           \
+#define RM_TRY(expr)                                                   \
+  if (expr == REDISMODULE_ERR) {                                       \
+    RedisModule_Log(ctx, "warning", "Could not run " STRINGIFY(expr)); \
+    return REDISMODULE_ERR;                                            \
   }
 
 #define IS_SST_RDB_IN_PROCESS(ctx) (RedisModule_GetContextFlags(ctx) & REDISMODULE_CTX_FLAGS_SST_RDB)
