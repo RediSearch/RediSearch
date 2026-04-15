523e18273e65be95e3017ac191855b12fcf7d6cb
Raz Monsonego <74051729+raz-mon@users.noreply.github.com>
2026-04-15 14:58:37 +0300

    Fix disk profile crash

--- src/profile/profile.c
+++ src/profile/profile.c
@@ -40,10 +40,8 @@ static void printInvIdxIteratorCounters(RedisModule_Reply *reply, const QueryIte
 }
 
 void printInvIdxIt(RedisModule_Reply *reply, const QueryIterator *root, const ProfileCounters *counters, double cpuTime, PrintProfileConfig *config) {
-  IndexFlags readerFlags = InvIndIterator_GetReaderFlags(root);
-
   RedisModule_Reply_Map(reply);
-  if (readerFlags == Index_DocIdsOnly) {
+  if (root->type == INV_IDX_TAG_ITERATOR) {
     RSQueryTerm *term = IndexResult_QueryTermRef(root->current);
     if (term != NULL) {
       printProfileType("TAG");
@@ -51,7 +49,7 @@ void printInvIdxIt(RedisModule_Reply *reply, const QueryIterator *root, const Pr
       const char *term_str = QueryTerm_GetStrAndLen(term, &term_len);
       RedisModule_ReplyKV_StringBuffer(reply, "Term", term_str, term_len);
     }
-  } else if (readerFlags & Index_StoreNumeric) {
+  } else if (root->type == INV_IDX_NUMERIC_ITERATOR) {
     const NumericFilter *flt = NumericInvIndIterator_GetNumericFilter(root);
     if (!flt || flt->geoFilter == NULL) {
       printProfileType("NUMERIC");
@@ -66,12 +64,14 @@ void printInvIdxIt(RedisModule_Reply *reply, const QueryIterator *root, const Pr
       decodeGeo(NumericInvIndIterator_GetProfileRangeMax(root), nw);
       RedisModule_Reply_SimpleStringf(reply, "%g,%g - %g,%g", se[0], se[1], nw[0], nw[1]);
     }
-  } else {
+  } else if (root->type == INV_IDX_TERM_ITERATOR) {
     printProfileType("TEXT");
     RSQueryTerm *term = IndexResult_QueryTermRef(root->current);
     size_t term_len = 0;
     const char *term_str = QueryTerm_GetStrAndLen(term, &term_len);
     RedisModule_ReplyKV_StringBuffer(reply, "Term", term_str, term_len);
+  } else {
+    RS_ABORT("unsupported inverted index iterator type");
   }
 
   printInvIdxIteratorCounters(reply, root, counters, cpuTime, config);
