a02dc1d0f8947c87250bc544603007731eb8bf72
meiravgri <meirav.grimberg@redis.com>
2026-04-16 12:12:33 +0000

    remove -qq

--- .install/ubuntu_20.04.sh
+++ .install/ubuntu_20.04.sh
@@ -4,16 +4,16 @@ export DEBIAN_FRONTEND=noninteractive
 MODE=$1 # whether to install using sudo or not
 source "$(dirname "${BASH_SOURCE[0]}")/apt_get_cmd.sh"
 
-apt_get_cmd "$MODE" update -qq
-apt_get_cmd "$MODE" upgrade -yqq
+apt_get_cmd "$MODE" update
+apt_get_cmd "$MODE" upgrade -y
 
 # Provides the add-apt-repository command
-apt_get_cmd "$MODE" install -yqq software-properties-common
+apt_get_cmd "$MODE" install -y software-properties-common
 
 $MODE add-apt-repository ppa:ubuntu-toolchain-r/test -y
 $MODE add-apt-repository ppa:deadsnakes/ppa -y
 
-apt_get_cmd "$MODE" install -yqq wget make clang-format gcc lcov git openssl libssl-dev \
+apt_get_cmd "$MODE" install -y wget make clang-format gcc lcov git openssl libssl-dev \
     unzip rsync build-essential gcc-11 g++-11 curl libclang-dev gdb
 
 $MODE update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 60 --slave /usr/bin/g++ g++ /usr/bin/g++-11
