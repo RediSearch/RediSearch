b88b09ff7c89aecb76d6128bd2eda1d7b4eaa6ce
Joan Fontanals Martinez <jfontanalsmartinez@gmail.com>
2026-05-18 15:17:00 +0200

    fix asan errors

--- tests/cpptests/llapi_test_helpers.cpp
+++ tests/cpptests/llapi_test_helpers.cpp
@@ -51,12 +51,6 @@ RefManager* RediSearch_CreateIndex(const char* name, const RSIndexOptions* optio
   //TODO: Should not be supported for SearchDisk, but no way to return error in programmatic API
   spec->flags = (IndexFlags)(spec->flags | Index_Temporary);  // temporary is so that we will not use threads!!
 
-  spec->rule = (SchemaRule *)rm_calloc(1, sizeof *spec->rule);
-  spec->rule->score_default = options->score ? options->score : DEFAULT_SCORE;
-  if (options->lang) {
-    spec->rule->lang_default = RSLanguage_Find(options->lang, 0);
-  }
-
   if (options->flags & RSIDXOPT_DOCTBLSIZE_UNLIMITED) {
     spec->docs.maxSize = DOCID_MAX;
   }
