5de6899df19a34c66a5546929d688a704d006041
Magnus Markling <magnus@markling.com>
2026-05-20 14:57:29 +0200

    Upgrade rand dependency (#9736)
    upgrade rand to 0.10.1

--- src/redisearch_rs/rqe_iterators_bencher/src/benchers/optional.rs
+++ src/redisearch_rs/rqe_iterators_bencher/src/benchers/optional.rs
@@ -15,7 +15,7 @@
 use std::{hint::black_box, time::Duration};
 
 use criterion::{BenchmarkGroup, Criterion, measurement::WallTime};
-use rand::{Rng as _, SeedableRng as _, rngs::StdRng};
+use rand::{RngExt as _, SeedableRng as _, rngs::StdRng};
 use rqe_iterators::{IdList, RQEIterator, SkipToOutcome, optional::Optional};
 
 #[derive(Default)]
