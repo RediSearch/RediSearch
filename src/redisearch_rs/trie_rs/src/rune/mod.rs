1b3bf08bc7e3686f8a6ee96b2627b3f758011a38
Magnus Markling <magnus@markling.com>
2026-05-23 14:36:50 +0200

    wip

--- src/redisearch_rs/trie_rs/src/rune/mod.rs
+++ src/redisearch_rs/trie_rs/src/rune/mod.rs
@@ -80,22 +80,52 @@ impl<Data> RuneTrieMap<Data> {
         todo!()
     }
 
-    pub fn iterate_range(
-        &self,
+    // pub fn range_iter<F>(&self, min: Option<RuneBound<'_>>, max: Option<RuneBound<'_>>, mut cb: F)
+    // where
+    //     F: FnMut(&[u16], &T),
+    // {
+    //     let min_packed = min.map(|b| (pack_runes(b.value), b.is_included));
+    //     let max_packed = max.map(|b| (pack_runes(b.value), b.is_included));
+    //     let filter = RangeFilter {
+    //         min: min_packed.as_ref().map(|(v, inc)| RangeBoundary {
+    //             value: v.as_slice(),
+    //             is_included: *inc,
+    //         }),
+    //         max: max_packed.as_ref().map(|(v, inc)| RangeBoundary {
+    //             value: v.as_slice(),
+    //             is_included: *inc,
+    //         }),
+    //     };
+    //     for (bytes, data) in self.inner.range_iter(filter) {
+    //         cb(&unpack_bytes(&bytes), data);
+    //     }
+    // }
+
+    //     pub fn range_iter<'a>(&'a self, filter: RangeFilter<'a>) -> RangeIter<'a, Data> {
+
+    pub fn iterate_range<'a>(
+        &'a self,
         _as_deref_1: Option<&[u16]>,
         _include_min: bool,
         _as_deref_2: Option<&[u16]>,
         _include_max: bool,
-    ) -> RuneTrieMapIter<'_, Data> {
-        self.inner.range_iter(RangeFilter {
-            min: Some(RangeBoundary::included(&rune_to_bytes(_as_deref_1.unwrap()))),
-            max: Some(RangeBoundary::included(&rune_to_bytes(_as_deref_2.unwrap()))),
-        })
+    ) -> RuneTrieMapRangeIter<'a, Data> {
+        let rf = RangeFilter {
+            min: Some(RangeBoundary::included(&rune_to_bytes(
+                _as_deref_1.unwrap(),
+            ))),
+            max: Some(RangeBoundary::included(&rune_to_bytes(
+                _as_deref_2.unwrap(),
+            ))),
+        };
+
+        RuneTrieMapRangeIter(self.inner.range_iter(rf))
     }
 
-    pub fn iterate_wildcard(&self, buf: &[u16]) -> RuneTrieMapIter<'_, Data> {
-        self.inner
-            .wildcard_iter(WildcardPattern::parse(&rune_to_bytes(buf)))
+    pub fn iterate_wildcard(&self, _buf: &[u16]) -> RuneTrieMapIter<'_, Data> {
+        // self.inner
+        //     .wildcard_iter(WildcardPattern::parse(&rune_to_bytes(buf)))
+        todo!()
     }
 }
 
@@ -112,6 +142,16 @@ impl<'a, Data> Iterator for RuneTrieMapIter<'a, Data> {
 pub struct RuneTrieMapContainsIter<'a, Data>(iter::ContainsIter<'a, Data>);
 
 impl<'a, Data> Iterator for RuneTrieMapContainsIter<'a, Data> {
+    type Item = (Vec<Rune>, &'a Data);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next().map(|(k, v)| (bytes_to_rune(&k), v))
+    }
+}
+
+pub struct RuneTrieMapRangeIter<'a, Data>(iter::RangeIter<'a, Data>);
+
+impl<'a, Data> Iterator for RuneTrieMapRangeIter<'a, Data> {
     type Item = (Vec<Rune>, &'a Data);
 
     fn next(&mut self) -> Option<Self::Item> {
