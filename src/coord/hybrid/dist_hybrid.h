6745df13c81c99a6083f009138e8aa7306027942
nafraf <nafraf@users.noreply.github.com>
2026-05-27 16:29:34 -0500

    MOD-14089: Deferring the effectiveK calculation to the IO thread (#9171) (cherry picked from commit fa2a9eaf8b7aae2d4a9e34a32fa615eaea2c0b70)

--- src/coord/hybrid/dist_hybrid.h
+++ src/coord/hybrid/dist_hybrid.h
@@ -28,13 +28,10 @@ int DistHybridTimeoutFailClient(RedisModuleCtx *ctx, RedisModuleString **argv, i
 int DistHybridReplyCallback(RedisModuleCtx *ctx, RedisModuleString **argv, int argc);
 
 // For testing purposes
-// numShards is passed from the main thread to ensure thread-safe access
 void HybridRequest_buildMRCommand(RedisModuleString **argv, int argc,
                             ProfileOptions profileOptions,
                             MRCommand *xcmd, arrayof(char*) serialized,
-                            IndexSpec *sp,
-                            const VectorQuery *vq,
-                            size_t numShards);
+                            IndexSpec *sp, int *outKArgIndex);
 
 #ifdef __cplusplus
 }
