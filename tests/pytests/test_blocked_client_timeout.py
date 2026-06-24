8f68fe377b70401cd798704328269c271763dd0e
Magnus Markling <magnus@markling.com>
2026-06-24 17:14:53 +0200

    order: index, index, population, population

--- tests/pytests/test_blocked_client_timeout.py
+++ tests/pytests/test_blocked_client_timeout.py
@@ -4830,9 +4830,6 @@ class TestCoordinatorTimeoutReturnStrictResp2:
 
         self.env.expect('FT.CREATE', 'idx', 'PREFIX', '1', 'doc',
                         'SCHEMA', 'name', 'TEXT').ok()
-        for i in range(self.n_docs):
-            conn.execute_command('HSET', f'doc{i}', 'name', f'hello{i}')
-
         # Index with a vector field for FT.HYBRID tests (different prefix).
         self.env.expect(
             'FT.CREATE', 'hybrid_idx', 'PREFIX', '1', 'hybrid_doc', 'SCHEMA',
@@ -4840,11 +4837,14 @@ class TestCoordinatorTimeoutReturnStrictResp2:
             'embedding', 'VECTOR', 'FLAT', '6', 'TYPE', 'FLOAT32', 'DIM', '2',
             'DISTANCE_METRIC', 'L2'
         ).ok()
+
         for i in range(self.n_docs):
+            conn.execute_command('HSET', f'doc{i}', 'name', f'hello{i}')
+
             vec = np.array([float(i), float(i)], dtype=np.float32).tobytes()
             conn.execute_command('HSET', f'hybrid_doc{i}', 'name', f'hello{i}',
                                  'embedding', vec)
-        
+
         # Warmup hybrid query and store the query vector for tests.
         self.hybrid_query_vec = np.array([0.0, 0.0], dtype=np.float32).tobytes()
         try:
