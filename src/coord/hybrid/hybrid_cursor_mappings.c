e85b0185b8caf1e1f7f833f0e016d95a68596534
Itzikvaknin <82322982+Itzikvaknin@users.noreply.github.com>
2026-07-05 12:08:16 +0300

    Update src/coord/hybrid/hybrid_cursor_mappings.c
    Co-authored-by: lerman25 <58445352+lerman25@users.noreply.github.com>

--- src/coord/hybrid/hybrid_cursor_mappings.c
+++ src/coord/hybrid/hybrid_cursor_mappings.c
@@ -301,7 +301,7 @@ bool ProcessHybridCursorMappings(const MRCommand *cmd, StrongRef searchMappingsR
 
     RequestSyncCtx_UnregisterAbortWakeChannel(syncCtx);
 
-    if (timedOut || RS_AtomicBoolLoadRelaxed(&syncCtx->timedOut)) {
+    if (timedOut || RequestSyncCtx_GetTimedOut(&syncCtx)) {
         QueryError_SetCode(status, QUERY_ERROR_CODE_TIMED_OUT);
         MRIterator_Release(it);
         return false;
