d3ad1957b7a5df566bda4d0424b2ce03b4f2d6ad
raz-mon <razmonsonego2@gmail.com>
2026-07-09 12:01:10 +0000

    Revert "Block NUMERIC field creation for disk (Flex) indexes (#10389)"
    This reverts commit a266825638b6164826fa2c8f251573e3a7c1377e.

--- tests/pytests/test_flex_validation.py
+++ tests/pytests/test_flex_validation.py
@@ -48,8 +48,6 @@ def test_invalid_field_type(env):
         .error().contains('GEO fields are not supported in Flex indexes')
     env.expect('FT.CREATE', 'idx', 'ON', 'HASH', 'SKIPINITIALSCAN', 'SCHEMA', 'field', 'GEOSHAPE') \
         .error().contains('GEOSHAPE fields are not supported in Flex indexes')
-    env.expect('FT.CREATE', 'idx', 'ON', 'HASH', 'SKIPINITIALSCAN', 'SCHEMA', 'field', 'NUMERIC') \
-        .error().contains('NUMERIC fields are not supported in Flex indexes')
 
 
 @skip(cluster=True)
