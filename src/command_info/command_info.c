7f333015971d6ff81017f67476e6e6222026ecdd
Omer Shadmi <76992134+oshadmi@users.noreply.github.com>
2026-07-14 14:14:45 +0300

    [8.8] MOD-16121: Restore FT.HYBRID COMBINE YIELD_SCORE_AS counted syntax (#10454)
    Revert "[8.8] MOD-16121: FT.HYBRID - Make YIELD_SCORE_AS positional in COMBINE (#10447)"
    
    This reverts commit 6573fbc520057091f3fb1d607e451cb0113ce28c.

--- src/command_info/command_info.c
+++ src/command_info/command_info.c
@@ -2579,6 +2579,12 @@ int SetFtHybridInfo(RedisModuleCommand *cmd) {
                     .type = REDISMODULE_ARG_TYPE_INTEGER,
                     .flags = REDISMODULE_CMD_ARG_OPTIONAL,
                   },
+                  {
+                    .name = "yield_score_as",
+                    .token = "YIELD_SCORE_AS",
+                    .type = REDISMODULE_ARG_TYPE_STRING,
+                    .flags = REDISMODULE_CMD_ARG_OPTIONAL,
+                  },
                   {0}
                 },
               },
@@ -2619,17 +2625,17 @@ int SetFtHybridInfo(RedisModuleCommand *cmd) {
                     .type = REDISMODULE_ARG_TYPE_INTEGER,
                     .flags = REDISMODULE_CMD_ARG_OPTIONAL,
                   },
+                  {
+                    .name = "yield_score_as",
+                    .token = "YIELD_SCORE_AS",
+                    .type = REDISMODULE_ARG_TYPE_STRING,
+                    .flags = REDISMODULE_CMD_ARG_OPTIONAL,
+                  },
                   {0}
                 },
               },
               {0}
             },
-          },
-          {
-            .name = "yield_score_as",
-            .token = "YIELD_SCORE_AS",
-            .type = REDISMODULE_ARG_TYPE_STRING,
-            .flags = REDISMODULE_CMD_ARG_OPTIONAL,
           },
           {0}
         },
