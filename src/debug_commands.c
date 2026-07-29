24caf29e4d83da6917e117cbf087d546d42f3ada
nafraf <nafraf@gmail.com>
2026-07-29 11:27:58 -0500

    Add guard for DiskResumeBackgroundWork to check if background work is paused

--- src/debug_commands.c
+++ src/debug_commands.c
@@ -1188,6 +1188,10 @@ DEBUG_COMMAND(DiskResumeBackgroundWork) {
     return RedisModule_ReplyWithError(ctx, "Index is not a disk index");
   }
 
+  if (!SearchDisk_IsBackgroundWorkPaused(sp->diskSpec)) {
+    return RedisModule_ReplyWithError(ctx, "Background work is not paused");
+  }
+
   SearchDisk_ContinueBackgroundWork(sp->diskSpec);
   RedisModule_ReplyWithSimpleString(ctx, "OK");
   return REDISMODULE_OK;
