14ae1d8575fb32ea7bba6daa12da6fee3dc2247c
GuyAv46 <guy.avimor@gmail.com>
2026-09-16 11:41:59 +0300

    Use the release dictionary header in the list coordinator

--- src/coord/index_list_command.c
+++ src/coord/index_list_command.c
@@ -21,7 +21,7 @@
 #include "rmalloc.h"
 #include "rmr/reply.h"
 #include "rmr/rmr.h"
-#include "util/dict/dict.h"
+#include "util/dict.h"
 #include "util/arr/arr.h"
 
 #define FT_LIST_CS_KEY_INDEX "index"
